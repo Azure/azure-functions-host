a6e376c7762d8f94ee2f611d624efed244d1b6fb
MikeStall <MikeStall@hotmail.com>
2012-11-02 14:58:39 -0700

    Improve Azure usage. Make web dashboard be an azure role instead of a separate antares site. Get dashboard endpoint via RoleEnvironment instead of hardcoded URL. Move passwords out of code and into .cscfg file.

---
+++ WebFrontEnd/WebWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFrontEnd
+{
+    // Entry point invoked for Azure.
+    // http://blog.liamcavanagh.com/2011/12/how-to-combine-a-worker-role-with-a-mvc4-web-role-into-a-single-instance
+    public class WebWorker : Microsoft.WindowsAzure.ServiceRuntime.RoleEntryPoint
+    {
+        public override bool OnStart()
+        {
+            return base.OnStart();
+        }
+    }
+}
