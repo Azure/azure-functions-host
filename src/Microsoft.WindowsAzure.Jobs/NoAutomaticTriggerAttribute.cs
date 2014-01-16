f28c2d2fd049757eae2d353f1f14b474c7c34e86
davidmatson <opencode@outlook.com>
2014-01-16 14:14:16 -0800

    Fix documentation typo.

--- src/Microsoft.WindowsAzure.Jobs/NoAutomaticTriggerAttribute.cs
+++ src/Microsoft.WindowsAzure.Jobs/NoAutomaticTriggerAttribute.cs
@@ -5,7 +5,7 @@ namespace Microsoft.WindowsAzure.Jobs
     /// <summary>
     /// Represents an attribute that is used to indicate that the JobHost should not listen to
     /// this method. This can be useful to avoid the performance impact of listening on a large container
-    /// or to avoid inadvertant triggering of the function.
+    /// or to avoid inadvertent triggering of the function.
     /// The method can be invoked explicitly using the Call method on the JobHost.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
