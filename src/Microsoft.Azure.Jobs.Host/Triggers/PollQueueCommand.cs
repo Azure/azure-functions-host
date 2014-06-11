10d4700143b199bf5ec91d65dcca2400b9335e35
davidmatson <opencode@outlook.com>
2014-06-11 10:10:37 -0700

    Don't pause polling queues until empty.

--- src/Microsoft.Azure.Jobs.Host/Triggers/PollQueueCommand.cs
+++ src/Microsoft.Azure.Jobs.Host/Triggers/PollQueueCommand.cs
@@ -36,45 +36,48 @@ namespace Microsoft.Azure.Jobs.Host.Triggers
             var visibilityTimeout = TimeSpan.FromMinutes(10); // long enough to process the job
             CloudQueueMessage message;
 
-            try
+            do
             {
-                message = _queue.GetMessage(visibilityTimeout);
-            }
-            catch (StorageException)
-            {
-                // TODO: Consider a more specific check here, like:
-                //if (exception.IsNotFound() || exception.IsServerSideError()) { return false; } else { throw; }
-
-                // Storage exceptions can happen naturally and intermittently from network connectivity issues.
-                // Just ignore.
-                return false;
-            }
-
-            if (message != null)
-            {
-                using (IntervalSeparationTimer timer = CreateUpdateMessageVisibilityTimer(_queue, message, visibilityTimeout))
-                {
-                    timer.Start(executeFirst: false);
-
-                    if (_context.CancellationToken.IsCancellationRequested)
-                    {
-                        return true;
-                    }
-
-                    _invoker.OnNewQueueItem(message, _trigger, _context);
-                }
-
                 try
                 {
-                    // Need to call Delete message only if function succeeded.
-                    _queue.DeleteMessage(message);
+                    message = _queue.GetMessage(visibilityTimeout);
                 }
                 catch (StorageException)
                 {
-                    // TODO: Consider a more specific check here.
+                    // TODO: Consider a more specific check here, like:
+                    //if (exception.IsNotFound() || exception.IsServerSideError()) { return false; } else { throw; }
+
+                    // Storage exceptions can happen naturally and intermittently from network connectivity issues.
+                    // Just ignore.
                     return false;
                 }
-            }
+
+                if (message != null)
+                {
+                    using (IntervalSeparationTimer timer = CreateUpdateMessageVisibilityTimer(_queue, message, visibilityTimeout))
+                    {
+                        timer.Start(executeFirst: false);
+
+                        if (_context.CancellationToken.IsCancellationRequested)
+                        {
+                            return true;
+                        }
+
+                        _invoker.OnNewQueueItem(message, _trigger, _context);
+                    }
+
+                    try
+                    {
+                        // Need to call Delete message only if function succeeded.
+                        _queue.DeleteMessage(message);
+                    }
+                    catch (StorageException)
+                    {
+                        // TODO: Consider a more specific check here.
+                        return false;
+                    }
+                }
+            } while (message != null);
 
             return true;
         }
