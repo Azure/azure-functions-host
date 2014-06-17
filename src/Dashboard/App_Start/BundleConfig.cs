59b28fa398fbc0c240cbd45cf52222354619ee12
davidmatson <opencode@outlook.com>
2014-06-17 11:49:09 -0700

    Remove modernizr.

--- src/Dashboard/App_Start/BundleConfig.cs
+++ src/Dashboard/App_Start/BundleConfig.cs
@@ -30,11 +30,6 @@ namespace Dashboard
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/bootstrap/css/bootstrap.css",
                 "~/Content/Site.css"));
--- src/Dashboard/Scripts/_references.js
+++ src/Dashboard/Scripts/_references.js
@@ -3,4 +3,3 @@
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-2.1.0.debug.js" />
-/// <reference path="modernizr-2.5.3.js" />
