d3ec6e6b481bea7e81eb58dae36acbc616258388
davidmatson <opencode@outlook.com>
2014-06-18 12:08:55 -0700

    Update version to 0.3.1.

--- src/Microsoft.Azure.Jobs/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.Jobs/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
