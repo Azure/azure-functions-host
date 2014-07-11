9c495a8aabd6b4ee7fe8c15f03632a7adeb95a2b
davidmatson <opencode@outlook.com>
2014-07-11 12:07:54 -0700

    Use faster polling for handling dashboard buttons.

--- src/Microsoft.Azure.Jobs.Host/Queues/Listeners/HostMessageListenerFactory.cs
+++ src/Microsoft.Azure.Jobs.Host/Queues/Listeners/HostMessageListenerFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Jobs.Host.Bindings;
+﻿using System;
+using Microsoft.Azure.Jobs.Host.Bindings;
 using Microsoft.Azure.Jobs.Host.Executors;
 using Microsoft.Azure.Jobs.Host.Indexers;
 using Microsoft.Azure.Jobs.Host.Listeners;
@@ -9,14 +10,17 @@ namespace Microsoft.Azure.Jobs.Host.Queues.Listeners
 {
     internal static class HostMessageListener
     {
+        private static readonly TimeSpan maxmimum = TimeSpan.FromMinutes(1);
+
         public static IListener Create(CloudQueue queue, IExecuteFunction executor, IFunctionIndexLookup functionLookup,
             IFunctionInstanceLogger functionInstanceLogger, HostBindingContext context)
         {
             ITriggerExecutor<CloudQueueMessage> triggerExecutor = new HostMessageExecutor(executor, functionLookup,
                 functionInstanceLogger, context);
             ICanFailCommand command = new PollQueueCommand(queue, poisonQueue: null, triggerExecutor: triggerExecutor);
+            // Use a shorter maximum polling interval for run/abort from dashboard.
             IntervalSeparationTimer timer = ExponentialBackoffTimerCommand.CreateTimer(command,
-                QueuePollingIntervals.Minimum, QueuePollingIntervals.Maximum);
+                QueuePollingIntervals.Minimum, maxmimum);
             return new TimerListener(timer);
         }
     }
