425e9b29f33395a723580ad8d80a1063721d9bd8
davidmatson <opencode@outlook.com>
2014-07-16 16:05:53 -0700

    Clean up private deployment script (fixes #189).

---
+++ src/AzureJobsTest/Deploy.ps1
@@ -0,0 +1,35 @@
+﻿if ($Args.Length -eq 0)
+{
+    Write-Host "Usage: Deploy.ps1 <scm_uri>"
+    exit
+}
+
+$ScmUri = $Args[0]
+
+if (Test-Path "env:ProgramFiles(x86)")
+{
+    $ProgramFiles = "${env:ProgramFiles(x86)}"
+}
+else
+{
+    $ProgramFiles = "$env:ProgramFiles"
+}
+
+$CurlExe = "$ProgramFiles\git\bin\curl.exe"
+
+if (!(Test-Path $CurlExe))
+{
+    Write-Host ($CurlExe + "does not exist")
+    exit
+}
+
+& "$ProgramFiles\MSBuild\12.0\Bin\MSBuild.exe" AzureJobsTest.proj
+$ZipFile = "bin\AzureJobsTest.zip"
+$CurlArguments = '-k -v -T "' + $ZipFile + '" "' + $ScmUri + '/zip"'
+
+Write-Host ($CurlExe + " " + '-k -v -T "' + $ZipFile + '" "' + $ScmUri + '/zip"')
+& $CurlExe -k -v -T "$ZipFile" "$ScmUri/zip"
+
+Write-Host
+Write-Host "Add the following app setting: WEBSITE_PRIVATE_EXTENSIONS = 1"
+Write-Host
