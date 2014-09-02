8b867f58ce152be09c203d2cd3031a7ca23bff7a
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2014-09-02 14:24:35 -0700

    Fixed a second string concat bug

--- perf/Microsoft.Azure.WebJobs.Perf/QueueOverheadPerfTest.cs
+++ perf/Microsoft.Azure.WebJobs.Perf/QueueOverheadPerfTest.cs
@@ -55,7 +55,7 @@ namespace Microsoft.Azure.WebJobs.Perf
 
                 MeasurementBlock.Mark(
                     perfRatio,
-                    disableLogging ? QueueNoLoggingOverheadMetric : QueueLoggingOverheadMetric + ";Ratio;Percent");
+                    (disableLogging ? QueueNoLoggingOverheadMetric : QueueLoggingOverheadMetric) + ";Ratio;Percent");
             }
             finally
             {
