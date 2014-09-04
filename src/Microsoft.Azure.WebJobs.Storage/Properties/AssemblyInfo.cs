396c546ba589e3f72aa5044b2ee6ecdab8d339a7
davidmatson <opencode@outlook.com>
2014-09-04 15:00:48 -0700

    Update version to 0.5.0.

--- src/Microsoft.Azure.WebJobs.Storage/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.WebJobs.Storage/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
