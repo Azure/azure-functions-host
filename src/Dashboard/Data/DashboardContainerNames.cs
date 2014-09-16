1b49a8d760123d551aff422e75f4fa65ece9b5cc
davidmatson <opencode@outlook.com>
2014-09-16 15:53:21 -0700

    Make dashboard container name match new conventions (fixes #164).

--- src/Dashboard/Data/DashboardContainerNames.cs
+++ src/Dashboard/Data/DashboardContainerNames.cs
@@ -6,6 +6,6 @@ namespace Dashboard.Data
     // Names of containers used only by the dashboard (not part of the protocol with hosts).
     internal static class DashboardContainerNames
     {
-        public const string Dashboard = "azure-jobs-dashboard";
+        public const string Dashboard = "azure-webjobs-dashboard";
     }
 }
