72aba7e991cdf92a7bc4fa6e8b5101c20c391816
davidmatson <opencode@outlook.com>
2014-09-16 12:40:27 -0700

    Update version to 0.7.0.

--- src/Microsoft.Azure.WebJobs.Protocols/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.WebJobs.Protocols/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
