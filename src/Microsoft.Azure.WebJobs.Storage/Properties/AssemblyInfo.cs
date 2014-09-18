c44dc67e5a109a812d5552da5b0ef9478609c331
davidmatson <opencode@outlook.com>
2014-09-18 13:54:50 -0700

    Update versions for v1 rc1.

--- src/Microsoft.Azure.WebJobs.Storage/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.WebJobs.Storage/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
