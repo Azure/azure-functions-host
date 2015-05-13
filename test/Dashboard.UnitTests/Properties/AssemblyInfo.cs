5badefec4e761d9ee4f7b3711332c5e1a8e5b73c
Mathew Charles <mathewc@microsoft.com>
2015-05-13 09:52:25 -0700

    Fixing msbuild test runs by disabling test parallelization

--- test/Dashboard.UnitTests/Properties/AssemblyInfo.cs
+++ test/Dashboard.UnitTests/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Reflection;
-using Xunit;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
