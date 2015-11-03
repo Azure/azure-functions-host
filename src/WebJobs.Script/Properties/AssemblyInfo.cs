8f44ba7f23449239e0c2c770ebb4d3e0c6a08520
Mathew Charles <mathewc@microsoft.com>
2015-11-03 15:15:24 -0800

    Updating assembly info

--- src/WebJobs.Script/Properties/AssemblyInfo.cs
+++ src/WebJobs.Script/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebJobs.Script")]
+[assembly: AssemblyTitle("Microsoft.Azure.WebJobs.Script")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebJobs.Script")]
+[assembly: AssemblyProduct("Microsoft.Azure.WebJobs.Script")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 
 [assembly: InternalsVisibleTo("WebJobs.Script.Tests")]
