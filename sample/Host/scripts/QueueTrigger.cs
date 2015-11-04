24b246fa698c05446c0e7ce9c2edeb9ef8ad586b
Mathew Charles <mathewc@microsoft.com>
2015-11-03 18:04:26 -0800

    Removing uneeded EF reference

--- sample/Host/scripts/QueueTrigger.cs
+++ sample/Host/scripts/QueueTrigger.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Data.Entity;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace Host
@@ -10,8 +9,6 @@ namespace Host
     {
         public static void QueueTrigger(Post post, TraceWriter trace)
         {
-            DbContext context = new DbContext("myconnection");
-
             trace.Info(string.Format("C# QueueTrigger function processed post '{0}'", post.Text));
         }
     }
