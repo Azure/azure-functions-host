22b2e79c6d938e58732b8195bfca39c2f779bbb7
Mathew Charles <mathewc@microsoft.com>
2015-11-10 13:28:54 -0800

    Adding example showing a Node function manifest entry can point to a node module directory rather than a single file

---
+++ sample/Host.Node/scripts/timerTrigger/index.js
@@ -0,0 +1,10 @@
+﻿var logger = require('./logger');
+
+// demonstrates that a function can be a node module, with the
+// manifest pointing to a module directory rather than a single file
+module.exports = function (context) {
+    var timeStamp = new Date().toISOString();
+    context.log('Node.js timer trigger function ran at ' + timeStamp);
+
+    logger.log(timeStamp, context.done);
+}
