5791c4fefe2c5a82ba0e511ebd1bb40b2b1933ac
Mathew Charles <mathewc@microsoft.com>
2015-11-19 13:21:06 -0800

    Removing FunctionDescriptor.FromMethod

--- src/WebJobs.Script/Description/FunctionDescriptor.cs
+++ src/WebJobs.Script/Description/FunctionDescriptor.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reflection;
 using System.Reflection.Emit;
 
 namespace Microsoft.Azure.WebJobs.Script
@@ -16,7 +13,11 @@ namespace Microsoft.Azure.WebJobs.Script
         {
         }
 
-        public FunctionDescriptor(string name, IFunctionInvoker invoker, Collection<ParameterDescriptor> parameters, Collection<CustomAttributeBuilder> attributes)
+        public FunctionDescriptor(
+            string name, 
+            IFunctionInvoker invoker, 
+            Collection<ParameterDescriptor> parameters, 
+            Collection<CustomAttributeBuilder> attributes)
         {
             Name = name;
             Invoker = invoker;
@@ -31,33 +32,5 @@ namespace Microsoft.Azure.WebJobs.Script
         public Collection<CustomAttributeBuilder> CustomAttributes { get; private set; }
 
         public IFunctionInvoker Invoker { get; private set; }
-
-        public static FunctionDescriptor FromMethod(MethodInfo method, IFunctionInvoker invoker)
-        {
-            Collection<ParameterDescriptor> parameters = new Collection<ParameterDescriptor>();
-            foreach (var parameter in method.GetParameters())
-            {
-                var parameterAttributeBuilders = BuildCustomAttributes(parameter.CustomAttributes);
-                ParameterDescriptor parameterDescriptor = new ParameterDescriptor(parameter.Name, parameter.ParameterType, new Collection<CustomAttributeBuilder>(parameterAttributeBuilders.ToList()));
-                parameters.Add(parameterDescriptor);
-            }
-
-            var functionParameterBuilders = BuildCustomAttributes(method.CustomAttributes);
-
-            return new FunctionDescriptor(method.Name, invoker, parameters, new Collection<CustomAttributeBuilder>(functionParameterBuilders.ToList()));
-        }
-
-        private static IEnumerable<CustomAttributeBuilder> BuildCustomAttributes(IEnumerable<CustomAttributeData> customAttributes)
-        {
-            return customAttributes.Select(attribute =>
-            {
-                var attributeArgs = attribute.ConstructorArguments.Select(a => a.Value).ToArray();
-                var namedPropertyInfos = attribute.NamedArguments.Select(a => a.MemberInfo).OfType<PropertyInfo>().ToArray();
-                var namedPropertyValues = attribute.NamedArguments.Where(a => a.MemberInfo is PropertyInfo).Select(a => a.TypedValue.Value).ToArray();
-                var namedFieldInfos = attribute.NamedArguments.Select(a => a.MemberInfo).OfType<FieldInfo>().ToArray();
-                var namedFieldValues = attribute.NamedArguments.Where(a => a.MemberInfo is FieldInfo).Select(a => a.TypedValue.Value).ToArray();
-                return new CustomAttributeBuilder(attribute.Constructor, attributeArgs, namedPropertyInfos, namedPropertyValues, namedFieldInfos, namedFieldValues);
-            });
-        }
     }
 }
