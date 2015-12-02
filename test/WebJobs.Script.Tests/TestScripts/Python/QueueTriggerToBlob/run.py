4896632b8b8350e23171c3e8e0406e826ecf28be
Mathew Charles <mathewc@microsoft.com>
2015-12-02 12:00:22 -0800

    Adding build scripts for command line / CI build

--- test/WebJobs.Script.Tests/TestScripts/Python/QueueTriggerToBlob/run.py
+++ test/WebJobs.Script.Tests/TestScripts/Python/QueueTriggerToBlob/run.py
@@ -1,7 +1,7 @@
 import os
 
 # read the queue message and write to stdout
-input = raw_input();
+input = input();
 message = "Python script processed queue message '{0}'".format(input)
 print(message)
 
