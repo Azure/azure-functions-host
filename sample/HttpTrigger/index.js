c4fc95b71a1594c36ba25e1b42711987ab8f093c
davidebbo <david.ebbo@microsoft.com>
2016-01-29 09:30:38 -0800

    Imporve Http trigger sample

--- sample/HttpTrigger/index.js
+++ sample/HttpTrigger/index.js
@@ -1,4 +1,10 @@
 ﻿module.exports = function (context) {
-    context.log('Node.js HTTP trigger function processed a request');
-    context.done(null, "Hello " + context.req.query.name);
+    context.log('Node.js HTTP trigger function processed a request. Name=' + context.req.query.name);
+
+    if (typeof context.req.query.name == 'undefined') {
+        context.done(null, "Please pass a name on the query string");
+    }
+    else {
+        context.done(null, "Hello " + context.req.query.name);
+    }
 }
