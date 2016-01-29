5caaf488fe4d156dda95c99773e5d969a79a99de
davidebbo <david.ebbo@microsoft.com>
2016-01-29 09:51:11 -0800

    Add more logging to GitHub sample

--- sample/WebHook-GitHub/index.js
+++ sample/WebHook-GitHub/index.js
@@ -1,4 +1,4 @@
 ﻿module.exports = function (context) {
-    context.log('GitHub WebHook triggered!');
+    context.log('GitHub WebHook triggered! ' + context.req.body.comment.body);
     context.done(null, 'New GitHub comment: ' + context.req.body.comment.body);
 }
