061a98d889cd22a78515b6fcb571fcab97c9ddcc
davidebbo <david.ebbo@microsoft.com>
2016-02-29 18:45:32 -0800

    Add JsonProperty attribs on secrets models

--- src/WebJobs.Script.WebHost/FunctionSecrets.cs
+++ src/WebJobs.Script.WebHost/FunctionSecrets.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Newtonsoft.Json;
+
 namespace WebJobs.Script.WebHost
 {
     public class FunctionSecrets
     {
+        [JsonProperty(PropertyName = "key")]
         public string Key { get; set; }
     }
 }
--- src/WebJobs.Script.WebHost/HostSecrets.cs
+++ src/WebJobs.Script.WebHost/HostSecrets.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Newtonsoft.Json;
+
 namespace WebJobs.Script.WebHost
 {
     public class HostSecrets
     {
+        [JsonProperty(PropertyName = "masterKey")]
         public string MasterKey { get; set; }
+        [JsonProperty(PropertyName = "functionKey")]
         public string FunctionKey { get; set; }
     }
 }
