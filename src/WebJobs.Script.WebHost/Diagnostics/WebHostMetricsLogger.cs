54c2669193c60d4506b58214e4d23bb56507a9ec
Mathew Charles <mathewc@microsoft.com>
2016-03-09 19:57:42 -0800

    Adding metrics logging

---
+++ src/WebJobs.Script.WebHost/Diagnostics/WebHostMetricsLogger.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Script.Diagnostics;
+
+namespace WebJobs.Script.WebHost.Diagnostics
+{
+    public class WebHostMetricsLogger : IMetricsLogger
+    {
+        public void BeginEvent(MetricEvent metricEvent)
+        {
+            // TODO
+            FunctionStartedEvent startedEvent = metricEvent as FunctionStartedEvent;
+            if (startedEvent != null)
+            {
+                startedEvent.StartTime = DateTime.Now;
+            }
+        }
+
+        public void EndEvent(MetricEvent metricEvent)
+        {
+            // TODO
+            FunctionStartedEvent startedEvent = metricEvent as FunctionStartedEvent;
+            if (startedEvent != null)
+            {
+                startedEvent.EndTime = DateTime.Now;
+            }
+        }
+    }
+}
--- src/WebJobs.Script/Description/CSharp/CSharpFunctionInvoker.cs
+++ src/WebJobs.Script/Description/CSharp/CSharpFunctionInvoker.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Binding;
+using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
@@ -31,6 +32,8 @@ namespace Microsoft.Azure.WebJobs.Script.Description
         private readonly Collection<FunctionBinding> _inputBindings;
         private readonly Collection<FunctionBinding> _outputBindings;
         private readonly IFunctionEntryPointResolver _functionEntryPointResolver;
+        private readonly FunctionMetadata _functionMetadata;
+        private readonly IMetricsLogger _metrics;
 
         private MethodInfo _function;
         private CSharpFunctionSignature _functionSignature;
@@ -53,6 +56,8 @@ namespace Microsoft.Azure.WebJobs.Script.Description
             _inputBindings = inputBindings;
             _outputBindings = outputBindings;
             _triggerInputName = GetTriggerInputName(functionMetadata);
+            _functionMetadata = functionMetadata;
+            _metrics = host.ScriptConfig.HostConfig.GetService<IMetricsLogger>();
 
             InitializeFileWatcherIfEnabled();
             _resultProcessor = CreateResultProcessor();
@@ -150,6 +155,9 @@ namespace Microsoft.Azure.WebJobs.Script.Description
 
         public override async Task Invoke(object[] parameters)
         {
+            FunctionStartedEvent startedEvent = new FunctionStartedEvent(_functionMetadata);
+            _metrics.BeginEvent(startedEvent);
+
             try
             {
                 TraceWriter.Verbose("Function started");
@@ -175,8 +183,16 @@ namespace Microsoft.Azure.WebJobs.Script.Description
             catch (Exception ex)
             {
                 TraceWriter.Error(ex.Message, ex is CompilationErrorException ? null : ex);
+
+                startedEvent.Success = false;
+                TraceWriter.Error(ex.Message, ex);
+
                 TraceWriter.Verbose("Function completed (Failure)");
                 throw;
+            }
+            finally
+            {
+                _metrics.EndEvent(startedEvent);
             }
         }
 
