c3fcdd1bbbc4afcd62b837837668d887345c9355
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-03-12 00:03:11 -0800

    Adding C# script reference to System.Net.Http.Formatting

--- src/WebJobs.Script/Description/CSharp/FunctionMetadataResolver.cs
+++ src/WebJobs.Script/Description/CSharp/FunctionMetadataResolver.cs
@@ -41,7 +41,8 @@ namespace Microsoft.Azure.WebJobs.Script.Description
                 typeof(IAsyncCollector<>).Assembly.Location, /*Microsoft.Azure.WebJobs*/
                 typeof(JobHost).Assembly.Location, /*Microsoft.Azure.WebJobs.Host*/
                 typeof(CoreJobHostConfigurationExtensions).Assembly.Location, /*Microsoft.Azure.WebJobs.Extensions*/
-                typeof(System.Web.Http.ApiController).Assembly.Location /*System.Web.Http*/
+                typeof(System.Web.Http.ApiController).Assembly.Location, /*System.Web.Http*/
+                typeof(System.Net.Http.HttpClientExtensions).Assembly.Location /*System.Net.Http.Formatting*/
             };
 
         private static readonly Assembly[] SharedAssemblies =
