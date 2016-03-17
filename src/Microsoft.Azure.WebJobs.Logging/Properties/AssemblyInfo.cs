ce9b2631daa47c73f0c4e205e814b7f0ae7bbed7
Mike Stall <mikestall@hotmail.com>
2016-03-17 16:42:27 -0700

    Fix assembly attributes.

--- src/Microsoft.Azure.WebJobs.Logging/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.WebJobs.Logging/Properties/AssemblyInfo.cs
@@ -3,18 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.WebJobs.Logging")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Azure.WebJobs.Logging")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
