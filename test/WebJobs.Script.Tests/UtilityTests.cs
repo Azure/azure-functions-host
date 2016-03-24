2c5b16f05f0d285e989e7fcf26da1ad369b74f78
Mathew Charles <mathewc@microsoft.com>
2016-03-24 16:57:38 -0700

    Error handling improvements

---
+++ test/WebJobs.Script.Tests/UtilityTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Script;
+using Xunit;
+
+namespace WebJobs.Script.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void FlattenException_AggregateException_ReturnsExpectedResult()
+        {
+            ApplicationException ex1 = new ApplicationException("Incorrectly configured setting 'Foo'");
+            ex1.Source = "Acme.CloudSystem";
+
+            // a dupe of the first
+            ApplicationException ex2 = new ApplicationException("Incorrectly configured setting 'Foo'");
+            ex1.Source = "Acme.CloudSystem";
+
+            AggregateException aex = new AggregateException("One or more errors occurred.", ex1, ex2);
+
+            string formattedResult = Utility.FlattenException(aex);
+            Assert.Equal("Acme.CloudSystem: Incorrectly configured setting 'Foo'.", formattedResult);
+        }
+
+        [Fact]
+        public void FlattenException_SingleException_ReturnsExpectedResult()
+        {
+            ApplicationException ex = new ApplicationException("Incorrectly configured setting 'Foo'");
+            ex.Source = "Acme.CloudSystem";
+
+            string formattedResult = Utility.FlattenException(ex);
+            Assert.Equal("Acme.CloudSystem: Incorrectly configured setting 'Foo'.", formattedResult);
+        }
+
+        [Fact]
+        public void FlattenException_MultipleInnerExceptions_ReturnsExpectedResult()
+        {
+            ApplicationException ex1 = new ApplicationException("Exception message 1");
+            ex1.Source = "Source1";
+
+            ApplicationException ex2 = new ApplicationException("Exception message 2.", ex1);
+            ex2.Source = "Source2";
+
+            ApplicationException ex3 = new ApplicationException("Exception message 3", ex2);
+
+            string formattedResult = Utility.FlattenException(ex3);
+            Assert.Equal("Exception message 3. Source2: Exception message 2. Source1: Exception message 1.", formattedResult);
+        }
+    }
+}
