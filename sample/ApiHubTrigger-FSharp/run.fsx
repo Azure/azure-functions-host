2b24f0287e05fa03411391c23372689f7e069957
Don Syme <donsyme@fastmail.fm>
2016-04-21 13:25:56 +0100

    add samples

---
+++ sample/ApiHubTrigger-FSharp/run.fsx
@@ -0,0 +1,19 @@
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I "../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the body of the function 
+
+open System
+open Microsoft.Azure.WebJobs.Host
+
+let Run (input: string, output: byref<string>, log: TraceWriter) =
+    log.Verbose(sprintf "F# ApiHub trigger function processed a file...");
+
+    output <- input
+
---
+++ sample/DocumentDB-FSharp/run.fsx
@@ -0,0 +1,20 @@
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I "../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the body of the function 
+
+open System
+open Microsoft.Azure.WebJobs.Host
+
+type Output =  { text : string }
+
+let Run (input: string, newItem: byref<obj>, log: TraceWriter) =
+    newItem <- { text = "Hello from F#! " + input }
+
+
