e254f89f8524ee4f070e831337093b4fd6e04245
Don Syme <donsyme@fastmail.fm>
2016-04-21 13:25:56 +0100

    add samples

---
+++ sample/HttpTrigger-FSharp/run.fsx
@@ -0,0 +1,37 @@
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I @"../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#r "System.Web.Http.dll"
+#r "System.Net.Http.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the body of the function 
+
+open System
+open System.Linq
+open System.Net
+open System.Net.Http
+open System.Threading.Tasks
+open Microsoft.Azure.WebJobs.Host
+
+let Run (req: HttpRequestMessage , log: TraceWriter) : Task<HttpResponseMessage> =
+    async { 
+        let queryParams = req.GetQueryNameValuePairs().ToDictionary((fun p -> p.Key), (fun p -> p.Value), StringComparer.OrdinalIgnoreCase)
+
+        log.Verbose(sprintf "F# HTTP trigger function processed a request. Name=%A" req.RequestUri)
+
+        let res =
+            match queryParams.TryGetValue("name") with 
+            | true, name -> 
+                new HttpResponseMessage(HttpStatusCode.OK, Content = new StringContent("Hello " + name))
+            | _ -> 
+                new HttpResponseMessage(HttpStatusCode.BadRequest, Content = new StringContent("Please pass a name on the query string"))
+
+        return res
+    } |> Async.StartAsTask
+
+
---
+++ sample/HttpTriggerWithObject-FSharp/run.fsx
@@ -0,0 +1,21 @@
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I "../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the implementation of the function 
+
+open System
+open Microsoft.Azure.WebJobs.Host
+
+[<CLIMutable>]
+type TestObject = 
+    { SenderName : string
+      Greeting : string }
+
+let Run (req: TestObject, log: TraceWriter) =
+    { req with Greeting = sprintf "Hello, %s" req.SenderName }
