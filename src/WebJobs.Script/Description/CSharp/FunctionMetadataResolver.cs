4120ec75da7d15c2b0c42c0485f0164103aab8cd
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-04-22 09:53:37 -0700

    Adding Microsoft.ServiceBus as a shared assembly

--- src/WebJobs.Script/Description/CSharp/FunctionMetadataResolver.cs
+++ src/WebJobs.Script/Description/CSharp/FunctionMetadataResolver.cs
@@ -49,6 +49,7 @@ namespace Microsoft.Azure.WebJobs.Script.Description
             {
                 new DirectSharedAssemblyProvider(typeof(Newtonsoft.Json.JsonConvert).Assembly), /* Newtonsoft.Json */
                 new DirectSharedAssemblyProvider(typeof(WindowsAzure.Storage.Table.ITableEntity).Assembly), /* Microsoft.WindowsAzure.Storage */
+                new DirectSharedAssemblyProvider(typeof(Microsoft.ServiceBus.Messaging.BrokeredMessage).Assembly), /* Microsoft.ServiceBus */
                 new LocalSharedAssemblyProvider(@"^Microsoft\.AspNet\.WebHooks\..*"), /* Microsoft.AspNet.WebHooks.* */
             };
 
