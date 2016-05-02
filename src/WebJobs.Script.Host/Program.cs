6230e3fd8b5610d154f4d93197d5393d260488b9
Mathew Charles <mathewc@microsoft.com>
2016-05-02 12:03:16 -0700

    Adding Edge.js as a package dependency (was missing)

--- src/WebJobs.Script.Host/Program.cs
+++ src/WebJobs.Script.Host/Program.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.Azure.WebJobs.Script;
 
 namespace Microsoft.Azure.WebJobs.Script.Host
 {
