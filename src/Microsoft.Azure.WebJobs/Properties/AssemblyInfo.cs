8b6507fa85fbb5f3fe1da3fde0f751b5235394e7
Mathew Charles <mathewc@microsoft.com>
2016-05-04 16:27:57 -0700

    Revving our major version to 2.0 due to dependency updates

--- src/Microsoft.Azure.WebJobs/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.WebJobs/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
