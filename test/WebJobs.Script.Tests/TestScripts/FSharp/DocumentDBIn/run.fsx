7e86c7695d1af28381b6ff08a125065700b81d42
Don Syme <donsyme@fastmail.fm>
2016-05-04 16:54:07 +0100

    add more tests and samples to solution

--- test/WebJobs.Script.Tests/TestScripts/FSharp/DocumentDBIn/run.fsx
+++ test/WebJobs.Script.Tests/TestScripts/FSharp/DocumentDBIn/run.fsx
@@ -1,4 +1,16 @@
-﻿#r "Newtonsoft.Json"
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I "../../../../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#r "Microsoft.Azure.WebJobs.Extensions.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the implementation of the function 
+
+#r "Newtonsoft.Json"
 
 open System
 open Microsoft.Azure.WebJobs.Host
--- test/WebJobs.Script.Tests/TestScripts/FSharp/NotificationHubOutNotification/run.fsx
+++ test/WebJobs.Script.Tests/TestScripts/FSharp/NotificationHubOutNotification/run.fsx
@@ -1,4 +1,16 @@
-﻿open System
+﻿//----------------------------------------------------------------------------------------
+// This prelude allows scripts to be edited in Visual Studio or another F# editing environment 
+
+#if !COMPILED
+#I "../../../../../bin/Binaries/WebJobs.Script.Host"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#r "Microsoft.Azure.WebJobs.Extensions.dll"
+#endif
+
+//----------------------------------------------------------------------------------------
+// This is the implementation of the function 
+
+open System
 open System.Collections.Generic
 open Microsoft.Azure.NotificationHubs
 
