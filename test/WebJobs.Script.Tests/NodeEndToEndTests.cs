3d2e32d9dd66bf56530d9f69e99fcda463069933
Pragna Gopa <pragna.gopa@hotmail.com>
2016-05-23 15:10:45 -0700

    [NotificationHub]Add support to send native notifications

--- test/WebJobs.Script.Tests/NodeEndToEndTests.cs
+++ test/WebJobs.Script.Tests/NodeEndToEndTests.cs
@@ -150,6 +150,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         }
 
         [Fact]
+        public async Task NotificationHubNative()
+        {
+            await NotificationHubTest("NotificationHubNative");
+        }
+
+        [Fact]
         public async Task MobileTables()
         {
             await MobileTablesTest();
