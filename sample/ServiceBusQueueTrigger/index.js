f86d8e02752a63411e3d2a212c4fb1a3e3bafe07
Mathew Charles <mathewc@microsoft.com>
2016-05-27 17:24:29 -0700

    Normalizing the ServiceBus binding to use byte[] (to support binary messages)

--- sample/ServiceBusQueueTrigger/index.js
+++ sample/ServiceBusQueueTrigger/index.js
@@ -1,12 +1,16 @@
-﻿module.exports = function (context, message) {
+﻿var util = require('util');
+
+module.exports = function (context, message) {
     context.log('Node.js ServiceBus queue trigger function processed message', message);
 
-    if (message.count < 2)
+    if (message.count < message.max)
     {
-        // write a message back to the queue that this function is triggered on
-        // ensuring that we only loop on this twice
+        // write a message back to the queue that this function is triggered
         message.count += 1;
         context.bindings.message = message;
+    }
+    else {
+        context.bindings.output = util.format('%d messages processed', message.count);
     }
 
     context.done();
