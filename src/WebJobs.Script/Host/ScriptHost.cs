tohling/azure-webjobs-sdk-script

PowerShell implementation

src/WebJobs.Script/Host/ScriptHost.cs
@@ -206,7 +206,8 @@ namespace Microsoft.Azure.WebJobs.Script
             {
                 new ScriptFunctionDescriptorProvider(this, ScriptConfig),
                 new NodeFunctionDescriptorProvider(this, ScriptConfig),
-                new DotNetFunctionDescriptionProvider(this, ScriptConfig)
+                new DotNetFunctionDescriptionProvider(this, ScriptConfig),
+                new PowerShellFunctionDescriptionProvider(this, ScriptConfig)
             };
 
             // read all script functions and apply to JobHostConfiguration

Azure/azure-functions-host

src/WebJobs.Script/Host/ScriptHost.cs
@@ -206,7 +206,8 @@ namespace Microsoft.Azure.WebJobs.Script
             {
                 new ScriptFunctionDescriptorProvider(this, ScriptConfig),
                 new NodeFunctionDescriptorProvider(this, ScriptConfig),
-                new DotNetFunctionDescriptionProvider(this, ScriptConfig)
+                new DotNetFunctionDescriptionProvider(this, ScriptConfig),
+                new PowerShellFunctionDescriptorProvider(this, ScriptConfig)
             };
 
             // read all script functions and apply to JobHostConfiguration