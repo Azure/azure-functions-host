918d593d2a54cc4e3450627ad0f33ad9fcd6f8e8
Ahmed ElSayed <ahmels@microsoft.com>
2016-07-06 17:30:45 -0700

    Host changes for azure-functions-cli

--- src/WebJobs.Script.WebHost/App_Start/AutofacBootstrap.cs
+++ src/WebJobs.Script.WebHost/App_Start/AutofacBootstrap.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Web.Http;
 using Autofac;
+using Autofac.Integration.WebApi;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.WebHost.WebHooks;
@@ -10,7 +12,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
 {
     public static class AutofacBootstrap
     {
-        internal static void Initialize(ScriptSettingsManager settingsManager, ContainerBuilder builder, WebHostSettings settings)
+        internal static void Initialize(ScriptSettingsManager settingsManager, ContainerBuilder builder, WebHostSettings settings, HttpConfiguration config)
         {
             builder.RegisterInstance(settingsManager);
 
@@ -23,6 +25,8 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             builder.Register<ISecretManager>(ct => ct.Resolve<WebHostResolver>().GetSecretManager(settings)).ExternallyOwned();
             builder.Register<WebScriptHostManager>(ct => ct.Resolve<WebHostResolver>().GetWebScriptHostManager(settings)).ExternallyOwned();
             builder.Register<WebHookReceiverManager>(ct => ct.Resolve<WebHostResolver>().GetWebHookReceiverManager(settings)).ExternallyOwned();
+            builder.RegisterInstance(settings);
+            builder.RegisterHttpRequestMessage(config);
         }
     }
 }
--- src/WebJobs.Script.WebHost/Controllers/AdminController.cs
+++ src/WebJobs.Script.WebHost/Controllers/AdminController.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -24,10 +25,12 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
     public class AdminController : ApiController
     {
         private readonly WebScriptHostManager _scriptHostManager;
+        private readonly WebHostSettings _webHostSettings;
 
-        public AdminController(WebScriptHostManager scriptHostManager)
+        public AdminController(WebScriptHostManager scriptHostManager, WebHostSettings webHostSettings)
         {
             _scriptHostManager = scriptHostManager;
+            _webHostSettings = webHostSettings;
         }
 
         [HttpPost]
@@ -59,8 +62,12 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
         [Route("admin/functions/{name}/status")]
         public FunctionStatus GetFunctionStatus(string name)
         {
-            FunctionStatus status = new FunctionStatus();
             Collection<string> functionErrors = null;
+            FunctionDescriptor function = _scriptHostManager.Instance.Functions.FirstOrDefault(p => p.Name.ToLowerInvariant() == name.ToLowerInvariant());
+            FunctionStatus status = new FunctionStatus
+            {
+                Metadata = function?.Metadata
+            };
 
             // first see if the function has any errors
             if (_scriptHostManager.Instance.FunctionErrors.TryGetValue(name, out functionErrors))
@@ -71,7 +78,6 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
             {
                 // if we don't have any errors registered, make sure the function exists
                 // before returning empty errors
-                FunctionDescriptor function = _scriptHostManager.Instance.Functions.FirstOrDefault(p => p.Name.ToLowerInvariant() == name.ToLowerInvariant());
                 if (function == null)
                 {
                     throw new HttpResponseException(HttpStatusCode.NotFound);
@@ -87,7 +93,10 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
         {
             HostStatus status = new HostStatus
             {
-                Id = _scriptHostManager.Instance?.ScriptConfig.HostConfig.HostId
+                Id = _scriptHostManager.Instance?.ScriptConfig.HostConfig.HostId,
+                WebHostSettings = _webHostSettings,
+                ProcessId = Process.GetCurrentProcess().Id,
+                IsDebuggerAttached = Debugger.IsAttached
             };
 
             var lastError = _scriptHostManager.LastError;
@@ -98,6 +107,17 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
             }
 
             return status;
+        }
+
+        [HttpPost]
+        [Route("admin/host/debug")]
+        public bool LaunchDebugger()
+        {
+            if (_webHostSettings.IsSelfHost)
+            {
+                return Debugger.Launch();
+            }
+            return false;
         }
 
         public override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
