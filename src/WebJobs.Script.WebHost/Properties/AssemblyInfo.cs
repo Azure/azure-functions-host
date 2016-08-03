276c4c1ee5188fe1626a4b2d00118f24e38143ac
Brett Samblanet <brettsam@microsoft.com>
2016-08-03 14:27:33 -0700

    simplifying version updates; allowing official build to update AssemblyFileVersion

--- src/WebJobs.Script.WebHost/Properties/AssemblyInfo.cs
+++ src/WebJobs.Script.WebHost/Properties/AssemblyInfo.cs
@@ -17,16 +17,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63c9b2f4-f122-4acc-968f-9b8f0880a72f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Microsoft.Azure.WebJobs.Script.Tests")]
