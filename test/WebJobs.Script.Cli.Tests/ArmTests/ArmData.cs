1be8da17f84def37610ba05ca039f15fd26d05e4
Ahmed ElSayed <ahmels@microsoft.com>
2016-08-19 14:08:22 -0700

    Adding license header

--- test/WebJobs.Script.Cli.Tests/ArmTests/ArmData.cs
+++ test/WebJobs.Script.Cli.Tests/ArmTests/ArmData.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using WebJobs.Script.Cli.Arm;
 
 namespace WebJobs.Script.Cli.Tests.ArmTests
--- test/WebJobs.Script.Cli.Tests/ArmTests/SubscriptionArmManagerTests.cs
+++ test/WebJobs.Script.Cli.Tests/ArmTests/SubscriptionArmManagerTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
 using FluentAssertions;
 using WebJobs.Script.Cli.Arm;
 using Xunit;
