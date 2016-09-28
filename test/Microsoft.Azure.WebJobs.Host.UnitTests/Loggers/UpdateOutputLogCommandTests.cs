cabe07dac13f8f7afc16ab26aeb479aa6fa924dd
Brett Samblanet <brettsam@microsoft.com>
2016-09-28 09:08:58 -0700

    adding regression test

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/Loggers/UpdateOutputLogCommandTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/Loggers/UpdateOutputLogCommandTests.cs
@@ -39,5 +39,41 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Loggers
             writer.SaveAndClose();
             Assert.Equal("123", content);
         }
+
+        [Fact]
+        public async Task TestMultipleThreads()
+        {
+            // This validates a bug where flushing from one thread while writing from another
+            // would cause an exception. 
+
+            string content = null;
+            Func<string, CancellationToken, Task> fp = (x, _) => { content = x; return Task.FromResult(0); };
+            UpdateOutputLogCommand writer = UpdateOutputLogCommand.Create(new Mock<IStorageBlockBlob>().Object, fp);
+
+            var tw = writer.Output;
+            bool writeDone = false;
+
+            // Start a Task to flush
+            Task flushTask = Task.Run(() =>
+            {
+                while (!writeDone)
+                {
+                    writer.TryExecute();
+                }
+            });
+
+            // Start a Task to write
+            Task writeTask = Task.Run(() =>
+            {
+                for (int i = 0; i < 10000000; i++)
+                {
+                    tw.WriteLine(string.Empty);
+                }
+                writeDone = true;
+            });
+
+            await flushTask;
+            await writeTask;
+        }
     }
 }
