tohling/azure-webjobs-sdk-script

Added Settings Manager

src/WebJobs.Script/Diagnostics/FileTraceWriter.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Settings;
 
 namespace Microsoft.Azure.WebJobs.Script
 {
@@ -253,7 +254,7 @@ namespace Microsoft.Azure.WebJobs.Script
 
         internal static string GetInstanceId()
         {
-            string instanceId = System.Environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteInstanceId);
+            string instanceId = ScriptSettingsManager.Instance.GetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteInstanceId);
             if (string.IsNullOrEmpty(instanceId))
             {
                 instanceId = Environment.MachineName;

test/WebJobs.Script.Tests/ApiHub/ApiHubTestHelper.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.ApiHub;
 using Microsoft.Azure.ApiHub.Table.Internal;
 using Microsoft.Azure.WebJobs.Extensions.ApiHub.Common;
+using Microsoft.Azure.WebJobs.Script.Settings;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
@@ -28,12 +29,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
 
         private static readonly Random Random = new Random();
 
+        private static readonly ISettingsManager SettingsManager = ScriptSettingsManager.Instance;
+
         public static void SetDefaultConnectionFactory()
         {
             // Setup the default ApiHub connection factory to use an actual SqlAzure connector 
             // if the AzureWebJobsSql environment variable specifies the connection string,
             // otherwise use a fake tabular connector.
-            var connectionString = Environment.GetEnvironmentVariable(Key);
+            var connectionString = SettingsManager.GetEnvironmentSetting(Key);
             if (string.IsNullOrEmpty(connectionString))
             {
                 var tableAdapter = new FakeTabularConnectorAdapter();
@@ -44,7 +47,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
                 // The value doesn't really matter here - this is just so we
                 // pass the Connection value validation that ScriptHost performs
                 // on startup
-                Environment.SetEnvironmentVariable(Key, "TestMockSqlConnection");
+                SettingsManager.SetEnvironmentSetting(Key, "TestMockSqlConnection");
             }
             else
             {

test/WebJobs.Script.Tests/Description/DotNet/PackageAssemblyResolverTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Linq;
 using Microsoft.Azure.WebJobs.Script.Description;
+using Microsoft.Azure.WebJobs.Script.Settings;
 using Microsoft.Azure.WebJobs.Script.Tests.Properties;
 using Xunit;
 
@@ -17,12 +18,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         private readonly string _runPath;
         private readonly string _targetAssemblyFilePath;
         private readonly string _targetAssemblyPath;
+        private readonly ISettingsManager _settingsManager;
 
         public PackageAssemblyResolverTests()
         {
+            _settingsManager = ScriptSettingsManager.Instance;
             _runPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
             _lockFilePath = Path.Combine(_runPath, DotNetConstants.ProjectLockFileName);
-            _oldHomeEnv = Environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath);
+            _oldHomeEnv = _settingsManager.GetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteHomePath);
             _targetAssemblyPath = Path.Combine(_runPath, "data\\Functions\\packages\\nuget\\Test.Package\\1.0.0\\lib\\net45");
             _targetAssemblyFilePath = Path.Combine(_targetAssemblyPath, Path.GetFileName(this.GetType().Assembly.Location));
             Directory.CreateDirectory(_targetAssemblyPath);
@@ -33,7 +36,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             // Create our Lock file using the current assembly as the target
             File.WriteAllText(_lockFilePath, string.Format(Resources.ProjectLockFileFormatString, Path.GetFileName(this.GetType().Assembly.Location)));
 
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath, _runPath);
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteHomePath, _runPath);
         }
 
         public void Dispose()
@@ -43,7 +46,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
                 Directory.Delete(_runPath, true);
             }
 
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath, _oldHomeEnv);
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteHomePath, _oldHomeEnv);
         }
 
         [Fact]

test/WebJobs.Script.Tests/ScriptHostTests.cs
@@ -13,6 +13,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Description;
+using Microsoft.Azure.WebJobs.Script.Settings;
 using Moq;
 using Newtonsoft.Json.Linq;
 using Xunit;
@@ -23,10 +24,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
     {
         private const string ID = "5a709861cab44e68bfed5d2c2fe7fc0c";
         private readonly TestFixture _fixture;
+        private readonly ISettingsManager _settingsManager;
 
         public ScriptHostTests(TestFixture fixture)
         {
             _fixture = fixture;
+            _settingsManager = ScriptSettingsManager.Instance;
         }
 
         [Theory]
@@ -283,7 +286,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             var ex = Assert.Throws<FormatException>(() =>
             {
-                ScriptHost.Create(scriptConfig);
+                ScriptHost.Create(_settingsManager, scriptConfig);
             });
 
             Assert.Equal(string.Format("Unable to parse {0} file.", ScriptConstants.HostMetadataFileName), ex.Message);
@@ -500,13 +503,13 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             try
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
+                _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
                 ScriptHost.ApplyConfiguration(config, scriptConfig);
                 Assert.Equal(TimeSpan.FromMinutes(5), scriptConfig.FunctionTimeout);
             }
             finally
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, null);
+                _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteSku, null);
             }
         }
 
@@ -537,7 +540,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             try
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
+                _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
 
                 config["functionTimeout"] = "00:05:01";
                 Assert.Throws<ArgumentException>(() => ScriptHost.ApplyConfiguration(config, scriptConfig));
@@ -547,7 +550,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             }
             finally
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, null);
+                _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteSku, null);
             }
         }
 
@@ -808,11 +811,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         public class TestFixture
         {
+            private readonly ISettingsManager _settingsManager;
+
             public TestFixture()
             {
                 ScriptHostConfiguration config = new ScriptHostConfiguration();
                 config.HostConfig.HostId = ID;
-                Host = ScriptHost.Create(config);
+                _settingsManager = ScriptSettingsManager.Instance;
+                Host = ScriptHost.Create(_settingsManager, config);
             }
 
             public ScriptHost Host { get; private set; }

test/WebJobs.Script.Tests/SystemTraceWriterTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Settings;
 using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Moq;
 using Xunit;
@@ -16,17 +17,20 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         private readonly Mock<IEventGenerator> _mockEventGenerator;
         private readonly string _websiteName;
         private readonly string _subscriptionId;
+        private readonly ISettingsManager _settingsManager;
 
         public SystemTraceWriterTests()
         {
+            _settingsManager = ScriptSettingsManager.Instance;
+
             _subscriptionId = "e3235165-1600-4819-85f0-2ab362e909e4";
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteOwnerName, $"{_subscriptionId}+westuswebspace");
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteOwnerName, $"{_subscriptionId}+westuswebspace");
 
             _websiteName = "functionstest";
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName, _websiteName);
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteName, _websiteName);
 
             _mockEventGenerator = new Mock<IEventGenerator>(MockBehavior.Strict);
-            _traceWriter = new SystemTraceWriter(_mockEventGenerator.Object, TraceLevel.Verbose);
+            _traceWriter = new SystemTraceWriter(_mockEventGenerator.Object, _settingsManager, TraceLevel.Verbose);
         }
 
         [Fact]
@@ -71,8 +75,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         public void Dispose()
         {
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteOwnerName, null);
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName, null);
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteOwnerName, null);
+            _settingsManager.SetEnvironmentSetting(EnvironmentSettingNames.AzureWebsiteName, null);
         }
     }
 }

test/WebJobs.Script.Tests/WebScriptHostHandlerTests.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Dependencies;
+using Microsoft.Azure.WebJobs.Script.Settings;
 using Microsoft.Azure.WebJobs.Script.WebHost;
 using Microsoft.Azure.WebJobs.Script.WebHost.Handlers;
 using Moq;
@@ -18,12 +19,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 {
     public class WebScriptHostHandlerTests
     {
+        private readonly ISettingsManager _settingsManager;
         private HttpMessageInvoker _invoker;
         private Mock<WebScriptHostManager> _managerMock;
 
         public WebScriptHostHandlerTests()
         {
-            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new ScriptHostConfiguration(), new SecretManager(), new WebHostSettings());
+            _settingsManager = ScriptSettingsManager.Instance;
+            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new ScriptHostConfiguration(), new SecretManager(), _settingsManager, new WebHostSettings());
             _managerMock.SetupGet(p => p.Initialized).Returns(true);
             Mock<IDependencyResolver> mockResolver = new Mock<IDependencyResolver>(MockBehavior.Strict);
             mockResolver.Setup(p => p.GetService(typeof(WebScriptHostManager))).Returns(_managerMock.Object);

Azure/azure-functions-host

src/WebJobs.Script/Diagnostics/FileTraceWriter.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Config;
 
 namespace Microsoft.Azure.WebJobs.Script
 {
@@ -253,7 +254,7 @@ namespace Microsoft.Azure.WebJobs.Script
 
         internal static string GetInstanceId()
         {
-            string instanceId = System.Environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteInstanceId);
+            string instanceId = ScriptSettingsManager.Instance.GetSetting(EnvironmentSettingNames.AzureWebsiteInstanceId);
             if (string.IsNullOrEmpty(instanceId))
             {
                 instanceId = Environment.MachineName;

test/WebJobs.Script.Tests/ApiHub/ApiHubTestHelper.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.ApiHub;
 using Microsoft.Azure.ApiHub.Table.Internal;
 using Microsoft.Azure.WebJobs.Extensions.ApiHub.Common;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
@@ -28,12 +29,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
 
         private static readonly Random Random = new Random();
 
+        private static readonly ScriptSettingsManager SettingsManager = ScriptSettingsManager.Instance;
+
         public static void SetDefaultConnectionFactory()
         {
             // Setup the default ApiHub connection factory to use an actual SqlAzure connector 
             // if the AzureWebJobsSql environment variable specifies the connection string,
             // otherwise use a fake tabular connector.
-            var connectionString = Environment.GetEnvironmentVariable(Key);
+            var connectionString = SettingsManager.GetSetting(Key);
             if (string.IsNullOrEmpty(connectionString))
             {
                 var tableAdapter = new FakeTabularConnectorAdapter();
@@ -44,7 +47,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApiHub
                 // The value doesn't really matter here - this is just so we
                 // pass the Connection value validation that ScriptHost performs
                 // on startup
-                Environment.SetEnvironmentVariable(Key, "TestMockSqlConnection");
+                SettingsManager.SetSetting(Key, "TestMockSqlConnection");
             }
             else
             {

test/WebJobs.Script.Tests/Description/DotNet/PackageAssemblyResolverTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Tests.Properties;
 using Xunit;
@@ -17,12 +18,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         private readonly string _runPath;
         private readonly string _targetAssemblyFilePath;
         private readonly string _targetAssemblyPath;
+        private readonly ScriptSettingsManager _settingsManager;
 
         public PackageAssemblyResolverTests()
         {
+            _settingsManager = ScriptSettingsManager.Instance;
             _runPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
             _lockFilePath = Path.Combine(_runPath, DotNetConstants.ProjectLockFileName);
-            _oldHomeEnv = Environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath);
+            _oldHomeEnv = _settingsManager.GetSetting(EnvironmentSettingNames.AzureWebsiteHomePath);
             _targetAssemblyPath = Path.Combine(_runPath, "data\\Functions\\packages\\nuget\\Test.Package\\1.0.0\\lib\\net45");
             _targetAssemblyFilePath = Path.Combine(_targetAssemblyPath, Path.GetFileName(this.GetType().Assembly.Location));
             Directory.CreateDirectory(_targetAssemblyPath);
@@ -33,7 +36,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             // Create our Lock file using the current assembly as the target
             File.WriteAllText(_lockFilePath, string.Format(Resources.ProjectLockFileFormatString, Path.GetFileName(this.GetType().Assembly.Location)));
 
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath, _runPath);
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteHomePath, _runPath);
         }
 
         public void Dispose()
@@ -43,7 +46,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
                 Directory.Delete(_runPath, true);
             }
 
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteHomePath, _oldHomeEnv);
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteHomePath, _oldHomeEnv);
         }
 
         [Fact]

test/WebJobs.Script.Tests/ScriptHostTests.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Moq;
 using Newtonsoft.Json.Linq;
@@ -23,10 +24,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
     {
         private const string ID = "5a709861cab44e68bfed5d2c2fe7fc0c";
         private readonly TestFixture _fixture;
+        private readonly ScriptSettingsManager _settingsManager;
 
         public ScriptHostTests(TestFixture fixture)
         {
             _fixture = fixture;
+            _settingsManager = ScriptSettingsManager.Instance;
         }
 
         [Theory]
@@ -283,7 +286,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             var ex = Assert.Throws<FormatException>(() =>
             {
-                ScriptHost.Create(scriptConfig);
+                ScriptHost.Create(_settingsManager, scriptConfig);
             });
 
             Assert.Equal(string.Format("Unable to parse {0} file.", ScriptConstants.HostMetadataFileName), ex.Message);
@@ -500,13 +503,13 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             try
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
+                _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
                 ScriptHost.ApplyConfiguration(config, scriptConfig);
                 Assert.Equal(TimeSpan.FromMinutes(5), scriptConfig.FunctionTimeout);
             }
             finally
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, null);
+                _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteSku, null);
             }
         }
 
@@ -537,7 +540,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             try
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
+                _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteSku, "Dynamic");
 
                 config["functionTimeout"] = "00:05:01";
                 Assert.Throws<ArgumentException>(() => ScriptHost.ApplyConfiguration(config, scriptConfig));
@@ -547,7 +550,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             }
             finally
             {
-                Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, null);
+                _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteSku, null);
             }
         }
 
@@ -808,11 +811,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         public class TestFixture
         {
+            private readonly ScriptSettingsManager _settingsManager;
+
             public TestFixture()
             {
                 ScriptHostConfiguration config = new ScriptHostConfiguration();
                 config.HostConfig.HostId = ID;
-                Host = ScriptHost.Create(config);
+                _settingsManager = ScriptSettingsManager.Instance;
+                Host = ScriptHost.Create(_settingsManager, config);
             }
 
             public ScriptHost Host { get; private set; }

test/WebJobs.Script.Tests/SystemTraceWriterTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Moq;
 using Xunit;
@@ -16,17 +17,20 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         private readonly Mock<IEventGenerator> _mockEventGenerator;
         private readonly string _websiteName;
         private readonly string _subscriptionId;
+        private readonly ScriptSettingsManager _settingsManager;
 
         public SystemTraceWriterTests()
         {
+            _settingsManager = ScriptSettingsManager.Instance;
+
             _subscriptionId = "e3235165-1600-4819-85f0-2ab362e909e4";
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteOwnerName, $"{_subscriptionId}+westuswebspace");
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteOwnerName, $"{_subscriptionId}+westuswebspace");
 
             _websiteName = "functionstest";
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName, _websiteName);
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteName, _websiteName);
 
             _mockEventGenerator = new Mock<IEventGenerator>(MockBehavior.Strict);
-            _traceWriter = new SystemTraceWriter(_mockEventGenerator.Object, TraceLevel.Verbose);
+            _traceWriter = new SystemTraceWriter(_mockEventGenerator.Object, _settingsManager, TraceLevel.Verbose);
         }
 
         [Fact]
@@ -71,8 +75,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         public void Dispose()
         {
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteOwnerName, null);
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName, null);
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteOwnerName, null);
+            _settingsManager.SetSetting(EnvironmentSettingNames.AzureWebsiteName, null);
         }
     }
 }

test/WebJobs.Script.Tests/WebScriptHostHandlerTests.cs
@@ -4,11 +4,11 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Dependencies;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.WebHost;
 using Microsoft.Azure.WebJobs.Script.WebHost.Handlers;
 using Moq;
@@ -18,12 +18,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 {
     public class WebScriptHostHandlerTests
     {
+        private readonly ScriptSettingsManager _settingsManager;
         private HttpMessageInvoker _invoker;
         private Mock<WebScriptHostManager> _managerMock;
 
         public WebScriptHostHandlerTests()
         {
-            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new ScriptHostConfiguration(), new SecretManager(), new WebHostSettings());
+            _settingsManager = ScriptSettingsManager.Instance;
+            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new ScriptHostConfiguration(), new SecretManager(), _settingsManager, new WebHostSettings());
             _managerMock.SetupGet(p => p.Initialized).Returns(true);
             Mock<IDependencyResolver> mockResolver = new Mock<IDependencyResolver>(MockBehavior.Strict);
             mockResolver.Setup(p => p.GetService(typeof(WebScriptHostManager))).Returns(_managerMock.Object);