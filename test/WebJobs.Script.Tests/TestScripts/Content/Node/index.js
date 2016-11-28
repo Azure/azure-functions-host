b1ffc78b92b3dd4ce895f37f1dd4de6584e3c1a5
Matt Mason <mamaso@microsoft.com>
2016-11-27 18:32:31 -0800

    add node http content tests

---
+++ test/WebJobs.Script.Tests/TestScripts/Content/Node/index.js
@@ -0,0 +1,11 @@
+﻿var util = require('util');
+
+module.exports = function (context, req) {
+    if (req.headers.return) {
+        context.res = req.body;
+        context.done();
+    } else {
+        var sendFunc = req.headers.raw ? 'raw' : 'send';
+        context.res.type(req.headers.type)[sendFunc](req.body);
+    }
+}
