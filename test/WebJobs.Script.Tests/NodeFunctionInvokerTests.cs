1275502b120dd76d0585437dd55b47db4ba7054c
Mathew Charles <mathewc@microsoft.com>
2016-12-01 14:57:40 -0800

    Removing custom DictionaryJsonConverter

--- test/WebJobs.Script.Tests/NodeFunctionInvokerTests.cs
+++ test/WebJobs.Script.Tests/NodeFunctionInvokerTests.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.Azure.WebJobs.Script.Description;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests
@@ -27,14 +26,43 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             Assert.True(didConvert);
 
             object[] objects = result as object[];
-            Dictionary<string, object> obj = (Dictionary<string, object>)objects[0];
+            IDictionary<string, object> obj = (IDictionary<string, object>)objects[0];
             Assert.Equal("Larry", obj["name"]);
 
-            obj = (Dictionary<string, object>)objects[1];
+            obj = (IDictionary<string, object>)objects[1];
             Assert.Equal("Moe", obj["name"]);
 
-            obj = (Dictionary<string, object>)objects[2];
+            obj = (IDictionary<string, object>)objects[2];
             Assert.Equal("Curly", obj["name"]);
+        }
+
+        [Fact]
+        public void TryConvertJson_JsonObjectString()
+        {
+            JObject child = new JObject
+            {
+                { "Name", "Mary" },
+                { "Location", "Seattle" },
+                { "Age", 5 }
+            };
+
+            JObject parent = new JObject
+            {
+                { "Name", "Bob" },
+                { "Location", "Seattle" },
+                { "Age", 40 },
+                { "Children", new JArray(child) }
+            };
+
+            object result;
+            string input = parent.ToString();
+            bool didConvert = NodeFunctionInvoker.TryConvertJson(input, out result);
+            Assert.True(didConvert);
+
+            var resultDictionary = (IDictionary<string, object>)result;
+            var resultChildren = (IEnumerable<object>)resultDictionary["Children"];
+            var resultChild = (IDictionary<string, object>)resultChildren.ElementAt(0);
+            Assert.Equal(5, (long)resultChild["Age"]);
         }
     }
 }
