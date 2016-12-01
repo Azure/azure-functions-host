552fcb854b9fdffd43e64ff4f4face668a2b7389
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2016-12-01 00:23:46 -0500

    Disable NuGet build on Unix as it does not currently work

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 CONFIG="${1:-Release}"
 SLNDIR="$( dirname ${0} )"
 
-msbuild WebJobs.Script.proj "/p:Configuration=${CONFIG};SolutionDir=${SLNDIR}"
+msbuild WebJobs.Script.proj /t:RestorePackages
+msbuild WebJobs.Script.sln "/p:Configuration=${CONFIG};SolutionDir=${SLNDIR}"
