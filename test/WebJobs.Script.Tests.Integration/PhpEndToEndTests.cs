8d2e128e4be5474ccfbdb35acca9392140922010
Matt Mason <mamaso@microsoft.com>
2017-01-26 15:24:45 -0800

    Handle json arrays for script languages

--- test/WebJobs.Script.Tests.Integration/PhpEndToEndTests.cs
+++ test/WebJobs.Script.Tests.Integration/PhpEndToEndTests.cs
@@ -1,8 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests
@@ -37,6 +44,35 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         public async Task QueueTriggerToBlob()
         {
             await QueueTriggerToBlobTest();
+        }
+
+        [Fact]
+        public async Task HttpTrigger_Get_Array()
+        {
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost/api/httptrigger"),
+                Method = HttpMethod.Get
+            };
+            request.SetConfiguration(new HttpConfiguration());
+
+            Dictionary<string, object> arguments = new Dictionary<string, object>
+            {
+                { "req", request }
+            };
+            await Fixture.Host.CallAsync("HttpTrigger", arguments);
+
+            HttpResponseMessage response = (HttpResponseMessage)request.Properties[ScriptConstants.AzureFunctionsHttpResponseKey];
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var contentType = response.Content.Headers.ContentType;
+            Assert.Equal("application/json", contentType.MediaType);
+            Assert.Equal("utf-8", contentType.CharSet);
+
+            ObjectContent objectContent = response.Content as ObjectContent;
+            Assert.NotNull(objectContent);
+            Assert.Equal(typeof(JArray), objectContent.ObjectType);
+            JArray content = await response.Content.ReadAsAsync<JArray>();
+            Assert.Equal("[{\"a\":\"b\"}]", content.ToString(Formatting.None));
         }
 
         public class TestFixture : EndToEndTestFixture
