e7b82f3bde5aa90ec51fbafd208f04ccc87ee444
Matt Mason <mamaso@microsoft.com>
2017-04-25 10:56:54 -0700

    Removed usage of WriteEvent object[] overload

--- src/WebJobs.Script.WebHost/Diagnostics/EventGenerator.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/EventGenerator.cs
@@ -56,8 +56,182 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
             FunctionsEventSource.Instance.LogFunctionExecutionEvent(executionId, siteName, concurrency, functionName, invocationId, executionStage, (ulong)executionTimeSpan, success);
         }
 
+        public unsafe class ExtendedEventSource : EventSource
+        {
+            [NonEvent]
+            [Obsolete("Do not use params object[] overload, create a explicit WriteEvent in ExtendedEventSource.", true)]
+            protected new void WriteEvent(int eventNum, params object[] args)
+            {
+                base.WriteEvent(eventNum, args);
+            }
+
+            // LogFunctionExecutionAggregateEvent
+            [NonEvent]
+            protected void WriteEvent(int eventNum, string a, string b, ulong c, ulong d, ulong e, ulong f)
+            {
+                const int count = 6;
+                fixed (char* aPtr = a, bPtr = b)
+                {
+                    EventData* data = stackalloc EventData[count];
+                    data[0].DataPointer = (IntPtr)aPtr;
+                    data[0].Size = (a.Length + 1) * sizeof(char);
+                    data[1].DataPointer = (IntPtr)bPtr;
+                    data[1].Size = (b.Length + 1) * sizeof(char);
+                    data[2].DataPointer = (IntPtr)(&c);
+                    data[2].Size = sizeof(ulong);
+                    data[3].DataPointer = (IntPtr)(&d);
+                    data[3].Size = sizeof(ulong);
+                    data[4].DataPointer = (IntPtr)(&e);
+                    data[4].Size = sizeof(ulong);
+                    data[5].DataPointer = (IntPtr)(&f);
+                    data[5].Size = sizeof(ulong);
+
+                    WriteEventCore(eventNum, count, data);
+                }
+            }
+
+            // LogFunctionDetailsEvent
+            [NonEvent]
+            protected void WriteEvent(int eventNum, string a, string b, string c, string d, string e, bool f)
+            {
+                const int count = 6;
+                fixed (char* aPtr = a,
+                    bPtr = b,
+                    cPtr = c,
+                    dPtr = d,
+                    ePtr = e)
+                {
+                    EventData* data = stackalloc EventData[count];
+                    data[0].DataPointer = (IntPtr)aPtr;
+                    data[0].Size = (a.Length + 1) * sizeof(char);
+                    data[1].DataPointer = (IntPtr)bPtr;
+                    data[1].Size = (b.Length + 1) * sizeof(char);
+                    data[2].DataPointer = (IntPtr)cPtr;
+                    data[2].Size = (c.Length + 1) * sizeof(char);
+                    data[3].DataPointer = (IntPtr)dPtr;
+                    data[3].Size = (d.Length + 1) * sizeof(char);
+                    data[4].DataPointer = (IntPtr)ePtr;
+                    data[4].Size = (e.Length + 1) * sizeof(char);
+                    data[5].DataPointer = (IntPtr)(&f);
+                    data[5].Size = 4; // boolean variables have size 4
+
+                    WriteEventCore(eventNum, count, data);
+                }
+            }
+
+            // LogFunctionExecutionEvent
+            [NonEvent]
+            protected void WriteEvent(int eventNum, string a, string b, int c, string d, string e, string f, ulong g, bool h)
+            {
+                const int count = 8;
+                fixed (char* aPtr = a,
+                    bPtr = b,
+                    dPtr = d,
+                    ePtr = e,
+                    fPtr = f)
+                {
+                    EventData* data = stackalloc EventData[count];
+                    data[0].DataPointer = (IntPtr)aPtr;
+                    data[0].Size = (a.Length + 1) * sizeof(char);
+                    data[1].DataPointer = (IntPtr)bPtr;
+                    data[1].Size = (b.Length + 1) * sizeof(char);
+                    data[2].DataPointer = (IntPtr)(&c);
+                    data[2].Size = sizeof(int);
+                    data[3].DataPointer = (IntPtr)dPtr;
+                    data[3].Size = (d.Length + 1) * sizeof(char);
+                    data[4].DataPointer = (IntPtr)ePtr;
+                    data[4].Size = (e.Length + 1) * sizeof(char);
+                    data[5].DataPointer = (IntPtr)fPtr;
+                    data[5].Size = (f.Length + 1) * sizeof(char);
+                    data[6].DataPointer = (IntPtr)(&g);
+                    data[6].Size = sizeof(ulong);
+                    data[7].DataPointer = (IntPtr)(&h);
+                    data[7].Size = 4; // boolean variables have size 4
+
+                    WriteEventCore(eventNum, count, data);
+                }
+            }
+
+            // RaiseFunctionsEventVerbose/Info/Warning/Error
+            [NonEvent]
+            protected void WriteEvent(int eventNum, string a, string b, string c, string d, string e, string f, string g, string h, string i)
+            {
+                const int count = 9;
+                fixed (char* aPtr = a,
+                    bPtr = b,
+                    cPtr = c,
+                    dPtr = d,
+                    ePtr = e,
+                    fPtr = f,
+                    gPtr = g,
+                    hPtr = h,
+                    iPtr = i)
+                {
+                    EventData* data = stackalloc EventData[count];
+                    data[0].DataPointer = (IntPtr)aPtr;
+                    data[0].Size = (a.Length + 1) * sizeof(char);
+                    data[1].DataPointer = (IntPtr)bPtr;
+                    data[1].Size = (b.Length + 1) * sizeof(char);
+                    data[2].DataPointer = (IntPtr)cPtr;
+                    data[2].Size = (c.Length + 1) * sizeof(char);
+                    data[3].DataPointer = (IntPtr)dPtr;
+                    data[3].Size = (d.Length + 1) * sizeof(char);
+                    data[4].DataPointer = (IntPtr)ePtr;
+                    data[4].Size = (e.Length + 1) * sizeof(char);
+                    data[5].DataPointer = (IntPtr)fPtr;
+                    data[5].Size = (f.Length + 1) * sizeof(char);
+                    data[6].DataPointer = (IntPtr)gPtr;
+                    data[6].Size = (g.Length + 1) * sizeof(char);
+                    data[7].DataPointer = (IntPtr)hPtr;
+                    data[7].Size = (h.Length + 1) * sizeof(char);
+                    data[8].DataPointer = (IntPtr)iPtr;
+                    data[8].Size = (i.Length + 1) * sizeof(char);
+
+                    WriteEventCore(eventNum, count, data);
+                }
+            }
+
+            // LogFunctionMetricEvent
+            [NonEvent]
+            protected void WriteEvent(int eventNum, string a, string b, string c, string d, long e, long f, long g, long h, string i, string j)
+            {
+                const int count = 10;
+                fixed (char* aPtr = a,
+                    bPtr = b,
+                    cPtr = c,
+                    dPtr = d,
+                    iPtr = i,
+                    jPtr = j)
+                {
+                    EventData* data = stackalloc EventData[count];
+                    data[0].DataPointer = (IntPtr)aPtr;
+                    data[0].Size = (a.Length + 1) * sizeof(char);
+                    data[1].DataPointer = (IntPtr)bPtr;
+                    data[1].Size = (b.Length + 1) * sizeof(char);
+                    data[2].DataPointer = (IntPtr)cPtr;
+                    data[2].Size = (c.Length + 1) * sizeof(char);
+                    data[3].DataPointer = (IntPtr)dPtr;
+                    data[3].Size = (d.Length + 1) * sizeof(char);
+                    data[4].DataPointer = (IntPtr)(&e);
+                    data[4].Size = sizeof(long);
+                    data[5].DataPointer = (IntPtr)(&f);
+                    data[5].Size = sizeof(long);
+                    data[6].DataPointer = (IntPtr)(&g);
+                    data[6].Size = sizeof(long);
+                    data[7].DataPointer = (IntPtr)(&h);
+                    data[7].Size = sizeof(long);
+                    data[8].DataPointer = (IntPtr)iPtr;
+                    data[8].Size = (i.Length + 1) * sizeof(char);
+                    data[9].DataPointer = (IntPtr)jPtr;
+                    data[9].Size = (j.Length + 1) * sizeof(char);
+
+                    WriteEventCore(eventNum, count, data);
+                }
+            }
+        }
+
         [EventSource(Guid = "08D0D743-5C24-43F9-9723-98277CEA5F9B")]
-        public class FunctionsEventSource : EventSource
+        public class FunctionsEventSource : ExtendedEventSource
         {
             internal static readonly FunctionsEventSource Instance = new FunctionsEventSource();
 
@@ -91,7 +265,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
 
         [EventSource(Guid = "a7044dd6-c8ef-4980-858c-942d972b6250")]
         [SuppressMessage("Microsoft.StyleCop.CSharp.NamingRules", "SA1306:FieldNamesMustBeginWithLowerCaseLetter", Justification = "Need to use Pascal Case for MDS column names")]
-        public class FunctionsSystemLogsEventSource : EventSource
+        public class FunctionsSystemLogsEventSource : ExtendedEventSource
         {
             internal static readonly FunctionsSystemLogsEventSource Instance = new FunctionsSystemLogsEventSource();
 
