ceb5e296528ff3a87498ac607a71f7e73832fa77
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-05-26 18:09:29 -0700

    Moving AppVeyor tests command to a script

---
+++ runappveyortests.cmd
@@ -0,0 +1,3 @@
+vstest.console.exe "%APPVEYOR_BUILD_FOLDER%\test\WebJobs.Script.Tests\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.dll" "%APPVEYOR_BUILD_FOLDER%\test\WebJobs.Script.Tests\bin\Release\xunit.runner.visualstudio.testadapter.dll" /logger:Appveyor /TestAdapterPath:"%APPVEYOR_BUILD_FOLDER%"
+call runNodeTests.cmd
+vstest.console.exe "%APPVEYOR_BUILD_FOLDER%\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" "%APPVEYOR_BUILD_FOLDER%\test\WebJobs.Script.Tests\bin\Release\xunit.runner.visualstudio.testadapter.dll" /logger:Appveyor /TestAdapterPath:"%APPVEYOR_BUILD_FOLDER%"
