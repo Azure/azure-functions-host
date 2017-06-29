3f3a1a5a9a14ec59e7cd26ac752c91b74882430b
Mathew Charles <mathewc@microsoft.com>
2017-06-29 11:23:34 -0700

    Authentication model for Extension WebHooks

--- src/WebJobs.Script.WebHost/WebScriptHostManager.cs
+++ src/WebJobs.Script.WebHost/WebScriptHostManager.cs
@@ -46,10 +46,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
         private readonly object _syncLock = new object();
         private readonly int _hostTimeoutSeconds;
         private readonly int _hostRunningPollIntervalMilliseconds;
-        private readonly IWebJobsRouter _router;
-
-        // TODO: FACAVAL
-        // private readonly WebJobsSdkExtensionHookProvider _bindingWebHookProvider = new WebJobsSdkExtensionHookProvider();
+        private readonly WebJobsSdkExtensionHookProvider _bindingWebHookProvider;
 
         private bool _warmupComplete = false;
         private bool _hostStarted = false;
@@ -95,10 +92,9 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             // _swaggerDocumentManager = new SwaggerDocumentManager(config);
 
             var secretsRepository = secretsRepositoryFactory.Create(settingsManager, webHostSettings, config);
-            _secretManager = secretManagerFactory.Create(settingsManager, config.HostConfig.LoggerFactory, secretsRepository);
+            _secretManager = secretManagerFactory.Create(settingsManager, config.TraceWriter, config.HostConfig.LoggerFactory, secretsRepository);
 
-            // TODO: FACAVAL
-            // _bindingWebHookProvider = new WebJobsSdkExtensionHookProvider(_secretManager);
+            _bindingWebHookProvider = new WebJobsSdkExtensionHookProvider(_secretManager);
         }
 
         public WebScriptHostManager(ScriptHostConfiguration config,
@@ -124,8 +120,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
         {
         }
 
-        // TODO: FACAVAL
-        // internal WebJobsSdkExtensionHookProvider BindingWebHookProvider => _bindingWebHookProvider;
+        internal WebJobsSdkExtensionHookProvider BindingWebHookProvider => _bindingWebHookProvider;
 
         public ISecretManager SecretManager => _secretManager;
 
