6454865b1759f930f05ba9f9b99bb9ca2112630c
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-06-30 10:49:53 -0700

    Updated to .NET Core 2.0 preview 2

--- src/WebJobs.Script.WebHost.Core/WebJobsScriptHostService.cs
+++ src/WebJobs.Script.WebHost.Core/WebJobsScriptHostService.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WebJobs.Script.WebHost.Core;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost.Core
@@ -15,8 +16,9 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Core
         private readonly CancellationTokenSource _cancellationTokenSource;
         private bool _disposed = false;
         private Task _hostTask;
+        private readonly ILogger<WebJobsScriptHostService> _logger;
 
-        public WebJobsScriptHostService(WebScriptHostManager scriptHostManager)
+        public WebJobsScriptHostService(WebScriptHostManager scriptHostManager, ILoggerFactory loggerFactory)
         {
             _scriptHostManager = scriptHostManager ?? throw new ArgumentException($@"Unable to locate the {nameof(WebScriptHostManager)} service. " +
                     $"Please add all the required services by calling '{nameof(IServiceCollection)}.{nameof(WebJobsServiceCollectionExtensions.AddWebJobsScriptHost)}' " +
@@ -24,20 +26,32 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Core
 
             _cancellationTokenSource = new CancellationTokenSource();
             _hostTask = Task.CompletedTask;
-        }
-        public void Start()
-        {
-            _hostTask = Task.Run(() => _scriptHostManager.Initialize(_cancellationTokenSource.Token));
+            _logger = loggerFactory.CreateLogger<WebJobsScriptHostService>();
         }
 
-        public void Stop()
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _hostTask = Task.Run(() =>
+            {
+                _logger.LogDebug("Initializing WebScriptHostManager.");
+                _scriptHostManager.Initialize(_cancellationTokenSource.Token);
+                _logger.LogDebug("WebScriptHostManager initialized.");
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
         {
             _cancellationTokenSource.Cancel();
 
-            _hostTask.Wait(TimeSpan.FromSeconds(10));
+            Task result = await Task.WhenAny(_hostTask, Task.Delay(TimeSpan.FromSeconds(10)));
+
+            if (result != _hostTask)
+            {
+                _logger.LogWarning("Script host manager did not shutdown within its allotted time.");
+            }
         }
-
-
 
         protected virtual void Dispose(bool disposing)
         {
