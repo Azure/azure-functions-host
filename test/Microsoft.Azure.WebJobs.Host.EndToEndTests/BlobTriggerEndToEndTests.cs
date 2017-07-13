d05cf0c9ff13d7820b4721c7fccd438887d671e9
Mike Stall <mikestall@hotmail.com>
2017-07-13 13:52:06 -0700

    Fix test bugs. No product change.

--- test/Microsoft.Azure.WebJobs.Host.EndToEndTests/BlobTriggerEndToEndTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.EndToEndTests/BlobTriggerEndToEndTests.cs
@@ -31,105 +31,124 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         private const string BlobChainOutputBlobName = "blob.out";
         private const string BlobChainOutputBlobPath = BlobChainContainerName + "/" + BlobChainOutputBlobName;
 
-        private static ManualResetEvent _completedEvent;
-        private static int _timesProcessed;
-
         private readonly CloudBlobContainer _testContainer;
-        private readonly JobHostConfiguration _hostConfiguration;
         private readonly CloudStorageAccount _storageAccount;
         private readonly RandomNameResolver _nameResolver;
 
         private static object _syncLock = new object();
-        private static List<string> poisonBlobMessages = new List<string>();
 
         public BlobTriggerEndToEndTests()
         {
-            _timesProcessed = 0;
-
             _nameResolver = new RandomNameResolver();
-            _hostConfiguration = new JobHostConfiguration()
-            {
-                NameResolver = _nameResolver,
-                TypeLocator = new FakeTypeLocator(this.GetType()),
-            };
 
-            _storageAccount = CloudStorageAccount.Parse(_hostConfiguration.StorageConnectionString);
+            var storageConnectionString = new JobHostConfiguration().StorageConnectionString;
+            _storageAccount = CloudStorageAccount.Parse(storageConnectionString);
             CloudBlobClient blobClient = _storageAccount.CreateCloudBlobClient();
             _testContainer = blobClient.GetContainerReference(_nameResolver.ResolveInString(SingleTriggerContainerName));
             Assert.False(_testContainer.Exists());
             _testContainer.Create();
         }
 
-        public static void BlobProcessorPrimary(
-            [BlobTrigger(PoisonTestContainerName + "/{name}")] string input)
+        public JobHostConfiguration NewConfig<TProgram>(TProgram program, params object[] services)
         {
-            // throw to generate a poison blob message
-            throw new Exception();
+            JobHostConfiguration config = new JobHostConfiguration();
+
+            var activator = new FakeActivator();
+            activator.Add(program);
+            config.TypeLocator = new FakeTypeLocator(typeof(TProgram));
+            config.JobActivator = activator;
+            
+            config.AddServices(services);
+            config.AddServices(_nameResolver);
+            return config;
         }
 
-        // process the poison queue for the primary storage account
-        public static void PoisonBlobQueueProcessorPrimary(
-            [QueueTrigger("webjobs-blobtrigger-poison")] JObject message)
+        public class Poison_Program
         {
-            lock (_syncLock)
+            public List<string> _poisonBlobMessages = new List<string>();
+
+            public void BlobProcessorPrimary(
+                [BlobTrigger(PoisonTestContainerName + "/{name}")] string input)
             {
-                string blobName = (string)message["BlobName"];
-                poisonBlobMessages.Add(blobName);
+                // throw to generate a poison blob message
+                throw new Exception();
+            }
+
+            // process the poison queue for the primary storage account
+            public void PoisonBlobQueueProcessorPrimary(
+                [QueueTrigger("webjobs-blobtrigger-poison")] JObject message)
+            {
+                lock (_syncLock)
+                {
+                    string blobName = (string)message["BlobName"];
+                    _poisonBlobMessages.Add(blobName);
+                }
+            }
+
+            public void BlobProcessorSecondary(
+                [StorageAccount("SecondaryStorage")]
+            [BlobTrigger(PoisonTestContainerName + "/{name}")] string input)
+            {
+                // throw to generate a poison blob message
+                throw new Exception();
+            }
+
+            // process the poison queue for the secondary storage account
+            public void PoisonBlobQueueProcessorSecondary(
+                [StorageAccount("SecondaryStorage")]
+            [QueueTrigger("webjobs-blobtrigger-poison")] JObject message)
+            {
+                lock (_syncLock)
+                {
+                    string blobName = (string)message["BlobName"];
+                    _poisonBlobMessages.Add(blobName);
+                }
             }
         }
 
-        public static void BlobProcessorSecondary(
-            [StorageAccount("SecondaryStorage")]
-            [BlobTrigger(PoisonTestContainerName + "/{name}")] string input)
+        public class BlobGetsProcessedOnlyOnce_SingleHost_Program
         {
-            // throw to generate a poison blob message
-            throw new Exception();
-        }
+            public int _timesProcessed;
+            public ManualResetEvent _completedEvent;
 
-        // process the poison queue for the secondary storage account
-        public static void PoisonBlobQueueProcessorSecondary(
-            [StorageAccount("SecondaryStorage")]
-            [QueueTrigger("webjobs-blobtrigger-poison")] JObject message)
-        {
-            lock (_syncLock)
+            public void SingleBlobTrigger(
+                [BlobTrigger(SingleTriggerContainerName + "/{name}")] string sleepTimeInSeconds)
             {
-                string blobName = (string)message["BlobName"];
-                poisonBlobMessages.Add(blobName);
+                Interlocked.Increment(ref _timesProcessed);
+
+                int sleepTime = int.Parse(sleepTimeInSeconds) * 1000;
+                Thread.Sleep(sleepTime);
+
+                _completedEvent.Set();
             }
         }
 
-        public static void SingleBlobTrigger(
-            [BlobTrigger(SingleTriggerContainerName + "/{name}")] string sleepTimeInSeconds)
+        public class BlobChainTest_Program
         {
-            Interlocked.Increment(ref _timesProcessed);
+            public ManualResetEvent _completedEvent;
 
-            int sleepTime = int.Parse(sleepTimeInSeconds) * 1000;
-            Thread.Sleep(sleepTime);
+            public void BlobChainStepOne(
+                [BlobTrigger(BlobChainTriggerBlobPath)] TextReader input,
+                [Blob(BlobChainIntermediateBlobPath)] TextWriter output)
+            {
+                string content = input.ReadToEnd();
+                output.Write(content);
+            }
 
-            _completedEvent.Set();
-        }
+            public void BlobChainStepTwo(
+                [BlobTrigger(BlobChainIntermediateBlobPath)] TextReader input,
+                [Blob(BlobChainOutputBlobPath)] TextWriter output,
+                [Queue(BlobChainCommittedQueueName)] out string committed)
+            {
+                string content = input.ReadToEnd();
+                output.Write("*" + content + "*");
+                committed = String.Empty;
+            }
 
-        public static void BlobChainStepOne(
-            [BlobTrigger(BlobChainTriggerBlobPath)] TextReader input,
-            [Blob(BlobChainIntermediateBlobPath)] TextWriter output)
-        {
-            string content = input.ReadToEnd();
-            output.Write(content);
-        }
-
-        public static void BlobChainStepTwo(
-            [BlobTrigger(BlobChainIntermediateBlobPath)] TextReader input,
-            [Blob(BlobChainOutputBlobPath)] TextWriter output, 
-            [Queue(BlobChainCommittedQueueName)] out string committed)
-        {
-            string content = input.ReadToEnd();
-            output.Write("*" + content + "*");
-            committed = String.Empty;
-        }
-
-        public static void BlobChainStepThree([QueueTrigger(BlobChainCommittedQueueName)] string ignore)
-        {
-            _completedEvent.Set();
+            public void BlobChainStepThree([QueueTrigger(BlobChainCommittedQueueName)] string ignore)
+            {
+                _completedEvent.Set();
+            }
         }
 
         [Theory]
@@ -137,8 +156,6 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [InlineData("AzureWebJobsStorage")]
         public async Task PoisonMessage_CreatedInCorrectStorageAccount(string storageAccountSetting)
         {
-            poisonBlobMessages.Clear();
-
             var storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable(storageAccountSetting));
             var blobClient = storageAccount.CreateCloudBlobClient();
             var containerName = _nameResolver.ResolveInString(PoisonTestContainerName);
@@ -149,14 +166,17 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
             CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
             blob.UploadText("0");
 
-            using (JobHost host = new JobHost(_hostConfiguration))
+            var prog = new Poison_Program();
+            var config = NewConfig(prog);
+
+            using (JobHost host = new JobHost(config))
             {
                 host.Start();
 
                 // wait for the poison message to be handled
                 await TestHelpers.Await(() =>
                 {
-                    return poisonBlobMessages.Contains(blobName);
+                    return prog._poisonBlobMessages.Contains(blobName);
                 });
             }
         }
@@ -173,41 +193,44 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
 
             int timeToProcess;
 
+            var prog = new BlobGetsProcessedOnlyOnce_SingleHost_Program();
+            var config = NewConfig(prog);
+                        
             // Process the blob first
-            using (_completedEvent = new ManualResetEvent(initialState: false))
-            using (JobHost host = new JobHost(_hostConfiguration))
+            using (prog._completedEvent = new ManualResetEvent(initialState: false))
+            using (JobHost host = new JobHost(config))
             {
                 DateTime startTime = DateTime.Now;
 
                 host.Start();
-                Assert.True(_completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
+                Assert.True(prog._completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
 
                 timeToProcess = (int)(DateTime.Now - startTime).TotalMilliseconds;
 
                 Console.SetOut(hold);
 
-                Assert.Equal(1, _timesProcessed);
+                Assert.Equal(1, prog._timesProcessed);
 
                 string[] consoleOutputLines = consoleOutput.ToString().Trim().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
                 var executions = consoleOutputLines.Where(p => p.Contains("Executing"));
                 Assert.Equal(1, executions.Count());
-                Assert.StartsWith(string.Format("Executing 'BlobTriggerEndToEndTests.SingleBlobTrigger' (Reason='New blob detected: {0}/{1}', Id=", blob.Container.Name, blob.Name), executions.Single());
+                Assert.StartsWith(string.Format("Executing 'BlobGetsProcessedOnlyOnce_SingleHost_Program.SingleBlobTrigger' (Reason='New blob detected: {0}/{1}', Id=", blob.Container.Name, blob.Name), executions.Single());
             }
 
             // Then start again and make sure the blob doesn't get reprocessed
             // wait twice the amount of time required to process first before 
             // deciding that it doesn't get reprocessed
-            using (_completedEvent = new ManualResetEvent(initialState: false))
-            using (JobHost host = new JobHost(_hostConfiguration))
+            using (prog._completedEvent = new ManualResetEvent(initialState: false))
+            using (JobHost host = new JobHost(config))
             {
                 host.Start();
 
-                bool blobReprocessed = _completedEvent.WaitOne(2 * timeToProcess);
+                bool blobReprocessed = prog._completedEvent.WaitOne(2 * timeToProcess);
 
                 Assert.False(blobReprocessed);
             }
 
-            Assert.Equal(1, _timesProcessed);
+            Assert.Equal(1, prog._timesProcessed);
         }
 
         [Fact]
@@ -220,11 +243,14 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
             CloudBlockBlob blob = container.GetBlockBlobReference(BlobChainTriggerBlobName);
             blob.UploadText("0");
 
-            using (_completedEvent = new ManualResetEvent(initialState: false))
-            using (JobHost host = new JobHost(_hostConfiguration))
+            var prog = new BlobChainTest_Program();
+            var config = NewConfig(prog);
+
+            using (prog._completedEvent = new ManualResetEvent(initialState: false))
+            using (JobHost host = new JobHost(config))
             {
                 host.Start();
-                Assert.True(_completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
+                Assert.True(prog._completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
             }
         }
 
@@ -235,17 +261,22 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
                 .GetBlockBlobReference(TestBlobName)
                 .UploadText("10");
 
-            using (_completedEvent = new ManualResetEvent(initialState: false))
-            using (JobHost host1 = new JobHost(_hostConfiguration))
-            using (JobHost host2 = new JobHost(_hostConfiguration))
+
+            var prog = new BlobGetsProcessedOnlyOnce_SingleHost_Program();
+            var config = NewConfig(prog);
+
+
+            using (prog._completedEvent = new ManualResetEvent(initialState: false))
+            using (JobHost host1 = new JobHost(config))
+            using (JobHost host2 = new JobHost(config))
             {
                 host1.Start();
                 host2.Start();
 
-                Assert.True(_completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
+                Assert.True(prog._completedEvent.WaitOne(TimeSpan.FromSeconds(60)));
             }
 
-            Assert.Equal(1, _timesProcessed);
+            Assert.Equal(1, prog._timesProcessed);
         }
 
         public void Dispose()
