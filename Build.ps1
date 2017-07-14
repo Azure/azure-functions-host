a3df62662fec12526b3502b35419affc249acb83
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-07-14 14:33:45 -0700

    Build cleanup

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,1 @@
-﻿if (Test-Path "env:ProgramFiles(x86)")
-{
-    $ProgramFiles = "${env:ProgramFiles(x86)}"
-}
-else
-{
-    $ProgramFiles = "$env:ProgramFiles"
-}
-
-& "$ProgramFiles\MSBuild\12.0\Bin\MSBuild.exe" WebJobs.proj $Args
+﻿dotnet build -v q
