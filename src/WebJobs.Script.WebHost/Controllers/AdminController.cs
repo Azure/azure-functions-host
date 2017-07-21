IlyaFinkelshteyn/azure-webjobs-sdk-script

Make Billing events include time in binding. Resolve   https://github.com/Azure/azure-webjobs-sdk-script/issues/578
Move billing notifications out of Invoker and instead use SDK's instrumentation endpoints.
This requires an SDK update.
This significantly simplifies the Invoker path.

src/WebJobs.Script.WebHost/Controllers/AdminController.cs
@@ -51,7 +51,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
                 return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
 
-            FunctionDescriptor function = _scriptHostManager.Instance.Functions.FirstOrDefault(p => p.Name.ToLowerInvariant() == name.ToLowerInvariant());
+            FunctionDescriptor function = _scriptHostManager.Instance.LookupFunction(name);
             if (function == null)
             {
                 return new HttpResponseMessage(HttpStatusCode.NotFound);

src/WebJobs.Script/Description/IFunctionInvoker.cs
@@ -9,6 +9,11 @@ namespace Microsoft.Azure.WebJobs.Script.Description
     public interface IFunctionInvoker
     {
         /// <summary>
+        /// Gets logging information for this function.
+        /// </summary>
+        FunctionLogInfo LogInfo { get; }
+
+        /// <summary>
         /// Invoke the function using the specified parameters.
         /// </summary>
         /// <param name="parameters">The parameters.</param>

test/WebJobs.Script.Tests.Shared/TestInvoker.cs
@@ -27,6 +27,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             }
         }
 
+        public FunctionLogInfo LogInfo => throw new NotImplementedException();
+
         public Task Invoke(object[] parameters)
         {
             Interlocked.Increment(ref _invokeCount);

test/WebJobs.Script.Tests/Description/FunctionInvokerBaseTests.cs
@@ -2,13 +2,17 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Loggers;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.Azure.WebJobs.Script.Eventing;
+using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Moq;
 using Xunit;
 
@@ -26,16 +30,23 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             _traceWriter = new TestTraceWriter(TraceLevel.Verbose);
             var config = new ScriptHostConfiguration();
             config.HostConfig.AddService<IMetricsLogger>(_metricsLogger);
+            var funcDescriptor = new FunctionDescriptor();
+            var funcDescriptors = new Collection<FunctionDescriptor>();
+            funcDescriptors.Add(funcDescriptor);
             var eventManager = new Mock<IScriptEventManager>();
             var hostMock = new Mock<ScriptHost>(MockBehavior.Strict, new object[] { new NullScriptHostEnvironment(), eventManager.Object, config, null });
             hostMock.SetupGet(h => h.FunctionTraceWriterFactory).Returns(new FunctionTraceWriterFactory(config));
+            hostMock.SetupGet(h => h.Functions).Returns(funcDescriptors);
             hostMock.Object.TraceWriter = _traceWriter;
 
             var metadata = new FunctionMetadata
             {
                 Name = "TestFunction"
             };
-            _invoker = new MockInvoker(hostMock.Object, metadata);
+            _invoker = new MockInvoker(hostMock.Object, _metricsLogger, metadata);
+            funcDescriptor.Metadata = metadata;
+            funcDescriptor.Invoker = _invoker;
+            funcDescriptor.Name = metadata.Name;
         }
 
         [Fact]
@@ -166,20 +177,45 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         private class MockInvoker : FunctionInvokerBase
         {
-            public MockInvoker(ScriptHost host, FunctionMetadata metadata) : base(host, metadata)
+            private readonly FastLogger _fastLogger;
+
+            public MockInvoker(ScriptHost host, IMetricsLogger metrics, FunctionMetadata metadata) : base(host, metadata)
             {
+                _fastLogger = new FastLogger(
+                    (name) => this.Host.LookupFunction(name),
+                    metrics);
             }
 
             protected override async Task InvokeCore(object[] parameters, FunctionInvocationContext context)
             {
+                FunctionInstanceLogEntry item = new FunctionInstanceLogEntry
+                {
+                     FunctionInstanceId = context.ExecutionContext.InvocationId,
+                     StartTime = DateTime.UtcNow,
+                     FunctionName = this.Metadata.Name,
+                     Properties = new Dictionary<string, object>()
+                };
+                await _fastLogger.AddAsync(item);
+
                 InvocationData invocation = parameters.OfType<InvocationData>().FirstOrDefault() ?? new InvocationData();
 
-                if (invocation.Throw)
+                string error = "failed";
+                try
                 {
-                    throw new InvalidOperationException("Kaboom!");
-                }
+                    if (invocation.Throw)
+                    {
+                        throw new InvalidOperationException("Kaboom!");
+                    }
 
-                await Task.Delay(invocation.Delay);
+                    await Task.Delay(invocation.Delay);
+                    error = null; // success
+                }
+                finally
+                {
+                    item.EndTime = DateTime.UtcNow;
+                    item.ErrorDetails = error;
+                    await _fastLogger.AddAsync(item);
+                }
             }
         }
 

Azure/azure-functions-host

src/WebJobs.Script.WebHost/Controllers/AdminController.cs
@@ -51,7 +51,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
                 return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
 
-            FunctionDescriptor function = _scriptHostManager.Instance.Functions.FirstOrDefault(p => p.Name.ToLowerInvariant() == name.ToLowerInvariant());
+            FunctionDescriptor function = _scriptHostManager.Instance.GetFunctionOrNull(name);
             if (function == null)
             {
                 return new HttpResponseMessage(HttpStatusCode.NotFound);

src/WebJobs.Script/Description/IFunctionInvoker.cs
@@ -9,6 +9,11 @@ namespace Microsoft.Azure.WebJobs.Script.Description
     public interface IFunctionInvoker
     {
         /// <summary>
+        /// Gets logging information for this function.
+        /// </summary>
+        FunctionLogger LogInfo { get; }
+
+        /// <summary>
         /// Invoke the function using the specified parameters.
         /// </summary>
         /// <param name="parameters">The parameters.</param>

test/WebJobs.Script.Tests.Shared/TestInvoker.cs
@@ -27,6 +27,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             }
         }
 
+        public FunctionLogger LogInfo => throw new NotImplementedException();
+
         public Task Invoke(object[] parameters)
         {
             Interlocked.Increment(ref _invokeCount);

test/WebJobs.Script.Tests/Description/FunctionInvokerBaseTests.cs
@@ -2,13 +2,17 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Loggers;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.Azure.WebJobs.Script.Eventing;
+using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Moq;
 using Xunit;
 
@@ -26,16 +30,23 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             _traceWriter = new TestTraceWriter(TraceLevel.Verbose);
             var config = new ScriptHostConfiguration();
             config.HostConfig.AddService<IMetricsLogger>(_metricsLogger);
+            var funcDescriptor = new FunctionDescriptor();
+            var funcDescriptors = new Collection<FunctionDescriptor>();
+            funcDescriptors.Add(funcDescriptor);
             var eventManager = new Mock<IScriptEventManager>();
             var hostMock = new Mock<ScriptHost>(MockBehavior.Strict, new object[] { new NullScriptHostEnvironment(), eventManager.Object, config, null });
             hostMock.SetupGet(h => h.FunctionTraceWriterFactory).Returns(new FunctionTraceWriterFactory(config));
+            hostMock.SetupGet(h => h.Functions).Returns(funcDescriptors);
             hostMock.Object.TraceWriter = _traceWriter;
 
             var metadata = new FunctionMetadata
             {
                 Name = "TestFunction"
             };
-            _invoker = new MockInvoker(hostMock.Object, metadata);
+            _invoker = new MockInvoker(hostMock.Object, _metricsLogger, metadata);
+            funcDescriptor.Metadata = metadata;
+            funcDescriptor.Invoker = _invoker;
+            funcDescriptor.Name = metadata.Name;
         }
 
         [Fact]
@@ -166,20 +177,45 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         private class MockInvoker : FunctionInvokerBase
         {
-            public MockInvoker(ScriptHost host, FunctionMetadata metadata) : base(host, metadata)
+            private readonly FunctionInstanceLogger _fastLogger;
+
+            public MockInvoker(ScriptHost host, IMetricsLogger metrics, FunctionMetadata metadata) : base(host, metadata)
             {
+                _fastLogger = new FunctionInstanceLogger(
+                    (name) => this.Host.GetFunctionOrNull(name),
+                    metrics);
             }
 
             protected override async Task InvokeCore(object[] parameters, FunctionInvocationContext context)
             {
+                FunctionInstanceLogEntry item = new FunctionInstanceLogEntry
+                {
+                     FunctionInstanceId = context.ExecutionContext.InvocationId,
+                     StartTime = DateTime.UtcNow,
+                     FunctionName = this.Metadata.Name,
+                     Properties = new Dictionary<string, object>()
+                };
+                await _fastLogger.AddAsync(item);
+
                 InvocationData invocation = parameters.OfType<InvocationData>().FirstOrDefault() ?? new InvocationData();
 
-                if (invocation.Throw)
+                string error = "failed";
+                try
                 {
-                    throw new InvalidOperationException("Kaboom!");
-                }
+                    if (invocation.Throw)
+                    {
+                        throw new InvalidOperationException("Kaboom!");
+                    }
 
-                await Task.Delay(invocation.Delay);
+                    await Task.Delay(invocation.Delay);
+                    error = null; // success
+                }
+                finally
+                {
+                    item.EndTime = DateTime.UtcNow;
+                    item.ErrorDetails = error;
+                    await _fastLogger.AddAsync(item);
+                }
             }
         }
 