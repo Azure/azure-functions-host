b1e8049751df47a87f49994d6c3b859ac0ac2c5f
Brett Samblanet <brettsam@microsoft.com>
2017-07-24 12:20:55 -0700

    fixing race condition in test

--- test/WebJobs.Script.Tests.Integration/ListenerFailureTests.cs
+++ test/WebJobs.Script.Tests.Integration/ListenerFailureTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -21,21 +22,21 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             await TestHelpers.Await(() =>
             {
                 logs = TestHelpers.GetFunctionLogsAsync("ListenerStartupException", false).Result;
-                return logs.Count > 0;
+
+                string logToFind = "The listener for function 'Functions.ListenerStartupException' was unable to start.";
+                return logs.Any(l => l.Contains(logToFind));
             });
 
-            // assert that listener error was captured
-            Assert.Contains(logs, (log) => log.Contains("The listener for function 'Functions.ListenerStartupException' was unable to start."));
-
             TestHelpers.ClearFunctionLogs("TimerTrigger");
+
+            // assert that timer function is still running
             await TestHelpers.Await(() =>
             {
                 logs = TestHelpers.GetFunctionLogsAsync("TimerTrigger", false).Result;
-                return logs.Count > 0;
-            });
 
-            // assert that timer function is still running
-            Assert.Contains(logs, (log) => log.Contains("Timer function ran!"));
+                string logToFind = "Timer function ran!";
+                return logs.Any(l => l.Contains(logToFind));
+            });
 
             // assert that Stop does not throw error
             Fixture.Host.Stop();
