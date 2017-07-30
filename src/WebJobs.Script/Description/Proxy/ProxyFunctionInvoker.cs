2413929eed2a388b05288fd2a687694984339296
Omkar More <omkarm@microsoft.com>
2017-07-30 01:27:36 -0700

    Updated to use proxyclient shim

--- src/WebJobs.Script/Description/Proxy/ProxyFunctionInvoker.cs
+++ src/WebJobs.Script/Description/Proxy/ProxyFunctionInvoker.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.AppService.Proxy.Client;
+using Microsoft.Azure.AppService.Proxy.Client.Contract;
 
 namespace Microsoft.Azure.WebJobs.Script.Description
 {
