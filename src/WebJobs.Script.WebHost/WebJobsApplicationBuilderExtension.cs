6c79d9c09f6e1d4f2a78350bc59afde73f22e5f3
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-08-17 15:25:21 -0700

    Updating stylecop reference to private assets. Refactoring service registrations

--- src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
+++ src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Script.WebHost;
+using Microsoft.Azure.WebJobs.Script.WebHost.Middleware;
 
 namespace WebJobs.Script.WebHost.Core
 {
@@ -20,7 +21,21 @@ namespace WebJobs.Script.WebHost.Core
         {
             WebScriptHostManager hostManager = builder.ApplicationServices.GetService(typeof(WebScriptHostManager)) as WebScriptHostManager;
 
-            return builder.UseHttpBindingRouting(applicationLifetime, routes);
+            builder.UseHttpBindingRouting(applicationLifetime, routes);
+
+            builder.UseWhen(context => !context.Request.Path.StartsWithSegments("/admin/host/status"), config =>
+            {
+                config.UseMiddleware<ScriptHostCheckMiddleware>();
+            });
+
+            builder.UseMvc(r =>
+            {
+                r.MapRoute(name: "Home",
+                    template: string.Empty,
+                    defaults: new { controller = "Home", action = "Get" });
+            });
+
+            return builder;
         }
     }
 }
