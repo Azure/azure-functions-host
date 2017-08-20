18836b960b30b8ea487afe39465e40737719d45c
Ville Rantala <ville.rantala@microsoft.com>
2017-08-20 09:54:58 +0300

    Allow launching the nodejs worker with inspector
    To enable passing the `--inspector` flag to the nodejs worker, a support
    was added to pass options to the worker executable.
    
    After this commit, the nodejs inspector debugger is enabled when the
    `AzureWebJobsEnv` is set to value `Development`.
    
    This feature is useful when debugging the entire stack and allows stepping
    through the code across programming language boundaries.

--- src/WebJobs.Script/Dispatch/NodeLanguageWorkerConfig.cs
+++ src/WebJobs.Script/Dispatch/NodeLanguageWorkerConfig.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Description;
 
 namespace Microsoft.Azure.WebJobs.Script.Dispatch
@@ -11,6 +12,10 @@ namespace Microsoft.Azure.WebJobs.Script.Dispatch
         public NodeLanguageWorkerConfig()
         {
             ExecutablePath = "node";
+            string value = ScriptSettingsManager.Instance.GetSetting(EnvironmentSettingNames.AzureWebJobsEnvironment);
+            if (string.Compare("Development", value, StringComparison.OrdinalIgnoreCase) == 0) {
+                Options = "--inspect";
+            }
             WorkerPath = Environment.GetEnvironmentVariable("NodeJSWorkerPath");
             ScriptType = ScriptType.Javascript;
             Extension = ".js";
