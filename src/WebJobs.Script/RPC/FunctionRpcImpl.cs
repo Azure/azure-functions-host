509e9bca6dd04d77f3490316b170b3ac67648df5
Matt Mason <mamaso@microsoft.com>
2017-08-31 12:40:30 -0700

    Handle cancellation in event stream

--- src/WebJobs.Script/RPC/FunctionRpcImpl.cs
+++ src/WebJobs.Script/RPC/FunctionRpcImpl.cs
@@ -24,25 +24,52 @@ namespace Microsoft.Azure.WebJobs.Script
 
         public override async Task EventStream(IAsyncStreamReader<StreamingMessage> requestStream, IServerStreamWriter<StreamingMessage> responseStream, ServerCallContext context)
         {
-            if (await requestStream.MoveNext(CancellationToken.None))
+            var cancelSource = new TaskCompletionSource<bool>();
+            IDisposable outboundEventSubscription = null;
+            try
             {
-                string workerId = requestStream.Current.StartStream.WorkerId;
-                _eventManager.OfType<OutboundEvent>()
-                    .Where(evt => evt.WorkerId == workerId)
-                    .ObserveOn(NewThreadScheduler.Default)
-                    .Subscribe(evt =>
-                    {
-                        // WriteAsync only allows one pending write at a time
-                        // For each responseStream subscription, observe as a blocking write, in series, on a new thread
-                        // Alternatives - could wrap responseStream.WriteAsync with a SemaphoreSlim to control concurrent access
-                        responseStream.WriteAsync(evt.Message).GetAwaiter().GetResult();
-                    });
+                context.CancellationToken.Register(() => cancelSource.SetResult(false));
 
-                do
+                Func<Task<bool>> messageAvailable = async () =>
                 {
-                    _eventManager.Publish(new InboundEvent(workerId, requestStream.Current));
+                    // GRPC does not accept cancellation tokens for individual reads, hence wrapper
+                    var requestTask = requestStream.MoveNext(CancellationToken.None);
+                    var completed = await Task.WhenAny(cancelSource.Task, requestTask);
+                    return completed.Result;
+                };
+
+                if (await messageAvailable())
+                {
+                    string workerId = requestStream.Current.StartStream.WorkerId;
+                    outboundEventSubscription = _eventManager.OfType<OutboundEvent>()
+                        .Where(evt => evt.WorkerId == workerId)
+                        .ObserveOn(NewThreadScheduler.Default)
+                        .Subscribe(evt =>
+                        {
+                            // WriteAsync only allows one pending write at a time
+                            // For each responseStream subscription, observe as a blocking write, in series, on a new thread
+                            // Alternatives - could wrap responseStream.WriteAsync with a SemaphoreSlim to control concurrent access
+                            responseStream.WriteAsync(evt.Message).GetAwaiter().GetResult();
+                        });
+
+                    do
+                    {
+                        _eventManager.Publish(new InboundEvent(workerId, requestStream.Current));
+                    }
+                    while (await messageAvailable());
                 }
-                while (await requestStream.MoveNext(CancellationToken.None));
+            }
+            catch (Exception exc)
+            {
+                // TODO: do this properly so it can be associated with workerid, if possible
+                _eventManager.Publish(new WorkerErrorEvent(this, exc));
+            }
+            finally
+            {
+                outboundEventSubscription?.Dispose();
+
+                // ensure cancellationSource task completes
+                cancelSource.TrySetResult(false);
             }
         }
     }
