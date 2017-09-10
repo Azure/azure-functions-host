f308acc1b8dc72018475fafa9075395b2e1186cc
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-09-10 14:41:08 -0700

    Re-enabling web host shutdown support

--- src/WebJobs.Script.WebHost/Program.cs
+++ src/WebJobs.Script.WebHost/Program.cs
@@ -2,20 +2,30 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Threading;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebJobs.Script.WebHost.Core
 {
     public class Program
     {
+        private static CancellationTokenSource _applicationCts = new CancellationTokenSource();
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            BuildWebHost(args)
+                .RunAsync(_applicationCts.Token)
+                .Wait();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             Microsoft.AspNetCore.WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        internal static void InitiateShutdown()
+        {
+            _applicationCts.Cancel();
+        }
     }
 }
