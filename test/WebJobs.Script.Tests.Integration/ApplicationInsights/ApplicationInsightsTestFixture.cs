e238bf17b41d472295423f2d71447cb06a377bbb
Mathew Charles <mathewc@microsoft.com>
2017-09-12 17:10:22 -0700

    Adding proper disposal to test fixture

--- test/WebJobs.Script.Tests.Integration/ApplicationInsights/ApplicationInsightsTestFixture.cs
+++ test/WebJobs.Script.Tests.Integration/ApplicationInsights/ApplicationInsightsTestFixture.cs
@@ -6,18 +6,42 @@ using Microsoft.WebJobs.Script.Tests;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.ApplicationInsights
 {
-    public abstract class ApplicationInsightsTestFixture : EndToEndTestFixture
+    public abstract class ApplicationInsightsTestFixture : IDisposable
     {
-        static ApplicationInsightsTestFixture()
-        {
-            // We need to set this to something in order to trigger App Insights integration. But since
-            // we're hitting a local HttpListener, it can be anything.
-            ScriptSettingsManager.Instance.ApplicationInsightsInstrumentationKey = TestChannelLoggerFactoryBuilder.ApplicationInsightsKey;
-        }
+        private readonly ScriptSettingsManager _settingsManager;
+        private readonly HttpConfiguration _config = new HttpConfiguration();
+        private readonly HttpServer _httpServer;
 
         public ApplicationInsightsTestFixture(string scriptRoot, string testId)
             : base(scriptRoot, testId)
         {
+            _settingsManager = ScriptSettingsManager.Instance;
+
+            HostSettings = new WebHostSettings
+            {
+                IsSelfHost = true,
+                ScriptPath = Path.Combine(Environment.CurrentDirectory, scriptRoot),
+                LogPath = Path.Combine(Path.GetTempPath(), @"Functions"),
+                SecretsPath = Environment.CurrentDirectory, // not used
+                LoggerFactoryBuilder = new TestLoggerFactoryBuilder(Channel),
+                IsAuthDisabled = true
+            };
+            WebApiConfig.Register(_config, _settingsManager, HostSettings);
+
+            var resolver = _config.DependencyResolver;
+            var hostConfig = resolver.GetService<WebHostResolver>().GetScriptHostConfiguration(HostSettings);
+
+            _settingsManager.ApplicationInsightsInstrumentationKey = TestChannelLoggerFactoryBuilder.ApplicationInsightsKey;
+
+            InitializeConfig(hostConfig);
+
+            _httpServer = new HttpServer(_config);
+            HttpClient = new HttpClient(_httpServer)
+            {
+                BaseAddress = new Uri("https://localhost/")
+            };
+
+            TestHelpers.WaitForWebHost(HttpClient);
         }
 
         public TestTelemetryChannel Channel { get; private set; } = new TestTelemetryChannel();
@@ -35,6 +59,69 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApplicationInsights
                 // Overwrite the generated function whitelist to only include one function.
                 c.Functions = new[] { "Scenarios" };
             };
+        }
+
+        public void Dispose()
+        {
+            _httpServer?.Dispose();
+            HttpClient?.Dispose();
+        }
+
+        private class TestLoggerFactoryBuilder : DefaultLoggerFactoryBuilder
+        {
+            private readonly TestTelemetryChannel _channel;
+
+            public TestLoggerFactoryBuilder(TestTelemetryChannel channel)
+            {
+                _channel = channel;
+            }
+
+            public override void AddLoggerProviders(ILoggerFactory factory, ScriptHostConfiguration scriptConfig, ScriptSettingsManager settingsManager)
+            {
+                // Replace TelemetryClient
+                var clientFactory = new TestTelemetryClientFactory(scriptConfig.LogFilter.Filter, _channel);
+                scriptConfig.HostConfig.AddService<ITelemetryClientFactory>(clientFactory);
+
+                base.AddLoggerProviders(factory, scriptConfig, settingsManager);
+            }
+        }
+
+        public class TestTelemetryChannel : ITelemetryChannel
+        {
+            public IList<ITelemetry> Telemetries { get; private set; } = new List<ITelemetry>();
+
+            public bool? DeveloperMode { get; set; }
+
+            public string EndpointAddress { get; set; }
+
+            public void Dispose()
+            {
+            }
+
+            public void Flush()
+            {
+            }
+
+            public void Send(ITelemetry item)
+            {
+                Telemetries.Add(item);
+            }
+        }
+
+        private class TestTelemetryClientFactory : ScriptTelemetryClientFactory
+        {
+            private TestTelemetryChannel _channel;
+
+            public TestTelemetryClientFactory(Func<string, LogLevel, bool> filter, TestTelemetryChannel channel)
+                : base(TestChannelLoggerFactoryBuilder.ApplicationInsightsKey, new SamplingPercentageEstimatorSettings(), filter)
+            {
+                _channel = channel;
+            }
+
+            protected override ITelemetryChannel CreateTelemetryChannel()
+            {
+                return _channel;
+            }
         }
     }
 }
