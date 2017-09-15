54393c28ce7837287cca9b35dd7f75d2a96114e3
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-09-14 19:34:51 -0700

    Build packaging and enhancements

--- src/WebJobs.Script.WebHost/Models/HostStatus.cs
+++ src/WebJobs.Script.WebHost/Models/HostStatus.cs
@@ -27,6 +27,12 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Models
         public string Version { get; set; }
 
         /// <summary>
+        /// Gets or sets the host version.
+        /// </summary>
+        [JsonProperty(PropertyName = "versionDetails", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string VersionDetails { get; set; }
+
+        /// <summary>
         /// Gets or sets the collection of errors for the host.
         /// </summary>
         [JsonProperty(PropertyName = "errors", DefaultValueHandling = DefaultValueHandling.Ignore)]
