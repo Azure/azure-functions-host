c435d8d9a645eb507783ad92c73361934d2059e8
Matt Mason <mamaso@microsoft.com>
2017-09-20 16:34:59 -0700

    Http fix for node/java return

--- test/WebJobs.Script.Tests/ScriptHostTests.cs
+++ test/WebJobs.Script.Tests/ScriptHostTests.cs
@@ -62,7 +62,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             var traceWriter = new TestTraceWriter(TraceLevel.Verbose);
             var functionErrors = new Dictionary<string, Collection<string>>();
             var metadata = ScriptHost.ReadFunctionMetadata(config, traceWriter, null, functionErrors);
-            Assert.Equal(51, metadata.Count);
+            Assert.Equal(53, metadata.Count);
         }
 
         [Fact]
