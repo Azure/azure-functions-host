5400027f3b21635b5718580ed460a57f596ce83f
Matt Mason <mamaso@microsoft.com>
2017-09-27 16:13:40 -0700

    Refactor WorkerConfig to IWorkerProvider

---
+++ src/WebJobs.Script.Grpc/Abstractions/IWorkerProvider.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Script.Abstractions.Rpc
+{
+    /// <summary>
+    /// Enables a language worker implementor to specify how to create and configure the language worker process.
+    /// </summary>
+    public interface IWorkerProvider
+    {
+        /// <summary>
+        /// Get the static description of the worker.
+        /// </summary>
+        /// <returns>The static description of the worker.</returns>
+        WorkerDescription GetDescription();
+
+        /// <summary>
+        /// Tries to configure the arguments with any configuration / environment specific settings.
+        /// </summary>
+        /// <param name="args">The default arguments constructed by the host.</param>
+        /// <param name="config">The host-level IConfiguration.</param>
+        /// <param name="logger">The startup ILogger.</param>
+        /// <returns>A bool that indicates if the args were configured successfully.</returns>
+        bool TryConfigureArguments(ArgumentsDescription args, IConfiguration config, ILogger logger);
+    }
+}
---
+++ src/WebJobs.Script/Dispatch/WorkerConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs.Script.Abstractions.Rpc;
+
+namespace Microsoft.Azure.WebJobs.Script.Dispatch
+{
+    internal class WorkerConfig
+    {
+        public WorkerDescription Description { get; set; }
+
+        public ArgumentsDescription Arguments { get; set; }
+
+        public string Extension => Description.Extension;
+
+        public string Language => Description.Language;
+    }
+}
