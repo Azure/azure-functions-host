0086a256c3cebadcb3de22c50e6893e705ca4b55
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-11-17 11:31:13 -0800

    Fixes to test run script

--- runappveyortests.ps1
+++ runappveyortests.ps1
@@ -12,48 +12,50 @@ function CheckExitCode([string] $step,[int] $currentCode)
 }
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.dll" -appveyor
-$exitCode = CheckExitCode("Unit tests", $exitCode)
+$exitCode = CheckExitCode "Unit tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Scaling.Tests\bin\Release\Microsoft.Azure.WebJobs.Script.Scaling.Tests.dll" -appveyor
-$exitCode = CheckExitCode("Scaling tests", $exitCode)
+$exitCode = CheckExitCode "Scaling tests" $exitCode
 
 .\runNodeTests.cmd
-$exitCode = CheckExitCode("Node tests", $exitCode)
+$exitCode = CheckExitCode "Node tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -notrait "Category=E2E"
-$exitCode = CheckExitCode("Non-E2E tests", $exitCode)
+$exitCode = CheckExitCode "Non-E2E tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=CSharpEndToEndTests"
-$exitCode = CheckExitCode("CSharpEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "CSharpEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=NodeEndToEndTests"
-$exitCode = CheckExitCode("NodeEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "NodeEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=BashEndToEndTests"
-$exitCode = CheckExitCode("BashEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "BashEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=DirectLoadEndToEndTests"
-$exitCode = CheckExitCode("DirectLoadEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "DirectLoadEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=FSharpEndToEndTests"
-$exitCode = CheckExitCode("FSharpEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "FSharpEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=PhpEndToEndTests"
-$exitCode = CheckExitCode("PhpEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "PhpEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=PowerShellEndToEndTests"
-$exitCode = CheckExitCode("PowerShellEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "PowerShellEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=PythonEndToEndTests"
-$exitCode = CheckExitCode("PythonEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "PythonEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=RawAssemblyEndToEndTests"
-$exitCode = CheckExitCode("RawAssemblyEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "RawAssemblyEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=SamplesEndToEndTests"
-$exitCode = CheckExitCode("SamplesEndToEndTests tests", $exitCode)
+$exitCode = CheckExitCode "SamplesEndToEndTests tests" $exitCode
 
 & $env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.exe "$env:APPVEYOR_BUILD_FOLDER\test\WebJobs.Script.Tests.Integration\bin\Release\Microsoft.Azure.WebJobs.Script.Tests.Integration.dll" -trait "E2E=NodeEndToEndTests"
-$exitCode = CheckExitCode("NodeEndToEndTests tests (x64)", $exitCode)
+$exitCode = CheckExitCode "NodeEndToEndTests tests (x64)" $exitCode
+
+Write-Host "Completed test with with exit code $exitCode"
 
 exit $exitCode
