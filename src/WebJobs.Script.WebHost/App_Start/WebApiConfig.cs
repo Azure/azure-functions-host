a0e7f9b026cd6e7c8e2d428bc01e816f0c4345d2
Mathew Charles <mathewc@microsoft.com>
2017-12-13 16:49:33 -0800

    Add a background specialization monitor (#2201)

--- src/WebJobs.Script.WebHost/App_Start/WebApiConfig.cs
+++ src/WebJobs.Script.WebHost/App_Start/WebApiConfig.cs
@@ -2,15 +2,12 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.IO;
-using System.Web;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using Autofac;
 using Autofac.Integration.WebApi;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.WebHost.Controllers;
-using Microsoft.Azure.WebJobs.Script.WebHost.Filters;
 using Microsoft.Azure.WebJobs.Script.WebHost.Handlers;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost
