1e2c0094d59fc4760ab2c0ad2839374a264a5cf5
Christopher Anderson <chrande@microsoft.com>
2018-01-12 15:32:14 -0800

    Clean up bindings tests

--- test/WebJobs.Script.Tests/Binding/ExtensionBindingTests.cs
+++ test/WebJobs.Script.Tests/Binding/ExtensionBindingTests.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-#if ALLEXTENSIONS
+
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Azure.WebJobs.Description;
 using Microsoft.Azure.WebJobs.Script.Binding;
 using Xunit;
 
@@ -10,63 +13,58 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
     public class ExtensionBindingTests
     {
         [Fact]
-        public void GetAttributeBuilderInfo_ServiceBusTriggerAttribute_Topic()
+        public void GetAttributeBuilderInfo_ReturnsExpectedAttribute()
         {
-            ServiceBusTriggerAttribute attribute = new ServiceBusTriggerAttribute("myTopic", "mySubscription", Microsoft.ServiceBus.Messaging.AccessRights.Listen);
-            var builderInfo = ExtensionBinding.GetAttributeBuilderInfo(attribute);
-
-            ServiceBusTriggerAttribute result = (ServiceBusTriggerAttribute)builderInfo.Constructor.Invoke(builderInfo.ConstructorArgs);
-
-            Assert.Equal(attribute.TopicName, result.TopicName);
-            Assert.Equal(attribute.SubscriptionName, result.SubscriptionName);
-            Assert.Equal(attribute.Access, result.Access);
-            Assert.Null(result.QueueName);
-
-            Assert.Equal(0, builderInfo.Properties.Count);
-        }
-
-        [Fact]
-        public void GetAttributeBuilderInfo_ServiceBusTriggerAttribute_Queue()
-        {
-            ServiceBusTriggerAttribute attribute = new ServiceBusTriggerAttribute("myQueue", Microsoft.ServiceBus.Messaging.AccessRights.Listen);
-            var builderInfo = ExtensionBinding.GetAttributeBuilderInfo(attribute);
-
-            ServiceBusTriggerAttribute result = (ServiceBusTriggerAttribute)builderInfo.Constructor.Invoke(builderInfo.ConstructorArgs);
-
-            Assert.Equal(attribute.QueueName, result.QueueName);
-            Assert.Equal(attribute.Access, result.Access);
-            Assert.Null(result.TopicName);
-            Assert.Null(result.SubscriptionName);
-            Assert.Equal(0, builderInfo.Properties.Count);
-        }
-
-        [Fact]
-        public void GetAttributeBuilderInfo_DocumentDBAttribute()
-        {
-            DocumentDBAttribute attribute = new DocumentDBAttribute("myDb", "myCollection")
+            var attribute = new TestAttribute("constructorSetAttributeValue")
             {
-                CreateIfNotExists = true,
-                ConnectionStringSetting = "myConnection",
-                PartitionKey = "myPartition",
-                Id = "myId",
-                CollectionThroughput = 123
+                BoolParameter = true,
+                StringParameter = "stringParameterValue",
+                IntParameter = 42,
+                DoesNotAutoResolveParameter = "doesNotAutoResolveParameterValue"
             };
             var builderInfo = ExtensionBinding.GetAttributeBuilderInfo(attribute);
 
-            DocumentDBAttribute result = (DocumentDBAttribute)builderInfo.Constructor.Invoke(builderInfo.ConstructorArgs);
+            TestAttribute result = (TestAttribute)builderInfo.Constructor.Invoke(builderInfo.ConstructorArgs);
 
-            Assert.Equal(attribute.DatabaseName, result.DatabaseName);
-            Assert.Equal(attribute.CollectionName, result.CollectionName);
+            Assert.Equal(attribute.ConstructorSetParameter, result.ConstructorSetParameter);
 
+            // 6 properties on the object, but AppSetting will be null, so 5 will be expected.
             Assert.Equal(5, builderInfo.Properties.Count);
 
             var properties = builderInfo.Properties.ToDictionary(p => p.Key.Name, p => p.Value);
-            Assert.True((bool)properties["CreateIfNotExists"]);
-            Assert.Equal("myConnection", (string)properties["ConnectionStringSetting"]);
-            Assert.Equal("myPartition", (string)properties["PartitionKey"]);
-            Assert.Equal("myId", (string)properties["Id"]);
-            Assert.Equal(123, (int)properties["CollectionThroughput"]);
+            Assert.Throws(typeof(KeyNotFoundException), () => properties["AppSettingSetParameter"]);
+            Assert.Equal(attribute.BoolParameter, (bool)properties["BoolParameter"]);
+            Assert.Equal(attribute.ConstructorSetParameter, (string)properties["ConstructorSetParameter"]);
+            Assert.Equal(attribute.DoesNotAutoResolveParameter, (string)properties["DoesNotAutoResolveParameter"]);
+            Assert.Equal(attribute.IntParameter, (int)properties["IntParameter"]);
+            Assert.Equal(attribute.StringParameter, (string)properties["StringParameter"]);
+        }
+
+        [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+        [Binding]
+        public sealed class TestAttribute : Attribute
+        {
+            public TestAttribute(string constructorSetParameter)
+            {
+                ConstructorSetParameter = constructorSetParameter;
+            }
+
+            [AutoResolve]
+            public int IntParameter { get; set; }
+
+            [AutoResolve]
+            public string StringParameter { get; set; }
+
+            [AutoResolve]
+            public string ConstructorSetParameter { get; set; }
+
+            [AutoResolve]
+            public bool BoolParameter { get; set; }
+
+            [AppSetting]
+            public string AppSettingSetParameter { get; set; }
+
+            public string DoesNotAutoResolveParameter { get; set; }
         }
     }
 }
-#endif
