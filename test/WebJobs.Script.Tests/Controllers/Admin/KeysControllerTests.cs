d3e25c854ffbb60b4f11ccc7e7e7e7d7979c1d0b
Alexey Rodionov <alrod@microsoft.com>
2018-01-22 12:20:27 -0800

    Enable KeysController tests. Fixes #2268

--- test/WebJobs.Script.Tests/Controllers/Admin/KeysControllerTests.cs
+++ test/WebJobs.Script.Tests/Controllers/Admin/KeysControllerTests.cs
@@ -1,19 +1,20 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-#if KEYMANAGEMENT
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Eventing;
 using Microsoft.Azure.WebJobs.Script.WebHost;
 using Microsoft.Azure.WebJobs.Script.WebHost.Controllers;
-using Microsoft.Azure.WebJobs.Script.WebHost.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Newtonsoft.Json.Linq;
 using WebJobs.Script.Tests;
@@ -41,19 +42,18 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             var config = new ScriptHostConfiguration();
             var environment = new NullScriptHostEnvironment();
             var eventManager = new Mock<IScriptEventManager>();
-            _hostMock = new Mock<ScriptHost>(MockBehavior.Strict, new object[] { environment, eventManager.Object, config, null, null });
+            var mockRouter = new Mock<IWebJobsRouter>();
+            _hostMock = new Mock<ScriptHost>(MockBehavior.Strict, new object[] { environment, eventManager.Object, config, null, null, null });
             _hostMock.Setup(p => p.Functions).Returns(_testFunctions);
             _hostMock.Setup(p => p.FunctionErrors).Returns(_testFunctionErrors);
 
             WebHostSettings settings = new WebHostSettings();
             settings.SecretsPath = _secretsDirectory.Path;
             _secretsManagerMock = new Mock<ISecretManager>(MockBehavior.Strict);
-            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new object[] { config, new TestSecretManagerFactory(_secretsManagerMock.Object), eventManager.Object, _settingsManager, settings });
+            _managerMock = new Mock<WebScriptHostManager>(MockBehavior.Strict, new object[] { config, new TestSecretManagerFactory(_secretsManagerMock.Object), eventManager.Object, _settingsManager, settings, mockRouter.Object, NullLoggerFactory.Instance});
             _managerMock.SetupGet(p => p.Instance).Returns(_hostMock.Object);
 
-            var traceWriter = new TestTraceWriter(TraceLevel.Verbose);
-
-            _testController = new KeysController(_managerMock.Object, _secretsManagerMock.Object, traceWriter, null);
+            _testController = new KeysController(_managerMock.Object, _secretsManagerMock.Object, new LoggerFactory());
 
             // setup some test functions
             string errorFunction = "ErrorFunction";
@@ -71,10 +71,11 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [Fact]
         public async Task GetKeys_FunctionInError_ReturnsKeys()
         {
-            _testController.Request = new HttpRequestMessage(HttpMethod.Get, "https://local/admin/functions/keys");
-            var result = (OkNegotiatedContentResult<ApiModel>)(await _testController.Get("ErrorFunction"));
+            SetHttpContext();
 
-            var content = (JObject)result.Content;
+            ObjectResult result = (ObjectResult)await _testController.Get("ErrorFunction");
+
+            var content = (JObject)result.Value;
             var keys = content["keys"];
             Assert.Equal("key1", keys[0]["name"]);
             Assert.Equal("secret1", keys[0]["value"]);
@@ -83,14 +84,14 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [Fact]
         public async Task PutKey_FunctionInError_Succeeds()
         {
-            _testController.Request = new HttpRequestMessage(HttpMethod.Get, "https://local/admin/functions/keys/key2");
+            SetHttpContext();
 
             var key = new Key("key2", "secret2");
             var keyOperationResult = new KeyOperationResult(key.Value, OperationResult.Updated);
             _secretsManagerMock.Setup(p => p.AddOrUpdateFunctionSecretAsync(key.Name, key.Value, "ErrorFunction", ScriptSecretsType.Function)).ReturnsAsync(keyOperationResult);
 
-            var result = (OkNegotiatedContentResult<ApiModel>)(await _testController.Put("ErrorFunction", key.Name, key));
-            var content = (JObject)result.Content;
+            ObjectResult result = (ObjectResult)await _testController.Put("ErrorFunction", key.Name, key);
+            var content = (JObject)result.Value;
             Assert.Equal("key2", content["name"]);
             Assert.Equal("secret2", content["value"]);
         }
@@ -98,12 +99,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [Fact]
         public async Task DeleteKey_FunctionInError_Succeeds()
         {
-            _testController.Request = new HttpRequestMessage(HttpMethod.Get, "https://local/admin/functions/keys/key2");
+            SetHttpContext();
 
             _secretsManagerMock.Setup(p => p.DeleteSecretAsync("key2", "ErrorFunction", ScriptSecretsType.Function)).ReturnsAsync(true);
 
             var result = (StatusCodeResult)(await _testController.Delete("ErrorFunction", "key2"));
-            Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
+            Assert.Equal(StatusCodes.Status204NoContent, result.StatusCode);
         }
 
         protected virtual void Dispose(bool disposing)
@@ -119,6 +120,15 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
         }
+
+        private void SetHttpContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Host = new HostString("local");
+            httpContext.Request.Path = "/admin/functions/keys/key2";
+            httpContext.Request.Method = "Get";
+            httpContext.Request.IsHttps = true;
+            _testController.ControllerContext.HttpContext = httpContext;
+        }
     }
 }
-#endif
