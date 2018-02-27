e63a65df4bb6876065fa6580d55a8b923cec8b0b
Mathew Charles <mathewc@microsoft.com>
2018-02-27 15:00:16 -0800

    Concurrency improvements for FileWriter (#2460)

--- test/WebJobs.Script.Tests/Diagnostics/FileWriterTests.cs
+++ test/WebJobs.Script.Tests/Diagnostics/FileWriterTests.cs
@@ -17,7 +17,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
         public FileWriterTests()
         {
-            _logFilePath = Path.Combine(Path.GetTempPath(), "WebJobs.Script.Tests", "FileTraceWriterTests");
+            _logFilePath = Path.Combine(Path.GetTempPath(), "WebJobs.Script.Tests", "FileWriterTests");
 
             if (Directory.Exists(_logFilePath))
             {
@@ -28,14 +28,13 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [Fact]
         public async Task MultipleConcurrentInstances_WritesAreSerialized()
         {
-            int numLines = 100;
-
             // first ensure the file exists by writing a single entry
             // this ensures all the instances below will be operating on
             // the same file
             WriteLogs(_logFilePath, 1);
 
             // start 3 concurrent instances
+            int numLines = 100;
             await Task.WhenAll(
                 Task.Run(() => WriteLogs(_logFilePath, numLines)),
                 Task.Run(() => WriteLogs(_logFilePath, numLines)),
@@ -47,62 +46,77 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         }
 
         [Fact]
-        public async Task SetNewLogFile_PurgesOldLogFiles()
+        public async Task SetLogFile_PurgesOldLogFiles()
         {
-            DirectoryInfo directory = new DirectoryInfo(_logFilePath);
+            var directory = new DirectoryInfo(_logFilePath);
             directory.Create();
 
             // below test expects the retention days to be set to 1
             Assert.Equal(1, FileWriter.LastModifiedCutoffDays);
 
             // create some log files
-            List<FileInfo> logFiles = new List<FileInfo>();
-            int initialCount = 5;
-            for (int i = 0; i < initialCount; i++)
+            var logFiles = new List<FileInfo>();
+            for (int i = 0; i < 5; i++)
             {
                 string fileName = string.Format("{0}-{1}.log", i, FileWriter.GetInstanceId());
                 string path = Path.Combine(_logFilePath, fileName);
-                Thread.Sleep(50);
                 File.WriteAllText(path, "Test Logs");
                 logFiles.Add(new FileInfo(path));
             }
 
-            // mark some of the files as old - we expect
-            // all of these to be purged
-            File.SetLastWriteTime(logFiles[2].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(1)));
-            File.SetLastWriteTime(logFiles[1].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(1)));
-            File.SetLastWriteTime(logFiles[0].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(2)));
+            // push all but the last file over size limit
+            string maxLog = TestHelpers.NewRandomString((int)FileWriter.MaxLogFileSizeBytes + 1);
+            File.AppendAllText(logFiles[3].FullName, maxLog);
+            File.AppendAllText(logFiles[2].FullName, maxLog);
+            File.AppendAllText(logFiles[1].FullName, maxLog);
+            File.AppendAllText(logFiles[0].FullName, maxLog);
 
-            await Task.Delay(2000);
+            // mark all the files as old to simulate the passage of time
+            File.SetLastWriteTime(logFiles[4].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(2)));
+            File.SetLastWriteTime(logFiles[3].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(2)));
+            File.SetLastWriteTime(logFiles[2].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(3)));
+            File.SetLastWriteTime(logFiles[1].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(4)));
+            File.SetLastWriteTime(logFiles[0].FullName, DateTime.Now.Subtract(TimeSpan.FromDays(5)));
 
             var files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
-            Assert.Equal(initialCount, files.Length);
+            Assert.Equal(5, files.Length);
 
-            FileWriter fileWriter = new FileWriter(_logFilePath);
-            fileWriter.SetNewLogFile();
+            // now cause a new log file to be created by writing a huge
+            // log pushing the current file over limit
+            var fileWriter = new FileWriter(_logFilePath);
+            fileWriter.AppendLine(maxLog);
 
-            files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
-
+            // wait for the new file to be created and the old files to be purged
             await TestHelpers.Await(() =>
             {
                 files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
                 return files.Length == 2;
-            }, timeout: 2000);
+            }, timeout: 5000);
 
-            // verify the correct log files were purged and the 2
-            // most recent files were retained
-            Assert.True(files[0].Name.StartsWith("4"));
-            Assert.True(files[1].Name.StartsWith("3"));
+            // expect only 2 files to remain - the new file we just created, as well
+            // as the oversize file we just wrote to last (it has a new timestamp now so
+            // wasn't purged)
+            Assert.True(files[1].Length > FileWriter.MaxLogFileSizeBytes);
+
+            // expect the new file to be empty because everything was flushed
+            // to the previous file before it was created
+            var fileLines = File.ReadAllLines(files[0].FullName);
+            Assert.Equal(0, fileLines.Length);
+
+            // make sure the new log is written to the new file
+            fileWriter.AppendLine("test message");
+            fileWriter.Flush();
+            fileLines = File.ReadAllLines(files[0].FullName);
+            Assert.Equal(1, fileLines.Length);
         }
 
         [Fact]
-        public void SetNewLogFile_EmptyDirectory_Succeeds()
+        public void Constructor_EmptyDirectory_DelayCreatesLogFile()
         {
-            DirectoryInfo directory = new DirectoryInfo(_logFilePath);
+            var directory = new DirectoryInfo(_logFilePath);
             directory.Create();
 
-            FileWriter fileWriter = new FileWriter(_logFilePath);
-            fileWriter.SetNewLogFile();
+            var fileWriter = new FileWriter(_logFilePath);
 
             var files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
             Assert.Equal(0, files.Length);
@@ -110,27 +124,57 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             fileWriter.AppendLine("Test log");
             fileWriter.Flush();
 
+            // log file is delay created
             files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
             Assert.Equal(1, files.Length);
         }
 
         [Fact]
-        public void Trace_ReusesLastFile()
+        public async Task SetLogFile_EmptyDirectory_MultipleInstances_Staggered_CreatesOneFile()
         {
-            DirectoryInfo directory = new DirectoryInfo(_logFilePath);
-            directory.Create();
-
-            int count = directory.EnumerateFiles().Count();
-            Assert.Equal(0, count);
-
-            for (int i = 0; i < 3; i++)
+            var writers = new List<FileWriter>();
+            for (int i = 0; i < 5; i++)
             {
-                FileWriter fileWriter = new FileWriter(_logFilePath);
-                fileWriter.AppendLine("Testing");
-                fileWriter.Flush();
+                writers.Add(new FileWriter(_logFilePath));
+                await Task.Delay(400);
             }
 
-            count = directory.EnumerateFiles().Count();
+            writers.ForEach(p => p.AppendLine($"test message"));
+            writers.ForEach(p => p.Flush());
+
+            var directory = new DirectoryInfo(_logFilePath);
+            var files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            Assert.Equal(1, files.Length);
+
+            var fileLines = File.ReadAllLines(files.Single().FullName);
+            Assert.Equal(5, fileLines.Length);
+        }
+
+        [Fact]
+        public async Task SetLogFile_EmptyDirectory_MultipleInstances_Concurrent_CreatesOneFile()
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() => WriteLogs(_logFilePath, 10)));
+            }
+            await Task.WhenAll(tasks);
+
+            var directory = new DirectoryInfo(_logFilePath);
+            var files = directory.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            Assert.Equal(1, files.Length);
+
+            var fileLines = File.ReadAllLines(files.Single().FullName);
+            Assert.Equal(100, fileLines.Length);
+        }
+
+        [Fact]
+        public void Flush_ReusesLastFile()
+        {
+            WriteLogs(_logFilePath, 3);
+
+            var directory = new DirectoryInfo(_logFilePath);
+            int count = directory.EnumerateFiles().Count();
             Assert.Equal(1, count);
 
             string logFile = directory.EnumerateFiles().First().FullName;
@@ -139,11 +183,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         }
 
         [Fact]
-        public async Task Trace_ThrottlesLogs()
+        public async Task Flush_ThrottlesLogs()
         {
-            DirectoryInfo directory = new DirectoryInfo(_logFilePath);
-            directory.Create();
-
             int numLogs = 10000;
             int numIterations = 3;
             for (int i = 0; i < numIterations; i++)
@@ -152,10 +193,42 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
                 await Task.Delay(1000);
             }
 
+            DirectoryInfo directory = new DirectoryInfo(_logFilePath);
             string logFile = directory.EnumerateFiles().First().FullName;
             string[] fileLines = File.ReadAllLines(logFile);
             Assert.True(fileLines.Length == ((FileWriter.MaxLogLinesPerFlushInterval * numIterations) + 3));
             Assert.Equal(3, fileLines.Count(p => p.Contains("Log output threshold exceeded.")));
+        }
+
+        [Fact]
+        public async Task Flush_LogFileDeleted_CreatesNewFile()
+        {
+            var fileWriter = new FileWriter(_logFilePath);
+
+            fileWriter.AppendLine("test trace");
+            fileWriter.Flush();
+
+            var directory = new DirectoryInfo(_logFilePath);
+            var firstLogFile = directory.EnumerateFiles().Single();
+
+            // delete the first log file to force another one to be
+            // created
+            File.Delete(firstLogFile.FullName);
+
+            // wait at least a second to ensure the file gets a distinct timestamp
+            await TestHelpers.Await(() => !File.Exists(firstLogFile.FullName));
+            await Task.Delay(1000);
+
+            fileWriter.AppendLine("test trace");
+            fileWriter.Flush();
+
+            var secondLogFile = directory.EnumerateFiles().Single();
+
+            // verify that a new log file was created with a different
+            // timestamp
+            Assert.NotEqual(firstLogFile.Name, secondLogFile.Name);
+            var logLine = File.ReadAllLines(secondLogFile.FullName).Single();
+            Assert.Equal("test trace", logLine);
         }
 
         private void WriteLogs(string logFilePath, int numLogs)
