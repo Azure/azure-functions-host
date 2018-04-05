77f70982b07d05d73e2b1ff69513abe349b92bf1
Mike <MikeStall@hotmail.com>
2018-04-05 13:34:41 -0700

    Ensure that timeout config is pushed to SDK in Dynamic sku.

--- src/WebJobs.Script/Host/ScriptHost.cs
+++ src/WebJobs.Script/Host/ScriptHost.cs
@@ -1541,13 +1541,13 @@ namespace Microsoft.Azure.WebJobs.Script
                 }
 
                 scriptConfig.FunctionTimeout = requestedTimeout;
-                scriptConfig.HostConfig.FunctionTimeout = ScriptHost.CreateTimeoutConfiguration(scriptConfig);
             }
             else if (ScriptSettingsManager.Instance.IsDynamicSku)
             {
                 // Apply a default if this is running on Dynamic.
                 scriptConfig.FunctionTimeout = DefaultFunctionTimeout;
             }
+            scriptConfig.HostConfig.FunctionTimeout = ScriptHost.CreateTimeoutConfiguration(scriptConfig);
 
             ApplyLoggerConfig(config, scriptConfig);
         }
