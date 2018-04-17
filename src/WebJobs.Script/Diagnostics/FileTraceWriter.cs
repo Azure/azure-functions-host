b6094e18332f3350d27f632e3e31dc412d970012
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-04-16 18:03:43 -0700

    Performance Improvements to FileTraceWriter

--- src/WebJobs.Script/Diagnostics/FileTraceWriter.cs
+++ src/WebJobs.Script/Diagnostics/FileTraceWriter.cs
@@ -9,7 +9,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Timers;
+using System.Threading;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
@@ -22,12 +22,13 @@ namespace Microsoft.Azure.WebJobs.Script
         internal const long MaxLogFileSizeBytes = 5 * 1024 * 1024;
         internal const int LogFlushIntervalMs = 1000;
         internal const int MaxLogLinesPerFlushInterval = 250;
+        private static readonly ConcurrentDictionary<string, object> _flushLocks = new ConcurrentDictionary<string, object>();
         private readonly string _logFilePath;
         private readonly string _instanceId;
         private readonly LogType _logType;
 
         private readonly DirectoryInfo _logDirectory;
-        private static object _syncLock = new object();
+        private readonly object _flushTimerSyncRoot = new object();
         private FileInfo _currentLogFileInfo;
         private bool _disposed = false;
         private Timer _flushTimer;
@@ -39,90 +40,137 @@ namespace Microsoft.Azure.WebJobs.Script
             _instanceId = GetInstanceId();
             _logType = logType;
             _logDirectory = new DirectoryInfo(logFilePath);
-
-            // start a timer to flush accumulated logs in batches
-            _flushTimer = new Timer
-            {
-                AutoReset = true,
-                Interval = LogFlushIntervalMs
-            };
-            _flushTimer.Elapsed += OnFlushLogs;
-            _flushTimer.Start();
         }
 
-        public override void Flush()
+        /// <summary>
+        /// Maintains a collection of objects used for locking, scoped to the file path.
+        /// The objects are not currently removed, but the list is pretty static and shouldn't be a concern.
+        /// These cannot be removed on disposal. If cleanup functionality is ever required, we can address this with a scheduled cleanup or something along those lines.
+        /// </summary>
+        /// <returns>A lock object scoped to the current <see cref="FileTraceWriter"/> log file path.</returns>
+        private object GetFlushLock() => _flushLocks.GetOrAdd(_logFilePath, new object());
+
+        private protected virtual void StartFlushTimer()
         {
-            if (_logBuffer.Count == 0)
+            if (_flushTimer != null)
             {
                 return;
             }
 
-            ConcurrentQueue<string> currentBuffer = null;
-            lock (_syncLock)
+            lock (_flushTimerSyncRoot)
+            {
+                if (_flushTimer == null)
+                {
+                    // start a timer to flush accumulated logs in batches
+                    _flushTimer = new Timer(FlushCallback, null, LogFlushIntervalMs, LogFlushIntervalMs);
+                }
+            }
+        }
+
+        private protected virtual void StopFlushTimer()
+        {
+            if (_flushTimer == null)
+            {
+                return;
+            }
+
+            lock (_flushTimerSyncRoot)
+            {
+                if (_flushTimer != null)
+                {
+                    _flushTimer.Dispose();
+                    _flushTimer = null;
+
+                    // Since we just stopped our timer, we'll perform an explicit
+                    // flush to ensure there was nothing written to the buffer
+                    Flush();
+                }
+            }
+        }
+
+        private void FlushCallback(object state) => Flush(true);
+
+        public override void Flush() => Flush(false);
+
+        private protected virtual bool Flush(bool timerCallback)
+        {
+            if (_logBuffer.Count == 0)
+            {
+                if (timerCallback)
+                {
+                    StopFlushTimer();
+                }
+
+                return true;
+            }
+
+            object flushLock = GetFlushLock();
+
+            // If this was an explicit Flush call, we'll block until the lock is acquired;
+            // otherwise, we'll return immediately.
+            if (!timerCallback)
+            {
+                Monitor.Enter(flushLock);
+            }
+            else if (!Monitor.TryEnter(flushLock))
+            {
+                return false;
+            }
+
+            try
             {
                 // Snapshot the current set of buffered logs
                 // and set a new queue. This ensures that any new
                 // logs are written to the new buffer.
-                // We do this snapshot in a lock since Flush might be
-                // called by multiple threads concurrently, and we need
-                // to ensure we only log each log once.
-                currentBuffer = _logBuffer;
+                ConcurrentQueue<string> currentBuffer = _logBuffer;
                 _logBuffer = new ConcurrentQueue<string>();
-            }
 
-            if (currentBuffer.Count == 0)
-            {
-                return;
-            }
-
-            // concatenate all lines into one string
-            StringBuilder sb = new StringBuilder();
-            string line = null;
-            while (currentBuffer.TryDequeue(out line))
-            {
-                sb.AppendLine(line);
-            }
-
-            if (_currentLogFileInfo == null)
-            {
-                // delay create log file
-                SetLogFile();
-            }
-
-            // write all lines in a single file operation
-            string content = sb.ToString();
-            try
-            {
-                AppendToFile(_currentLogFileInfo, content);
-            }
-            catch (Exception ex) when (ex is DirectoryNotFoundException || ex is FileNotFoundException)
-            {
-                // need to handle cases where log files or directories might
-                // have been deleted from underneath us
-                SetLogFile();
-                AppendToFile(_currentLogFileInfo, content);
-            }
-
-            // check to see if we need to roll to a new log file
-            _currentLogFileInfo.Refresh();
-            if (LogFileIsOverSize(_currentLogFileInfo))
-            {
-                SetLogFile();
-            }
-        }
-
-        private static void AppendToFile(FileInfo fileInfo, string content)
-        {
-            lock (_syncLock)
-            {
-                var fs = fileInfo.Open(FileMode.Open, FileAccess.Write);
-                fs.Seek(0, SeekOrigin.End);
-
-                using (var sw = new StreamWriter(fs))
+                if (currentBuffer.Count == 0)
                 {
-                    sw.Write(content);
+                    return true;
+                }
+
+                // concatenate all lines into one string
+                StringBuilder sb = new StringBuilder();
+                string line = null;
+                while (currentBuffer.TryDequeue(out line))
+                {
+                    sb.AppendLine(line);
+                }
+
+                if (_currentLogFileInfo == null)
+                {
+                    // delay create log file
+                    SetLogFile();
+                }
+
+                // write all lines in a single file operation
+                string content = sb.ToString();
+                try
+                {
+                    AppendToFile(_currentLogFileInfo, content);
+                }
+                catch (Exception ex) when (ex is DirectoryNotFoundException || ex is FileNotFoundException)
+                {
+                    // need to handle cases where log files or directories might
+                    // have been deleted from underneath us
+                    SetLogFile();
+                    AppendToFile(_currentLogFileInfo, content);
+                }
+
+                // check to see if we need to roll to a new log file
+                _currentLogFileInfo.Refresh();
+                if (LogFileIsOverSize(_currentLogFileInfo))
+                {
+                    SetLogFile();
                 }
             }
+            finally
+            {
+                Monitor.Exit(flushLock);
+            }
+
+            return true;
         }
 
         public override void Trace(TraceEvent traceEvent)
@@ -132,8 +180,7 @@ namespace Microsoft.Azure.WebJobs.Script
                 throw new ArgumentNullException(nameof(traceEvent));
             }
 
-            object value;
-            if (traceEvent.Properties.TryGetValue(ScriptConstants.TracePropertyIsSystemTraceKey, out value)
+            if (traceEvent.Properties.TryGetValue(ScriptConstants.TracePropertyIsSystemTraceKey, out object value)
                 && value is bool && (bool)value)
             {
                 // we don't want to write system traces to the user trace files
@@ -173,6 +220,9 @@ namespace Microsoft.Azure.WebJobs.Script
                     AppendLine(traceEvent, traceEvent.Exception.ToFormattedString());
                 }
             }
+
+            // Start our flush timer
+            StartFlushTimer();
         }
 
         internal static string GetTracePrefix(TraceEvent traceEvent, LogType logType)
@@ -195,13 +245,9 @@ namespace Microsoft.Azure.WebJobs.Script
             {
                 if (disposing)
                 {
-                    if (_flushTimer != null)
-                    {
-                        _flushTimer.Dispose();
-                    }
-
-                    // ensure any remaining logs are flushed
-                    Flush();
+                    // Stop our timer
+                    // This will also flush if there's anything buffered.
+                    StopFlushTimer();
                 }
 
                 _disposed = true;
@@ -228,6 +274,17 @@ namespace Microsoft.Azure.WebJobs.Script
             _logBuffer.Enqueue(line);
         }
 
+        private static void AppendToFile(FileInfo fileInfo, string content)
+        {
+            var fs = fileInfo.Open(FileMode.Open, FileAccess.Write, FileShare.Write);
+            fs.Seek(0, SeekOrigin.End);
+
+            using (var sw = new StreamWriter(fs))
+            {
+                sw.Write(content);
+            }
+        }
+
         /// <summary>
         /// Format the log line for the current event being traced.
         /// </summary>
@@ -241,11 +298,6 @@ namespace Microsoft.Azure.WebJobs.Script
             string formattedLine = $"{timestamp} [{tracePrefix}] {line.Trim()}";
 
             return formattedLine;
-        }
-
-        private void OnFlushLogs(object sender, ElapsedEventArgs e)
-        {
-            Flush();
         }
 
         /// <summary>
@@ -269,29 +321,14 @@ namespace Microsoft.Azure.WebJobs.Script
             }
             else
             {
-                lock (_syncLock)
-                {
-                    // we perform any file create operations in a lock to avoid
-                    // race conditions with multiple instances of this class on
-                    // multiple threads, where multiple log files might be created
-                    // unnecessarily
-                    logFile = GetCurrentLogFile();
-                    if (logFile != null)
-                    {
-                        _currentLogFileInfo = logFile;
-                    }
-                    else
-                    {
-                        // create a new log file
-                        // we include a machine identifier in the log file name to ensure we don't have any
-                        // log file contention between scaled out instances
-                        var timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ssK");
-                        string filePath = Path.Combine(_logFilePath, $"{timestamp}-{_instanceId}.log");
-                        _currentLogFileInfo = new FileInfo(filePath);
-                        _currentLogFileInfo.Create().Close();
-                        newLogFileCreated = true;
-                    }
-                }
+                // create a new log file
+                // we include a machine identifier in the log file name to ensure we don't have any
+                // log file contention between scaled out instances
+                var timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ssK");
+                string filePath = Path.Combine(_logFilePath, $"{timestamp}-{_instanceId}.log");
+                _currentLogFileInfo = new FileInfo(filePath);
+                _currentLogFileInfo.Create().Close();
+                newLogFileCreated = true;
             }
 
             if (newLogFileCreated)
