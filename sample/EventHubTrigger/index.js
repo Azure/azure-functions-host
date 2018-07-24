41f16e8a5d13151220d746c4773ec17a2f26333a
mhoeger <marie.hoeger@gmail.com>
2018-07-23 19:08:28 -0700

    improving javascript of tests

--- sample/EventHubTrigger/index.js
+++ sample/EventHubTrigger/index.js
@@ -1,18 +1,17 @@
 var util = require('util');
 
 module.exports = function (context, input) {
-    var parsedInput = JSON.parse(input);
-    context.log(util.format("Node.js script processed %d events", parsedInput.length));
-    context.log("IsArray", util.isArray(parsedInput));
+    context.log(util.format("Node.js script processed %d events", input.length));
+    context.log("IsArray", util.isArray(input));
 
-    for (i = 0; i < parsedInput.length; i++)
+    for (i = 0; i < input.length; i++)
     {
         // EventData properties can be accessed via binding data,
         // including custom properties, system properties, etc.
         var bindingData = context.bindingData,
             eventProperties = bindingData.propertiesArray[i],
             systemProperties = bindingData.systemPropertiesArray[i],
-            id = parsedInput[i].value;
+            id = JSON.parse(input[i]).value;
 
         context.log('EventId: %s, EnqueuedTime: %s, Sequence: %d, Index: %s',
             id,
--- test/WebJobs.Script.Tests.Integration/TestScripts/ListenerExceptions/ListenerStartupException/index.js
+++ test/WebJobs.Script.Tests.Integration/TestScripts/ListenerExceptions/ListenerStartupException/index.js
@@ -1,7 +1,6 @@
 ﻿var util = require('util');
 
 module.exports = function (context, input) {
-    var parsedInput = JSON.parse(input);
-    context.log(util.format("Node.js script processed %d events", parsedInput.length));
+    context.log(util.format("Node.js script processed %d events", input.length));
     context.done();
 }
