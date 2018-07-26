54db2dd9b133f99eda0208bb47efcc9c2c226fae
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-07-25 17:44:32 -0700

    Updating host check attribute to use injected manager

--- src/WebJobs.Script.WebHost/Filters/RequiresRunningHostAttribute.cs
+++ src/WebJobs.Script.WebHost/Filters/RequiresRunningHostAttribute.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost.Filters
 {
@@ -12,8 +15,10 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Filters
     /// where functions can be invoked.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class RequiresRunningHostAttribute : ActionFilterAttribute
+    public sealed class RequiresRunningHostAttribute : Attribute, IFilterFactory
     {
+        private ObjectFactory _factory;
+
         public RequiresRunningHostAttribute(int timeoutSeconds = ScriptConstants.HostTimeoutSeconds, int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds)
         {
             TimeoutSeconds = timeoutSeconds;
@@ -24,16 +29,68 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Filters
 
         public int PollingIntervalMilliseconds { get; }
 
-        public override async Task OnActionExecutionAsync(ActionExecutingContext actionContext, ActionExecutionDelegate next)
+        public bool IsReusable => false;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
         {
-            var scriptHostManager = (WebScriptHostManager)actionContext.HttpContext.Items[ScriptConstants.AzureFunctionsHostKey];
+            if (_factory == null)
+            {
+               _factory = ActivatorUtilities.CreateFactory(typeof(RunningHostCheckAttribute), Type.EmptyTypes);
+            }
 
-            // If the host is not ready, we'll wait a bit for it to initialize.
-            // This might happen if http requests come in while the host is starting
-            // up for the first time, or if it is restarting.
-            await scriptHostManager.DelayUntilHostReady(TimeoutSeconds, PollingIntervalMilliseconds);
+            var hostCheckFilter = (RunningHostCheckAttribute)_factory(serviceProvider, null);
 
-            await next();
+            hostCheckFilter.PollingIntervalMilliseconds = PollingIntervalMilliseconds;
+            hostCheckFilter.TimeoutSeconds = TimeoutSeconds;
+
+            return hostCheckFilter;
+        }
+
+        private class RunningHostCheckAttribute : ActionFilterAttribute
+        {
+            private readonly IScriptHostManager _hostManager;
+
+            public RunningHostCheckAttribute(IScriptHostManager hostManager)
+            {
+                _hostManager = hostManager;
+            }
+
+            public int TimeoutSeconds { get; internal set; }
+
+            public int PollingIntervalMilliseconds { get; internal set; }
+
+            public override async Task OnActionExecutionAsync(ActionExecutingContext actionContext, ActionExecutionDelegate next)
+            {
+                // If the host is not ready, we'll wait a bit for it to initialize.
+                // This might happen if http requests come in while the host is starting
+                // up for the first time, or if it is restarting.
+                await DelayUntilHostReady(_hostManager, TimeoutSeconds, PollingIntervalMilliseconds);
+
+                await next();
+            }
+
+            internal static async Task<bool> DelayUntilHostReady(IScriptHostManager hostManager, int timeoutSeconds = ScriptConstants.HostTimeoutSeconds, int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds, bool throwOnFailure = true)
+            {
+                bool CanInvoke()
+                {
+                    return hostManager.State == ScriptHostState.Running || hostManager.State == ScriptHostState.Initialized;
+                }
+
+                await Utility.DelayAsync(timeoutSeconds, pollingIntervalMilliseconds, () =>
+                {
+                    return !CanInvoke() &&
+                            hostManager.State != ScriptHostState.Error;
+                });
+
+                bool hostReady = CanInvoke();
+
+                if (throwOnFailure && !hostReady)
+                {
+                    throw new HttpException(HttpStatusCode.ServiceUnavailable, "Function host is not running.");
+                }
+
+                return hostReady;
+            }
         }
     }
 }
