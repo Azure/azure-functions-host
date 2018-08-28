e33fc4caf52e472973415ffa225899e853077551
Paul Batum <paul.batum@gmail.com>
2018-08-27 21:14:19 -0700

    Rename RequiresRunningHostAttribute.cs -> WaitForHostAttribute.cs

--- src/WebJobs.Script.WebHost/Filters/WaitForHostAttribute.cs
+++ src/WebJobs.Script.WebHost/Filters/WaitForHostAttribute.cs
@@ -11,34 +11,6 @@ using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost.Filters
 {
-    /// <summary>
-    /// Filter applied to actions that require the host instance to be in a state
-    /// where functions can be invoked.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class RequiresRunningHostAttribute : WaitForHostAttribute
-    {
-        public RequiresRunningHostAttribute(
-            int timeoutSeconds = ScriptConstants.HostTimeoutSeconds,
-            int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds) : base(timeoutSeconds, pollingIntervalMilliseconds, false)
-        {
-        }
-    }
-
-    /// <remarks>
-    /// Filter applied to actions that might return different results based on whether the host has started or not and the client might want to request to wait (up to some server specified timeout) for the host to start.
-    /// Do not use this on an API that requires a running host to execute successfully.
-    /// </remarks>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class ClientCanRequestRunningHost : WaitForHostAttribute
-    {
-        public ClientCanRequestRunningHost(
-            int timeoutSeconds = ScriptConstants.HostTimeoutSeconds,
-            int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds) : base(timeoutSeconds, pollingIntervalMilliseconds, true)
-        {
-        }
-    }
-
     public abstract class WaitForHostAttribute : Attribute, IFilterFactory
     {
         private ObjectFactory _factory;
@@ -118,6 +90,34 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Filters
 
                 await next();
             }
+        }
+    }
+
+    /// <summary>
+    /// Filter applied to actions that require the host instance to be in a state
+    /// where functions can be invoked.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class RequiresRunningHostAttribute : WaitForHostAttribute
+    {
+        public RequiresRunningHostAttribute(
+            int timeoutSeconds = ScriptConstants.HostTimeoutSeconds,
+            int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds) : base(timeoutSeconds, pollingIntervalMilliseconds, false)
+        {
+        }
+    }
+
+    /// <remarks>
+    /// Filter applied to actions that might return different results based on whether the host has started or not and the client might want to request to wait (up to some server specified timeout) for the host to start.
+    /// Do not use this on an API that requires a running host to execute successfully.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class ClientCanRequestRunningHost : WaitForHostAttribute
+    {
+        public ClientCanRequestRunningHost(
+            int timeoutSeconds = ScriptConstants.HostTimeoutSeconds,
+            int pollingIntervalMilliseconds = ScriptConstants.HostPollingIntervalMilliseconds) : base(timeoutSeconds, pollingIntervalMilliseconds, true)
+        {
         }
     }
 }
