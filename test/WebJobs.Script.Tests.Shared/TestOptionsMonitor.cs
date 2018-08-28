d59878bb1fb306576b6bc8d4dfd325556e09515f
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-08-27 17:58:29 -0700

    Fixing HostIdProvider tests

---
+++ test/WebJobs.Script.Tests.Shared/TestOptionsMonitor.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Options;
+using System;
+using System.Reactive.Disposables;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests
+{
+    public class TestOptionsMonitor<T> : IOptionsMonitor<T> where T : class, new()
+    {
+        private readonly Func<T> _optionsFactory;
+
+        public TestOptionsMonitor(T options)
+            : this(() => options ?? new T())
+        {
+        }
+
+        public TestOptionsMonitor(Func<T> optionsFactory)
+        {
+            _optionsFactory = optionsFactory ?? throw new ArgumentNullException(nameof(optionsFactory));
+        }
+
+        public T CurrentValue => _optionsFactory();
+
+        public T Get(string name)
+        {
+            return _optionsFactory();
+        }
+
+        public IDisposable OnChange(Action<T, string> listener)
+        {
+            return Disposable.Empty;
+        }
+    }
+}
--- test/WebJobs.Script.Tests/ScriptHostIdProviderTests.cs
+++ test/WebJobs.Script.Tests/ScriptHostIdProviderTests.cs
@@ -7,10 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Configuration;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 
@@ -21,7 +19,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [Fact]
         public async Task GetHostIdAsync_WithConfigurationHostId_ReturnsConfigurationHostId()
         {
-            var options = new ScriptJobHostOptions();
+            var options = new ScriptApplicationHostOptions();
             var environment = new TestEnvironment();
 
             var config = new ConfigurationBuilder()
@@ -31,7 +29,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
                 })
                 .Build();
 
-            var provider = new ScriptHostIdProvider(config, environment, new OptionsWrapper<ScriptJobHostOptions>(options));
+            var provider = new ScriptHostIdProvider(config, environment, new TestOptionsMonitor<ScriptApplicationHostOptions>(options));
 
             string hostId = await provider.GetHostIdAsync(CancellationToken.None);
 
@@ -45,26 +43,31 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         [InlineData(null, null)]
         public void GetDefaultHostId_AzureHost_ReturnsExpectedResult(string siteName, string expected)
         {
-            var config = new ScriptJobHostOptions();
+            var options = new ScriptApplicationHostOptions
+            {
+                ScriptPath = @"c:\testscripts"
+            };
+
             var environment = new TestEnvironment();
+            environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteInstanceId, "123123");
             environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteName, siteName);
 
-            string hostId = ScriptHostIdProvider.GetDefaultHostId(environment, config);
+            string hostId = ScriptHostIdProvider.GetDefaultHostId(environment, options);
             Assert.Equal(expected, hostId);
         }
 
         [Fact]
         public void GetDefaultHostId_SelfHost_ReturnsExpectedResult()
         {
-            var config = new ScriptJobHostOptions
+            var options = new ScriptApplicationHostOptions
             {
                 IsSelfHost = true,
-                RootScriptPath = @"c:\testing\FUNCTIONS-TEST\test$#"
+                ScriptPath = @"c:\testing\FUNCTIONS-TEST\test$#"
             };
 
             var environmentMock = new Mock<IEnvironment>();
 
-            string hostId = ScriptHostIdProvider.GetDefaultHostId(environmentMock.Object, config);
+            string hostId = ScriptHostIdProvider.GetDefaultHostId(environmentMock.Object, options);
 
             // This suffix is a stable hash code derived from the "RootScriptPath" string passed in the configuration.
             // We're using the literal here as we want this test to fail if this compuation ever returns something different.
