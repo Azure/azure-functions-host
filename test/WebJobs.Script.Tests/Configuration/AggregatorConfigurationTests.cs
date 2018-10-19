aa9307061cf9a38cbf9f8d574214071673a0f80b
Brett Samblanet <brettsam@microsoft.com>
2018-10-19 11:42:17 -0700

    re-enabling FunctionResultAggregator

--- test/WebJobs.Script.Tests/Configuration/AggregatorConfigurationTests.cs
+++ test/WebJobs.Script.Tests/Configuration/AggregatorConfigurationTests.cs
@@ -3,8 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Azure.WebJobs.Host.Loggers;
 using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Azure.WebJobs.Script.Configuration;
+using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,17 +20,42 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Configuration
 {
     public class AggregatorConfigurationTests
     {
+        private static readonly string AggregatorPath = ConfigurationPath.Combine(ConfigurationSectionNames.JobHost, ConfigurationSectionNames.Aggregator);
+
+        [Fact]
+        public void Aggregator_Registered_ByDefault()
+        {
+            IHost host = new HostBuilder()
+                .ConfigureDefaultTestWebScriptHost()
+                .Build();
+
+            // Make sure there are two providers registered, and that one has a type with
+            // "FunctionResultAggregatorProvider" (since it is internal to WebJobs)
+            var eventCollectorProviders = host.Services.GetServices<IEventCollectorProvider>();
+            Assert.Equal(2, eventCollectorProviders.Count());
+            Assert.Single(eventCollectorProviders.OfType<FunctionInstanceLogCollectorProvider>());
+            Assert.Single(eventCollectorProviders.Where(p => p.GetType().Name.Contains("FunctionResultAggregatorProvider")));
+
+            // Also make sure that when requesting the collectors, we end up with a composite that
+            // includes both of the collectors above. This prevents any overriding of the IEventCollectorFactory
+            var eventLogger = host.Services.GetServices<IAsyncCollector<FunctionInstanceLogEntry>>().Single();
+            var field = eventLogger.GetType().GetField("_collectors", BindingFlags.NonPublic | BindingFlags.Instance);
+            var collectors = (IEnumerable<IAsyncCollector<FunctionInstanceLogEntry>>)field.GetValue(eventLogger);
+            Assert.Equal(2, collectors.Count());
+            Assert.Single(collectors.OfType<FunctionInstanceLogger>());
+            Assert.Single(collectors.Where(p => p.GetType().Name.Contains("FunctionResultAggregator")));
+        }
+
         [Fact]
         public void Configuration_BindsTo_AggregatorOptions()
         {
-            string aggregatorPath = ConfigurationPath.Combine(ConfigurationSectionNames.JobHost, ConfigurationSectionNames.Aggregator);
             IHost host = new HostBuilder()
                 .ConfigureAppConfiguration(c =>
                 {
                     c.AddInMemoryCollection(new Dictionary<string, string>
                     {
-                        { ConfigurationPath.Combine(aggregatorPath, "BatchSize"), "33" },
-                        { ConfigurationPath.Combine(aggregatorPath, "FlushTimeout"), "00:00:33" }
+                        { ConfigurationPath.Combine(AggregatorPath, "BatchSize"), "33" },
+                        { ConfigurationPath.Combine(AggregatorPath, "FlushTimeout"), "00:00:33" }
                     });
                 })
                 .ConfigureDefaultTestWebScriptHost()
