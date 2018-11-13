005f7a0ecc617af3f65aa213553a23728b13b0f5
Tyler James Leonhardt <tylerl0706@gmail.com>
2018-11-13 15:02:36 -0800

    use ErrorAction SilentlyContinue for gci

--- build.ps1
+++ build.ps1
@@ -273,7 +273,8 @@ function cleanExtension([string] $bitness) {
     } elseif ($bitness -eq "32bit") {
         $keepRuntimes = @('win', 'win-x86', 'win10-x86')
     }
-    Get-ChildItem "$privateSiteExtensionPath\$bitness\workers\powershell\runtimes" -Exclude $keepRuntimes | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    Get-ChildItem "$privateSiteExtensionPath\$bitness\workers\powershell\runtimes" -Exclude $keepRuntimes -ErrorAction SilentlyContinue |
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 }
   
 dotnet --version
