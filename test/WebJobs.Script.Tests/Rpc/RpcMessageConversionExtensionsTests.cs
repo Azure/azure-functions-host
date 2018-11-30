f3a32997ac18dfe17eb0855bda0bd804a0ef9fa3
mhoeger <marie.hoeger@gmail.com>
2018-11-29 18:24:50 -0800

    update test

--- test/WebJobs.Script.Tests/Rpc/RpcMessageConversionExtensionsTests.cs
+++ test/WebJobs.Script.Tests/Rpc/RpcMessageConversionExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Script.Rpc;
 using Microsoft.WebJobs.Script.Tests;
@@ -24,6 +25,26 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Rpc
             string contentType;
             rpcRequestObject.Http.Headers.TryGetValue("content-type", out contentType);
             Assert.Equal(expectedContentType, contentType);
+        }
+
+        [Theory]
+        [InlineData("say=Hi&to=Mom", new string[] { "say", "to" }, new string[] { "Hi", "Mom" })]
+        [InlineData("say=Hi", new string[] { "say" }, new string[] { "Hi" })]
+        [InlineData("say=Hi&to=", new string[] { "say" }, new string[] { "Hi" })] // Removes empty value query params
+        public void HttpObjects_Query(string queryString, string[] expectedKeys, string[] expectedValues)
+        {
+            HttpRequest request = HttpTestHelpers.CreateHttpRequest("GET", $"http://localhost/api/httptrigger-scenarios?{queryString}");
+
+            var rpcRequestObject = request.ToRpc();
+            // Same number of expected key value pairs
+            Assert.Equal(rpcRequestObject.Http.Query.Count, expectedKeys.Length);
+            Assert.Equal(rpcRequestObject.Http.Query.Count, expectedValues.Length);
+            // Same key and value strings for each pair
+            for (int i = 0; i < expectedKeys.Length; i++)
+            {
+                Assert.True(rpcRequestObject.Http.Query.ContainsKey(expectedKeys[i]));
+                Assert.Equal(rpcRequestObject.Http.Query.GetValueOrDefault(expectedKeys[i]), expectedValues[i]);
+            }
         }
     }
 }
