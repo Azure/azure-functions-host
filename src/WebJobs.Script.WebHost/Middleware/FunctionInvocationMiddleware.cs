cb1688ac6b5269bcf20fdea86e3b5635495f11c2
Alexey Rodionov <alrod@microsoft.com>
2019-01-24 18:51:00 -0800

    Fix RedirectResult for AspNetCore 2.2. Fixes #3986

--- src/WebJobs.Script.WebHost/Middleware/FunctionInvocationMiddleware.cs
+++ src/WebJobs.Script.WebHost/Middleware/FunctionInvocationMiddleware.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Logging;
@@ -62,11 +63,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Middleware
                     return;
                 }
 
-                var actionContext = new ActionContext
-                {
-                    HttpContext = context
-                };
-
+                ActionContext actionContext = new ActionContext(context, new RouteData(), new ActionDescriptor());
                 await result.ExecuteResultAsync(actionContext);
             }
         }
