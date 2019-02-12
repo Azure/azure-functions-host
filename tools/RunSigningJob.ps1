912d8c084f21218ee8de45a04c63d08f58d05ee0
Brett Samblanet <brettsam@microsoft.com>
2019-02-12 07:54:56 -0800

    creating official ExtensionsMetadataGenerator build

---
+++ tools/RunSigningJob.ps1
@@ -0,0 +1,22 @@
+$bypassPackaging = $env:APPVEYOR_PULL_REQUEST_NUMBER -and -not $env:APPVEYOR_PULL_REQUEST_TITLE.Contains("[pack]")
+$directoryPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+if (-not $bypassPackaging) {
+  # Only sign the ExtensionsMetadataGenerator
+  New-Item -ItemType Directory -Force -Path "$directoryPath\..\buildoutput\signing"
+  Compress-Archive $directoryPath\..\buildoutput\Microsoft.Azure.WebJobs.Script.ExtensionsMetadataGenerator*.nupkg $directoryPath\..\buildoutput\signing\tosign.zip  
+  Remove-Item $directoryPath\..\buildoutput\Microsoft.Azure.WebJobs.Script.ExtensionsMetadataGenerator*.nupkg
+
+  if ($env:SkipAssemblySigning -eq "true") {
+    "Signing disabled. Skipping signing process."
+    exit 0;
+  }
+
+  $ctx = New-AzureStorageContext $env:FILES_ACCOUNT_NAME $env:FILES_ACCOUNT_KEY
+  Set-AzureStorageBlobContent "$directoryPath/../buildoutput/signing/tosign.zip" "azure-functions-host" -Blob "$env:APPVEYOR_BUILD_VERSION.zip" -Context $ctx
+
+  $queue = Get-AzureStorageQueue "signing-jobs" -Context $ctx
+
+  $messageBody = "SignNupkgs;azure-functions-host;$env:APPVEYOR_BUILD_VERSION.zip"
+  $queue.CloudQueue.AddMessage($messageBody)
+}
