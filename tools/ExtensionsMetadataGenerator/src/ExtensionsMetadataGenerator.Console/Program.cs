8b879366f429a70df68cab90bf2f21c87ca8e540
Brett Samblanet <brettsam@microsoft.com>
2019-04-18 11:00:56 -0700

    [ExtensionsMetadataGenerator] improving logging

--- tools/ExtensionsMetadataGenerator/src/ExtensionsMetadataGenerator.Console/Program.cs
+++ tools/ExtensionsMetadataGenerator/src/ExtensionsMetadataGenerator.Console/Program.cs
@@ -13,17 +13,25 @@ namespace ExtensionsMetadataGenerator.Console
     {
         public static void Main(string[] args)
         {
-            if (args.Length < 2)
+            try
             {
-                System.Console.WriteLine("Usage: ");
-                System.Console.WriteLine("metadatagen <sourcepath> <output>");
+                if (args.Length < 2)
+                {
+                    System.Console.WriteLine("Usage: ");
+                    System.Console.WriteLine("metadatagen <sourcepath> <output>");
 
-                return;
+                    return;
+                }
+
+                string sourcePath = args[0];
+                AssemblyLoader.Initialize(sourcePath, Log);
+                ExtensionsMetadataGenerator.Generate(sourcePath, args[1], Log);
             }
-
-            string sourcePath = args[0];
-            AssemblyLoader.Initialize(sourcePath, Log);
-            ExtensionsMetadataGenerator.Generate(sourcePath, args[1], Log);
+            catch (Exception ex)
+            {
+                Log("Error generating extension metadata: " + ex.ToString());
+                throw;
+            }
         }
 
         private static void Log(string message)
@@ -41,32 +49,40 @@ namespace ExtensionsMetadataGenerator.Console
                 {
                     AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
                     {
-                        string assemblyName = new AssemblyName(args.Name).Name;
-                        string assemblyPath = Path.Combine(basePath, assemblyName + ".dll");
-
-                        if (File.Exists(assemblyPath))
-                        {
-                            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
-                            return assembly;
-                        }
-
-                        // If the assembly file is not found, it may be a runtime assembly for a different
-                        // runtime version (i.e. the Function app assembly targets .NET Core 2.2, yet this
-                        // process is running 2.0). In that case, just try to return the currently-loaded assembly,
-                        // even if it's the wrong version; we won't be running it, just reflecting.
                         try
                         {
-                            var assembly = Assembly.Load(assemblyName);
-                            return assembly;
-                        }
-                        catch (Exception exc)
-                        {
-                            // Log and continue. This will likely fail as the assembly won't be found, but we have a clear
-                            // message now that can help us diagnose.
-                            logger($"Unable to find fallback for assmebly `{assemblyName}`. {exc.GetType().Name} {exc.Message}");
-                        }
+                            string assemblyName = new AssemblyName(args.Name).Name;
+                            string assemblyPath = Path.Combine(basePath, assemblyName + ".dll");
 
-                        return null;
+                            if (File.Exists(assemblyPath))
+                            {
+                                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+                                return assembly;
+                            }
+
+                            // If the assembly file is not found, it may be a runtime assembly for a different
+                            // runtime version (i.e. the Function app assembly targets .NET Core 2.2, yet this
+                            // process is running 2.0). In that case, just try to return the currently-loaded assembly,
+                            // even if it's the wrong version; we won't be running it, just reflecting.
+                            try
+                            {
+                                var assembly = Assembly.Load(assemblyName);
+                                return assembly;
+                            }
+                            catch (Exception exc)
+                            {
+                                // Log and continue. This will likely fail as the assembly won't be found, but we have a clear
+                                // message now that can help us diagnose.
+                                logger($"Unable to find fallback for assmebly `{assemblyName}`. {exc.GetType().Name} {exc.Message}");
+                            }
+
+                            return null;
+                        }
+                        catch (Exception ex)
+                        {
+                            logger($"Error resolving assembly '{args.Name}': {ex}");
+                            throw;
+                        }
                     };
                 }
             }
