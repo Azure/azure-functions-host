4b0fd245aee820444901ab5d0529873d59d7e547
pragnagopa <pgopa@microsoft.com>
2019-04-29 09:24:59 -0700

    Update grpc dependencies

--- src/WebJobs.Script.Grpc/generate_protos.sh
+++ src/WebJobs.Script.Grpc/generate_protos.sh
@@ -49,9 +49,9 @@ else
 	NUGET_PATH=$NUGET_ROOT/packages
 fi
 
-GRPC_TOOLS_PATH=$NUGET_PATH/grpc.tools/1.18.0/tools/$PLATFORM
+GRPC_TOOLS_PATH=$NUGET_PATH/grpc.tools/1.20.1/tools/$PLATFORM
 PROTO_PATH=./azure-functions-language-worker-protobuf/src/proto
-PROTOBUF_TOOLS=$NUGET_PATH/google.protobuf.tools/3.6.1/tools
+PROTOBUF_TOOLS=$NUGET_PATH/google.protobuf.tools/3.7.0/tools
 PROTO=./azure-functions-language-worker-protobuf/src/proto/FunctionRpc.proto
 MSGDIR=./Messages
 
