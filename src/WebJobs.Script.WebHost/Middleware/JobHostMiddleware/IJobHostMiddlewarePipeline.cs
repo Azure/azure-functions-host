d02baf654c053def95d6b63e4970d81559181351
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-05-28 13:18:28 -0700

    Adding Job Host scroped middleware support

---
+++ src/WebJobs.Script.WebHost/Middleware/JobHostMiddleware/IJobHostMiddlewarePipeline.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Azure.WebJobs.Script.Middleware
+{
+    internal interface IJobHostMiddlewarePipeline
+    {
+        RequestDelegate Pipeline { get; }
+    }
+}
---
+++ src/WebJobs.Script.WebHost/Middleware/JobHostPipelineMiddleware.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Script.Middleware;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Middleware
+{
+    /// <summary>
+    /// A middleware responsible for the execution of the Job Host scoped middleware pipeline.
+    /// </summary>
+    internal class JobHostPipelineMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JobHostPipelineMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IJobHostMiddlewarePipeline middleware)
+        {
+            httpContext.Items.Add(ScriptConstants.JobHostMiddlewarePipelineRequestDelegate, _next);
+            await middleware.Pipeline(httpContext);
+        }
+    }
+}
