075d3ecfa69279c4644b657a5035c2ac6ef93b2e
pragnagopa <pgopa@microsoft.com>
2019-06-12 11:02:00 -0700

    Added more logging

--- src/WebJobs.Script/Rpc/LanguageWorkerChannel.cs
+++ src/WebJobs.Script/Rpc/LanguageWorkerChannel.cs
@@ -178,11 +178,13 @@ namespace Microsoft.Azure.WebJobs.Script.Rpc
 
             if (_isWebHostChannel)
             {
+                _workerChannelLogger.LogDebug("Publishing RpcWebHostChannelReadyEvent for runtime:{language}, workerId:{id}", _workerConfig.Language, _workerId);
                 RpcWebHostChannelReadyEvent readyEvent = new RpcWebHostChannelReadyEvent(_workerId, _runtime, this, _initMessage.WorkerVersion, _initMessage.Capabilities);
                 _eventManager.Publish(readyEvent);
             }
             else
             {
+                _workerChannelLogger.LogDebug("Publishing RpcJobHostChannelReadyEvent for runtime:{language}, workerId:{id}", _workerConfig.Language, _workerId);
                 RpcJobHostChannelReadyEvent readyEvent = new RpcJobHostChannelReadyEvent(_workerId, _runtime, this, _initMessage.WorkerVersion, _initMessage.Capabilities);
                 _eventManager.Publish(readyEvent);
             }
