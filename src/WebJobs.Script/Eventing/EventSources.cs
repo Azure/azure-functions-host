b23de41a27f97330773efefd3ac49b342f7c23ad
Yogesh <yojagad@microsoft.com>
2019-07-12 10:32:13 -0700

    Addressed code review comments

--- src/WebJobs.Script/Eventing/EventSources.cs
+++ src/WebJobs.Script/Eventing/EventSources.cs
@@ -1,6 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Microsoft.Azure.WebJobs.Script.Eventing
 {
     public static class EventSources
