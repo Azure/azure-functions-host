331c901452d1f5fc80d0093aa4da10f0df27d3bf
Brett Samblanet <brettsam@microsoft.com>
2019-09-12 17:54:58 -0700

    updating perf runs to handle v3

---
+++ tools/WebJobs.Script.Performance/WebJobs.Script.Performance.Dashboard/Options/V2PerformanceRunOptionsFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobs.Script.Tests.Perf.Dashboard.Options
+{
+    public class V2PerformanceRunOptionsFactory : IPerformanceRunOptionsFactory
+    {
+        private const string Branch = "dev";
+        private ILogger _log;
+
+        public V2PerformanceRunOptionsFactory(ILogger log)
+        {
+            _log = log;
+        }
+
+        public async Task<PerformanceRunOptions> CreateAsync()
+        {
+            var options = new PerformanceRunOptions();
+
+            using (var appVeyorClient = new AppVeyorClient(_log))
+            {
+                // Get latest private extension url from appvayor build
+                string lastSuccessfulVersion = await appVeyorClient.GetLastSuccessfulBuildVersionAsync(Branch, options.FunctionsHostSlug);
+                options.ExtensionUrl = await appVeyorClient.GetArtifactUrlAsync(lastSuccessfulVersion, options.FunctionsHostSlug, "Image: Visual Studio 2017", "inproc");
+                options.AppUrl = await appVeyorClient.GetArtifactUrlAsync(lastSuccessfulVersion, options.FunctionsHostSlug, "Image: Visual Studio 2017", "WebJobs.Script.Performance.App");
+            }
+
+            return options;
+        }
+    }
+}
--- tools/WebJobs.Script.Performance/WebJobs.Script.Performance.Dashboard/RunPerfHttp.cs
+++ tools/WebJobs.Script.Performance/WebJobs.Script.Performance.Dashboard/RunPerfHttp.cs
@@ -1,14 +1,12 @@
 
 using System;
-using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using WebJobs.Script.Tests.Perf.Dashboard.Options;
 
 namespace WebJobs.Script.Tests.Perf.Dashboard
 {
@@ -24,7 +22,27 @@ namespace WebJobs.Script.Tests.Perf.Dashboard
                 string testId = string.Empty;
                 req.GetQueryParameterDictionary().TryGetValue("testId", out testId);
 
-                await PerformanceManager.Execute(testId, log);
+                if (!req.GetQueryParameterDictionary().TryGetValue("version", out string version))
+                {
+                    return new BadRequestObjectResult("Specify 'version' of 'v2' or 'v3' on the query string.");
+                }
+
+                PerformanceRunOptions options = null;
+                switch (version.ToLowerInvariant())
+                {
+                    case "v2":
+                        IPerformanceRunOptionsFactory v2factory = new V2PerformanceRunOptionsFactory(log);
+                        options = await v2factory.CreateAsync();
+                        break;
+                    case "v3":
+                        IPerformanceRunOptionsFactory v3factory = new V3PerformanceRunOptionsFactory(log);
+                        options = await v3factory.CreateAsync();
+                        break;
+                    default:
+                        return new BadRequestObjectResult("Specify 'version' of 'v2' or 'v3' on the query string.");
+                }
+
+                await PerformanceManager.Execute(testId, options, log);
 
                 return new ContentResult()
                 {
