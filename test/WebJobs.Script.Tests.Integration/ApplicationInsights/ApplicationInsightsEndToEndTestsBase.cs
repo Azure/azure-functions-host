1d0ff1f2fb573a967bff222ca3e6d38b57a5e12b
Ankit Kumar <ankikuma@microsoft.com>
2019-09-23 11:53:43 -0700

    Temporarily adding logging to app insights test

--- test/WebJobs.Script.Tests.Integration/ApplicationInsights/ApplicationInsightsEndToEndTestsBase.cs
+++ test/WebJobs.Script.Tests.Integration/ApplicationInsights/ApplicationInsightsEndToEndTestsBase.cs
@@ -204,8 +204,19 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.ApplicationInsights
                    .OfType<RequestTelemetry>()
                    .Where(t => GetInvocationId(t) == invocationId)
                    .SingleOrDefault();
-
                 return requestTelemetry != null;
+            },
+            userMessageCallback: () =>
+            {
+                foreach (var tel in _fixture.Channel.Telemetries.OfType<RequestTelemetry>())
+                {
+                    TelemetryDebugWriter.WriteTelemetry(tel);
+                }
+                foreach (var tel in _fixture.Channel.Telemetries)
+                {
+                    TelemetryDebugWriter.WriteTelemetry(tel);
+                }
+                return "</ERROR>";
             });
 
             ValidateRequest(requestTelemetry, invocationId, functionName, "req", functionSuccess);
