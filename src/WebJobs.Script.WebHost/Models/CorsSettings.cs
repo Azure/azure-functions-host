929330bd97256e77d98234c96e13aac0b02f2ea3
Bala <balag@outlook.com>
2019-09-24 13:25:01 -0700

    Add CORS Specialization payload

---
+++ src/WebJobs.Script.WebHost/Models/CorsSettings.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Models
+{
+    public class CorsSettings
+    {
+        public IEnumerable<string> AllowedOrigins { get; set; }
+
+        public bool SupportCredentials { get; set; }
+    }
+}
