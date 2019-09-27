270656ebb6ad77e4921d87492f7319a74c13ab91
Brett Samblanet <brettsam@microsoft.com>
2019-09-27 07:38:50 -0700

    reducing ILogger messages in SecretManager

--- src/WebJobs.Script.WebHost/Security/DefaultSecretManagerFactory.cs
+++ src/WebJobs.Script.WebHost/Security/DefaultSecretManagerFactory.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Extensions.Logging;
@@ -15,6 +10,6 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
     public sealed class DefaultSecretManagerFactory : ISecretManagerFactory
     {
         public ISecretManager Create(ScriptSettingsManager settingsManager, TraceWriter traceWriter, ILoggerFactory loggerFactory, ISecretsRepository secretsRepository)
-            => new SecretManager(settingsManager, secretsRepository, traceWriter, loggerFactory);
+            => new SecretManager(settingsManager, secretsRepository, traceWriter);
     }
 }
