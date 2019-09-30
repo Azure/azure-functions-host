fb66d29b40efd33c31b1bf996be5815616556b8e
Brett Samblanet <brettsam@microsoft.com>
2019-09-30 14:44:03 -0700

    improving header test

--- test/WebJobs.Script.Tests/Middleware/CustomHttpHeadersMiddlewareTests.cs
+++ test/WebJobs.Script.Tests/Middleware/CustomHttpHeadersMiddlewareTests.cs
@@ -2,22 +2,17 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Azure.WebJobs.Script.Middleware;
 using Microsoft.Azure.WebJobs.Script.WebHost.Configuration;
 using Microsoft.Azure.WebJobs.Script.WebHost.Middleware;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -26,6 +21,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
     public class CustomHttpHeadersMiddlewareTests
     {
         private bool _nextInvoked = false;
+        private IWebHost _host;
 
         [Fact]
         public async Task Invoke_hasCustomHeaders_AddsResponseHeaders()
@@ -37,31 +33,42 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
             };
             var headerOptions = new OptionsWrapper<CustomHttpHeadersOptions>(headers);
 
-            HttpClient client = GetTestHttpClient(o =>
+            using (var host = GetTestHost(o =>
             {
                 o.Add("X-Content-Type-Options", "nosniff");
                 o.Add("Feature-Policy", "camera 'none'; geolocation 'none'");
-            });
-            HttpResponseMessage response = await client.GetAsync(string.Empty);
+            }))
+            {
+                await host.StartAsync();
+                HttpResponseMessage response = await host.GetTestClient().GetAsync(string.Empty);
+                await host.StopAsync();
 
-            Assert.True(_nextInvoked);
-            Assert.Equal(response.Headers.GetValues("X-Content-Type-Options").Single(), "nosniff");
-            Assert.Equal(response.Headers.GetValues("Feature-Policy").Single(), "camera 'none'; geolocation 'none'");
+                Assert.True(_nextInvoked);
+                Assert.Equal(response.Headers.GetValues("X-Content-Type-Options").Single(), "nosniff");
+                Assert.Equal(response.Headers.GetValues("Feature-Policy").Single(), "camera 'none'; geolocation 'none'");
+            }
         }
 
         [Fact]
         public async Task Invoke_noCustomHeaders_DoesNotAddResponseHeader()
         {
-            HttpResponseMessage response = await GetTestHttpClient().GetAsync(string.Empty);
-            Assert.True(_nextInvoked);
-            Assert.Equal(response.Headers.Count(), 0);
+            using (var host = GetTestHost())
+            {
+                await _host.StartAsync();
+                HttpResponseMessage response = await host.GetTestClient().GetAsync(string.Empty);
+                await _host.StopAsync();
+
+                Assert.True(_nextInvoked);
+                Assert.True(response.Headers.Count() == 0,
+                    $"Expected 0 headers. Actual: {string.Join(", ", response.Headers.Select(h => h.Key))}");
+            }
         }
 
-        private HttpClient GetTestHttpClient(Action<CustomHttpHeadersOptions> configureOptions = null)
+        private IWebHost GetTestHost(Action<CustomHttpHeadersOptions> configureOptions = null)
         {
             // The custom middleware relies on the host starting the request (thus invoking OnStarting),
             // so we need to create a test host to flow through the entire pipeline.
-            var host = new WebHostBuilder()
+            _host = new WebHostBuilder()
                 .UseTestServer()
                 .ConfigureServices(s =>
                 {
@@ -81,8 +88,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
                 })
                 .Build();
 
-            host.Start();
-            return host.GetTestClient();
+            return _host;
         }
     }
 }
