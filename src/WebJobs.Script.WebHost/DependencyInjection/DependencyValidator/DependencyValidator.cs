d7c761e24427df4bf4678a49d6a24d36770dca15
Yogesh <yojagad@microsoft.com>
2019-10-03 13:29:55 -0700

    Revert "Use single instance of WebhostMetricsLogger"
    This reverts commit e9d2df354f4f19c35158d77f3964e01717e9256c.

--- src/WebJobs.Script.WebHost/DependencyInjection/DependencyValidator/DependencyValidator.cs
+++ src/WebJobs.Script.WebHost/DependencyInjection/DependencyValidator/DependencyValidator.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.DependencyInjection
             expected.ExpectNone<IEventGenerator>();
 
             expected.Expect<ILoggerFactory, ScriptLoggerFactory>();
-            expected.ExpectInstance<IMetricsLogger, WebHostMetricsLogger>();
+            expected.Expect<IMetricsLogger, WebHostMetricsLogger>();
 
             expected.Expect<IWebJobsExceptionHandler, WebScriptHostExceptionHandler>();
 
