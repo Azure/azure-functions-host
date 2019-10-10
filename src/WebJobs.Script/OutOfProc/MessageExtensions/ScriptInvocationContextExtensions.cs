736c6fd4ca62c0c4e3ad78f16e3e526fa2ae44a8
Yogesh <yojagad@microsoft.com>
2019-10-09 18:50:44 -0700

    Addressed case where there can be duplicate tags.

--- src/WebJobs.Script/OutOfProc/MessageExtensions/ScriptInvocationContextExtensions.cs
+++ src/WebJobs.Script/OutOfProc/MessageExtensions/ScriptInvocationContextExtensions.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
-using Microsoft.Azure.WebJobs.Script.OutOfProc;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Script.Rpc
@@ -61,7 +60,7 @@ namespace Microsoft.Azure.WebJobs.Script.Rpc
                 }
                 else
                 {
-                    traceContext.Attributes.Add(tag.Key, tag.Value);
+                    traceContext.Attributes[tag.Key] = tag.Value;
                 }
             }
 
