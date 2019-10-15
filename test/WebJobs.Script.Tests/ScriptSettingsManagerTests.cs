9203bc3ccb33fabcbbba2b0c7da7bc42bffd245d
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-10-15 10:18:22 -0700

    Removing slotname from settings manager

--- test/WebJobs.Script.Tests/ScriptSettingsManagerTests.cs
+++ test/WebJobs.Script.Tests/ScriptSettingsManagerTests.cs
@@ -14,29 +14,6 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 {
     public class ScriptSettingsManagerTests
     {
-        [Theory]
-        [InlineData("testsite", "production", "testsite")]
-        [InlineData("testsite", "Production", "testsite")]
-        [InlineData("testsite", null, "testsite")]
-        [InlineData("testsite", "staging", "testsite-staging")]
-        [InlineData("testsite", "dev", "testsite-dev")]
-        [InlineData("TestSite", "Dev", "testsite-dev")]
-        public void UniqueSlotName_ReturnsExpectedValue(string siteName, string slotName, string expectedValue)
-        {
-            var settingsManager = ScriptSettingsManager.Instance;
-
-            var variables = new Dictionary<string, string>
-            {
-                { EnvironmentSettingNames.AzureWebsiteName, siteName },
-                { EnvironmentSettingNames.AzureWebsiteSlotName, slotName },
-            };
-
-            using (var tempVariables = new TestScopedEnvironmentVariable(variables))
-            {
-                Assert.Equal(expectedValue, ScriptSettingsManager.Instance.AzureWebsiteUniqueSlotName);
-            }
-        }
-
         [Fact]
         public void SettingsAreNotCached()
         {
