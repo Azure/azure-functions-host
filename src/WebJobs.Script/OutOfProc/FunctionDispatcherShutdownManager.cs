ce4513f6bfd81b9bc5a9ae9ef9e7c920a6a10814
Yogesh <yojagad@microsoft.com>
2019-10-18 13:49:08 -0700

    Added logger

--- src/WebJobs.Script/OutOfProc/FunctionDispatcherShutdownManager.cs
+++ src/WebJobs.Script/OutOfProc/FunctionDispatcherShutdownManager.cs
@@ -4,16 +4,19 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Script.Rpc
 {
     internal sealed class FunctionDispatcherShutdownManager : IHostedService
     {
-        private IFunctionDispatcher _functionDispatcher;
+        private readonly IFunctionDispatcher _functionDispatcher;
+        private readonly ILogger _logger;
 
-        public FunctionDispatcherShutdownManager(IFunctionDispatcher functionDispatcher)
+        public FunctionDispatcherShutdownManager(IFunctionDispatcher functionDispatcher, ILogger<FunctionDispatcherShutdownManager> logger)
         {
             _functionDispatcher = functionDispatcher;
+            _logger = logger;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -23,7 +26,16 @@ namespace Microsoft.Azure.WebJobs.Script.Rpc
 
         public async Task StopAsync(CancellationToken cancellationToken)
         {
-            await Task.WhenAny(Task.Delay(5000), _functionDispatcher.ShutdownAsync());
+            Task timeoutTask = Task.Delay(5000);
+            Task completedTask = await Task.WhenAny(timeoutTask, _functionDispatcher.ShutdownAsync());
+            if (completedTask.Equals(timeoutTask))
+            {
+                _logger.LogDebug("FunctionDispatcher StopAsync() completing due to timeout");
+            }
+            else
+            {
+                _logger.LogDebug("FunctionDispatcher StopAsync() completing due to completion of in-buffer tasks");
+            }
         }
     }
 }
