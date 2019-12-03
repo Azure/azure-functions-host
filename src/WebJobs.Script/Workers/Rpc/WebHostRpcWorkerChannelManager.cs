fa022c4f66bc5b82aedaefed11f04157c6e33bc0
mhoeger <marie.hoeger@gmail.com>
2019-12-03 15:58:40 -0800

    do not use placeholder worker if ~3 and v2 compatability enabled

--- src/WebJobs.Script/Workers/Rpc/WebHostRpcWorkerChannelManager.cs
+++ src/WebJobs.Script/Workers/Rpc/WebHostRpcWorkerChannelManager.cs
@@ -133,10 +133,12 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc
             if (!string.IsNullOrEmpty(workerRuntime))
             {
                 // Special case: node apps must be read-only to use the placeholder mode channel
+                // Also cannot use placeholder worker that is targeting ~3 but has backwards compatibility with V2 enabled
                 // TODO: Remove special casing when resolving https://github.com/Azure/azure-functions-host/issues/4534
                 if (string.Equals(workerRuntime, RpcWorkerConstants.NodeLanguageWorkerName, StringComparison.OrdinalIgnoreCase))
                 {
-                    return _environment.IsFileSystemReadOnly();
+                    // Use if readonly and not v2 compatible on ~3 extension
+                    return _environment.IsFileSystemReadOnly() && !_environment.IsV2CompatabileOnV3Extension();
                 }
                 return true;
             }
