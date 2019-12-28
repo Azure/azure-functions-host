a8d43ce667ae9a081196530dcd6cbe15a98d35fd
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-12-27 17:44:11 -0800

    Resolving issue with native dependencies collision

---
+++ src/WebJobs.Script/Description/DotNet/RuntimeAsset.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Diagnostics;
+
+namespace Microsoft.Azure.WebJobs.Script.Description
+{
+    [DebuggerDisplay("{" + nameof(Display) + ",nq}")]
+    public class RuntimeAsset
+    {
+        public RuntimeAsset(string rid, string path)
+        {
+            Rid = rid;
+            Path = path;
+        }
+
+        public string Rid { get; }
+
+        public string Path { get; }
+
+        private string Display => $"({Rid ?? "no RID"}) - {Path}";
+    }
+}
