5c4c3f298d28b4d354a2ebce9fe1cd1071621756
Brett Samblanet <brettsam@microsoft.com>
2020-01-15 08:26:33 -0800

    adding support for IWebJobsConfigurationStartup

---
+++ src/WebJobs.Script/DependencyInjection/ExternalConfigurationStartupValidator.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Script.DependencyInjection
+{
+    internal class ExternalConfigurationStartupValidator : IHostedService
+    {
+        private readonly IConfigurationRoot _originalConfig;
+        private readonly IConfiguration _config;
+        private readonly IFunctionMetadataManager _metadataManager;
+        private readonly ILogger<ExternalConfigurationStartupValidator> _logger;
+
+        private static readonly IDictionary<string, KnownTrigger> _knownTriggers = new Dictionary<string, KnownTrigger>
+        {
+            { "blobTrigger", new KnownTrigger("connection", "AzureWebJobsStorage") },
+            { "queueTrigger", new KnownTrigger("connection", "AzureWebJobsStorage") },
+            { "cosmosDBTrigger", new KnownTrigger("connectionStringSetting", "ConnectionStrings:CosmosDB") }
+            // TODO: the rest
+        };
+
+        public ExternalConfigurationStartupValidator(IConfigurationRoot originalConfig, IConfiguration config, IFunctionMetadataManager metadataManager, ILogger<ExternalConfigurationStartupValidator> logger)
+        {
+            _originalConfig = originalConfig;
+            _config = config;
+            _metadataManager = metadataManager;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var functions = _metadataManager.Functions;
+
+            foreach (var function in functions)
+            {
+                var trigger = function.Bindings.SingleOrDefault(b => b.IsTrigger);
+                if (_knownTriggers.TryGetValue(trigger?.Type, out KnownTrigger details))
+                {
+                    // If connection is used, you cannot override it.
+                    string connection = trigger.Raw[details.ConnectionSettingName]?.ToString() ?? details.DefaultConnectionSetting;
+
+                    if (!string.IsNullOrEmpty(connection))
+                    {
+                        string originalKey = _originalConfig[connection];
+                        string key = _config[connection];
+                        if (originalKey != key)
+                        {
+                            _logger.LogWarning($"The configuration value '{connection}' was changed in an external startup class. The Functions scale controller may not scale the function '{function.Name}' correctly.");
+                        }
+                    }
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private class KnownTrigger
+        {
+            public KnownTrigger(string connectionSettingName, string defaultConnectionSetting)
+            {
+                ConnectionSettingName = connectionSettingName;
+                DefaultConnectionSetting = defaultConnectionSetting;
+            }
+
+            /// <summary>
+            /// Gets the default connection key ("AzureWebJobsStorage", "AzureWebJobsCosmosDb") for the binding.
+            /// </summary>
+            public string DefaultConnectionSetting { get; }
+
+            /// <summary>
+            /// Gets the connection key ("connection", "connectionStringSetting", etc) for the binding.
+            /// </summary>
+            public string ConnectionSettingName { get; }
+        }
+    }
+}
--- src/WebJobs.Script/ScriptHostBuilderExtensions.cs
+++ src/WebJobs.Script/ScriptHostBuilderExtensions.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Azure.WebJobs.Logging.ApplicationInsights;
 using Microsoft.Azure.WebJobs.Script.Abstractions;
@@ -42,6 +43,11 @@ namespace Microsoft.Azure.WebJobs.Script
 {
     public static class ScriptHostBuilderExtensions
     {
+        private const string BundleManagerKey = "MS_BundleManager";
+        private const string StartupTypeLocatorKey = "MS_StartupTypeLocator";
+        private const string DelayedConfigurationActionKey = "MS_DelayedConfigurationAction";
+        private const string ConfigurationSnapshotKey = "MS_ConfigurationSnapshot";
+
         public static IHostBuilder AddScriptHost(this IHostBuilder builder, Action<ScriptApplicationHostOptions> configureOptions, ILoggerFactory loggerFactory = null)
         {
             if (configureOptions == null)
@@ -96,12 +102,61 @@ namespace Microsoft.Azure.WebJobs.Script
             });
 
             // WebJobs configuration
-            return builder.AddScriptHostCore(applicationOptions, metricsLogger, configureWebJobs, loggerFactory);
+            builder.AddScriptHostCore(applicationOptions, configureWebJobs, loggerFactory);
+
+            // Allow FunctionsStartup to add configuration after all other configuration is registered.
+            builder.ConfigureAppConfiguration((context, configBuilder) =>
+            {
+                // Pre-build configuration here to load bundles and to store for later validation.
+                var config = configBuilder.Build();
+                var extensionBundleOptions = GetExtensionBundleOptions(config);
+                var bundleManager = new ExtensionBundleManager(extensionBundleOptions, SystemEnvironment.Instance, loggerFactory);
+                var metadataServiceProvider = applicationOptions.RootServiceProvider.GetService<IFunctionMetadataProvider>();
+                var locator = new ScriptStartupTypeLocator(applicationOptions.ScriptPath, loggerFactory.CreateLogger<ScriptStartupTypeLocator>(), bundleManager, metadataServiceProvider, metricsLogger);
+
+                // The locator (and thus the bundle manager) need to be created now in order to configure app configuration.
+                // Store them so they do not need to be re-created later when configuring services.
+                context.Properties[BundleManagerKey] = bundleManager;
+                context.Properties[StartupTypeLocatorKey] = locator;
+
+                if (context.Properties[DelayedConfigurationActionKey] is Action<IWebJobsConfigurationStartupTypeLocator> delayedConfigAction)
+                {
+                    context.Properties.Remove(DelayedConfigurationActionKey);
+
+                    // store the snapshot for validation later
+                    context.Properties[ConfigurationSnapshotKey] = config;
+
+                    delayedConfigAction(locator);
+                }
+            });
+
+            return builder;
         }
 
-        public static IHostBuilder AddScriptHostCore(this IHostBuilder builder, ScriptApplicationHostOptions applicationHostOptions, IMetricsLogger metricsLogger, Action<IWebJobsBuilder> configureWebJobs = null, ILoggerFactory loggerFactory = null)
+        public static IHostBuilder AddScriptHostCore(this IHostBuilder builder, ScriptApplicationHostOptions applicationHostOptions, Action<IWebJobsBuilder> configureWebJobs = null, ILoggerFactory loggerFactory = null)
         {
             var skipHostInitialization = builder.Properties.ContainsKey(ScriptConstants.SkipHostInitializationKey);
+
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<IHostedService>(s =>
+                {
+                    var environment = s.GetService<IEnvironment>();
+                    var originalConfig = context.Properties.GetAndRemove<IConfigurationRoot>(ConfigurationSnapshotKey);
+
+                    if (environment.IsWindowsConsumption() || environment.IsCoreTools())
+                    {
+                        var config = s.GetService<IConfiguration>();
+                        var metadataManager = s.GetService<IFunctionMetadataManager>();
+                        var logger = s.GetService<ILoggerFactory>().CreateLogger<ExternalConfigurationStartupValidator>();
+
+                        return new ExternalConfigurationStartupValidator(originalConfig, config, metadataManager, logger);
+                    }
+
+                    return NullHostedService.Instance;
+                });
+            });
+
             builder.ConfigureWebJobs((context, webJobsBuilder) =>
             {
                 // Built in binding registrations
@@ -114,18 +169,27 @@ namespace Microsoft.Azure.WebJobs.Script
                 .AddManualTrigger()
                 .AddWarmup();
 
-                var extensionBundleOptions = GetExtensionBundleOptions(context);
-                var bundleManager = new ExtensionBundleManager(extensionBundleOptions, SystemEnvironment.Instance, loggerFactory);
+                var bundleManager = context.Properties.GetAndRemove<IExtensionBundleManager>(BundleManagerKey);
                 webJobsBuilder.Services.AddSingleton<IExtensionBundleManager>(_ => bundleManager);
+
                 if (!skipHostInitialization)
                 {
                     // Only set our external startup if we're not suppressing host initialization
                     // as we don't want to load user assemblies otherwise.
-                    webJobsBuilder.UseScriptExternalStartup(applicationHostOptions, loggerFactory, bundleManager, metricsLogger);
+                    var locator = context.Properties.GetAndRemove<ScriptStartupTypeLocator>(StartupTypeLocatorKey);
+                    webJobsBuilder.UseExternalStartup(locator);
                 }
 
                 configureWebJobs?.Invoke(webJobsBuilder);
-            }, o => o.AllowPartialHostStartup = true);
+            }, o => o.AllowPartialHostStartup = true,
+            (context, webJobsConfigBuilder) =>
+            {
+                if (!skipHostInitialization)
+                {
+                    // Delay this call so we can call the customer's setup last.
+                    context.Properties[DelayedConfigurationActionKey] = new Action<IWebJobsConfigurationStartupTypeLocator>(locator => webJobsConfigBuilder.UseExternalConfigurationStartup(locator));
+                }
+            });
 
             // Script host services - these services are scoped to a host instance, and when a new host
             // is created, these services are recreated
@@ -235,11 +299,9 @@ namespace Microsoft.Azure.WebJobs.Script
             AddProcessRegistry(services);
         }
 
-        public static IWebJobsBuilder UseScriptExternalStartup(this IWebJobsBuilder builder, ScriptApplicationHostOptions applicationHostOptions, ILoggerFactory loggerFactory, IExtensionBundleManager extensionBundleManager, IMetricsLogger metricsLogger)
+        public static IWebJobsBuilder UseScriptExternalStartup(this IWebJobsBuilder builder, ScriptStartupTypeLocator locator)
         {
-            var logger = loggerFactory?.CreateLogger<ScriptStartupTypeLocator>() ?? throw new ArgumentNullException(nameof(loggerFactory));
-            var functionMetadataManager = applicationHostOptions.RootServiceProvider.GetService<IFunctionMetadataManager>();
-            return builder.UseExternalStartup(new ScriptStartupTypeLocator(applicationHostOptions.ScriptPath, logger, extensionBundleManager, functionMetadataManager, metricsLogger), loggerFactory);
+            return builder.UseExternalStartup(locator);
         }
 
         public static IHostBuilder SetAzureFunctionsEnvironment(this IHostBuilder builder)
@@ -313,11 +375,11 @@ namespace Microsoft.Azure.WebJobs.Script
             }
         }
 
-        internal static ExtensionBundleOptions GetExtensionBundleOptions(HostBuilderContext context)
+        internal static ExtensionBundleOptions GetExtensionBundleOptions(IConfiguration configuration)
         {
             var options = new ExtensionBundleOptions();
-            var optionsSetup = new ExtensionBundleConfigurationHelper(context.Configuration, SystemEnvironment.Instance);
-            context.Configuration.Bind(options);
+            var optionsSetup = new ExtensionBundleConfigurationHelper(configuration, SystemEnvironment.Instance);
+            configuration.Bind(options);
             optionsSetup.Configure(options);
             return options;
         }
@@ -345,6 +407,34 @@ namespace Microsoft.Azure.WebJobs.Script
             else
             {
                 services.AddSingleton<IProcessRegistry, EmptyProcessRegistry>();
+            }
+        }
+
+        /// <summary>
+        /// Gets and removes the specified value, if it exists and is of type T.
+        /// Throws an InvalidOperationException if the key does not exist or is not of type T.
+        /// </summary>
+        /// <typeparam name="T">The expected type of the value.</typeparam>
+        /// <param name="dictionary">The dictinoary.</param>
+        /// <param name="key">They key to remove and return.</param>
+        /// <returns>The value.</returns>
+        private static T GetAndRemove<T>(this IDictionary<object, object> dictionary, string key) where T : class
+        {
+            if (dictionary.TryGetValue(key, out object valueObj))
+            {
+                if (valueObj is T value)
+                {
+                    dictionary.Remove(key);
+                    return value;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"The key '{key}' exists in the dictionary but is type '{valueObj.GetType()}' instead of type '{typeof(T)}'.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"The key '{key}' does not exist in the dictionary.");
             }
         }
     }
