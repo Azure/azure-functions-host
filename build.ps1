ab211a53ada7abdefb1569b14d52519799c5f80f
yojagad <yojagad@microsoft.com>
2020-03-04 17:32:03 -0800

    Testing again

--- build.ps1
+++ build.ps1
@@ -10,9 +10,7 @@ if ($includeSuffix)
 }
 
 $pullRequestNumber = $env:APPVEYOR_PULL_REQUEST_NUMBER + $env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
-$buildArtifacts = $(BuildArtifacts)
 Write-Host "Pull request number: $pullRequestNumber"
-Write-Host "Build Artifacts: $buildArtifacts"
 
 $currentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $buildOutput = Join-Path $currentDir "buildoutput"
