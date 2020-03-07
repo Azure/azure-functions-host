9baa04ac8997a94a1c9cb21028f1eb156e99604a
Marie Hoeger <mahoeger@microsoft.com>
2020-03-06 16:50:38 -0800

    update test

--- test/WebJobs.Script.Tests/Binding/ActionResults/RawScriptResultTests.cs
+++ test/WebJobs.Script.Tests/Binding/ActionResults/RawScriptResultTests.cs
@@ -83,7 +83,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Binding.ActionResults
             await result.ExecuteResultAsync(context);
             context.HttpContext.Response.Headers.TryGetValue("Set-Cookie", out StringValues cookies);
 
-            Assert.Equal(2, cookies.Count);
+            Assert.Equal(3, cookies.Count);
             Assert.Equal("firstCookie=cookieValue; path=/; samesite=lax", cookies[0]);
             Assert.Equal("secondCookie=cookieValue2; max-age=20; path=/; httponly", cookies[1]);
             Assert.Equal("thirdCookie=cookieValue3; path=/; samesite=none", cookies[2]);
