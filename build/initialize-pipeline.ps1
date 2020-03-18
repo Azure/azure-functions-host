81bc1e5792f4c45393a9a72ad072fd5815a41544
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-03-18 08:23:18 -0700

    Moving v2 build to devops (#5781)
    Moving v2 build to devops.

---
+++ build/initialize-pipeline.ps1
@@ -0,0 +1,23 @@
+$buildReason = $env:BUILD_REASON
+$sourceBranch = $env:BUILD_SOURCEBRANCH
+$bypassPackaging = $true
+$includeSuffix = $true
+Write-Host "SourceBranch: $sourceBranch, Build reason: $buildReason"
+
+if($sourceBranch.endsWith('master') -and ($buildReason -ne "PullRequest"))
+{
+  $includeSuffix = $false
+  $bypassPackaging = $false
+}
+elseif($buildReason -eq "PullRequest")
+{
+  $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+  $title = $response.title.ToLowerInvariant()
+  if ($title.Contains("[pack]")) {
+    $bypassPackaging = $false
+  }
+}
+
+# Write to output
+"##vso[task.setvariable variable=IncludeSuffix;isOutput=true]$includeSuffix"
+"##vso[task.setvariable variable=BypassPackaging;isOutput=true]$bypassPackaging"
