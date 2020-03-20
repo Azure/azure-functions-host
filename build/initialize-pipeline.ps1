67e1b954376fe99c31e61e57bbdb8a0e7dcf7dc8
yojagad <yojagad@microsoft.com>
2020-03-20 13:04:09 -0700

    yml change

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -13,13 +13,12 @@ elseif($buildReason -eq "PullRequest")
 {
   $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
   $title = $response.title.ToLowerInvariant()
-  Write-Host "Title: $title"
   if ($title.Contains("[pack]")) {
     $bypassPackaging = $false
   }
 }
 
-Write-Host "bypassPackaging: $bypassPackaging"
+Write-Host "BypassPackaging: $bypassPackaging, IncludeSuffix: $includeSuffix"
 
 # Write to output
 "##vso[task.setvariable variable=IncludeSuffix;isOutput=true]$includeSuffix"
