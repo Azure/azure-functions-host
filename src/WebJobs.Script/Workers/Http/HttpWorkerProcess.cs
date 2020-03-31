f3641cceac4eef3aa4c49cad4667ef3a2eaad374
Pragna Gopa <pgopa@microsoft.com>
2020-03-30 17:28:41 -0700

    [HttpWorker][LinuxConsumption]Give executable permissions to user files (#5834)

--- src/WebJobs.Script/Workers/Http/HttpWorkerProcess.cs
+++ src/WebJobs.Script/Workers/Http/HttpWorkerProcess.cs
@@ -5,6 +5,7 @@ using System;
 using System.Diagnostics;
 using Microsoft.Azure.WebJobs.Script.Eventing;
 using Microsoft.Extensions.Logging;
+using Mono.Unix;
 
 namespace Microsoft.Azure.WebJobs.Script.Workers.Http
 {
@@ -17,6 +18,7 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Http
         private readonly string _scriptRootPath;
         private readonly string _workerId;
         private readonly WorkerProcessArguments _workerProcessArguments;
+        private readonly IEnvironment _environment;
 
         internal HttpWorkerProcess(string workerId,
                                        string rootScriptPath,
@@ -25,7 +27,8 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Http
                                        IWorkerProcessFactory processFactory,
                                        IProcessRegistry processRegistry,
                                        ILogger workerProcessLogger,
-                                       IWorkerConsoleLogSource consoleLogSource)
+                                       IWorkerConsoleLogSource consoleLogSource,
+                                       IEnvironment environment)
             : base(eventManager, processRegistry, workerProcessLogger, consoleLogSource)
         {
             _processFactory = processFactory;
@@ -35,6 +38,7 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Http
             _scriptRootPath = rootScriptPath;
             _httpWorkerOptions = httpWorkerOptions;
             _workerProcessArguments = _httpWorkerOptions.Arguments;
+            _environment = environment;
         }
 
         internal override Process CreateWorkerProcess()
@@ -49,7 +53,31 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Http
             };
             workerContext.EnvironmentVariables.Add(HttpWorkerConstants.PortEnvVarName, _httpWorkerOptions.Port.ToString());
             workerContext.EnvironmentVariables.Add(HttpWorkerConstants.WorkerIdEnvVarName, _workerId);
-            return _processFactory.CreateWorkerProcess(workerContext);
+            Process workerProcess = _processFactory.CreateWorkerProcess(workerContext);
+            if (_environment.IsLinuxConsumption())
+            {
+                AssignUserExecutePermissionsIfNotExists(workerProcess.StartInfo.FileName);
+            }
+            return workerProcess;
+        }
+
+        private void AssignUserExecutePermissionsIfNotExists(string filePath)
+        {
+            try
+            {
+                UnixFileInfo fileInfo = new UnixFileInfo(filePath);
+                if (!fileInfo.FileAccessPermissions.HasFlag(FileAccessPermissions.UserExecute))
+                {
+                    _workerProcessLogger.LogDebug("Assigning execute permissions to file: {filePath}", filePath);
+                    fileInfo.FileAccessPermissions |= FileAccessPermissions.UserExecute |
+                                                      FileAccessPermissions.GroupExecute |
+                                                      FileAccessPermissions.OtherExecute;
+                }
+            }
+            catch (Exception ex)
+            {
+                _workerProcessLogger.LogWarning(ex, "Error while assigning execute permission.");
+            }
         }
 
         internal override void HandleWorkerProcessExitError(WorkerProcessExitException httpWorkerProcessExitException)
