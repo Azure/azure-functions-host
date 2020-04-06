180e5333cee1cad9fb8f2bb3ad93f1535b7746ee
Brett Samblanet <brettsam@microsoft.com>
2020-04-06 13:51:25 -0700

    protecting against IOExceptions when flushing FileWriter

---
+++ src/WebJobs.Script/Diagnostics/DefaultFileWriterFactory.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.WebJobs.Script
+{
+    internal class DefaultFileWriterFactory : IFileWriterFactory
+    {
+        /// <summary>
+        /// Creates an IFileWriter. The caller is responsible for disposing of the IFileWriter.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <returns>An IFileWriter.</returns>
+        public IFileWriter Create(string filePath) => new FileWriter(filePath);
+    }
+}
