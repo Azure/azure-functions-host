dbaea16faa94a7b38b33529482990d083faeeaeb
Alexey Rodionov <alrod@microsoft.com>
2020-04-13 13:32:24 -0700

    Use DevOps output artifacts instead of AppVeyor for perf tests. Fixes #5850.

--- tools/WebJobs.Script.Performance/WebJobs.Script.Performance.Dashboard/Options/V2PerformanceRunOptionsFactory.cs
+++ tools/WebJobs.Script.Performance/WebJobs.Script.Performance.Dashboard/Options/V2PerformanceRunOptionsFactory.cs
@@ -5,7 +5,7 @@ namespace WebJobs.Script.Tests.Perf.Dashboard.Options
 {
     public class V2PerformanceRunOptionsFactory : IPerformanceRunOptionsFactory
     {
-        private const string Branch = "dev";
+        private const string Branch = "refs/heads/v2.x";
         private ILogger _log;
 
         public V2PerformanceRunOptionsFactory(ILogger log)
@@ -17,13 +17,10 @@ namespace WebJobs.Script.Tests.Perf.Dashboard.Options
         {
             var options = new PerformanceRunOptions();
 
-            using (var appVeyorClient = new AppVeyorClient(_log))
-            {
-                // Get latest private extension url from appvayor build
-                string lastSuccessfulVersion = await appVeyorClient.GetLastSuccessfulBuildVersionAsync(Branch, options.FunctionsHostSlug);
-                options.ExtensionUrl = await appVeyorClient.GetArtifactUrlAsync(lastSuccessfulVersion, options.FunctionsHostSlug, "Image: Visual Studio 2017", "inproc");
-                options.AppUrl = await appVeyorClient.GetArtifactUrlAsync(lastSuccessfulVersion, options.FunctionsHostSlug, "Image: Visual Studio 2017", "WebJobs.Script.Performance.App");
-            }
+            var devOpsClient = new DevOpsClient(_log);
+            var artifactResult = await devOpsClient.GetArtifacts(Branch, options.DevOpsAccessToken);
+            options.ExtensionUrl = artifactResult.ExtensionUrl;
+            options.AppUrl = artifactResult.AppUrl;
 
             return options;
         }
