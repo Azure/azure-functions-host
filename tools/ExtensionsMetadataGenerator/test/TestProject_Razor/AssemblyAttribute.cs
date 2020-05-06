cd7d740bcc572ef4473ec39bd9d90d63aa0e2e1a
yojagad <yojagad@microsoft.com>
2020-05-06 10:41:51 -0700

    Initial commit

--- tools/ExtensionsMetadataGenerator/test/TestProject_Razor/AssemblyAttribute.cs
+++
@@ -1,3 +0,0 @@
-﻿// This attribute will not be resolve-able because the Razor.Runtime assembly will not be
-// present in the output directory. This is used by tests to ensure we don't crash.
-[assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyName("something", "something")]
