a1181bdcd6b3f98aecd902d4b365230820ea2c84
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-06-10 11:55:56 -0700

    Fix proxy behavior. (#6204)
    Fix proxy behavior.

--- test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_HttpWorker.cs
+++ test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_HttpWorker.cs
@@ -34,6 +34,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
             await InvokeHttpTrigger("HttpTrigger");
         }
 
+        [Fact]
+        public async Task ProxyGetSucceeds()
+        {
+            await InvokeProxy();
+        }
+
         private async Task InvokeHttpTrigger(string functionName)
         {
             string functionKey = await _fixture.Host.GetFunctionSecretAsync($"{functionName}");
@@ -47,6 +53,18 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
             string responseContent = await response.Content.ReadAsStringAsync();
             JObject res = JObject.Parse(responseContent);
             Assert.True(res["functionName"].ToString().StartsWith(functionName));
+        }
+
+        private async Task InvokeProxy()
+        {
+            string uri = "something";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+
+            var response = await _fixture.Host.HttpClient.SendAsync(request);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(responseContent, uri);
         }
 
         public class TestFixture : EndToEndTestFixture
