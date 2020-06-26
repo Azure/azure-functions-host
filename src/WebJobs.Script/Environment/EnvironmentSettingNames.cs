a6133938e92e4f60fd4b6c336b99c7f07f680fe9
Mathew Charles <mathewc@microsoft.com>
2020-06-26 10:43:09 -0700

    Stop inlining large TestData (#5647)

--- src/WebJobs.Script/Environment/EnvironmentSettingNames.cs
+++ src/WebJobs.Script/Environment/EnvironmentSettingNames.cs
@@ -52,6 +52,7 @@ namespace Microsoft.Azure.WebJobs.Script
         public const string CloudName = "WEBSITE_CLOUD_NAME";
         public const string RoleInstanceId = "RoleInstanceId";
         public const string HealthPingEnabled = "WEBSITE_FUNCTIONS_HEALTH_PING_ENABLED";
+        public const string TestDataCapEnabled = "WEBSITE_FUNCTIONS_TESTDATA_CAP_ENABLED";
 
         /// <summary>
         /// Environment variable dynamically set by the platform when it is safe to
