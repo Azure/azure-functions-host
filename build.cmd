7a28f1cd9d0898236f13ec8f98542e5a42757af3
Brett Samblanet <brettsam@microsoft.com>
2020-06-29 12:28:45 -0700

    fixing pipelines build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @ECHO Off
-REM call dotnet --version
+REM call dotnet --info
 call dotnet restore WebJobs.Script.sln
 call dotnet build WebJobs.Script.sln
 REM call dotnet test WebJobs.Script.sln --no-build
