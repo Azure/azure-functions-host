857338c5d9e8ea7ccc425cf0acb67d05e2b7f8d6
Pragna Gopa <pgopa@microsoft.com>
2020-06-30 11:56:01 -0700

    Use CancellationToken during ScriptHost Initialization (#6265) (#6271)

--- src/WebJobs.Script/Workers/Http/HttpFunctionInvocationDispatcher.cs
+++ src/WebJobs.Script/Workers/Http/HttpFunctionInvocationDispatcher.cs
@@ -84,6 +84,8 @@ namespace Microsoft.Azure.WebJobs.Script.Workers
 
         public async Task InitializeAsync(IEnumerable<FunctionMetadata> functions, CancellationToken cancellationToken = default)
         {
+            cancellationToken.ThrowIfCancellationRequested();
+
             if (functions == null || !functions.Any())
             {
                 // do not initialize function dispachter if there are no functions
