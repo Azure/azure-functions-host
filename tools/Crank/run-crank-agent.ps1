6afec18dcf4f16f21c877e574f59fed1ace385b8
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-08 13:40:12 -0700

    Exclude crank-agent.exe

--- tools/Crank/run-crank-agent.ps1
+++ tools/Crank/run-crank-agent.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = 'Stop'
 
 Set-MpPreference -DisableRealtimeMonitoring $true
 Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
+Add-MpPreference -ExclusionProcess 'crank-agent.exe'
 
 crank-agent
