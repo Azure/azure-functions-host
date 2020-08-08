72db74085361b74c3b025d73886a3fa714f06faf
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-08 13:25:46 -0700

    Schedule crank-agent start

--- tools/Crank/setup-crank-agent.ps1
+++ tools/Crank/setup-crank-agent.ps1
@@ -1,4 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
+function ScheduleCrankAgentStart {
+    $action = New-ScheduledTaskAction -Execute 'crank-agent'
+    $trigger = New-ScheduledTaskTrigger -AtStartup
+
+    Register-ScheduledTask `
+        -TaskName "CrankAgent" -Description "Start crank-agent" `
+        -Action $action -Trigger $trigger `
+        -User 'Functions'
+}
+
 Invoke-WebRequest 'https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'
 ./dotnet-install.ps1
 
 dotnet tool install -g Microsoft.Crank.Agent --version "0.1.0-*"
+
+ScheduleCrankAgentStart
