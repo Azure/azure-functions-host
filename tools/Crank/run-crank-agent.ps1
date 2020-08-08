0b89d6fbd39503bc832c54f5b041ce67e4fc8d47
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-08 00:28:49 -0700

    Don't stop WinDefend service

--- tools/Crank/run-crank-agent.ps1
+++ tools/Crank/run-crank-agent.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
 Set-MpPreference -DisableRealtimeMonitoring $true
-if (Get-Service WinDefend) {
-    Stop-Service WinDefend
-}
-
 Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
 
 crank-agent
