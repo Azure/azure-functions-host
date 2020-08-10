5627093635a1276fb192169a6bd37f7ab5289d9d
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-09 17:33:19 -0700

    Use provided credentials

--- tools/Crank/setup-crank-agent.ps1
+++ tools/Crank/setup-crank-agent.ps1
@@ -9,7 +9,7 @@ function InstallCrankAgent {
     dotnet tool install --tool-path c:\dotnet-tools Microsoft.Crank.Agent --version "0.1.0-*"
 }
 
-function ScheduleCrankAgentStart {
+function ScheduleCrankAgentStart([pscredential]$Credential) {
     $logsDir = 'C:\crank-agent-logs'
     if (-not (Test-Path $logsDir -PathType Container)) {
         New-Item -Path $logsDir -ItemType Container
@@ -17,16 +17,28 @@ function ScheduleCrankAgentStart {
 
     $action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/C C:\dotnet-tools\crank-agent.exe 2>&1 >> $logsDir\crank-agent.log"
     $trigger = New-ScheduledTaskTrigger -AtStartup
-    $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\NETWORKSERVICE" -LogonType ServiceAccount -RunLevel Highest
+
+    $auth =
+        if ($Credential) {
+            @{
+                User = $Credential.UserName
+                Password = $Credential.GetNetworkCredential().Password
+            }
+        } else {
+            @{
+                Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\NETWORKSERVICE" `
+                                -LogonType ServiceAccount -RunLevel Highest
+            }
+        }
 
     Register-ScheduledTask `
         -TaskName "CrankAgent" -Description "Start crank-agent" `
         -Action $action -Trigger $trigger `
-        -Principal $principal
+        @auth
 }
 
 #####################################
 
 InstallDotNet
 InstallCrankAgent
-ScheduleCrankAgentStart
+ScheduleCrankAgentStart -Credential (Get-Credential)
