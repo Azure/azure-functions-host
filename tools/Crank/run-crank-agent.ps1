ac95abc0f0f5ccdb75012a661a33a4e8f8f97627
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-09 17:42:21 -0700

    Redirect output to C:\crank-agent-logs

--- tools/Crank/run-crank-agent.ps1
+++ tools/Crank/run-crank-agent.ps1
@@ -4,4 +4,9 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
 Add-MpPreference -ExclusionProcess 'crank-agent.exe'
 
-c:\dotnet-tools\crank-agent
+$logsDir = 'C:\crank-agent-logs'
+if (-not (Test-Path $logsDir -PathType Container)) {
+    New-Item -Path $logsDir -ItemType Container
+}
+
+& 'C:\dotnet-tools\crank-agent.exe' 2>&1 >> $logsDir\crank-agent.log
