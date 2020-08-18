87e6cbcf2fe709831d5cb706222754985437889f
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-18 10:56:15 -0700

    Add CrankAgentVm parameter

--- tools/Crank/run-benchmarks.ps1
+++ tools/Crank/run-benchmarks.ps1
@@ -1,4 +1,8 @@
 param(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $CrankAgentVm,
+
     [string]
     $FunctionsHostBranchOrCommit = 'dev',
 
@@ -55,9 +59,9 @@ if ($WriteResultsToDatabase) {
 
     $sqlConnectionString = "Server=tcp:functions-crank-sql.database.windows.net,1433;Initial Catalog=functions-crank-db;Persist Security Info=False;User ID=Functions;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
-    & $InvokeCrankCommand --config $crankConfigPath --scenario functionApp --profile local --variable FunctionApp=$FunctionApp --variable FunctionsHostBranchOrCommit=$FunctionsHostBranchOrCommit --sql $sqlConnectionString --table FunctionsPerf
+    & $InvokeCrankCommand --config $crankConfigPath --scenario functionApp --profile local --variable CrankAgentVm=$CrankAgentVm --variable FunctionApp=$FunctionApp --variable FunctionsHostBranchOrCommit=$FunctionsHostBranchOrCommit --sql $sqlConnectionString --table FunctionsPerf
 } else {
-    & $InvokeCrankCommand --config $crankConfigPath --scenario functionApp --profile local --variable FunctionApp=$FunctionApp --variable FunctionsHostBranchOrCommit=$FunctionsHostBranchOrCommit
+    & $InvokeCrankCommand --config $crankConfigPath --scenario functionApp --profile local --variable CrankAgentVm=$CrankAgentVm --variable FunctionApp=$FunctionApp --variable FunctionsHostBranchOrCommit=$FunctionsHostBranchOrCommit
 }
 
 #endregion
