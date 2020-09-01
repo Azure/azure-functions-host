93689d5e2be5fa6da23a0ac56f6509a8545f3895
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-01 10:51:27 -0700

    Enable running dotnet/crank benchmarks on Linux (#6571)
    * Enable running dotnet/crank benchmarks on Linux
    
    * Add UserName parameter
    
    * Replace the ?: operator with if/else

--- tools/Crank/run-crank-agent.ps1
+++ tools/Crank/run-crank-agent.ps1
@@ -1,14 +1,23 @@
+param(
+    [string]
+    $UserName = 'Functions'
+)
+
 $ErrorActionPreference = 'Stop'
 
-Set-MpPreference -DisableRealtimeMonitoring $true
-Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
-Add-MpPreference -ExclusionProcess 'crank-agent.exe'
-
-$logsDir = 'C:\crank-agent-logs'
-if (-not (Test-Path $logsDir -PathType Container)) {
-    New-Item -Path $logsDir -ItemType Container
+if ($IsWindows) {
+    Set-MpPreference -DisableRealtimeMonitoring $true
+    Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
+    Add-MpPreference -ExclusionProcess 'crank-agent.exe'
 }
 
-$logFileName = "$logsDir\crank-agent_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').log"
+$logsDir = $IsWindows ? 'C:\crank-agent-logs' : "/home/$UserName/crank-agent-logs"
+if (-not (Test-Path $logsDir -PathType Container)) {
+    New-Item -Path $logsDir -ItemType Container > $null
+}
 
-& 'C:\dotnet-tools\crank-agent.exe' 2>&1 >> $logFileName
+$logFileName = Join-Path -Path $logsDir -ChildPath "$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').log"
+
+$invokeCrankAgentCommand = $IsWindows ? 'C:\dotnet-tools\crank-agent.exe' : "/home/$UserName/.dotnet/tools/crank-agent";
+
+& $invokeCrankAgentCommand 2>&1 >> $logFileName
--- tools/Crank/setup-crank-agent.ps1
+++ tools/Crank/setup-crank-agent.ps1
@@ -1,4 +1,5 @@
-params (
+[CmdletBinding()]
+param (
     [bool]$InstallDotNet = $true,
     [bool]$InstallCrankAgent = $true
 )
@@ -8,46 +9,80 @@ $ErrorActionPreference = 'Stop'
 #region Utilities
 
 function InstallDotNet {
-    Invoke-WebRequest 'https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'
-    ./dotnet-install.ps1
+    Write-Verbose 'Installing dotnet...'
+    if ($IsWindows) {
+        Invoke-WebRequest 'https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'
+        ./dotnet-install.ps1
+    } else {
+        # From https://docs.microsoft.com/dotnet/core/install/linux-ubuntu#install-the-sdk
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https
+        sudo apt-get update
+        sudo apt-get install -y dotnet-sdk-3.1
+    }
 }
 
 function InstallCrankAgent {
-    dotnet tool install --tool-path c:\dotnet-tools Microsoft.Crank.Agent --version "0.1.0-*"
+    Write-Verbose 'Installing crank-agent...'
+    if ($IsWindows) {
+        dotnet tool install --tool-path c:\dotnet-tools Microsoft.Crank.Agent --version "0.1.0-*"
+    } else {
+        dotnet tool install -g Microsoft.Crank.Agent --version "0.1.0-*"
+    }
 }
 
-function ScheduleCrankAgentStart([pscredential]$Credential) {
-    $logsDir = 'C:\crank-agent-logs'
-    if (-not (Test-Path $logsDir -PathType Container)) {
-        New-Item -Path $logsDir -ItemType Container
+function ScheduleCrankAgentStartWindows($RunScriptPath, [pscredential]$Credential) {
+    $taskName = 'CrankAgent'
+
+    if (Get-ScheduledTask -TaskName $taskName) {
+        Write-Warning "Task '$taskName' already exists, no changes performed"
+    } else {
+        $action = New-ScheduledTaskAction -Execute 'pwsh.exe' `
+                    -Argument "-NoProfile -WindowStyle Hidden -File $RunScriptPath"
+
+        $trigger = New-ScheduledTaskTrigger -AtStartup
+
+        $auth =
+            if ($Credential) {
+                @{
+                    User = $Credential.UserName
+                    Password = $Credential.GetNetworkCredential().Password
+                }
+            } else {
+                @{
+                    Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\NETWORKSERVICE" `
+                                    -LogonType ServiceAccount -RunLevel Highest
+                }
+            }
+
+        $null = Register-ScheduledTask `
+                    -TaskName $taskName -Description "Start crank-agent" `
+                    -Action $action -Trigger $trigger `
+                    @auth
+    }
+}
+
+function ScheduleCrankAgentStartLinux($RunScriptPath) {
+    $currentCrontabContent = (crontab -l) ?? $null
+    if ($currentCrontabContent -match '\bcrank-agent\b') {
+        Write-Warning "crank-agent reference is found in crontab, no changes performed"
+    } else {
+        $currentCrontabContent, "@reboot $RunScriptPath" | crontab -
+    }
+}
+
+function ScheduleCrankAgentStart {
+    Write-Verbose 'Scheduling crank-agent start...'
+
+    $scriptPath = Join-Path -Path (Split-Path $PSCommandPath -Parent) -ChildPath 'run-crank-agent.ps1'
+
+    if ($IsWindows) {
+        ScheduleCrankAgentStartWindows -RunScriptPath $scriptPath -Credential (Get-Credential)
+    } else {
+        ScheduleCrankAgentStartLinux -RunScriptPath $scriptPath
     }
 
-    $runCrankAgentScriptPath = Join-Path `
-                                    -Path (Split-Path $PSCommandPath -Parent) `
-                                    -ChildPath 'run-crank-agent.ps1'
-
-    $action = New-ScheduledTaskAction -Execute 'powershell.exe' `
-                  -Argument "-NoProfile -WindowStyle Hidden -File $runCrankAgentScriptPath"
-
-    $trigger = New-ScheduledTaskTrigger -AtStartup
-
-    $auth =
-        if ($Credential) {
-            @{
-                User = $Credential.UserName
-                Password = $Credential.GetNetworkCredential().Password
-            }
-        } else {
-            @{
-                Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\NETWORKSERVICE" `
-                                -LogonType ServiceAccount -RunLevel Highest
-            }
-        }
-
-    $null = Register-ScheduledTask `
-                -TaskName "CrankAgent" -Description "Start crank-agent" `
-                -Action $action -Trigger $trigger `
-                @auth
+    Write-Warning 'Please reboot to start crank-agent'
 }
 
 #endregion
@@ -56,6 +91,6 @@ function ScheduleCrankAgentStart([pscredential]$Credential) {
 
 if ($InstallDotNet) { InstallDotNet }
 if ($InstallCrankAgent) { InstallCrankAgent }
-ScheduleCrankAgentStart -Credential (Get-Credential)
+ScheduleCrankAgentStart
 
 #endregion
