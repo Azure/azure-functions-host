1783e8008eb3a5157f432310e6553d151e5bf265
Mathew Charles <mathewc@microsoft.com>
2020-09-03 10:09:03 -0700

    Adding Crank AspNet Benchmark app and scenario

---
+++ tools/Crank/BenchmarkApps/AspNetBenchmark/AspNetBenchmark/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetBenchmark.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return new OkObjectResult("Hello from ASP.NET!");
+        }
+    }
+}
