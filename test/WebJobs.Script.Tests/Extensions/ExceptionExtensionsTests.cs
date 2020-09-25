ab809da52309285eff1ea7c02c7e9e4e5c981fc4
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-09-25 08:52:32 -0700

    Log exception details from outermost exception (#6680)

---
+++ test/WebJobs.Script.Tests/Extensions/ExceptionExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Extensions
+{
+    public class ExceptionExtensionsTests
+    {
+        [Fact]
+        public void GetExceptionDetails_ReturnsExpectedResult()
+        {
+            Exception innerException = new InvalidOperationException("Some inner exception");
+            Exception outerException = new Exception("some outer exception", innerException);
+            Exception fullException;
+
+            try
+            {
+                throw outerException;
+            }
+            catch (Exception e)
+            {
+                fullException = e;  // Outer exception will have stack trace whereas the inner exception's stack trace will be null
+            }
+
+            (string exceptionType, string exceptionMessage, string exceptionDetails) = fullException.GetExceptionDetails();
+
+            Assert.Equal("System.InvalidOperationException", exceptionType);
+            Assert.Equal("Some inner exception", exceptionMessage);
+            Assert.Contains("System.Exception : some outer exception ---> System.InvalidOperationException : Some inner exception \r\n   End of inner exception\r\n   at Microsoft.Azure.WebJobs.Script.Tests.Extensions.ExceptionExtensionsTests.GetExceptionDetails_ReturnsExpectedResult()", exceptionDetails);
+            Assert.Contains("Extensions\\ExceptionExtensionsTests.cs : 20", exceptionDetails);
+        }
+    }
+}
