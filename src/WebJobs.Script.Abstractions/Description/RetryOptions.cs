29cfe69acecf00ebbc84fbc8634896d30cc47259
pragnagopa <pgopa@microsoft.com>
2020-09-29 08:05:46 -0700

    Update interval to be nullable type

--- src/WebJobs.Script.Abstractions/Description/RetryOptions.cs
+++ src/WebJobs.Script.Abstractions/Description/RetryOptions.cs
@@ -17,16 +17,16 @@ namespace Microsoft.Azure.WebJobs.Script.Description
         /// <summary>
         /// Gets or sets the delay that will be used between retries when using <see cref="RetryStrategy.FixedDelay"/> strategy
         /// </summary>
-        public TimeSpan DelayInterval { get; set; }
+        public TimeSpan? DelayInterval { get; set; }
 
         /// <summary>
         /// Gets or sets the minimum retry delay when using <see cref="RetryStrategy.ExponentialBackoff"/> strategy
         /// </summary>
-        public TimeSpan MinimumInterval { get; set; }
+        public TimeSpan? MinimumInterval { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum retry delay when using <see cref="RetryStrategy.ExponentialBackoff"/> strategy
         /// </summary>
-        public TimeSpan MaximumInterval { get; set; }
+        public TimeSpan? MaximumInterval { get; set; }
     }
 }
