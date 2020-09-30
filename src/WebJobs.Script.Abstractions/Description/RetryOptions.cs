ac526fca803d53a7db7824e884031cb294c73d93
Pragna Gopa <pgopa@microsoft.com>
2020-09-30 07:16:32 -0700

    Update RetryOptions  property types to be nullable (#6711)

--- src/WebJobs.Script.Abstractions/Description/RetryOptions.cs
+++ src/WebJobs.Script.Abstractions/Description/RetryOptions.cs
@@ -12,21 +12,21 @@ namespace Microsoft.Azure.WebJobs.Script.Description
         /// <summary>
         /// Gets or sets the maximum number of retries allowed per function execution
         /// </summary>
-        public int MaxRetryCount { get; set; }
+        public int? MaxRetryCount { get; set; }
 
         /// <summary>
         /// Gets or sets the delay that will be used between retries when using <see cref="RetryStrategy.FixedDelay"/> strategy
         /// </summary>
-        public TimeSpan DelayInterval { get; set; }
+        public TimeSpan? DelayInterval { get; set; }
 
         /// <summary>
         /// Gets or sets the minimum retry delay when using <see cref="RetryStrategy.ExponentialBackoff"/> strategy
         /// </summary>
-        public TimeSpan MinimumInterval { get; set; }
+        public TimeSpan? MinimumInterval { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum retry delay when using <see cref="RetryStrategy.ExponentialBackoff"/> strategy
         /// </summary>
-        public TimeSpan MaximumInterval { get; set; }
+        public TimeSpan? MaximumInterval { get; set; }
     }
 }
