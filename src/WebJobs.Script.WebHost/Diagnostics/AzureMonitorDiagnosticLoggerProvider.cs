6479d9f1adeffbd58646b59deaa38eea19d9d12d
Brett Samblanet <brettsam@microsoft.com>
2020-09-30 13:07:08 -0700

    adding more properties to Azure Monitor logs (#6679)

--- src/WebJobs.Script.WebHost/Diagnostics/AzureMonitorDiagnosticLoggerProvider.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/AzureMonitorDiagnosticLoggerProvider.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Azure.WebJobs.Script.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -13,24 +14,28 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
         private readonly IEventGenerator _eventGenerator;
         private readonly IEnvironment _environment;
         private readonly HostNameProvider _hostNameProvider;
+        private readonly IOptionsMonitor<AppServiceOptions> _appServiceOptions;
         private IExternalScopeProvider _scopeProvider;
 
-        public AzureMonitorDiagnosticLoggerProvider(IOptions<ScriptJobHostOptions> scriptOptions, IEventGenerator eventGenerator, IEnvironment environment, HostNameProvider hostNameProvider)
-            : this(scriptOptions.Value.InstanceId, eventGenerator, environment, hostNameProvider)
+        public AzureMonitorDiagnosticLoggerProvider(IOptions<ScriptJobHostOptions> scriptOptions, IEventGenerator eventGenerator, IEnvironment environment,
+            HostNameProvider hostNameProvider, IOptionsMonitor<AppServiceOptions> appServiceOptions)
+            : this(scriptOptions.Value.InstanceId, eventGenerator, environment, hostNameProvider, appServiceOptions)
         {
         }
 
-        public AzureMonitorDiagnosticLoggerProvider(string hostInstanceId, IEventGenerator eventGenerator, IEnvironment environment, HostNameProvider hostNameProvider)
+        public AzureMonitorDiagnosticLoggerProvider(string hostInstanceId, IEventGenerator eventGenerator, IEnvironment environment,
+            HostNameProvider hostNameProvider, IOptionsMonitor<AppServiceOptions> appServiceOptions)
         {
             _hostInstanceId = hostInstanceId ?? throw new ArgumentNullException(hostInstanceId);
             _eventGenerator = eventGenerator ?? throw new ArgumentNullException(nameof(eventGenerator));
             _environment = environment ?? throw new ArgumentException(nameof(environment));
             _hostNameProvider = hostNameProvider ?? throw new ArgumentException(nameof(hostNameProvider));
+            _appServiceOptions = appServiceOptions ?? throw new ArgumentNullException(nameof(appServiceOptions));
         }
 
         public ILogger CreateLogger(string categoryName)
         {
-            return new AzureMonitorDiagnosticLogger(categoryName, _hostInstanceId, _eventGenerator, _environment, _scopeProvider, _hostNameProvider);
+            return new AzureMonitorDiagnosticLogger(categoryName, _hostInstanceId, _eventGenerator, _environment, _scopeProvider, _hostNameProvider, _appServiceOptions);
         }
 
         public void Dispose()
