eb62b5da615b7eae592fe94fd07eca64eb644686
Pragna Gopa <pgopa@microsoft.com>
2020-10-05 11:44:38 -0700

    Skip parsing worker configs for dotnet apps (#6751)

---
+++ test/WebJobs.Script.Tests/Configuration/LanguageWorkerOptionsSetupTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Configuration
+{
+    public class LanguageWorkerOptionsSetupTests
+    {
+        [Theory]
+        [InlineData("DotNet")]
+        [InlineData("dotnet")]
+        [InlineData(null)]
+        [InlineData("node")]
+        public void LanguageWorkerOptions_Expected_ListOfConfigs(string workerRuntime)
+        {
+            var testEnvironment = new TestEnvironment();
+            var configurationBuilder = new ConfigurationBuilder()
+                .Add(new ScriptEnvironmentVariablesConfigurationSource());
+
+            var configuration = configurationBuilder.Build();
+            if (!string.IsNullOrEmpty(workerRuntime))
+            {
+                testEnvironment.SetEnvironmentVariable(RpcWorkerConstants.FunctionWorkerRuntimeSettingName, workerRuntime);
+            }
+            LanguageWorkerOptionsSetup setup = new LanguageWorkerOptionsSetup(configuration, NullLoggerFactory.Instance, testEnvironment, new TestMetricsLogger());
+            LanguageWorkerOptions options = new LanguageWorkerOptions();
+            setup.Configure(options);
+            if (string.IsNullOrEmpty(workerRuntime))
+            {
+                Assert.Equal(4, options.WorkerConfigs.Count);
+            }
+            else if (workerRuntime.Equals(RpcWorkerConstants.DotNetLanguageWorkerName, StringComparison.OrdinalIgnoreCase))
+            {
+                Assert.Empty(options.WorkerConfigs);
+            }
+            else
+            {
+                Assert.Equal(1, options.WorkerConfigs.Count);
+            }
+        }
+    }
+}
