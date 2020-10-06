35bece2b68a90f3e6d96f7d3e5c38e2794a797c9
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-05 21:06:02 -0700

    Move DeployCrankVM to a separate script

--- tools/Crank/Agent/VmDeployment/deploy.ps1
+++ tools/Crank/Agent/VmDeployment/deploy.ps1
@@ -10,15 +10,15 @@ param (
     [string]
     $BaseName,
 
+    [string[]]
+    $NamePostfixes = @('-app', '-load'),
+
     [Parameter(Mandatory = $true)]
     [ValidateSet('Linux', 'Windows')]
     $OsType,
 
     [switch]
     $Docker,
-
-    [switch]
-    $SingleVm,
 
     [string]
     $VmSize = 'Standard_E2s_v3',
@@ -35,63 +35,23 @@ param (
 
 $ErrorActionPreference = 'Stop'
 
-#region Utilities
-
-function DeployCrankVm($NamePostfix) {
-    $resourceGroupName = "FunctionsCrank-$OsType-$BaseName$NamePostfix"
-    $vmName = "functions-crank-$OsType-$BaseName$NamePostfix".ToLower()
-    Write-Verbose "Creating VM '$vmName' in resource group '$resourceGroupName'"
-
-    Set-AzContext -Subscription $SubscriptionName | Out-Null
-
-    New-AzResourceGroup -Name $resourceGroupName -Location $Location | Out-Null
-
-    $vaultSubscriptionId = (Get-AzSubscription -SubscriptionName 'Antares-Demo').Id
-
-    $customScriptParameters = @{
-        CrankBranch = 'master'
-        Docker = $Docker.IsPresent
-    }
-
-    New-AzResourceGroupDeployment `
-        -ResourceGroupName $resourceGroupName `
-        -TemplateFile "$PSScriptRoot\template.json" `
-        -TemplateParameterObject @{
-            vmName = $vmName
-            dnsLabelPrefix = $vmName
-            vmSize = $VmSize
-            osDiskType = $OsDiskType
-            adminUsername = $UserName
-            authenticationType = 'sshPublicKey'
-            vaultName = 'functions-crank-kv'
-            vaultResourceGroupName = 'FunctionsCrank'
-            vaultSubscription = $vaultSubscriptionId
-            secretName = 'LinuxCrankAgentVmSshKey-Public'
-            customScriptParameters = $customScriptParameters | ConvertTo-Json -Compress
-        } | Out-Null
-
-    Write-Verbose 'Restarting the VM...'
-    Restart-AzVM -ResourceGroupName $resourceGroupName -Name $vmName | Out-Null
-    Start-Sleep -Seconds 30
-
-    Write-Host "The crank VM is ready: $vmName"
-}
-
-#endregion
-
-#region Main
-
 if ($OsType -ne 'Linux') {
     throw 'Only Linux is supported now'
 }
 
-if ($SingleVm) {
-    DeployCrankVm
-} else {
-    '-app', '-load' | ForEach-Object -Parallel { using:DeployCrankVm -NamePostfix $_ }
+$NamePostfixes | ForEach-Object -Parallel {
+    & "$using:PSScriptRoot/deploy-vm.ps1" `
+        -SubscriptionName $using:SubscriptionName `
+        -BaseName $using:BaseName `
+        -NamePostfix $_ `
+        -OsType $using:OsType `
+        -Docker:$using:Docker `
+        -VmSize $using:VmSize `
+        -OsDiskType $using:OsDiskType `
+        -Location $using:Location `
+        -UserName $using:UserName `
+        -Verbose:$using:VerbosePreference
 }
 
 # TODO: remove this warning when app deployment is automated
 Write-Warning "Remember to deploy the Function apps to /home/$UserName/FunctionApps"
-
-#endregion
