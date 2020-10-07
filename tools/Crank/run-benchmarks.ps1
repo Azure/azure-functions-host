c09789690b148053967201a1811e04ba2046f9ab
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-07 01:06:55 -0700

    Patch the serverUri value in the crank config

--- tools/Crank/run-benchmarks.ps1
+++ tools/Crank/run-benchmarks.ps1
@@ -84,43 +84,56 @@ $tmpLogPath = if ($isLinuxApp) { "/tmp/functions/log" } else { 'C:\Temp\Function
 $aspNetUrls = "http://$($CrankAgentAppVm):5000"
 $profileName = "default"
 
-$crankArgs =
-    '--config', $crankConfigPath,
-    '--scenario', $Scenario,
-    '--profile', $profileName,
-    '--chart',
-    '--chart-type hex',
-    '--application.collectCounters', $true,
-    '--variable', "CrankAgentAppVm=$CrankAgentAppVm",
-    '--variable', "CrankAgentLoadVm=$CrankAgentLoadVm",
-    '--variable', "FunctionAppPath=`"$functionAppPath`"",
-    '--variable', "HomePath=`"$homePath`"",
-    '--variable', "TempLogPath=`"$tmpLogPath`"",
-    '--variable', "BranchOrCommit=$BranchOrCommit",
-    '--variable', "duration=$Duration",
-    '--variable', "warmup=$Warmup",
-    '--variable', "AspNetUrls=$aspNetUrls"
+$patchedConfigFile = New-TemporaryFile |
+    ForEach-Object { Rename-Item -Path $_.FullName -NewName ($_.Name + '.yml') -PassThru }
 
-if ($Trace) {
-    $crankArgs += '--application.collect', $true
+try {
+    # This is a temporary hack to work around a Crank issue: variables are not expanded in some contexts.
+    # So, we patch the config file with the required data.
+    Get-Content -Path $crankConfigPath |
+        ForEach-Object { $_ -replace 'serverUri: http://{{ CrankAgentAppVm }}', "serverUri: http://$CrankAgentAppVm" } |
+        Out-File -FilePath $patchedConfigFile.FullName
+
+    $crankArgs =
+        '--config', $patchedConfigFile.FullName,
+        '--scenario', $Scenario,
+        '--profile', $profileName,
+        '--chart',
+        '--chart-type hex',
+        '--application.collectCounters', $true,
+        '--variable', "CrankAgentAppVm=$CrankAgentAppVm",
+        '--variable', "CrankAgentLoadVm=$CrankAgentLoadVm",
+        '--variable', "FunctionAppPath=`"$functionAppPath`"",
+        '--variable', "HomePath=`"$homePath`"",
+        '--variable', "TempLogPath=`"$tmpLogPath`"",
+        '--variable', "BranchOrCommit=$BranchOrCommit",
+        '--variable', "duration=$Duration",
+        '--variable', "warmup=$Warmup",
+        '--variable', "AspNetUrls=$aspNetUrls"
+
+    if ($Trace) {
+        $crankArgs += '--application.collect', $true
+    }
+
+    if ($WriteResultsToDatabase) {
+        Set-AzContext -Subscription 'Antares-Demo' > $null
+        $sqlPassword = (Get-AzKeyVaultSecret -vaultName 'functions-crank-kv' -name 'SqlAdminPassword').SecretValueText
+
+        $sqlConnectionString = "Server=tcp:functions-crank-sql.database.windows.net,1433;Initial Catalog=functions-crank-db;Persist Security Info=False;User ID=Functions;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+        $crankArgs += '--sql', $sqlConnectionString
+        $crankArgs += '--table', 'FunctionsPerf'
+    }
+
+    if ($Iterations -gt 1) {
+        $crankArgs += '--iterations', $Iterations
+        $crankArgs += '--display-iterations'
+    }
+
+    & $InvokeCrankCommand $crankArgs 2>&1 | Tee-Object -Variable crankOutput
+} finally {
+    Remove-Item -Path $patchedConfigFile.FullName
 }
-
-if ($WriteResultsToDatabase) {
-    Set-AzContext -Subscription 'Antares-Demo' > $null
-    $sqlPassword = (Get-AzKeyVaultSecret -vaultName 'functions-crank-kv' -name 'SqlAdminPassword').SecretValueText
-
-    $sqlConnectionString = "Server=tcp:functions-crank-sql.database.windows.net,1433;Initial Catalog=functions-crank-db;Persist Security Info=False;User ID=Functions;Password=$sqlPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-
-    $crankArgs += '--sql', $sqlConnectionString
-    $crankArgs += '--table', 'FunctionsPerf'
-}
-
-if ($Iterations -gt 1) {
-    $crankArgs += '--iterations', $Iterations
-    $crankArgs += '--display-iterations'
-}
-
-& $InvokeCrankCommand $crankArgs 2>&1 | Tee-Object -Variable crankOutput
 
 $badResponses = $crankOutput | Where-Object { $_ -match '\bBad responses\b\s*\|\s*(\S*)\s' } | ForEach-Object { $Matches[1] }
 if ($null -eq $badResponses) {
