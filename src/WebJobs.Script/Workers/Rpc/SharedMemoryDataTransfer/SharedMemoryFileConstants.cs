b37a9a57005e85a2cc63f315161e58943b911e37
Gohar Irfan Chaudhry <gochaudh@microsoft.com>
2020-10-19 15:58:15 +0500

    Stub for SharedMemoryFile unit tests

--- src/WebJobs.Script/Workers/Rpc/SharedMemoryDataTransfer/SharedMemoryFileConstants.cs
+++ src/WebJobs.Script/Workers/Rpc/SharedMemoryDataTransfer/SharedMemoryFileConstants.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.IO.MemoryMappedFiles;
 
 namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc.SharedMemoryDataTransfer
 {
