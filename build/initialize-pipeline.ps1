5e30dce5545ee2610bf954fd3ba9cd3f5b335fe3
Pragna Gopa <pgopa@microsoft.com>
2020-12-18 14:35:33 -0800

    Add more logs to help with Auto-Upgrade (#6988)

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -4,11 +4,11 @@ $bypassPackaging = $true
 $suffix = "-ci"
 Write-Host "SourceBranch: $sourceBranch, Build reason: $buildReason"
 
-if($sourceBranch.endsWith('release/2.0')) {
+if($sourceBranch.endsWith('release/2.0') -or $sourceBranch.startsWith('release/2.0-hotfix')) {
   $suffix = ""
 }
 
-if(($sourceBranch.endsWith('v2.x') -or $sourceBranch.endsWith('release/2.0')) -and ($buildReason -ne "PullRequest"))
+if(($sourceBranch.endsWith('v2.x') -or $sourceBranch.endsWith('release/2.0') -or $sourceBranch.startsWith('release/2.0-hotfix')) -and ($buildReason -ne "PullRequest"))
 {
   $bypassPackaging = $false
 }
