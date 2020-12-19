aabf18e4773bf886d9ceda3b309a17c6643483f4
Hamid Safi <hamids@microsoft.com>
2020-12-18 23:22:17 -0800

    fix typo in comments

--- src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
+++ src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
@@ -82,7 +82,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             // Register /admin/vfs, and /admin/zip to the VirtualFileSystem middleware.
             builder.UseWhen(VirtualFileSystemMiddleware.IsVirtualFileSystemRequest, config => config.UseMiddleware<VirtualFileSystemMiddleware>());
 
-            // MVC routes (routes defined by Controllers like HostController, FunctionsController, ... must be added before functions/proxy routes so they are matched first and can not be overridden by functions orproxy routes)
+            // MVC routes (routes defined by Controllers like HostController, FunctionsController, ... must be added before functions/proxy routes so they are matched first and can not be overridden by functions or proxy routes)
             // source here: https://github.com/aspnet/Mvc/blob/master/src/Microsoft.AspNetCore.Mvc.Core/Builder/MvcApplicationBuilderExtensions.cs
             builder.UseMvc();
 
