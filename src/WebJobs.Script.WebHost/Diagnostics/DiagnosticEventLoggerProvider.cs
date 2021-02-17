51e04aeceb9a52250cf5012ac2848979c489eb45
soninaren <nasoni@microsoft.com>
2021-02-17 00:41:31 -0800

    Add DiagnosticEventLogger and IDiagnosticEventRepository definitions

---
+++ src/WebJobs.Script.WebHost/Diagnostics/DiagnosticEventLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
+{
+    public class DiagnosticEventLoggerProvider : ILoggerProvider
+    {
+        private readonly IDiagnosticEventRepository _diagnosticEventRepository;
+
+        public DiagnosticEventLoggerProvider(IDiagnosticEventRepository diagnosticEventRepository)
+        {
+            _diagnosticEventRepository = diagnosticEventRepository;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new DiagnosticEventLogger(_diagnosticEventRepository);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
