0efdc42551c39b4916d41eeee58ff8cbebedc7b7
Katy Shimizu <kashimiz@microsoft.com>
2021-03-30 23:21:13 -0700

    Created analyzer project and initial framework.

---
+++ src/WebJobs.Script.Analyzers/Constants.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Functions.Analyzers
+{
+    internal static class Constants
+    {
+        internal static class Types
+        {
+            public const string FunctionNameAttribute = "Microsoft.Azure.WebJobs.FunctionNameAttribute";
+        }
+
+        internal static class DiagnosticsCategories
+        {
+            public const string Usage = "Usage";
+        }
+    }
+}
---
+++ src/WebJobs.Script.Analyzers/DiagnosticDescriptors.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.Azure.Functions.Analyzers
+{
+    internal class DiagnosticDescriptors
+    {
+        private static DiagnosticDescriptor Create(string id, string title, string messageFormat, string category, DiagnosticSeverity severity)
+        {
+            string helpLink = $"https://docs.microsoft.com/azure/azure-functions/errors-diagnostics/sdk-rules/{id}"; 
+            return new DiagnosticDescriptor(id, title, messageFormat, category, severity, isEnabledByDefault: true, helpLinkUri: helpLink);
+        }
+
+        public static DiagnosticDescriptor AsyncVoidDiscouraged { get; }
+            = Create(id: "AZF0001", title: "Avoid async void", 
+                messageFormat: "Async void can lead to unexpected behavior. Return Task instead.",
+                category: Constants.DiagnosticsCategories.Usage,
+                severity: DiagnosticSeverity.Error);
+    }
+}
