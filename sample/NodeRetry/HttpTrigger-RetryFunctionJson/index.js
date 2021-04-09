1813b60c0fe5132f5494d841c598573ce0338762
Mathew Charles <mathewc@microsoft.com>
2021-04-09 15:53:32 -0700

    Moving to SDK 3.0.28 packages

--- sample/NodeRetry/HttpTrigger-RetryFunctionJson/index.js
+++ sample/NodeRetry/HttpTrigger-RetryFunctionJson/index.js
@@ -1,27 +1,21 @@
-﻿var invocationCount = 0;
-var errorString = 'An error occurred';
+﻿var errorString = 'An error occurred';
+var maxRetries = 4;
 
 module.exports = async function (context, req) {
-    if (context.executionContext.retryContext && (context.executionContext.retryContext.retryCount !== invocationCount
-        || !(context.executionContext.retryContext.maxRetryCount === 4 || context.executionContext.retryContext.maxRetryCount === 0)
-        || !(context.executionContext.retryContext.exception.message.includes(errorString)))) {
+    var retryContext = context.executionContext.retryContext;
+
+    if (retryContext.maxRetryCount != maxRetries || (retryContext.retryCount > 0 && !retryContext.exception.message.includes(errorString))) {
         context.res = {
             status: 500
         };
     } else {
-        const reset = req.query.reset;
-        invocationCount = reset ? 0 : invocationCount
+        context.log('JavaScript HTTP trigger function processed a request. retryCount: ' + retryContext.retryCount);
 
-        context.log('JavaScript HTTP trigger function processed a request.invocationCount: ' + invocationCount);
-
-        invocationCount = invocationCount + 1;
-        const responseMessage = "invocationCount: " + invocationCount;
-        if (invocationCount < 4) {
+        if (retryContext.retryCount < maxRetries) {
             throw new Error(errorString);
         }
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: responseMessage
+            body: 'retryCount: ' + retryContext.retryCount
         };
     }
 }
