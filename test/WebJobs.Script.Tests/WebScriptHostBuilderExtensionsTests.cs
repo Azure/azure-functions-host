4b6f56cf67987d92c0f8af10fef329255fac64c5
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-04-21 16:21:27 -0700

    Adding option to disable environment name updates when in debug mode (#7304)

---
+++ test/WebJobs.Script.Tests/WebScriptHostBuilderExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.WebJobs.Script.Tests;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests
+{
+    public class WebScriptHostBuilderExtensionsTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var builder = new HostBuilder().ConfigureDefaultTestWebScriptHost(null, null, false, configureRootServices: s =>
+                {
+                    s.AddSingleton<IEnvironment>(p =>
+                    {
+                        var environment = new TestEnvironment();
+                        environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsFeatureFlags, ScriptConstants.FeatureFlagDisableDevInDebug);
+
+                        return environment;
+                    });
+
+                    var stateProvider = new Mock<IDebugStateProvider>();
+
+                    stateProvider.Setup(d => d.InDebugMode)
+                    .Returns(() => true);
+
+                    s.AddSingleton<IDebugStateProvider>(stateProvider.Object);
+                });
+
+            var host = builder.Build();
+
+            var hostingEnvironment = host.Services.GetRequiredService<IHostEnvironment>();
+
+            Assert.False(hostingEnvironment.IsDevelopment());
+        }
+    }
+}
