16d3562e5a24769390259be573de012913caacf2
Yogesh Jagadeesan <yojagad@microsoft.com>
2021-05-06 19:29:27 -0700

    Removed break

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -17,19 +17,16 @@ $XMLContents.GetElementsByTagName("MajorVersion") |  ForEach-Object {
   $majorVersion = $_.InnerText
   Write-Host "##vso[task.setvariable variable=MajorVersion;isOutput=true]$majorVersion"
   Write-Host "Setting 'MajorVersion' to $majorVersion"
-  break
 }
 
 $XMLContents.GetElementsByTagName("MinorVersion") |  ForEach-Object {
   $minorVersion = $_.InnerText
   Write-Host "##vso[task.setvariable variable=MinorVersion;isOutput=true]$minorVersion"
   Write-Host "Setting 'MinorVersion' to $minorVersion"
-  break
 }
 
 $XMLContents.GetElementsByTagName("PatchVersion") |  ForEach-Object {
   $patchVersion = $_.InnerText
   Write-Host "##vso[task.setvariable variable=PatchVersion;isOutput=true]$patchVersion"
   Write-Host "Setting 'PatchVersion' to $patchVersion"
-  break
 }
