05bed12574c702aa1d6f398c70d84fe8e6133808
karshinlin <karslin@microsoft.com>
2021-06-16 14:51:57 -0400

    Revert "[pack] Reacting to ScriptHost configuration changes for AzureStorageProvider (#7424)" (#7467)
    This reverts commit 80061a7da06cb285de51d159321e0d186886a466.

--- src/WebJobs.Script/StorageProvider/BlobServiceClientProvider.cs
+++ src/WebJobs.Script/StorageProvider/BlobServiceClientProvider.cs
@@ -16,8 +16,8 @@ namespace Microsoft.Azure.WebJobs.Script.StorageProvider
     /// </summary>
     internal class BlobServiceClientProvider : StorageClientProvider<BlobServiceClient, BlobClientOptions>
     {
-        public BlobServiceClientProvider(AzureComponentFactory componentFactory, AzureEventSourceLogForwarder logForwarder, ILogger<BlobServiceClient> logger)
-            : base(componentFactory, logForwarder, logger) { }
+        public BlobServiceClientProvider(IConfiguration configuration, AzureComponentFactory componentFactory, AzureEventSourceLogForwarder logForwarder, ILogger<BlobServiceClient> logger)
+            : base(configuration, componentFactory, logForwarder, logger) { }
 
         /// <inheritdoc/>
         protected override string ServiceUriSubDomain
