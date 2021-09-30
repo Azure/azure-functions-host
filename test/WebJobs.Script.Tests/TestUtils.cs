ecfdd5fccb508b9008e3e3ce474e06ed62c9ad7a
Madeline Gordon <magordon@microsoft.com>
2021-09-29 17:25:08 -0700

    removed unnecessary usings

--- test/WebJobs.Script.Tests/TestUtils.cs
+++ test/WebJobs.Script.Tests/TestUtils.cs
@@ -5,9 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Moq;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests
 {
