ed5a1233b06739d8af80bb355757fdfb975d3355
Michael Peng <michaelpeng@microsoft.com>
2021-10-05 17:07:29 -0700

    Removed lock from NullFunctionDataCache

--- src/WebJobs.Script/Workers/FunctionDataCache/NullFunctionDataCache.cs
+++ src/WebJobs.Script/Workers/FunctionDataCache/NullFunctionDataCache.cs
@@ -7,18 +7,6 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.FunctionDataCache
 {
     public class NullFunctionDataCache : IFunctionDataCache
     {
-        /// <summary>
-        /// Lock to be used with any operations that access state of this cache.
-        /// </summary>
-        private readonly object _lock = new object();
-
-        /// <summary>
-        /// Mapping of <see cref="FunctionDataCacheKey"/> to the <see cref="SharedMemoryMetadata"/>
-        /// which indicates where in shared memory the object exists.
-        /// Note: This must be accessed while holding the <see cref="_lock"/> lock.
-        /// </summary>
-        private readonly Dictionary<FunctionDataCacheKey, SharedMemoryMetadata> _localCache = new Dictionary<FunctionDataCacheKey, SharedMemoryMetadata>();
-
         public bool IsEnabled { get; } = false;
 
         public void Dispose()
@@ -31,17 +19,8 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.FunctionDataCache
 
         public bool TryGet(FunctionDataCacheKey cacheKey, bool isIncrementActiveReference, out SharedMemoryMetadata sharedMemoryMeta)
         {
-            try
-            {
-                lock (_lock)
-                {
-                    _localCache.TryGetValue(cacheKey, out sharedMemoryMeta);
-                    return false;
-                }
-            }
-            finally
-            {
-            }
+            sharedMemoryMeta = null;
+            return false;
         }
 
         public bool TryPut(FunctionDataCacheKey cacheKey, SharedMemoryMetadata sharedMemoryMeta, bool isIncrementActiveReference, bool isDeleteOnFailure)
