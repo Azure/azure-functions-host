55cd012e23db2aec01a68a3a2a40dc770501bc37
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-10-08 16:57:15 -0700

    Updating metadata generator runtime assembly manifest

--- test/WebJobs.Script.Tests.Integration/Storage/StorageEndToEndTests.cs
+++ test/WebJobs.Script.Tests.Integration/Storage/StorageEndToEndTests.cs
@@ -40,18 +40,20 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Integration.Storage
             {
             }
 
-            protected override ExtensionPackageReference[] GetExtensionsToInstall()
-            {
-                // This test fixture should use the most recent major version of the storage extension, so update this as required.
-                return new ExtensionPackageReference[]
-                {
-                    new ExtensionPackageReference
-                    {
-                        Id = "Microsoft.Azure.WebJobs.Extensions.Storage",
-                        Version = "4.0.4"
-                    }
-                };
-            }
+            // TODO: Metadata installation of storage packages does not currently work as expected as the 
+            // test host installs the extension by default and this may lead to mismatches.
+            //protected override ExtensionPackageReference[] GetExtensionsToInstall()
+            //{
+            //    // This test fixture should use the most recent major version of the storage extension, so update this as required.
+            //    return new ExtensionPackageReference[]
+            //    {
+            //        new ExtensionPackageReference
+            //        {
+            //            Id = "Microsoft.Azure.WebJobs.Extensions.Storage",
+            //            Version = "4.0.4"
+            //        }
+            //    };
+            //}
 
             public override void ConfigureScriptHost(IWebJobsBuilder webJobsBuilder)
             {
