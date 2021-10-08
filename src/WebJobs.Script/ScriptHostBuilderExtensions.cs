69446c7cb8d1c4176b61e0f080cafa5c5b6346e6
Brett Samblanet <brettsam@microsoft.com>
2021-10-08 07:08:51 -0700

    trying a new approach...

--- src/WebJobs.Script/ScriptHostBuilderExtensions.cs
+++ src/WebJobs.Script/ScriptHostBuilderExtensions.cs
@@ -377,7 +377,6 @@ namespace Microsoft.Azure.WebJobs.Script
 
         internal static ExtensionBundleOptions GetExtensionBundleOptions(IConfiguration configuration)
         {
-            AppContext.SetSwitch("Microsoft.Extensions.Configuration.BindSingleElementsToArray", false);
             var options = new ExtensionBundleOptions();
             var optionsSetup = new ExtensionBundleConfigurationHelper(configuration, SystemEnvironment.Instance);
             configuration.Bind(options);
