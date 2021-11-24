90e4bc3e4f80e6c4fca253def0efc8afff30a652
Alexey Rodionov <alrod@microsoft.com>
2021-11-24 13:20:12 -0800

    Revert "Removing host level retry specification"
    This reverts commit 57a5447641c4a9593ee8a0233f86dbac5521a31b.

---
+++ sample/NodeRetry/HttpTrigger-RetryHostJson/index.js
@@ -0,0 +1,22 @@
+﻿var errorString = 'An error occurred';
+var maxRetries = 2;
+
+module.exports = async function (context, req) {
+    var retryContext = context.executionContext.retryContext;
+
+    if (retryContext.maxRetryCount != maxRetries || (retryContext.retryCount > 0 && !retryContext.exception.message.includes(errorString))) {
+        debugger;
+        context.res = {
+            status: 500
+        };
+    } else {
+        context.log('JavaScript HTTP trigger function processed a request. retryCount: ' + retryContext.retryCount);
+
+        if (retryContext.retryCount < maxRetries) {
+            throw new Error(errorString);
+        }
+        context.res = {
+            body: 'retryCount: ' + retryContext.retryCount
+        };
+    }
+}
