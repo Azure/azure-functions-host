3d96563aa0b1b630d153d7ba53b0c9a463a572a8
Nick Craver <nrcraver@gmail.com>
2021-12-09 10:24:52 -0500

    ...and commit the comments

--- src/WebJobs.Script.WebHost/DependencyInjection/JobHostScopedServiceProviderFactory.cs
+++ src/WebJobs.Script.WebHost/DependencyInjection/JobHostScopedServiceProviderFactory.cs
@@ -26,6 +26,17 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.DependencyInjection
 
         public IServiceCollection CreateBuilder(IServiceCollection services) => services;
 
+        /// <summary>
+        /// This creates the service provider *and the end* of spinning up the JobHost.
+        /// When we build the ScriptHost (<see cref="DefaultScriptHostBuilder.BuildHost(bool, bool)"/>),
+        /// all services are fed in here (<paramref name="services"/>), and using that list we build
+        /// a provider that has all of the base level services that we want to copy, then adds all of the
+        /// SciptHost level services on top. It is not a proxying provider, we are copying the services
+        /// references into that (rarely - e.g. startup and specialization) created ScriptHost layer scope.
+        /// </summary>
+        /// <param name="services">The ScriptHost services to add on top of the copied root services.</param>
+        /// <returns>A provider containing the superset of base (application) level services and ScriptHost servics.</returns>
+        /// <exception cref="HostInitializationException">If service validation fails (e.g. user touches something they shouldn't have.</exception>
         public IServiceProvider CreateServiceProvider(IServiceCollection services)
         {
             try
