e7dbd3d972407ef91ccaa18e6abb19e313c46fba
Nick Craver <nrcraver@gmail.com>
2021-12-14 17:25:32 -0500

    FunctionMetrics queue: eliminate the copy
    We don't really need the List<T> here since we can swap the structure easily enough - this should net us a lot less LOH cost when the queue builds and overall better performance in the snapshot. I think we can make the LINQ quite a bit faster too, but that's a _great_ candidate to demo after #7994 goes in.

--- src/WebJobs.Script.WebHost/Models/FunctionMetrics.cs
+++ src/WebJobs.Script.WebHost/Models/FunctionMetrics.cs
@@ -14,17 +14,15 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Models
 
     public readonly struct FunctionMetrics
     {
+        public readonly long ExecutionTimeInMS;
+        public readonly ExecutionStage ExecutionStage;
+        public readonly string FunctionName;
+
         public FunctionMetrics(string functionName, ExecutionStage executionStage, long executionTimeInMS)
         {
             FunctionName = functionName;
             ExecutionStage = executionStage;
             ExecutionTimeInMS = executionTimeInMS;
         }
-
-        public string FunctionName { get; }
-
-        public ExecutionStage ExecutionStage { get; }
-
-        public long ExecutionTimeInMS { get; }
     }
 }
