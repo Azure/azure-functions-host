3ef4ee73e35c0aa018ff41a62e4c21bc9756f93d
Jameson Z <jzaballos@microsoft.com>
2022-01-11 15:20:49 -0600

    adding using statement

--- test/WebJobs.Script.Tests/Workers/Rpc/AspNetCoreGrpcServerTests.cs
+++ test/WebJobs.Script.Tests/Workers/Rpc/AspNetCoreGrpcServerTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Script.Eventing;
 using Microsoft.Azure.WebJobs.Script.Grpc;
 using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
 using Microsoft.Extensions.Logging.Abstractions;
