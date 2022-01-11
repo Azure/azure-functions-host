9f33e6686c71d889d95ac50b4f218d31f5f10082
Brett Samblanet <brettsam@microsoft.com>
2022-01-11 10:08:15 -0800

    improve scoping of FunctionRpcService; remove call to CreateDefaultBuilder (#8050)

---
+++ test/WebJobs.Script.Tests/Workers/Rpc/AspNetCoreGrpcServerTests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Script.Grpc;
+using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Workers.Rpc
+{
+    public class AspNetCoreGrpcServerTests
+    {
+        [Fact]
+        public void CleanDisposal()
+        {
+            var server = new AspNetCoreGrpcServer(new Mock<FunctionRpc.FunctionRpcBase>().Object, new TestScriptEventManager(), NullLogger<AspNetCoreGrpcServer>.Instance);
+            server.Dispose();
+        }
+
+        [Fact]
+        public async Task CleanDisposalAsync()
+        {
+            var server = new AspNetCoreGrpcServer(new Mock<FunctionRpc.FunctionRpcBase>().Object, new TestScriptEventManager(), NullLogger<AspNetCoreGrpcServer>.Instance);
+            await server.DisposeAsync();
+        }
+    }
+}
