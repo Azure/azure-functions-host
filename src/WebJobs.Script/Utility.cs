494c85bc6904765fb514e3de9262e448b5f9520c
Nick Craver <nickcraver@microsoft.com>
2022-01-11 20:21:50 -0500

    Earlier iteration cleanup

--- src/WebJobs.Script/Utility.cs
+++ src/WebJobs.Script/Utility.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Diagnostics;
 using System.Dynamic;
 using System.Globalization;
 using System.IO;
