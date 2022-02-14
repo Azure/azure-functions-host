71dd6848261b05a8c14a9d3b2840e893fdfc5a7d
Tsuyoshi Ushio <ushio@simplearchitect.com>
2022-02-14 10:34:59 -0800

    Introduce admin/host/config API

---
+++ src/WebJobs.Script/IHostOptionsProvider.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.WebJobs.Script
+{
+    public interface IHostOptionsProvider
+    {
+        JObject GetOptions();
+    }
+}
