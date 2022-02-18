b47996204a73d64400fa51964b63e2b600d85599
Jameson Z <jzaballos@microsoft.com>
2022-02-18 13:46:24 -0600

    fixing to be release only, adding other test to unit tests per Pragna

--- test/WebJobs.Script.Tests/PreJit/JitReadTest.cs
+++ test/WebJobs.Script.Tests/PreJit/JitReadTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -13,25 +13,20 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.PreJIT
 {
     [Trait(TestTraits.Category, TestTraits.EndToEnd)]
     [Trait(TestTraits.Group, TestTraits.ReleaseTests)]
-    public class JitPreparesTest
+    public class JitReadTest
     {
         [Theory]
-        [InlineData(WarmUpConstants.JitTraceFileName, 1.0)]
-        [InlineData(WarmUpConstants.LinuxJitTraceFileName, 1.0)]
-        public void ColdStart_JitFailuresTest(string fileName, double threshold)
+        [InlineData(WarmUpConstants.JitTraceFileName)]
+        [InlineData(WarmUpConstants.LinuxJitTraceFileName)]
+        public void ColdStart_JitFailuresTest(string fileName)
         {
             var path = Path.Combine(Path.GetDirectoryName(new Uri(typeof(HostWarmupMiddleware).Assembly.CodeBase).LocalPath), WarmUpConstants.PreJitFolderName, fileName);
 
             var file = new FileInfo(path);
 
             Assert.True(file.Exists, $"Expected PGO file '{file.FullName}' does not exist. The file was either renamed or deleted.");
-
-            JitTraceRuntime.Prepare(file, out int successfulPrepares, out int failedPrepares);
-
-            var failurePercentage = (double)failedPrepares / successfulPrepares * 100;
-
-            // using 1% as approximate number of allowed failures before we need to regenrate a new PGO file.
-            Assert.True(failurePercentage < threshold, $"Number of failed PGOs are more than {threshold} percent! Current number of failures are {failedPrepares}. This will definitely impact cold start! Time to regenrate PGOs and update the {fileName} file!");
+            var lineCount = File.ReadAllLines(path).Length;
+            Assert.True(lineCount > 6500, $"Jit Trace file line count of {lineCount} for {fileName} is less than 6500 lines! There is likely a bug removing lines from the linux trace.");
         }
     }
 }
