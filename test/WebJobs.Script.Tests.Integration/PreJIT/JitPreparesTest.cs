28252012286314b4690d508e95f08d8b3aa603c0
Jameson Z <89222642+VpOfEngineering@users.noreply.github.com>
2022-02-23 10:21:22 -0600

    Update Jit Trace Integration Test (#8152)
    * initial commit
    
    * Update common.props
    
    * removing threshold parameter
    
    * removing requirement for release branch for Release Verification Test
    
    * more information log
    
    * more descriptive log
    
    * fixing to be release only, adding other test to unit tests per Pragna
    
    * moving test back to JitPreparesTest
    
    * testing per-test release trait
    
    * moving to % of failures for windows and file size for Linux
    
    * Update common.props
    
    Testing theory.
    
    * Ok! They have to be different names!
    
    * Update common.props

--- test/WebJobs.Script.Tests.Integration/PreJIT/JitPreparesTest.cs
+++ test/WebJobs.Script.Tests.Integration/PreJIT/JitPreparesTest.cs
@@ -12,13 +12,12 @@ using Xunit;
 namespace Microsoft.Azure.WebJobs.Script.Tests.PreJIT
 {
     [Trait(TestTraits.Category, TestTraits.EndToEnd)]
-    [Trait(TestTraits.Group, TestTraits.ReleaseTests)]
     public class JitPreparesTest
     {
         [Theory]
         [InlineData(WarmUpConstants.JitTraceFileName, 1.0)]
-        [InlineData(WarmUpConstants.LinuxJitTraceFileName, 1.0)]
-        public void ColdStart_JitFailuresTest(string fileName, double threshold)
+        [Trait(TestTraits.Group, TestTraits.ReleaseTests)]
+        public void ColdStart_JitFailuresTest_Windows(string fileName, double threshold)
         {
             var path = Path.Combine(Path.GetDirectoryName(new Uri(typeof(HostWarmupMiddleware).Assembly.CodeBase).LocalPath), WarmUpConstants.PreJitFolderName, fileName);
 
@@ -32,6 +31,19 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.PreJIT
 
             // using 1% as approximate number of allowed failures before we need to regenrate a new PGO file.
             Assert.True(failurePercentage < threshold, $"Number of failed PGOs are more than {threshold} percent! Current number of failures are {failedPrepares}. This will definitely impact cold start! Time to regenrate PGOs and update the {fileName} file!");
+        }
+
+        [Theory]
+        [InlineData(WarmUpConstants.LinuxJitTraceFileName)]
+        public void ColdStart_JitFailuresTest_Linux(string fileName)
+        {
+            var path = Path.Combine(Path.GetDirectoryName(new Uri(typeof(HostWarmupMiddleware).Assembly.CodeBase).LocalPath), WarmUpConstants.PreJitFolderName, fileName);
+
+            var file = new FileInfo(path);
+
+            Assert.True(file.Exists, $"Expected PGO file '{file.FullName}' does not exist. The file was either renamed or deleted.");
+            var lineCount = File.ReadAllLines(path).Length;
+            Assert.True(lineCount > 6500, $"Jit Trace file line count of {lineCount} for {fileName} is less than 6500 lines! There is likely a bug removing lines from the linux trace.");
         }
     }
 }
