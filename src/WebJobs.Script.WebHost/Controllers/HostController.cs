db84c66c7cc149ae4263bd79521663bda05cb645
Paula Gombar <gombarica@gmail.com>
2022-03-09 15:00:37 -0800

    update comment

--- src/WebJobs.Script.WebHost/Controllers/HostController.cs
+++ src/WebJobs.Script.WebHost/Controllers/HostController.cs
@@ -65,7 +65,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
         [TypeFilter(typeof(EnableDebugModeFilter))]
         public async Task<IActionResult> GetHostStatus([FromServices] IScriptHostManager scriptHostManager, [FromServices] IHostIdProvider hostIdProvider, [FromServices] IServiceProvider serviceProvider = null)
         {
-            // When making changes to HostStatus, ensure that you update the HostStatus class in Antares as well: https://msazure.visualstudio.com/One/_git/AAPT-Antares-Websites?path=/src/Hosting/AdministrationService/Microsoft.Web.Hosting.Administration.Api/WebEntities/Kudu/HostStatus.cs
+            // When making changes to HostStatus, ensure that you update the HostStatus class in AAPT-Antares-Websites repo as well.
             var status = new HostStatus
             {
                 State = scriptHostManager.State.ToString(),
