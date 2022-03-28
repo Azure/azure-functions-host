f320d1e9e1dd2c003c2a9911177940c7a1b426d4
Lilian Kasem <likasem@microsoft.com>
2022-03-28 13:06:40 -0700

    cleanup error message

--- src/WebJobs.Script.WebHost/Diagnostics/Extensions/ScriptHostServiceSecurityExtension.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/Extensions/ScriptHostServiceSecurityExtension.cs
@@ -9,18 +9,17 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics.Extensions
     public static class ScriptHostServiceSecurityExtension
     {
         // EventId range is 600-699
-
         private static readonly Action<ILogger, string, string, Exception> _blobStorageSecretRepoError =
             LoggerMessage.Define<string, string>(
                 LogLevel.Error,
                 new EventId(600, nameof(BlobStorageSecretRepoError)),
-                "There was an error performing a {operation} operation on the Blob Storage Secret Repository. Please ensure the '{appSettingName}' connection string is valid. {exception}.");
+                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.\nException: {exception}.");
 
         private static readonly Action<ILogger, string, string, Exception> _blobStorageSecretSasRepoError =
             LoggerMessage.Define<string, string>(
                 LogLevel.Error,
                 new EventId(601, nameof(BlobStorageSecretSasRepoError)),
-                "There was an error performing a {operation} operation on the Blob Storage Secret Repository. Please ensure the '{appSettingName}' SAS URL has Read, Write, and List permissions. {exception}");
+                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.\nException: {exception}");
 
         public static void BlobStorageSecretRepoError(this ILogger logger, string operation, string appSettingName, Exception exception)
         {
