76fc9672fd6cfcbfa632bbb21b2ab64e3c90b3c3
Lilian Kasem <likasem@microsoft.com>
2022-03-29 19:02:37 -0700

    remove exception string

--- src/WebJobs.Script.WebHost/Diagnostics/Extensions/ScriptHostServiceSecurityExtension.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/Extensions/ScriptHostServiceSecurityExtension.cs
@@ -13,13 +13,13 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics.Extensions
             LoggerMessage.Define<string, string>(
                 LogLevel.Error,
                 new EventId(600, nameof(BlobStorageSecretRepoError)),
-                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.\nException: {exception}.");
+                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.");
 
         private static readonly Action<ILogger, string, string, Exception> _blobStorageSecretSasRepoError =
             LoggerMessage.Define<string, string>(
                 LogLevel.Error,
                 new EventId(601, nameof(BlobStorageSecretSasRepoError)),
-                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.\nException: {exception}");
+                "There was an error performing a {operation} operation on the Blob Storage Secret Repository.");
 
         public static void BlobStorageSecretRepoError(this ILogger logger, string operation, string appSettingName, Exception exception)
         {
