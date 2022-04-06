5561415ffd10e12fa5f4c9d203cf02a9cfdac342
Rohit Ranjan <roranjan@microsoft.com>
2022-04-05 23:39:51 -0700

    Try/Finally

--- src/WebJobs.Script.WebHost/Controllers/FunctionsController.cs
+++ src/WebJobs.Script.WebHost/Controllers/FunctionsController.cs
@@ -137,10 +137,10 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Controllers
                         {
                             // Current activity is null due to SuppressFlow. Start a new activity and set baggage so that it's included in the custom dimensions.
                             Activity activity = new Activity($"{nameof(FunctionsController)}.Invoke");
+                            activity?.Start();
+                            activity?.AddBaggage(ScriptConstants.LiveLogsSessionAIKey, sessionId);
                             try
                             {
-                                activity?.Start();
-                                activity?.AddBaggage(ScriptConstants.LiveLogsSessionAIKey, sessionId);
                                 await scriptHost.CallAsync(function.Name, arguments);
                             }
                             finally
