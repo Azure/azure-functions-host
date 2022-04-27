894bd3d3fb582593874cb04d4f27ec8a2802d570
Alexey Rodionov <alrod@inbox.ru>
2022-04-27 09:42:27 -0700

    Updating Microsoft.Azure.WebJobs to 3.0.33-11924 (#8324)

---
+++ sample/NodeRetry/HttpTrigger-RetryWarning/index.js
@@ -0,0 +1,12 @@
+﻿module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    if (context.executionContext.retryContext) {
+        context.res = {
+            status: 500
+        };
+    } else {
+        context.res = {
+            body: 'OK'
+        };
+    }
+}
