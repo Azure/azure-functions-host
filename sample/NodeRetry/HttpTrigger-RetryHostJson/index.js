744401c80a5c6e6138e857812cbba3a11945fb55
Alexey Rodionov <alrod@microsoft.com>
2022-05-16 15:15:59 -0700

    Revert "Updating Microsoft.Azure.WebJobs to 3.0.33-11924 (#8324)"
    This reverts commit c7535d6b55b3d44da0093461325d5fc6ef37db97.

--- sample/NodeRetry/HttpTrigger-RetryHostJson/index.js
+++ sample/NodeRetry/HttpTrigger-RetryHostJson/index.js
@@ -1,19 +1,22 @@
 ﻿var errorString = 'An error occurred';
-var maxRetries = 4;
+var maxRetries = 2;
 
-module.exports = async function (context, timer) {
+module.exports = async function (context, req) {
     var retryContext = context.executionContext.retryContext;
 
     if (retryContext.maxRetryCount != maxRetries || (retryContext.retryCount > 0 && !retryContext.exception.message.includes(errorString))) {
-        console.log('Unexpected error');
-        throw 'Unexpected error';
+        debugger;
+        context.res = {
+            status: 500
+        };
     } else {
         context.log('JavaScript HTTP trigger function processed a request. retryCount: ' + retryContext.retryCount);
 
         if (retryContext.retryCount < maxRetries) {
-            console.log(errorString);
-            throw errorString;
+            throw new Error(errorString);
         }
-        console.log('Execution completed');
+        context.res = {
+            body: 'retryCount: ' + retryContext.retryCount
+        };
     }
 }
