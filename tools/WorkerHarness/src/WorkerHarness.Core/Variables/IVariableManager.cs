788f3dfd80f4a4ede311cff2f31982c0b6198346
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-06-15 09:44:27 -0700

    Add infrastructure to resolve variables

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/Variables/IVariableManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerHarness.Core
+{
+    internal interface IVariableManager
+    {
+        void Subscribe(Expression expression);
+
+        void AddVariable(string variableName, object variableValue);
+    }
+}
