b1457f016548a6602df9bab703f3f8e61ae57480
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-06-26 10:48:07 -0700

    Add RpcActionMessageType.cs

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/Actions/Messages/RpcActionMessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerHarness.Core
+{
+    internal class RpcActionMessageType
+    {
+        public static string Incoming = "incoming";
+
+        public static string Outgoing = "outgoing";
+    }
+}
