11589e344f67166c645802cd4db12aab6036948a
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-06-27 11:37:39 -0700

    Created an ActionResult.cs class to store the result of executing an action

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/Actions/ActionResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerHarness.Core
+{
+    enum StatusCode
+    {
+        Success,
+        Error
+    }
+
+    internal class ActionResult
+    {
+        public string ActionType { get; set; }
+
+        public string ActionName { get; set; }
+
+        public StatusCode Status { get; set; }
+
+        public IEnumerable<string> Messages { get; set; }
+
+        public ActionResult(string actionType, string actionName)
+        {
+            ActionType = actionType;
+            ActionName = actionName;
+            Messages = new List<string>();
+        }
+    }
+}
