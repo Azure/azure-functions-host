f9b3bddbf9a1daa607ee8a6cd90e44a95e9c68e5
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-06-29 16:05:55 -0700

    Create a separate service to validate user arguments that were captured in HarnessOptions

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/HarnessOptions/IHarnessOptionsValidate.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace WorkerHarness.Core
+{
+    /// <summary>
+    /// Provides an abtraction to validate a HarnessOptions object
+    /// </summary>
+    public interface IHarnessOptionsValidate
+    {
+        /// <summary>
+        /// Validate a HarnessOptions object
+        /// </summary>
+        /// <param name="options" cref="HarnessOptions"></param>
+        /// <returns></returns>
+        bool Validate(HarnessOptions options);
+    }
+}
