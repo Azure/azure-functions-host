142b23179cdb7c9696b0d644905cd1c3471e43dc
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-06-29 08:35:00 -0700

    Delete IWorkerDescriptionBuilder and WorkerDescriptionBuilder

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/WorkerDescription.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace WorkerHarness.Core
+{
+    /// <summary>
+    /// WorkerDescription encapsulates information about a language worker
+    /// such as the language type, the executable path for that worker,
+    /// the worker binary file location and the worker directory
+    /// </summary>
+    public class WorkerDescription
+    {
+        public string? Language { get; set; }
+
+        public string? DefaultExecutablePath { get; set; }
+
+        public string? DefaultWorkerPath { get; set; }
+
+        public string? WorkerDirectory { get; set; }
+
+    }
+}
