7d8bff0e7489a3cd91187ddc2b9996c993777d74
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-02 23:18:50 -0700

    Create a separate class for RpcActionError

--- tools/WorkerHarness/src/WorkerHarness.Core/Actions/ActionResult.cs
+++ tools/WorkerHarness/src/WorkerHarness.Core/Actions/ActionResult.cs
@@ -9,45 +9,7 @@ namespace WorkerHarness.Core
         Failure,
     }
 
-    internal enum ErrorCode
-    {
-        None,
-        Message_Not_Sent_Error,
-        Message_Not_Received_Error,
-        Validation_Error,
-    }
-
-    internal class Error
-    {
-        internal ErrorCode Type { get; set; } = ErrorCode.None;
-
-        internal string ConciseMessage { get; set; } = string.Empty;
-
-        internal string VerboseMessage { get; set; } = string.Empty;
-
-        internal string Advice { get; set; } = string.Empty;
-
-    }
-
-    internal static class ActionConstants
-    {
-        public static string MessageNotReceived = "No \"{0}\" message from language worker matches the given criteria.";
-        // TODO: to replace with real link
-        public static string MessageNotReceivedLink = "https://github.com/azure/azure-functions-host";
-
-        public static string ValidationFailed = "The \"{0}\" message that matches the given criteria fails validation.";
-        // TODO: to replace with real link
-        public static string ValidationFailedLink = "https://github.com/azure/azure-functions-host";
-
-        public static string MessageNotSent = "The \"{0}\" message fails to send to languague worker.";
-        // TODO: to replace with real link
-        public static string MessageNotSentLink = "https://github.com/azure/azure-functions-host";
-
-        public static string GeneralErrorAdvice = "For more information on {0}, please visit {1}";
-
-    }
-
-    public class ActionStatus
+    public class ActionResult
     {
         public StatusCode Status { get; set; }
 
---
+++ tools/WorkerHarness/src/WorkerHarness.Core/Actions/RpcActionError.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace WorkerHarness.Core
+{
+    internal class RpcActionError
+    {
+        internal RpcErrorCode Type { get; set; } = RpcErrorCode.None;
+
+        internal string ConciseMessage { get; set; } = string.Empty;
+
+        internal string VerboseMessage { get; set; } = string.Empty;
+
+        internal string Advice { get; set; } = string.Empty;
+    }
+
+    internal enum RpcErrorCode
+    {
+        None,
+        Message_Not_Sent_Error,
+        Message_Not_Received_Error,
+        Validation_Error,
+    }
+
+    internal static class RpcErrorConstants
+    {
+        public static string MessageNotReceived = "No \"{0}\" message from language worker matches the given criteria.";
+        // TODO: to replace with real link
+        public static string MessageNotReceivedLink = "https://github.com/azure/azure-functions-host";
+
+        public static string ValidationFailed = "The \"{0}\" message that matches the given criteria fails validation.";
+        // TODO: to replace with real link
+        public static string ValidationFailedLink = "https://github.com/azure/azure-functions-host";
+
+        public static string MessageNotSent = "The \"{0}\" message fails to send to languague worker.";
+        // TODO: to replace with real link
+        public static string MessageNotSentLink = "https://github.com/azure/azure-functions-host";
+
+        public static string GeneralErrorAdvice = "For more information on {0}, please visit {1}";
+
+    }
+}
