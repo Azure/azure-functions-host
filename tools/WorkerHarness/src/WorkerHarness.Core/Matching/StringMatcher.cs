44b7d219f193280f5aa20bf9038e84798e80dde4
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-06 13:49:03 -0700

    Remove comments and add license comments

--- tools/WorkerHarness/src/WorkerHarness.Core/Matching/StringMatcher.cs
+++ tools/WorkerHarness/src/WorkerHarness.Core/Matching/StringMatcher.cs
@@ -1,45 +1,14 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 using WorkerHarness.Core.Commons;
 
 namespace WorkerHarness.Core
 {
-    /// <summary>
-    /// Provides a matching service that uses string comparison
-    /// </summary>
     public class StringMatcher : IMatcher
     {
-        private readonly string objectVariablePattern = @"^(((\$\{)([^\{\}]+)(\}\.*))|(\$\.))";
-
         public bool Match(MatchingContext match, object source)
         {
-            //// find all properties to index in match.query
-            //string replacement = Regex.Replace(match.Query, objectVariablePattern, string.Empty);
-            //string[] properties = replacement.Split(".");
-
-            //// convert the actual object to a JsonNode
-            //JsonSerializerOptions options = new();
-            //options.Converters.Add(new JsonStringEnumConverter());
-            //string jsonString = JsonSerializer.Serialize(source, options);
-            //JsonNode jsonNode = JsonNode.Parse(jsonString) ?? throw new InvalidOperationException($"Unable to convert a string to a JsonNode object");
-
-            //// recursively index into jsonNode to find the actual string data
-            //string actual = VariableHelper.RecursiveIndex(jsonNode, properties, 0).GetValue<string>() ??
-            //    throw new InvalidDataException($"Unable to find a string value for the query {match.Query}");
-
-            //// if the expected value in a match still has unresolved dependency, return false
-            //if (!match.TryEvaluate(out string? expected))
-            //{
-            //    return false;
-            //}
-
-            //return actual.Equals(expected);
-
             try
             {
                 string queryResult = source.Query(match.Query);
--- tools/WorkerHarness/src/WorkerHarness.Core/Validators/StringValidator.cs
+++ tools/WorkerHarness/src/WorkerHarness.Core/Validators/StringValidator.cs
@@ -4,59 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using WorkerHarness.Core.Commons;
 
 namespace WorkerHarness.Core.Validators
 {
     internal class StringValidator : IValidator
     {
-        /// <summary>
-        /// Validate a message given a validation context.
-        /// The validation context contains 2 fields:
-        ///     - expected: the expected string value
-        ///     - query: the query that identify a string value inside message
-        ///         E.g. message = {A: {B: "hello"}}. Then query "message.A.B" yields "hello"
-        ///         
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidDataException"></exception>
-        /// <exception cref="InvalidOperationException"></exception>
         public bool Validate(ValidationContext context, object message)
         {
-            //// context has the query and and expected value
-            //if (context.Query == null || context.Expected == null)
-            //{
-            //    throw new InvalidDataException($"Either {nameof(context.Query)} or {nameof(context.Expected)} is null. Both are required for validation");
-            //}
-
-            //// index into message to find the property to validate
-            //// 1. extract properties from context.Query and put them to an array. E.g "root.A.B.C" => [A, B, C]
-            //int firstDotPosition = context.Query.IndexOf(".");
-            //string propertiesInString = context.Query[(firstDotPosition + 1)..];
-            //string[] propertiesInArray = propertiesInString.Split(".");
-
-            //// convert message to a JsonNode
-            //JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
-            //options.Converters.Add(new JsonStringEnumConverter());
-            //string messageAsJsonString = JsonSerializer.Serialize(message, options);
-            //JsonNode messageAsJsonNode = JsonNode.Parse(messageAsJsonString) ?? throw new InvalidOperationException($"Cannot convert a string to a JsonNode");
-
-            //// call VariableHelper.RecursiveIndex to find the actual value
-            //JsonNode actualValueAsObject = VariableHelper.RecursiveIndex(messageAsJsonNode, propertiesInArray, 0);
-            //// try to extract a string value from JsonNode. If JsonNode is not of type JsonValue, the underlying value is not a string
-            //if (actualValueAsObject is not JsonValue)
-            //{
-            //    throw new InvalidDataException($"The current query {context.Query} return a non-string object. The application only support string validation");
-            //}
-            //string actualValueAsString = actualValueAsObject.AsValue().ToString();
-
-            //return actualValueAsString == context.Expected;
-
             try
             {
                 string query = context.Query;
