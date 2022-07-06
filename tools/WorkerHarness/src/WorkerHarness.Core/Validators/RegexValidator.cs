c98242790833f75096ea91a2bd7387dd662cd80d
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-06 12:50:50 -0700

    Use ObjectQuery in RegexValidator.cs and StringValidator.cs

--- tools/WorkerHarness/src/WorkerHarness.Core/Validators/RegexValidator.cs
+++ tools/WorkerHarness/src/WorkerHarness.Core/Validators/RegexValidator.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WorkerHarness.Core.Commons;
 
 namespace WorkerHarness.Core.Validators
 {
@@ -27,34 +28,46 @@ namespace WorkerHarness.Core.Validators
         /// <exception cref="NotImplementedException"></exception>
         public bool Validate(ValidationContext context, object message)
         {
-            // context has the query and and expected value
-            if (context.Query == null || context.Expected == null)
+            //// context has the query and and expected value
+            //if (context.Query == null || context.Expected == null)
+            //{
+            //    throw new InvalidDataException($"Either {nameof(context.Query)} or {nameof(context.Expected)} is null. Both are required for validation");
+            //}
+
+            //// index into message to find the property to validate
+            //// 1. extract properties from context.Query and put them to an array. E.g "root.A.B.C" => [A, B, C]
+            //int firstDotPosition = context.Query.IndexOf(".");
+            //string propertiesInString = context.Query[(firstDotPosition + 1)..];
+            //string[] propertiesInArray = propertiesInString.Split(".");
+
+            //// convert message to a JsonNode
+            //JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+            //options.Converters.Add(new JsonStringEnumConverter());
+            //string messageAsJsonString = JsonSerializer.Serialize(message, options);
+            //JsonNode messageAsJsonNode = JsonNode.Parse(messageAsJsonString) ?? throw new InvalidOperationException($"Cannot convert a string to a JsonNode");
+
+            //// call VariableHelper.RecursiveIndex to find the actual value
+            //JsonNode actualValueAsObject = VariableHelper.RecursiveIndex(messageAsJsonNode, propertiesInArray, 0);
+            //// try to extract a string value from JsonNode. If JsonNode is not of type JsonValue, the underlying value is not a string
+            //if (actualValueAsObject is not JsonValue)
+            //{
+            //    throw new InvalidDataException($"The current query {context.Query} return a non-string object. The application only support string validation");
+            //}
+            //string actualValueAsString = actualValueAsObject.AsValue().ToString();
+
+            //return Regex.IsMatch(actualValueAsString, context.Expected);
+
+            try
             {
-                throw new InvalidDataException($"Either {nameof(context.Query)} or {nameof(context.Expected)} is null. Both are required for validation");
+                string query = context.Query;
+                string queryResult = message.Query(query);
+
+                return Regex.IsMatch(queryResult, context.Expected);
             }
-
-            // index into message to find the property to validate
-            // 1. extract properties from context.Query and put them to an array. E.g "root.A.B.C" => [A, B, C]
-            int firstDotPosition = context.Query.IndexOf(".");
-            string propertiesInString = context.Query[(firstDotPosition + 1)..];
-            string[] propertiesInArray = propertiesInString.Split(".");
-
-            // convert message to a JsonNode
-            JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
-            options.Converters.Add(new JsonStringEnumConverter());
-            string messageAsJsonString = JsonSerializer.Serialize(message, options);
-            JsonNode messageAsJsonNode = JsonNode.Parse(messageAsJsonString) ?? throw new InvalidOperationException($"Cannot convert a string to a JsonNode");
-
-            // call VariableHelper.RecursiveIndex to find the actual value
-            JsonNode actualValueAsObject = VariableHelper.RecursiveIndex(messageAsJsonNode, propertiesInArray, 0);
-            // try to extract a string value from JsonNode. If JsonNode is not of type JsonValue, the underlying value is not a string
-            if (actualValueAsObject is not JsonValue)
+            catch (ArgumentException ex)
             {
-                throw new InvalidDataException($"The current query {context.Query} return a non-string object. The application only support string validation");
+                throw ex;
             }
-            string actualValueAsString = actualValueAsObject.AsValue().ToString();
-
-            return Regex.IsMatch(actualValueAsString, context.Expected);
         }
     }
 }
