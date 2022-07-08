0feec44d3b8cc485a6cace35d90fddc76eb78880
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-08 16:32:20 -0700

    Put IMatcher.cs, MatchingContext.cs, and StringMatcher.cs into WorkerHarness.Core.Matching assembly; Added unit test for MatchingContext.cs

---
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Matching/MatchingContextTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using WorkerHarness.Core.Matching;
+
+namespace WorkerHarness.Core.Tests.Matching
+{
+    [TestClass]
+    public class MatchingContextTests
+    {
+        [TestMethod]
+        public void Constructor_DefaultToStringType()
+        {
+            // Arrage + Act
+            MatchingContext matchingContext = new();
+
+            // Assert
+            Assert.AreEqual("string", matchingContext.Type);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("hello")]
+        [DataRow("${dog}.@{attribute}.color")]
+        public void ConstructExpression_ExpressionPropertyMatchesExpectedProperty(string expected)
+        {
+            // Arrange
+            MatchingContext matchingContext = new()
+            {
+                Expected = expected
+            };
+
+            // Act
+            matchingContext.ConstructExpression();
+
+            // Assert
+            Assert.AreEqual(expected, matchingContext.Expression);
+        }
+    }
+}
