2f9189bc86486f09c3c236b32e3aa1822dbd907e
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-10 12:37:28 -0700

    Added unit tests for StringValidatorTests.cs

--- tools/WorkerHarness/test/WorkerHarness.Core.Tests/Usings.cs
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Usings.cs
@@ -4,32 +4,20 @@ namespace WorkerHarness.Core.Tests.Helpers
 {
     public class WeatherForecast
     {
-        public Location? Location { get; set; }
-        public int TemperatureInFahrenheit { get; set; }
-        public string? Summary { get; set; }
+        public Location Location { get; set; } = new Location();
+        public int TemperatureInFahrenheit { get; set; } = 73;
+        public string Summary { get; set; } = "Cloudy, Rainy";
 
         public static object CreateWeatherForecastObject()
         {
-            var obj = new WeatherForecast()
-            {
-                Location = new Location()
-                {
-                    City = "Redmond",
-                    State = "WA",
-                    ZipCode = "98052"
-                },
-                TemperatureInFahrenheit = 73,
-                Summary = "Cloudy, Rainy"
-            };
-
-            return obj;
+            return new WeatherForecast();
         }
     }
 
     public class Location
     {
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? ZipCode { get; set; }
+        public string City { get; set; } = "Redmond";
+        public string State { get; set; } = "WA";
+        public string ZipCode { get; set; } = "98052";
     }
 }
