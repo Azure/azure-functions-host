62a9fbebf5ae817ec49833c9895c171e4a6c8e5f
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-13 09:13:48 -0700

    Add IPayloadVariableSolver.cs and PayloadVariableSolver.cs; add unit tests

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/GrpcService/IPayloadVariableSolver.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Text.Json.Nodes;
+using WorkerHarness.Core.Variables;
+
+namespace WorkerHarness.Core.GrpcService
+{
+    /// <summary>
+    /// Abtraction to solve variables in a message payload
+    /// </summary>
+    public interface IPayloadVariableSolver
+    {
+        /// <summary>
+        /// Solve variables in a JsonNode payload.
+        /// </summary>
+        /// <param name="newPayload" cref="JsonNode">a new payload where all variables in payload are solved; empty payload otherwise</param>
+        /// <param name="payload" cref="JsonNode">a payload that has zero or more variables</param>
+        /// <param name="variableObservable" cref="IVariableObservable">store variables and their values</param>
+        /// <returns>true if all variables in payload are solved, false otherwise</returns>
+        bool SolveVariables(out JsonNode newPayload, JsonNode payload, IVariableObservable variableObservable);
+    }
+}
