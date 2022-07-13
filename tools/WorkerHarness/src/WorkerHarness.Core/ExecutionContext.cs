ac69eab685dfece9114eaee09405f74467b519b0
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-12 17:03:00 -0700

    Introduce ExecutionContext.cs for WorkerHarnessExecutor.cs

---
+++ tools/WorkerHarness/src/WorkerHarness.Core/ExecutionContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using WorkerHarness.Core.Variables;
+
+namespace WorkerHarness.Core
+{
+    public class ExecutionContext
+    {
+        public IVariableObservable GlobalVariables { get; private set; }
+
+        public ExecutionContext(IVariableObservable variableManager)
+        {
+            GlobalVariables = variableManager;
+        }
+    }
+}
