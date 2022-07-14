9f8f517ad539786bd662813d2d75c8ba32c9aea4
Henry Hamid Safi <hamids@microsoft.com>
2022-07-14 11:34:38 -0700

    Best effort to avoid GC during cold start specialization (#8521) (#8547)

--- src/WebJobs.Script.WebHost/WebJobsScriptHostService.cs
+++ src/WebJobs.Script.WebHost/WebJobsScriptHostService.cs
@@ -161,7 +161,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
         {
             get
             {
-                return _healthMonitorOptions.Value.Enabled && _environment.IsAppService();
+                return _healthMonitorOptions.Value.Enabled && _environment.IsAppService() && !_scriptWebHostEnvironment.InStandbyMode;
             }
         }
 
