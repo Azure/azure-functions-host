a06574390e78c37d8a279634b5a3d5f7b8a5cdc4
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-13 20:42:41 -0700

    Add unit tests for DelayAction.cs and DelayActionProvider.cs

---
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Actions/DelayActionProviderTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Text.Json.Nodes;
+using WorkerHarness.Core.Actions;
+
+namespace WorkerHarness.Core.Tests.Actions
+{
+    [TestClass]
+    public class DelayActionProviderTests
+    {
+        [TestMethod]
+        public void Create_ActionNodeHasNoDelayProperty_ReturnDelayAction()
+        {
+            // Arrange
+            DelayActionProvider provider = new();
+
+            JsonNode actionNode = new JsonObject
+            {
+                ["actionType"] = "delay"
+            };
+
+            // Act
+            IAction action = provider.Create(actionNode);
+
+            // Assert
+            Assert.IsTrue(action is DelayAction);
+            Assert.AreEqual(ActionTypes.Delay, provider.Type);
+            Assert.AreEqual(0, ((DelayAction) action).MilisecondsDelay);
+        }
+
+        [TestMethod]
+        public void Create_ActionNodeHasDelayProperty_ReturnDelayAction()
+        {
+            // Arrange
+            DelayActionProvider provider = new();
+
+            int milisecondsDelay = 5000;
+            JsonNode actionNode = new JsonObject
+            {
+                ["actionType"] = "delay",
+                ["delay"] = milisecondsDelay
+            };
+
+            // Act
+            IAction action = provider.Create(actionNode);
+
+            // Assert
+            Assert.IsTrue(action is DelayAction);
+            Assert.AreEqual(ActionTypes.Delay, provider.Type);
+            Assert.AreEqual(milisecondsDelay, ((DelayAction)action).MilisecondsDelay);
+        }
+    }
+}
---
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Actions/DelayActionTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using WorkerHarness.Core.Actions;
+using WorkerHarness.Core.Variables;
+
+namespace WorkerHarness.Core.Tests.Actions
+{
+    [TestClass]
+    public class DelayActionTests
+    {
+        [TestMethod]
+        public void Constructor_NegativeMilisecondsDelay_ThrowArgumentOutOfRangeException()
+        {
+            // Arrange
+            int milisecondsDelay = -10;
+
+            // Act
+            try
+            {
+                DelayAction action = new(milisecondsDelay);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(DelayAction.InvalidArgumentMessage, ex.Message);
+                return;
+            }
+
+            Assert.Fail($"The expected {typeof(ArgumentException)} is not thrown");
+        }
+
+        [TestMethod]
+        public void Constructor_PositiveMilisecondsDelay_MilisecondsDelaySet()
+        {
+            // Arrange
+            int miliseconds = 5000;
+
+            // Act
+            DelayAction action = new(miliseconds);
+
+            // Assert
+            Assert.AreEqual(miliseconds, action.MilisecondsDelay);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsync_ReturnActionResult()
+        {
+            // Arrange
+            int miliseconds = 500;
+            DelayAction action = new(miliseconds);
+
+            ExecutionContext context = new(new VariableManager());
+
+            // Act
+            ActionResult result = await action.ExecuteAsync(context);
+
+            // Assert
+            Assert.AreEqual(StatusCode.Success, result.Status);
+        }
+    }
+}
