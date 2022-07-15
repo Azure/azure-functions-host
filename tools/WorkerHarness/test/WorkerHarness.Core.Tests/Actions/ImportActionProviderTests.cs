67c92c309c5c99a0904200afa459331fd7ee639d
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-15 16:42:28 -0700

    Added ImportActionProvider.cs and unit tests

---
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Actions/ImportActionProviderTests.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Text.Json.Nodes;
+using WorkerHarness.Core.Actions;
+
+namespace WorkerHarness.Core.Tests.Actions
+{
+    [TestClass]
+    public class ImportActionProviderTests
+    {
+        [TestMethod]
+        public void Create_ActionNodeMissingScenarioFile_ThrowArgumentException()
+        {
+            // Arrange
+            JsonNode stubNode = new JsonObject
+            {
+                ["actionType"] = "import"
+            };
+
+            var mockILoggerFactory = new Mock<ILoggerFactory>();
+
+            ImportActionProvider provider = new(mockILoggerFactory.Object);
+
+            // Act
+            try
+            {
+                provider.Create(stubNode);
+            }
+            catch (ArgumentException ex)
+            {
+                StringAssert.Contains(ex.Message, ImportActionProvider.MissingScenarioFileException);
+                return;
+            }
+
+            Assert.Fail($"The expected {typeof(ArgumentException)} is not thrown");
+        }
+
+        [TestMethod]
+        public void Create_ActionNodeHasNonexistingScenarioFile_ThrowArgumentException()
+        {
+            // Arrange
+            JsonNode stubNode = new JsonObject
+            {
+                ["actionType"] = "import",
+                ["scenarioFile"] = "path\\to\\non\\existing\\file"
+            };
+
+            var mockILoggerFactory = new Mock<ILoggerFactory>();
+
+            ImportActionProvider provider = new(mockILoggerFactory.Object);
+
+            // Act
+            try
+            {
+                provider.Create(stubNode);
+            }
+            catch (ArgumentException ex)
+            {
+                StringAssert.Contains(ex.Message, ImportActionProvider.ScenarioFileDoesNotExist);
+                return;
+            }
+
+            Assert.Fail($"The expected {typeof(ArgumentException)} is not thrown");
+        }
+
+        [TestMethod]
+        public void Create_ActionNodeIsValid_ReturnImportAction()
+        {
+            // Arrange
+            JsonNode stubNode = new JsonObject
+            {
+                ["actionType"] = "import",
+                ["scenarioFile"] = Directory.GetFiles(Directory.GetCurrentDirectory())[0]
+            };
+
+            var mockILoggerFactory = new Mock<ILoggerFactory>();
+
+            ImportActionProvider provider = new(mockILoggerFactory.Object);
+
+            // Act
+            IAction action = provider.Create(stubNode);
+
+            // Assert
+            Assert.AreEqual(ActionTypes.Import, provider.Type);
+            Assert.IsTrue(action is ImportAction);
+            ImportAction importAction = (ImportAction)action;
+            Assert.AreEqual(stubNode["scenarioFile"]!.GetValue<string>(), importAction.ScenarioFile);
+            Assert.AreEqual(ActionTypes.Import, importAction.Type);
+        }
+    }
+}
