cc561da18cdb6b278069acd86ce998ff7781465e
Emma Dang <54388429+ehhdang@users.noreply.github.com>
2022-07-15 17:16:24 -0700

    Added ImportAction.cs and unit tests; Added reference to the IScenarioParser object in ExecutionContext.cs

--- tools/WorkerHarness/src/WorkerHarness.Core/Actions/ActionTypes.cs
+++ tools/WorkerHarness/src/WorkerHarness.Core/Actions/ActionTypes.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 namespace WorkerHarness.Core.Actions
 {
     internal class ActionTypes
     {
-        public static string Delay = "delay";
-        public static string Rpc = "rpc";
+        internal static string Delay = "delay";
+        internal static string Rpc = "rpc";
+        internal static string Import = "import";
     }
 }
---
+++ tools/WorkerHarness/test/WorkerHarness.Core.Tests/Actions/ImportActionTests.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+
+using Microsoft.Extensions.Logging;
+using Moq;
+using WorkerHarness.Core.Actions;
+using WorkerHarness.Core.Parsing;
+using WorkerHarness.Core.Variables;
+
+namespace WorkerHarness.Core.Tests.Actions
+{
+    [TestClass]
+    public class ImportActionTests
+    {
+        [TestMethod]
+        public async Task ExecuteAsync_AllActionsSucceed_ReturnActionResultWithStatusSuccess()
+        {
+            // Arrange
+            var mockIAction1 = new Mock<IAction>();
+            mockIAction1.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionContext>()))
+                .ReturnsAsync(new ActionResult() { Status = StatusCode.Success });
+
+            var mockIAction2 = new Mock<IAction>();
+            mockIAction2.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionContext>()))
+                .ReturnsAsync(new ActionResult() { Status = StatusCode.Success });
+
+            var stubScenario = new Scenario("test");
+            stubScenario.Actions.Add(mockIAction1.Object);
+            stubScenario.Actions.Add(mockIAction2.Object);
+
+            var mockIScenarioParser = new Mock<IScenarioParser>();
+            mockIScenarioParser.Setup(x => x.Parse(It.IsAny<string>()))
+                .Returns(stubScenario);
+
+            ExecutionContext executionContext = new(
+                new Mock<IVariableObservable>().Object,
+                mockIScenarioParser.Object
+            );
+
+            var stubLogger = new LoggerFactory().CreateLogger<ImportAction>(); 
+
+            ImportAction action = new("path\\to\\scenario\\file", stubLogger);
+
+            // Act
+            ActionResult actionResult = await action.ExecuteAsync(executionContext);
+
+            // Assert
+            Assert.AreEqual(StatusCode.Success, actionResult.Status);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsync_OneActionFails_ReturnActionResultWithStatusSuccess()
+        {
+            // Arrange
+            var mockIAction1 = new Mock<IAction>();
+            mockIAction1.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionContext>()))
+                .ReturnsAsync(new ActionResult() { Status = StatusCode.Success });
+
+            var mockIAction2 = new Mock<IAction>();
+            mockIAction2.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionContext>()))
+                .ReturnsAsync(new ActionResult() { Status = StatusCode.Failure });
+
+            var stubScenario = new Scenario("test");
+            stubScenario.Actions.Add(mockIAction1.Object);
+            stubScenario.Actions.Add(mockIAction2.Object);
+
+            var mockIScenarioParser = new Mock<IScenarioParser>();
+            mockIScenarioParser.Setup(x => x.Parse(It.IsAny<string>()))
+                .Returns(stubScenario);
+
+            ExecutionContext executionContext = new(
+                new Mock<IVariableObservable>().Object,
+                mockIScenarioParser.Object
+            );
+
+            var stubLogger = new LoggerFactory().CreateLogger<ImportAction>();
+
+            ImportAction action = new("path\\to\\scenario\\file", stubLogger);
+
+            // Act
+            ActionResult actionResult = await action.ExecuteAsync(executionContext);
+
+            // Assert
+            Assert.AreEqual(StatusCode.Failure, actionResult.Status);
+        }
+    }
+}
