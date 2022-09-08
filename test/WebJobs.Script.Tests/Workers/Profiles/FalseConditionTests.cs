70607659ac5e0d4e5f7a06aab4fe6a0cba261687
Lilian Kasem <likasem@microsoft.com>
2022-09-08 14:50:59 -0700

    adjustment

--- test/WebJobs.Script.Tests/Workers/Profiles/FalseConditionTests.cs
+++ test/WebJobs.Script.Tests/Workers/Profiles/FalseConditionTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Azure.WebJobs.Script.Workers;
+using Microsoft.Azure.WebJobs.Script.Workers.Profiles;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.Workers.Profiles
