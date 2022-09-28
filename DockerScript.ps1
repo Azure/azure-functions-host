44e847413f5584baf361ce57439f6fdeeb130d7a
Tony Choi <tonychoi@microsoft.com>
2022-09-28 15:53:46 -0700

    removed unnecessary files

--- DockerScript.ps1
+++
@@ -1,4 +0,0 @@
-dotnet publish src/WebJobs.Script.WebHost/WebJobs.Script.WebHost.csproj -c Release --runtime debian.10-x64 --output $PWD/azure-functions-host
-docker build -t customfunctionsimageforlegion .
-docker tag customfunctionsimageforlegion yogeshster/customfunctionsimage:1.1.0
-docker push yogeshster/customfunctionsimage:1.1.0
