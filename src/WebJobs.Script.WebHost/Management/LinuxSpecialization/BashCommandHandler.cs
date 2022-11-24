e9490a67bc49857ff90357a0c00446fee26a23c8
Tsuyoshi Ushio <ushio@simplearchitect.com>
2022-11-23 16:08:22 -0800

    remove unnecessary using

--- src/WebJobs.Script.WebHost/Management/LinuxSpecialization/BashCommandHandler.cs
+++ src/WebJobs.Script.WebHost/Management/LinuxSpecialization/BashCommandHandler.cs
@@ -3,9 +3,6 @@
 
 using System;
 using System.Diagnostics;
-using System.IO;
-using System.IO.Compression;
-using System.Text.RegularExpressions;
 using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.Extensions.Logging;
