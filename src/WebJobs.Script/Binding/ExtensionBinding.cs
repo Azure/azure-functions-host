11a0add2eb0c14588e5070aed3d0489782762be8
Surgupta <surgupta@microsoft.com>
2023-01-10 14:35:19 -0800

    Support collection of model-type bindings for out-of-proc workers (#8948)
    * Collection support changes

--- src/WebJobs.Script/Binding/ExtensionBinding.cs
+++ src/WebJobs.Script/Binding/ExtensionBinding.cs
@@ -111,6 +111,10 @@ namespace Microsoft.Azure.WebJobs.Script.Binding
             {
                 await BindStringAsync(context);
             }
+            else if (_binding.DefaultType == typeof(ParameterBindingData[]))
+            {
+                await BindCollectionAsync<ParameterBindingData>(context);
+            }
             else if (_binding.DefaultType == typeof(ParameterBindingData))
             {
                 await BindParameterBindingDataAsync(context);
