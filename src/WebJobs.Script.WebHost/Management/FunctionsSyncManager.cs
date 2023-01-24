999af896dca0fa801f70acaffc39e3d985892830
Krishna Jaju <krishna.jaju24@gmail.com>
2023-01-24 21:40:48 +0530

    addressing comments from data plane PR

--- src/WebJobs.Script.WebHost/Management/FunctionsSyncManager.cs
+++ src/WebJobs.Script.WebHost/Management/FunctionsSyncManager.cs
@@ -679,7 +679,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
             var url = default(string);
             if (_environment.IsKubernetesManagedHosting() || _environment.IsManagedEnvironment())
             {
-                var buildServiceHostname =
+                var buildServiceHostname = _environment.GetEnvironmentVariable("FUNCTIONS_API_SERVER") ??
                     _environment.GetEnvironmentVariable("BUILD_SERVICE_HOSTNAME");
                 if (string.IsNullOrEmpty(buildServiceHostname))
                 {
@@ -731,9 +731,9 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
 
                 if (_environment.IsManagedEnvironment())
                 {
-                    request.Headers.Add(ScriptConstants.KubernetesManagedAppName, _environment.GetEnvironmentVariable("CONTAINER_APP_NAME"));
-                    request.Headers.Add(ScriptConstants.KubernetesManagedAppNamespace, _environment.GetEnvironmentVariable("CONTAINER_APP_NAMESPACE"));
-                    request.Headers.Add("K8SE_APP_REVISION", _environment.GetEnvironmentVariable("CONTAINER_APP_REVISION"));
+                    request.Headers.Add("K8SE-APP-NAME", _environment.GetEnvironmentVariable("CONTAINER_APP_NAME"));
+                    request.Headers.Add("K8SE-APP-NAMESPACE", _environment.GetEnvironmentVariable("CONTAINER_APP_NAMESPACE"));
+                    request.Headers.Add("K8SE-APP-REVISION", _environment.GetEnvironmentVariable("CONTAINER_APP_REVISION"));
                 }
 
                 if (_environment.IsKubernetesManagedHosting())
