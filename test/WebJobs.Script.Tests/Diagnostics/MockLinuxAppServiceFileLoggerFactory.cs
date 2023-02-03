afd6c72661b8d2cec8ca6101ad57a7bf9a3ce43e
Pragna Gopa <pgopa@microsoft.com>
2023-02-03 10:46:34 -0800

    add missing files

---
+++ test/WebJobs.Script.Tests/Diagnostics/MockLinuxAppServiceFileLoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.IO.Abstractions;
+using Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Diagnostics
+{
+    public class MockLinuxAppServiceFileLoggerFactory : ILinuxAppServiceFileLoggerFactory
+    {
+        public Lazy<ILinuxAppServiceFileLogger> Create(string category, bool backoffEnabled)
+        {
+            return new Lazy<ILinuxAppServiceFileLogger>(() => new MockLinuxAppServiceFileLogger());
+        }
+    }
+}
