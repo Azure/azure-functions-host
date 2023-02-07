15cfcf1b9bb257bcb393333ec6d0346e8138957d
Naren Soni <soninaren@users.noreply.github.com>
2023-02-07 11:30:49 -0800

    v4.15.2 hotfix (#9080)
    * Reverting "Removing MultiLanguage feature flag (#8727)"
    
    * bumping the patch version

--- src/WebJobs.Script/ScriptConstants.cs
+++ src/WebJobs.Script/ScriptConstants.cs
@@ -122,6 +122,7 @@ namespace Microsoft.Azure.WebJobs.Script
         public const string FeatureFlagDisableMergedWebHostScriptHostConfiguration = "DisableMergedConfiguration";
         public const string FeatureFlagEnableWorkerIndexing = "EnableWorkerIndexing";
         public const string FeatureFlagEnableDebugTracing = "EnableDebugTracing";
+        public const string FeatureFlagEnableMultiLanguageWorker = "EnableMultiLanguageWorker";
         public const string FeatureFlagEnableProxies = "EnableProxies";
         public const string FeatureFlagEnableLinuxEPExecutionCount = "EnableLinuxFEC";
 
