fc5e9fbc8c0a88610f955387be96032486a89f62
Siddharth <siddharth@microsoft.com>
2023-02-09 13:42:49 +0530

    Revert "Reverting "Removing MultiLanguage feature flag (#8727)" (#9079)"
    This reverts commit d4655cc4fbb34fc14e6861731991118a9acd02ed.

--- src/WebJobs.Script/Environment/EnvironmentExtensions.cs
+++ src/WebJobs.Script/Environment/EnvironmentExtensions.cs
@@ -364,7 +364,7 @@ namespace Microsoft.Azure.WebJobs.Script
         {
             if (!isMultiLanguageEnabled.HasValue)
             {
-                isMultiLanguageEnabled = environment.IsLogicApp() && FeatureFlags.IsEnabled(ScriptConstants.FeatureFlagEnableMultiLanguageWorker, environment);
+                isMultiLanguageEnabled = environment.IsLogicApp();
             }
             return isMultiLanguageEnabled.Value;
         }
