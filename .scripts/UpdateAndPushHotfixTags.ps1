4f88d24d688db1df4576074f1973a1ec45675f9d
azfuncgh <azfuncgh@github.com>
2023-04-10 14:27:49 -0500

    Adding try-catch logic

--- .scripts/UpdateAndPushHotfixTags.ps1
+++ .scripts/UpdateAndPushHotfixTags.ps1
@@ -1,8 +1,10 @@
-# The below variables are passed via ADO
 git config --global user.email "azfuncgh@github.com"
 git config --global user.name "azfuncgh"
+# The below variables are passed via ADO
+$githubToken = "$(GithubPATazfuncgh)"
 $listOfTags = "$(commaSeparatedListOfTags)".split(",")
 $commitID = "$(commitId)"
+git clone https://$githubToken@github.com/Azure/azure-functions-host
 git checkout dev
 git pull
 foreach ($tag in $listOfTags)
@@ -19,5 +21,14 @@ foreach ($tag in $listOfTags)
     }
     git add .
     git commit -m "Hotfix release for $tag"
-    git push
+    try {
+        git push https://$githubToken@github.com/Azure/azure-functions-host.git
+        if (-not $?) {
+            throw "Error with git push tag!"
+        }
+    }
+    catch {
+        Write-Host $_.ScriptStackTrace
+         throw "Error occurred while trying to do the final push. Exiting"
+    }
 }
