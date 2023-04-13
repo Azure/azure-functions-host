b03b9ff57bf52dfdaae0dcc9c3cdb31b6ea240a3
Jacob Viau <javia@microsoft.com>
2023-04-13 10:31:09 -0700

    Add extension channel prototype

--- src/WebJobs.Script.Grpc/Server/Startup.cs
+++ src/WebJobs.Script.Grpc/Server/Startup.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.WebJobs.Host.Grpc;
 using Microsoft.Azure.WebJobs.Script.Grpc.Messages;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +24,10 @@ namespace Microsoft.Azure.WebJobs.Script.Grpc
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IEnumerable<IWebJobsGrpcExtension> extensions)
         {
             if (env.IsDevelopment())
             {
@@ -34,6 +39,10 @@ namespace Microsoft.Azure.WebJobs.Script.Grpc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<FunctionRpc.FunctionRpcBase>();
+                foreach (IWebJobsGrpcExtension ext in extensions)
+                {
+                    ext.Apply(endpoints);
+                }
             });
         }
     }
