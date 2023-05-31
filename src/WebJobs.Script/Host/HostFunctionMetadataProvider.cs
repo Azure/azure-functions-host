6007712be01f7f6be347d0983398175469045858
Brett Samblanet <brettsam@microsoft.com>
2023-05-31 09:09:14 -0700

    PR comments

--- src/WebJobs.Script/Host/HostFunctionMetadataProvider.cs
+++ src/WebJobs.Script/Host/HostFunctionMetadataProvider.cs
@@ -184,20 +184,22 @@ namespace Microsoft.Azure.WebJobs.Script
             // determine the script type based on the primary script file extension
             string extension = Path.GetExtension(scriptFilePath).ToLowerInvariant().TrimStart('.');
 
-            // In the special case of a null FUNCTIONS_WORKER_RUNTIME and an extension of "dll",
-            // we want to skip the worker config check (to prevent the dotnet-isolated worker from
+            // In the special case of a null `functionsWorkerRuntime` and an extension of "dll",
+            // we want to short-circuit the worker config check (to prevent the dotnet-isolated worker from
             // being chosen) and allow in-proc handling to be used.
             // Other notes:
-            // - when dotnet-isolated, the FUNCTIONS_WORKER_RUNTIME is required to be set to "dotnet-isolated"
-            // - when FUNCTIONS_WORKER_RUNTIME is set to "dotnet", workerConfigs is empty so this correctly falls
+            // - when dotnet-isolated, the `functionsWorkerRuntime` is required to be set to "dotnet-isolated"
+            // - when `functionsWorkerRuntime` is set to "dotnet", workerConfigs is empty so this correctly falls
             //   through to choose DotNetAssembly with a "dll" extension
-            if (!(functionsWorkerRuntime is null && extension == "dll"))
+            if (functionsWorkerRuntime is null && string.Equals(extension, "dll", StringComparison.OrdinalIgnoreCase))
             {
-                var workerConfig = workerConfigs.FirstOrDefault(config => config.Description.Extensions.Contains("." + extension));
-                if (workerConfig != null)
-                {
-                    return workerConfig.Description.Language;
-                }
+                return DotNetScriptTypes.DotNetAssembly;
+            }
+
+            var workerConfig = workerConfigs.FirstOrDefault(config => config.Description.Extensions.Contains($".{extension}", StringComparer.OrdinalIgnoreCase));
+            if (workerConfig != null)
+            {
+                return workerConfig.Description.Language;
             }
 
             // If no worker claimed these extensions, use in-proc.
