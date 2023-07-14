091868dcf91c7d531dc19422143d3759fe0348c7
Jacob Viau <javia@microsoft.com>
2023-07-14 15:34:02 -0700

    Add ExtensionsCompositeEndpointDataSource tests

---
+++ test/WebJobs.Script.Tests/Workers/Rpc/ExtensionsCompositeEndpointDataSourceTests.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Rpc.Core.Internal;
+using Microsoft.Azure.WebJobs.Script.Grpc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Script.Tests.Workers.Rpc
+{
+    public class ExtensionsCompositeEndpointDataSourceTests
+    {
+        [Fact]
+        public void NoActiveHost_NoEndpoints()
+        {
+            ExtensionsCompositeEndpointDataSource dataSource = new(Mock.Of<IScriptHostManager>());
+            Assert.Empty(dataSource.Endpoints);
+        }
+
+        [Fact]
+        public void ActiveHostChanged_NullHost_NoEndpoints()
+        {
+            Mock<IScriptHostManager> manager = new();
+            ExtensionsCompositeEndpointDataSource dataSource = new(manager.Object);
+
+            IChangeToken token = dataSource.GetChangeToken();
+            Assert.False(token.HasChanged);
+            manager.Raise(x => x.ActiveHostChanged += null, new ActiveHostChangedEventArgs(null, null));
+            Assert.True(token.HasChanged);
+            Assert.Empty(dataSource.Endpoints);
+        }
+
+        [Fact]
+        public void ActiveHostChanged_NoExtensions_NoEndpoints()
+        {
+            Mock<IScriptHostManager> manager = new();
+            ExtensionsCompositeEndpointDataSource dataSource = new(manager.Object);
+
+            IChangeToken token = dataSource.GetChangeToken();
+            Assert.False(token.HasChanged);
+            manager.Raise(x => x.ActiveHostChanged += null, new ActiveHostChangedEventArgs(null, GetHost()));
+            Assert.True(token.HasChanged);
+            Assert.Empty(dataSource.Endpoints);
+        }
+
+        [Fact]
+        public void ActiveHostChanged_NewExtensions_NewEndpoints()
+        {
+            Mock<IScriptHostManager> manager = new();
+            ExtensionsCompositeEndpointDataSource dataSource = new(manager.Object);
+            IHost host = GetHost(new TestEndoints(new Endpoint(null, null, "Test1"), new Endpoint(null, null, "Test2")));
+
+            IChangeToken token = dataSource.GetChangeToken();
+            Assert.False(token.HasChanged);
+            manager.Raise(x => x.ActiveHostChanged += null, new ActiveHostChangedEventArgs(null, host));
+            Assert.True(token.HasChanged);
+            Assert.Collection(
+                dataSource.Endpoints,
+                endpoint => Assert.Equal("Test1", endpoint.DisplayName),
+                endpoint => Assert.Equal("Test2", endpoint.DisplayName));
+        }
+
+        private static IHost GetHost(params WebJobsRpcEndpointDataSource[] extensions)
+        {
+            ServiceCollection services = new();
+            foreach (WebJobsRpcEndpointDataSource endpoint in extensions)
+            {
+                services.AddSingleton(endpoint);
+            }
+
+            IServiceProvider sp = services.BuildServiceProvider();
+            return Mock.Of<IHost>(x => x.Services == sp);
+        }
+
+        private class TestEndoints : WebJobsRpcEndpointDataSource
+        {
+            public TestEndoints(params Endpoint[] endpoints)
+            {
+                Endpoints = endpoints;
+            }
+
+            public override IReadOnlyList<Endpoint> Endpoints { get; }
+
+            public override IChangeToken GetChangeToken() => NullChangeToken.Singleton;
+        }
+    }
+}
