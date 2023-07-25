acbeeecdff9c25b7c02612231cfa27e42b8470c0
Sarah Vu <sarahvu@microsoft.com>
2023-07-24 17:44:32 -0700

    update capability logging tests

--- test/WebJobs.Script.Tests/Workers/Rpc/GrpcCapabilitiesTests.cs
+++ test/WebJobs.Script.Tests/Workers/Rpc/GrpcCapabilitiesTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Google.Protobuf.Collections;
 using Microsoft.Azure.WebJobs.Script.Grpc;
@@ -49,9 +51,12 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Workers.Rpc
 
             Assert.Equal(value, _capabilities.GetCapabilityState(testCapability2));
 
+            IDictionary<string, string> finalDict = new Dictionary<string, string> { { "testCapability2", value.ToString() } };
+
             var logs = _loggerProvider.GetAllLogMessages().Select(p => p.FormattedMessage).ToArray();
             Assert.Collection(logs,
-                p => Assert.Equal($"Updating capabilities: {{ \"{testCapability2}\": \"{value}\" }}", p));
+                p => Assert.Equal($"Updating capabilities using {GrpcCapabilitiesUpdateStrategy.Merge} strategy. Current: {new Dictionary<string, string>()} Incoming: {addedCapabilities}", p),
+                p => Assert.Equal($"Updated capabilities: {finalDict}", p));
         }
 
         [Fact]
@@ -84,10 +89,54 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Workers.Rpc
 
             Assert.Equal("false", _capabilities.GetCapabilityState(testCapability2));
 
+            IDictionary<string, string> firstUpdates = new Dictionary<string, string>()
+            {
+                { testCapability1, TestCapability.State1.ToString() },
+                { "testCapability2", "true" }
+            };
+
+            IDictionary<string, string> finalCapabilities = new Dictionary<string, string>()
+            {
+                { testCapability1, TestCapability.State1.ToString() },
+                { "testCapability2", "false" }
+            };
+
             var logs = _loggerProvider.GetAllLogMessages().Select(p => p.FormattedMessage).ToArray();
             Assert.Collection(logs,
-                p => Assert.Equal($"Updating capabilities: {{ \"{testCapability1}\": \"{TestCapability.State1}\", \"{testCapability2}\": \"true\" }}", p),
-                p => Assert.Equal($"Updating capabilities: {{ \"{testCapability2}\": \"false\" }}", p));
+                p => Assert.Equal($"Updating capabilities using {GrpcCapabilitiesUpdateStrategy.Merge} strategy. Current: {new Dictionary<string, string>()} Incoming: {addedCapabilities}", p),
+                p => Assert.Equal($"Updated capabilities: {firstUpdates}", p),
+                p => Assert.Equal($"Updating capabilities using {GrpcCapabilitiesUpdateStrategy.Merge} strategy. Current: {firstUpdates} Incoming: {changedCapabilities}", p),
+                p => Assert.Equal($"Updated capabilities: {finalCapabilities}", p));
+        }
+
+        [Fact]
+        public void Capability_Replace_Strategy_Handled_Correctly()
+        {
+            MapField<string, string> initialCapabilities = new MapField<string, string>
+            {
+                { testCapability2, "true" },
+                { testCapability1, "false" }
+            };
+
+            _capabilities.UpdateCapabilities(initialCapabilities, GrpcCapabilitiesUpdateStrategy.Replace);
+
+            IDictionary<string, string> initialDict = new Dictionary<string, string> { { "testCapability2", "false" } };
+
+            MapField<string, string> changedCapabilities = new MapField<string, string>
+            {
+                { testCapability2, "false" }
+            };
+
+            _capabilities.UpdateCapabilities(changedCapabilities, GrpcCapabilitiesUpdateStrategy.Replace);
+
+            IDictionary<string, string> finalDict = new Dictionary<string, string> { { "testCapability2", "false" } };
+
+            var logs = _loggerProvider.GetAllLogMessages().Select(p => p.FormattedMessage).ToArray();
+            Assert.Collection(logs,
+                p => Assert.Equal($"Updating capabilities using {GrpcCapabilitiesUpdateStrategy.Replace} strategy. Current: {new Dictionary<string, string>()} Incoming: {initialCapabilities}", p),
+                p => Assert.Equal($"Updated capabilities: {initialDict}", p),
+                p => Assert.Equal($"Updating capabilities using {GrpcCapabilitiesUpdateStrategy.Replace} strategy. Current: {new Dictionary<string, string>()} Incoming: {changedCapabilities}", p),
+                p => Assert.Equal($"Updated capabilities: {finalDict}", p));
         }
     }
 }
