379861c42201dbb296780d3ce9d11d227b6e11bb
Fabio Cavalcante <facaval@microsoft.com>
2023-09-25 18:29:40 -0700

    Upgrading to .NET 8

--- test/WebJobs.Script.Tests.Integration/GlobalSuppressions.cs
+++ test/WebJobs.Script.Tests.Integration/GlobalSuppressions.cs
@@ -3,6 +3,9 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "<Pending>", Scope = "type", Target = "~T:Microsoft.Azure.WebJobs.Script.Tests.FunctionGeneratorTests")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text must match", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1306:Field names should begin with lower-case letter", Justification = "<Pending>", Scope = "member", Target = "~F:Microsoft.Azure.WebJobs.Script.Tests.NodeScriptHostTests.Fixture")]
+[assembly: SuppressMessage("Usage", "ASP0019:Suggest using IHeaderDictionary.Append or the indexer", Justification = "<Pending>")]
