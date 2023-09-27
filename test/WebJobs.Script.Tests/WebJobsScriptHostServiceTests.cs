a1ce5f91e941d3421c3eb7a6e816ac1bebd0573e
Fabio Cavalcante <facaval@microsoft.com>
2023-09-27 15:35:43 -0700

    Fixing tests

--- test/WebJobs.Script.Tests/WebJobsScriptHostServiceTests.cs
+++ test/WebJobs.Script.Tests/WebJobsScriptHostServiceTests.cs
@@ -603,11 +603,11 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
                 {
                     _count++;
                     var services = new ServiceCollection();
-                    var rootServiceProvider = new WebHostServiceProvider(services);
+                    var rootServiceProvider = services.BuildServiceProvider();
                     var mockDepValidator = new Mock<IDependencyValidator>();
 
                     var host = new HostBuilder()
-                        .UseServiceProviderFactory(new JobHostScopedServiceProviderFactory(rootServiceProvider, rootServiceProvider, mockDepValidator.Object))
+                        .UseServiceProviderFactory(new JobHostScopedServiceProviderFactory(rootServiceProvider, services, mockDepValidator.Object))
                         .Build();
 
                     throw new InvalidOperationException("boom!");
