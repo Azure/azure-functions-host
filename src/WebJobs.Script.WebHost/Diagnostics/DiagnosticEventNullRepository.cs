6774b9292e83c01d1b242095e68e7ddf3808a844
Naren Soni <soninaren@users.noreply.github.com>
2023-11-02 16:17:05 -0700

    Fixing null reference exception when AzureWebJobsStorage is not set. (#9641)

---
+++ src/WebJobs.Script.WebHost/Diagnostics/DiagnosticEventNullRepository.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Azure.WebJobs.Script.WebHost.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
+{
+    public class DiagnosticEventNullRepository : IDiagnosticEventRepository
+    {
+        public void WriteDiagnosticEvent(DateTime timestamp, string errorCode, LogLevel level, string message, string helpLink, Exception exception) { }
+    }
+}
