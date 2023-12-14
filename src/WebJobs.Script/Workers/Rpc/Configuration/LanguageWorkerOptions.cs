2d1796481255cab34744616da3d2af3e579b9c64
Aishwarya Bhandari <aibhandari@microsoft.com>
2023-12-14 11:31:31 -0800

    changes

--- src/WebJobs.Script/Workers/Rpc/Configuration/LanguageWorkerOptions.cs
+++ src/WebJobs.Script/Workers/Rpc/Configuration/LanguageWorkerOptions.cs
@@ -2,11 +2,25 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.WebJobs.Hosting;
 
 namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc
 {
-    public class LanguageWorkerOptions
+    public class LanguageWorkerOptions : IOptionsFormatter
     {
         public IList<RpcWorkerConfig> WorkerConfigs { get; set; }
+
+        public string Format()
+        {
+            return JsonSerializer.Serialize(WorkerConfigs, typeof(List<RpcWorkerConfig>), SourceGenerationContextWorkerConfig.Default);
+        }
+    }
+
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(IList<RpcWorkerConfig>))]
+    internal partial class SourceGenerationContextWorkerConfig : JsonSerializerContext
+    {
     }
 }
