bdaf2ac00ae186cd97ef205c04bb7f9a28954531
Jacob Viau <javia@microsoft.com>
2024-01-11 09:30:42 -0800

    Add function group to metadata and sync triggers (#9777) (#9790)
    * Add function group to metadata and sync triggers
    
    * update release_notes.md
    
    * Fix csproj changes, check for webhook, only add grouping for flex
    
    * Add IsWebHookTrigger tests
    
    * Fix test build
    
    * fix tests

--- src/WebJobs.Script/Extensions/BindingMetadataExtensions.cs
+++ src/WebJobs.Script/Extensions/BindingMetadataExtensions.cs
@@ -1,12 +1,90 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
 using Microsoft.Azure.WebJobs.Script.Description;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Azure.WebJobs.Script.Extensions
 {
     public static class BindingMetadataExtensions
     {
+        private const string HttpTriggerKey = "httpTrigger";
+        private const string EventGridTriggerKey = "eventGridTrigger";
+        private const string BlobTriggerKey = "blobTrigger";
+
+        private static readonly HashSet<string> DurableTriggers = new(StringComparer.OrdinalIgnoreCase)
+        {
+            "entityTrigger",
+            "activityTrigger",
+            "orchestrationTrigger"
+        };
+
+        /// <summary>
+        /// Checks if a <see cref="BindingMetadata"/> represents an HTTP trigger.
+        /// </summary>
+        /// <param name="binding">The binding metadata to check.</param>
+        /// <returns><c>true</c> if an HTTP trigger, <c>false</c> otherwise.</returns>
+        public static bool IsHttpTrigger(this BindingMetadata binding)
+        {
+            if (binding is null)
+            {
+                throw new ArgumentNullException(nameof(binding));
+            }
+
+            return string.Equals(HttpTriggerKey, binding.Type, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Checks if a <see cref="BindingMetadata"/> represents an webhook trigger.
+        /// </summary>
+        /// <param name="binding">The binding metadata to check.</param>
+        /// <returns><c>true</c> if a webhook trigger, <c>false</c> otherwise.</returns>
+        /// <remarks>
+        /// Known webhook triggers includes Event Grid and Event Grid sourced blob triggers.
+        /// </remarks>
+        public static bool IsWebHookTrigger(this BindingMetadata binding)
+        {
+            if (binding is null)
+            {
+                throw new ArgumentNullException(nameof(binding));
+            }
+
+            if (string.Equals(EventGridTriggerKey, binding.Type, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (string.Equals(BlobTriggerKey, binding.Type, StringComparison.OrdinalIgnoreCase))
+            {
+                if (binding.Raw is { } obj)
+                {
+                    if (obj.TryGetValue("source", StringComparison.OrdinalIgnoreCase, out JToken token) && token is not null)
+                    {
+                        return string.Equals(token.ToString(), "eventGrid", StringComparison.OrdinalIgnoreCase);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a <see cref="BindingMetadata"/> represents a durable trigger (entity, orchestration, or activity).
+        /// </summary>
+        /// <param name="binding">The binding metadata to check.</param>
+        /// <returns><c>true</c> if a durable trigger, <c>false</c> otherwise.</returns>
+        public static bool IsDurableTrigger(this BindingMetadata binding)
+        {
+            if (binding is null)
+            {
+                throw new ArgumentNullException(nameof(binding));
+            }
+
+            return DurableTriggers.Contains(binding.Type);
+        }
+
         public static bool SupportsDeferredBinding(this BindingMetadata metadata)
         {
             Utility.TryReadAsBool(metadata.Properties, ScriptConstants.SupportsDeferredBindingKey, out bool result);
