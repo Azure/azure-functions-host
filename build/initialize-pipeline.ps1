74b0050a831748adf1248208548cbac9403cbb48
Jacob Viau <javia@microsoft.com>
2024-01-24 13:00:17 -0800

    Handle rate limiting as warning, continue

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -5,13 +5,19 @@ Write-Host "BUILD_REASON: '$buildReason'"
 Write-Host "BUILD_SOURCEBRANCH: '$sourceBranch'"
 
 if ($buildReason -eq "PullRequest") {
-  # parse PR title to see if we should pack this
-  $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
-  $title = $response.title.ToLowerInvariant()
-  Write-Host "Pull request '$title'"
-  if ($title.Contains("[pack]")) {
-    Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
-    Write-Host "Setting 'BuildArtifacts' to true."
+  try {
+    # parse PR title to see if we should pack this
+    $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+    $title = $response.title.ToLowerInvariant()
+    Write-Host "Pull request '$title'"
+    if ($title.Contains("[pack]")) {
+      Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
+      Write-Host "Setting 'BuildArtifacts' to true."
+    }
+  }
+  catch {
+    Write-Warning "Failed to get pull request title."
+    Write-Warning $_
   }
 }
 
