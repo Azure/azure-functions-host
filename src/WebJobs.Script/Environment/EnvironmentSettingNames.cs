4652174716291de67e7b1b7af3b92d68b12ada67
Lilian Kasem <likasem@microsoft.com>
2024-01-30 16:45:11 -0800

    Update environment variables and extensions

--- src/WebJobs.Script/Environment/EnvironmentSettingNames.cs
+++ src/WebJobs.Script/Environment/EnvironmentSettingNames.cs
@@ -76,6 +76,8 @@ namespace Microsoft.Azure.WebJobs.Script
         public const string FunctionsAlwaysReadyInstance = "FUNCTIONS_ALWAYSREADY_INSTANCE";
         public const string FunctionsTimeZone = "TZ";
         public const string FunctionsWebsiteTimeZone = "WEBSITE_TIME_ZONE";
+        public const string FunctionsTargetGroup = "FUNCTIONS_TARGET_GROUP";
+        public const string WebsiteArmResourceId = "WEBSITE_ARM_RESOURCE_ID";
 
         //Function in Kubernetes
         public const string PodNamespace = "POD_NAMESPACE";
