13b812c2af757b5a3b6053335d577db2737d5da5
Jacob Viau <javia@microsoft.com>
2024-02-08 10:30:58 -0800

    Allow for packing via --pack

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -4,20 +4,34 @@ $sourceBranch = $env:BUILD_SOURCEBRANCH
 Write-Host "BUILD_REASON: '$buildReason'"
 Write-Host "BUILD_SOURCEBRANCH: '$sourceBranch'"
 
+# See if artifacts should be built
 if ($buildReason -eq "PullRequest") {
+
+  # first we see if [pack] is in the PR title.
+  # This often gets rate limited, so it isn't reliable.
+
+  $pack = $false
   try {
-    # parse PR title to see if we should pack this
     $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
     $title = $response.title.ToLowerInvariant()
     Write-Host "Pull request '$title'"
-    if ($title.Contains("[pack]")) {
-      Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
-      Write-Host "Setting 'BuildArtifacts' to true."
-    }
+    $pack = $title.Contains("[pack]")
   }
   catch {
-    Write-Warning "Failed to get pull request title. Artifacts will not be built or packed."
+    Write-Warning "Failed to get pull request title."
     Write-Warning $_
+  }
+
+  # Next we check if the commit message contains '--pack'
+  $commitMessage = $env:BUILD_SOURCEVERSIONMESSAGE
+  if (!$pack && $commitMessage.Contains("--pack")) {
+    $pack = $true
+  }
+
+  Write-Host "Pack: '$pack'"
+  if ($pack) {
+    Write-Host "##vso[task.setvariable variable=BuildArtifacts;isOutput=true]true"
+    Write-Host "Setting 'BuildArtifacts' to true."
   }
 }
 
