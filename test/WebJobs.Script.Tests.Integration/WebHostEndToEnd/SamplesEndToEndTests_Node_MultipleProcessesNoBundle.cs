bffb3c9098aee3a3108ab582b654e9937f8910ed
Brett Samblanet <brettsam@microsoft.com>
2024-02-27 07:03:36 -0800

    debugging test

--- test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_Node_MultipleProcessesNoBundle.cs
+++ test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_Node_MultipleProcessesNoBundle.cs
@@ -6,9 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Script.Config;
@@ -44,14 +41,18 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
 
             await SamplesTestHelpers.InvokeAndValidateHttpTrigger(_fixture, "HttpTrigger");
 
+            Console.WriteLine("--- BEFORE: " + string.Join(",", nodeProcessesBeforeHostRestart));
+
             // Wait for all the 3 process to start
-            await Task.Delay(TimeSpan.FromMinutes(1));
-
-            IEnumerable<int> nodeProcessesAfter = Process.GetProcessesByName("node").Select(p => p.Id);
-
-            // Verify node process is different after host restart
-            var result = nodeProcessesAfter.Where(pId1 => !nodeProcessesBeforeHostRestart.Any(pId2 => pId2 == pId1) && !_fixture.NodeProcessesBeforeTestStarted.Any(pId3 => pId3 == pId1));
-            Assert.Equal(3, result.Count());
+            await TestHelpers.Await(() =>
+                {
+                    IEnumerable<int> nodeProcessesAfter = Process.GetProcessesByName("node").Select(p => p.Id);
+                    Console.WriteLine("--- AFTER: " + string.Join(", ", nodeProcessesAfter));
+                    // Verify node process is different after host restart
+                    var result = nodeProcessesAfter.Where(pId1 => !nodeProcessesBeforeHostRestart.Any(pId2 => pId2 == pId1) && !_fixture.NodeProcessesBeforeTestStarted.Any(pId3 => pId3 == pId1));
+                    bool success = result.Count() == 3;
+                    return success;
+                }, timeout: 120000, pollingInterval: 1000);
         }
 
         public class MultiplepleProcessesTestFixtureNoBundles : EndToEndTestFixture
