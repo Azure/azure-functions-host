26be3cc8f8bb0a68ee711ddc98e02cc0c9c564cd
Fabio Cavalcante <facaval@microsoft.com>
2024-03-05 14:56:18 -0800

    Fixing container scoping and reusing service provider in WebJobs context

---
+++ src/WebJobs.Script/DependencyInjection/InstanceServices/ScriptInstanceServicesProvider.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Host.Executors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.WebJobs.Script.DependencyInjection
+{
+    internal class ScriptInstanceServicesProvider : IInstanceServicesProvider, IDisposable
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private IServiceScope _instanceServicesScope;
+        private IServiceProvider _instanceServices;
+
+        public ScriptInstanceServicesProvider(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public ScriptInstanceServicesProvider(IServiceProvider instanceServices)
+        {
+            _instanceServices = instanceServices;
+        }
+
+        public IServiceProvider InstanceServices
+        {
+            get
+            {
+                if (_instanceServices == null && _serviceScopeFactory != null)
+                {
+                    _instanceServicesScope = _serviceScopeFactory.CreateScope();
+                    _instanceServices = _instanceServicesScope.ServiceProvider;
+                }
+
+                return _instanceServices;
+            }
+
+            set
+            {
+                _instanceServices = value;
+            }
+        }
+
+        public void Dispose()
+        {
+            _instanceServicesScope?.Dispose();
+
+            _instanceServicesScope = null;
+            _instanceServices = null;
+        }
+    }
+}
---
+++ src/WebJobs.Script/Host/ScriptInvocationArguments.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.WebJobs.Script.Host
+{
+    public class ScriptInvocationArguments(IServiceProvider serviceProvider) : Dictionary<string, object>
+    {
+        public IServiceProvider ServiceProvider { get; } = serviceProvider;
+    }
+}
