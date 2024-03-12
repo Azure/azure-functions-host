9413f2a165d6b30dc6c3286c61b5d3b059e134ae
Fabio Cavalcante <facaval@microsoft.com>
2024-03-12 09:03:17 -0700

    Fixing tests and improve child container request resolution (#9923)
    * Fixing controller test service resolution
    
    * Logging when ScriptHost request services are not available

--- src/WebJobs.Script.WebHost/Middleware/ScriptHostRequestServiceProviderMiddleware.cs
+++ src/WebJobs.Script.WebHost/Middleware/ScriptHostRequestServiceProviderMiddleware.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost.Middleware
 {
@@ -21,10 +22,15 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Middleware
 
         public Task Invoke(HttpContext httpContext)
         {
-            var scopeFactory = _scriptHostService.Services.GetService<IServiceScopeFactory>();
+            var scopeFactory = _scriptHostService.Services?.GetService<IServiceScopeFactory>();
             if (scopeFactory is not null)
             {
                 httpContext.Features.Set<IServiceProvidersFeature>(new RequestServicesFeature(httpContext, scopeFactory));
+            }
+            else
+            {
+                var logger = httpContext.RequestServices.GetService<ILogger<ScriptHostRequestServiceProviderMiddleware>>();
+                logger.LogDebug("Unable to resolve IServiceScopeFactory. RequestServices will not be scoped to ScriptHost.");
             }
 
             return _next(httpContext);
