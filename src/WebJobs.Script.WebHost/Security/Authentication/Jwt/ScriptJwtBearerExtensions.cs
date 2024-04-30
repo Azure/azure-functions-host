ba302c064baa3ff97fa2265a3132082c0109fb17
Tony Choi <tonychoi@microsoft.com>
2024-04-30 11:59:08 -0700

    verbose logging

--- src/WebJobs.Script.WebHost/Security/Authentication/Jwt/ScriptJwtBearerExtensions.cs
+++ src/WebJobs.Script.WebHost/Security/Authentication/Jwt/ScriptJwtBearerExtensions.cs
@@ -34,6 +34,7 @@ namespace Microsoft.Extensions.DependencyInjection
                             {
                                 OnMessageReceived = c =>
                                 {
+                                    Console.WriteLine("[TEST] OnMessageReceived 1");
                                     // By default, tokens are passed via the standard Authorization Bearer header. However we also support
                                     // passing tokens via the x-ms-site-token header.
                                     if (c.Request.Headers.TryGetValue(ScriptConstants.SiteTokenHeaderName, out StringValues values))
@@ -42,17 +43,23 @@ namespace Microsoft.Extensions.DependencyInjection
                                         c.Token = values.FirstOrDefault();
                                     }
 
+                                    Console.WriteLine("[TEST] OnMessageReceived 2");
+
                                     // Temporary: Tactical fix to address specialization issues. This should likely be moved to a token validator
                                     // TODO: DI (FACAVAL) This will be fixed once the permanent fix is in place
                                     if (_specialized == 0 && !SystemEnvironment.Instance.IsPlaceholderModeEnabled() && Interlocked.CompareExchange(ref _specialized, 1, 0) == 0)
                                     {
+                                        Console.WriteLine("[TEST] OnMessageReceived not Specialized yet");
                                         o.TokenValidationParameters = CreateTokenValidationParameters();
                                     }
+
+                                    Console.WriteLine("[TEST] OnMessageReceived 3");
 
                                     return Task.CompletedTask;
                                 },
                                 OnTokenValidated = c =>
                                 {
+                                    Console.WriteLine("[TEST] OnTokenValidated 1");
                                     var claims = new List<Claim>
                                     {
                                         new Claim(SecurityConstants.AuthLevelClaimType, AuthorizationLevel.Admin.ToString())
@@ -70,17 +77,23 @@ namespace Microsoft.Extensions.DependencyInjection
                                 },
                                 OnAuthenticationFailed = c =>
                                 {
+                                    Console.WriteLine("[TEST] OnAuthenticationFailed");
                                     LogAuthenticationFailure(c);
 
                                     return Task.CompletedTask;
                                 }
                             };
 
+                            Console.WriteLine("[TEST] AddScriptJwt 1");
+
                             o.TokenValidationParameters = CreateTokenValidationParameters();
+
+                            Console.WriteLine("[TEST] AddScriptJwt 2");
 
                             // TODO: DI (FACAVAL) Remove this once the work above is completed.
                             if (!SystemEnvironment.Instance.IsPlaceholderModeEnabled())
                             {
+                                Console.WriteLine("[TEST] AddScriptJwt 3");
                                 // We're not in standby mode, so flag as specialized
                                 _specialized = 1;
                             }
@@ -91,11 +104,17 @@ namespace Microsoft.Extensions.DependencyInjection
             if (SystemEnvironment.Instance.IsPlaceholderModeEnabled()
                 && SystemEnvironment.Instance.IsLinuxConsumptionOnAtlas())
             {
+                Console.WriteLine("[TEST] GetValidAudiences 1");
                 return new string[]
                 {
                     ScriptSettingsManager.Instance.GetSetting(ContainerName)
                 };
             }
+
+            Console.WriteLine("[TEST] GetValidAudiences 2");
+
+            Console.WriteLine($"[TEST] AzureWebsiteName: {string.Format(SiteAzureFunctionsUriFormat, ScriptSettingsManager.Instance.GetSetting(AzureWebsiteName))}");
+            Console.WriteLine($"[TEST] AzureWebsiteName 2: {string.Format(SiteUriFormat, ScriptSettingsManager.Instance.GetSetting(AzureWebsiteName))}");
 
             return new string[]
             {
@@ -106,10 +125,14 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static TokenValidationParameters CreateTokenValidationParameters()
         {
+            Console.WriteLine("[TEST] CreateTokenValidationParameters 1");
             var signingKeys = SecretsUtility.GetTokenIssuerSigningKeys();
+            Console.WriteLine("[TEST] CreateTokenValidationParameters 2");
             var result = new TokenValidationParameters();
+            Console.WriteLine("[TEST] CreateTokenValidationParameters 3");
             if (signingKeys.Length > 0)
             {
+                Console.WriteLine("[TEST] CreateTokenValidationParameters 4");
                 result.IssuerSigningKeys = signingKeys;
                 result.AudienceValidator = AudienceValidator;
                 result.IssuerValidator = IssuerValidator;
@@ -120,7 +143,11 @@ namespace Microsoft.Extensions.DependencyInjection
                     string.Format(ScmSiteUriFormat, ScriptSettingsManager.Instance.GetSetting(AzureWebsiteName)),
                     string.Format(SiteUriFormat, ScriptSettingsManager.Instance.GetSetting(AzureWebsiteName))
                 };
+                // console log valid issuers
+                Console.WriteLine($"[TEST] Valid Issuers: {string.Join(", ", result.ValidIssuers)}");
             }
+
+            Console.WriteLine("[TEST] CreateTokenValidationParameters 5");
 
             return result;
         }
@@ -153,6 +180,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
         private static void LogAuthenticationFailure(AuthenticationFailedContext context)
         {
+            Console.WriteLine("[TEST] Console Output: LogAuthenticationFailure()");
             if (!context.Request.IsAdminRequest())
             {
                 return;
@@ -161,17 +189,22 @@ namespace Microsoft.Extensions.DependencyInjection
             var loggerFactory = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>();
             var logger = loggerFactory.CreateLogger(ScriptConstants.LogCategoryHostAuthentication);
 
+            Console.WriteLine("[TEST] Console Output: Authentication failed.");
+
             string message = null;
             switch (context.Exception)
             {
                 case SecurityTokenInvalidIssuerException iex:
                     message = $"Token issuer validation failed for issuer '{iex.InvalidIssuer}'.";
+                    Console.WriteLine($"[TEST] SecurityTokenInvalidIssuerException: {message}");
                     break;
                 case SecurityTokenInvalidAudienceException iaex:
                     message = $"Token audience validation failed for audience '{iaex.InvalidAudience}'.";
+                    Console.WriteLine($"[TEST] SecurityTokenInvalidAudienceException: {message}");
                     break;
                 default:
                     message = $"Token validation failed.";
+                    Console.WriteLine($"[TEST] Default: {message}");
                     break;
             }
 
