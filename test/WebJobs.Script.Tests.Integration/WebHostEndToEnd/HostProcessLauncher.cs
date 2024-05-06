ad5102be9be58e4a02ad6f5520b03a95bd0b1258
Lilian Kasem <likasem@microsoft.com>
2024-05-06 10:05:16 -0700

    Update integration test path & build sln for tests, HostProcessLauncher testpath to debug

--- test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/HostProcessLauncher.cs
+++ test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/HostProcessLauncher.cs
@@ -56,7 +56,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Integration.WebHostEndToEnd
             var dirPath = Path.GetDirectoryName(typeof(HostProcessLauncher).Assembly.Location);
             var dirName = new DirectoryInfo(dirPath).Name;
 
-            string workingDir = Path.Combine("..", "..", "WebJobs.Script.WebHost", dirName);
+            string workingDir = Path.Combine("..", "..", "WebJobs.Script.WebHost", $"{dirName}_net8.0");
             workingDir = Path.GetFullPath(workingDir);
             string filePath = Path.Combine(workingDir, "Microsoft.Azure.WebJobs.Script.WebHost.exe");
 
