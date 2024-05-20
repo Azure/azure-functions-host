ab084df56a09081f389d595247c3c3641f64211a
Jacob Viau <javia@microsoft.com>
2024-05-20 16:02:35 -0700

    Move msbuild props/targets to eng/build

--- build/Get-AzureFunctionsVersion.ps1
+++
@@ -1,4 +0,0 @@
-# use the same logic as the projects to generate the site extension version
-$cmd = "build", "$PSScriptRoot/../src/WebJobs.Script/WebJobs.Script.csproj", "-t:EchoVersion", "--no-restore", "--nologo", "-clp:NoSummary"
-$version = (& dotnet $cmd).Trim()
-return $version
--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -1,33 +1,19 @@
-param (
-  [ValidateSet("6", "8", "")][string]$minorVersionPrefix = ""
-)
-
-$buildReason = $env:BUILD_REASON
-$sourceBranch = $env:BUILD_SOURCEBRANCH
-$provider = $env:BUILD_REPOSITORY_PROVIDER
-
-Write-Host "BUILD_REASON: '$buildReason'"
-Write-Host "BUILD_SOURCEBRANCH: '$sourceBranch'"
-
 # See if artifacts should be built
-if ($buildReason -eq "PullRequest") {
+if ($env:BUILD_REASON -eq "PullRequest") {
 
   # first we see if [pack] is in the PR title.
   # This often gets rate limited, so it isn't reliable.
-
   $pack = $false
 
-  if ($provider -eq "GitHub") {
-    try {
-      $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
-      $title = $response.title.ToLowerInvariant()
-      Write-Host "Pull request '$title'"
-      $pack = $title.Contains("[pack]")
-    }
-    catch {
-      Write-Warning "Failed to get pull request title."
-      Write-Warning $_
-    }
+  try {
+    $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+    $title = $response.title.ToLowerInvariant()
+    Write-Host "Pull request '$title'"
+    $pack = $title.Contains("[pack]")
+  }
+  catch {
+    Write-Warning "Failed to get pull request title."
+    Write-Warning $_
   }
 
   # Next we check if the commit message contains '--pack'
@@ -42,24 +28,3 @@ if ($buildReason -eq "PullRequest") {
     Write-Host "Setting 'BuildArtifacts' to true."
   }
 }
-
-$buildNumber = ""
-
-$branch = $sourceBranch.ToLower();
-$isRelease = $branch.Contains("release/4") -or $branch.Contains("release/inproc6/4") -or $branch.Contains("release/inproc8/4")
-
-if(($buildReason -eq "PullRequest") -or !$isRelease)
-{
-  $buildNumber = $env:buildNumber
-  Write-Host "BuildNumber: '$buildNumber'"
-}
-else 
-{
-  Write-Host "Release build; Not using a build number."
-}
-
-Import-Module $PSScriptRoot\Get-AzureFunctionsVersion -Force
-$version = & "$PSScriptRoot/Get-AzureFunctionsVersion.ps1"
-
-Write-Host "Site extension version: $version"
-Write-Host "##vso[build.updatebuildnumber]$version"
