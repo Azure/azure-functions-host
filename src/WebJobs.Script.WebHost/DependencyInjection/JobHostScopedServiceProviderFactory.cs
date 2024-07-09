5c6f22d06d03bee58e7ca8b6be5763c7ce8accf6
Jacob Viau <javia@microsoft.com>
2024-07-09 15:53:15 -0700

    Remove unused using

--- src/WebJobs.Script.WebHost/DependencyInjection/JobHostScopedServiceProviderFactory.cs
+++ src/WebJobs.Script.WebHost/DependencyInjection/JobHostScopedServiceProviderFactory.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
