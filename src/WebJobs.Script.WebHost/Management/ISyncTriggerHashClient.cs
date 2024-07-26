105358ba1d5d65fface26be4cb4ac59fd7381119
Ishank Gupta <ishankgupta@microsoft.com>
2024-07-26 16:40:43 +0530

    managing synctrigger last hash for local file storage
    (cherry picked from commit 93aea28d5a6a085d72a2edc96b2a7990f8b1cf7d)

---
+++ src/WebJobs.Script.WebHost/Management/ISyncTriggerHashClient.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
+{
+    public interface ISyncTriggerHashClient
+    {
+        Task<string> CheckHashAsync(string content);
+
+        Task UpdateHashAsync(string hash);
+
+        Task DeleteIfExistsAsync();
+
+        Task<bool> ExistsAsync();
+
+        Task<string> GetHashAsync();
+    }
+}
