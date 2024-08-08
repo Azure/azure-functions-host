28334e897c9563e56e4897366f68545331c76d32
Surgupta <surgupta@microsoft.com>
2024-08-08 15:10:29 -0700

    Remove ununsed usings

--- src/WebJobs.Script/Sanitizer.cs
+++ src/WebJobs.Script/Sanitizer.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using BenchmarkDotNet.Attributes;
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Azure.WebJobs.Logging
