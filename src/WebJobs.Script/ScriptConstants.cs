75940de09c676745209e4517ce570042287ebbb5
Mathew Charles <mathewc@microsoft.com>
2024-09-30 10:11:31 -0700

    Configuration based whitelisting of /admin API internal auth (#10487)

--- src/WebJobs.Script/ScriptConstants.cs
+++ src/WebJobs.Script/ScriptConstants.cs
@@ -142,6 +142,7 @@ namespace Microsoft.Azure.WebJobs.Script
         public const string FeatureFlagEnableLegacyDurableVersionCheck = "EnableLegacyDurableVersionCheck";
         public const string HostingConfigSwtAuthenticationEnabled = "SwtAuthenticationEnabled";
         public const string HostingConfigSwtIssuerEnabled = "SwtIssuerEnabled";
+        public const string HostingConfigInternalAuthApisAllowList = "InternalAuthApisAllowList";
 
         public const string SiteAzureFunctionsUriFormat = "https://{0}.azurewebsites.net/azurefunctions";
         public const string ScmSiteUriFormat = "https://{0}.scm.azurewebsites.net";
