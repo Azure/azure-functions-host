cee5066d1691b2cdbf9b28eec1e586d14262c273
Jacob Viau <javia@microsoft.com>
2024-12-12 15:45:40 -0800

    Flatten SiteExtension artifacts, custom hash task

--- src/WebJobs.Script.SiteExtension/New-PrivateSiteExtension.ps1
+++ src/WebJobs.Script.SiteExtension/New-PrivateSiteExtension.ps1
@@ -6,10 +6,13 @@
     Takes in a published site extension and produces a private site extension.
 
     .PARAMETER InputPath
-    The path of the published 'SiteExtension'. Default is "./SiteExtension".
+    The path of the published 'SiteExtension'. Leave null to scan for root in a child from here.
 
     .PARAMETER OutputPath
-    The path to produce the private site extension to. When zipping, this is the name of the zip file. Default is "./PrivateSiteExtension".
+    The path to produce the private site extension to. When zipping, this is the name of the zip file. Leave null to compute this name.
+
+    .PARAMETER Bitness
+    The bitness to produce the private site extension with. Default is '32bit'.
 
     .PARAMETER Zip
     [Switch] Include to produce site extension as a zip.
@@ -25,17 +28,45 @@
 #>
 
 param (
-    [string] $InputPath = "./SiteExtension",
-    [string] $OutputPath = "./PrivateSiteExtension",
+    [string] $InputPath = $null,
+    [string] $OutputPath = $null,
+    [ValidateSet('x64', '64bit', 'x86', '32bit')][string] $Bitness = '32bit',
     [switch] $Zip,
     [switch] $Force
 )
 
+$normalizeBitness = @{
+    'x64' = '64bit'
+    '64bit' = '64bit'
+    'x86' = '64bit'
+    '32bit' = '32bit'
+}
+
+$Bitness = $normalizeBitness[$Bitness]
+
+if (-not $InputPath)
+{
+    $InputPath = (Get-ChildItem -Path . -Filter "extension.xml" -Recurse).Directory.FullName
+}
+
+if (Test-Path (Join-Path $InputPath "WebJobs.Script.SiteExtension.csproj"))
+{
+    Write-Error "This script should not be ran in the WebJobs.Script.SiteExtension project folder. Run this script in the root of the published site extension folder."
+    exit 1
+}
+
 if (-not (Join-Path $InputPath "extension.xml" | Test-Path))
 {
-    Write-Error "InputPath should be the path to the root of 'SiteExtension' folder (where 'extension.xml' is)."
-    Write-Error "Make sure to publish the site extension before running this script."
+    Write-Error "Unable to find published site extension."
     exit 1
+}
+
+if (-not $OutputPath)
+{
+    $runtime = $Bitness -eq '32bit' ? 'win-x86' : 'win-x64'
+    $leaf = (Split-Path $InputPath -Leaf)
+    $split = $leaf.IndexOf('.')
+    $OutputPath = "$($leaf.Substring(0, $split)).Private.$($leaf.Substring($split + 1)).$runtime"
 }
 
 function New-TemporaryDirectory {
@@ -73,14 +104,14 @@ function Write-Folder ($outputPath)
         }
     }
 
-    $inputPath = Get-ChildItem -Path .\SiteExtension\ -Attributes Directory
+    $inputPath = Get-ChildItem -Path $InputPath -Attributes Directory
     $outputPath = Join-Path $outputPath "SiteExtensions" "Functions"
     New-Item -ItemType Directory -Path $outputPath | Out-Null
 
     Copy-Item "$inputPath/applicationHost.xdt" -Destination $outputPath | Out-Null
 
-    $filesDest = (Join-Path $outputPath "32bit")
-    Copy-Item "$inputPath/32bit/" -Destination "$filesDest/" -Container -Recurse | Out-Null
+    $filesDest = (Join-Path $outputPath "$Bitness")
+    Copy-Item "$inputPath/$Bitness/" -Destination "$filesDest/" -Container -Recurse | Out-Null
 
     $workerDest = (Join-Path $filesDest "workers")
     Copy-Item "$inputPath/workers/" -Destination "$workerDest/" -Container -Recurse | Out-Null
