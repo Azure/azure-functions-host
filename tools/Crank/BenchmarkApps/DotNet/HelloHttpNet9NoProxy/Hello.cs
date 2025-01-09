823d28e6285f04fdcfda227ffa906613cd9c6e0a
Shyju Krishnankutty <connectshyju@gmail.com>
2025-01-09 13:25:07 -0800

    Adding a function app which does not use aspnet core integration.

---
+++ tools/Crank/BenchmarkApps/DotNet/HelloHttpNet9NoProxy/Hello.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class HelloHttp
+    {
+        private readonly ILogger _logger;
+
+        public HelloHttp(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HelloHttp>();
+        }
+
+        [Function("HelloHttp")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
