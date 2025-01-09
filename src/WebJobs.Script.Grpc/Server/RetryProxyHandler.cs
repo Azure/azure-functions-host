9ed13c0a3506a45cf10476f3a851003eb920dd87
Lilian Kasem <likasem@microsoft.com>
2025-01-08 16:42:53 -0800

    Add 'System' import

--- src/WebJobs.Script.Grpc/Server/RetryProxyHandler.cs
+++ src/WebJobs.Script.Grpc/Server/RetryProxyHandler.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
