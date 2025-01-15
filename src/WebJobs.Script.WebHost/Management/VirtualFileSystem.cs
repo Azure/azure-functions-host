2dd13885f2e8ad5214b9fc71215bc7019de7cf6d
Surbhi Gupta <surgupta@microsoft.com>
2025-01-15 14:34:15 -0600

    switch exp

--- src/WebJobs.Script.WebHost/Management/VirtualFileSystem.cs
+++ src/WebJobs.Script.WebHost/Management/VirtualFileSystem.cs
@@ -653,10 +653,19 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Management
                     };
 
                     // Check if the payload is a string or an exception
-                    payload = payload is string ? payload as string : payload is Exception ? (payload as Exception).Message : payload;
+                    payload = payload switch
+                    {
+                        string str => str,
+                        Exception ex => ex.Message,
+                        _ => payload
+                    };
 
                     // Sanitize the payload if it's an object
-                    var content = payload is string ? payload as string : JsonConvert.SerializeObject(payload, jsonSerializerSettings);
+                    var content = payload switch
+                    {
+                        string str => str,
+                        _ => JsonConvert.SerializeObject(payload, jsonSerializerSettings)
+                    };
                     response.Content = new StringContent(content, Encoding.UTF8, "application/json");
                 }
             }
