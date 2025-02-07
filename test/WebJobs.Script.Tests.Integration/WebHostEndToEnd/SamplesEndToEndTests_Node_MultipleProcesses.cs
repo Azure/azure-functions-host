82a0fa8b79c7f0b9f028befdf7a0e2ad4414243d
Brett Samblanet <brettsam@microsoft.com>
2025-02-07 11:36:25 -0800

    fixing some test races (hopefully) (#10802)

--- test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_Node_MultipleProcesses.cs
+++ test/WebJobs.Script.Tests.Integration/WebHostEndToEnd/SamplesEndToEndTests_Node_MultipleProcesses.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -11,7 +10,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.WebJobs.Script.Tests;
@@ -21,65 +19,52 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
 {
     [Trait(TestTraits.Category, TestTraits.EndToEnd)]
     [Trait(TestTraits.Group, TestTraits.SamplesEndToEnd)]
-    public class SamplesEndToEndTests_Node_MultipleProcesses : IClassFixture<SamplesEndToEndTests_Node_MultipleProcesses.MultiplepleProcessesTestFixture>
+    public class SamplesEndToEndTests_Node_MultipleProcesses : IAsyncDisposable
     {
-        private readonly ScriptSettingsManager _settingsManager;
-        private MultiplepleProcessesTestFixture _fixture;
-        private IEnumerable<int> _nodeProcessesBeforeTestStarted;
+        private readonly MultiplepleProcessesTestFixture _fixture;
 
-        public SamplesEndToEndTests_Node_MultipleProcesses(MultiplepleProcessesTestFixture fixture)
+        public SamplesEndToEndTests_Node_MultipleProcesses()
         {
-            _fixture = fixture;
-            _nodeProcessesBeforeTestStarted = fixture.NodeProcessesBeforeTestStarted;
-            _settingsManager = ScriptSettingsManager.Instance;
+            // We want a new fixture for each test
+            _fixture = new MultiplepleProcessesTestFixture();
         }
 
         [Fact]
         public async Task NodeProcess_Different_AfterHostRestart()
         {
+            await _fixture.InitializeAsync();
+            await WaitForWebHostChannelCountAsync(3);
+
             await SamplesTestHelpers.InvokeAndValidateHttpTrigger(_fixture, "HttpTrigger");
-            IEnumerable<int> nodeProcessesBeforeHostRestart = Process.GetProcessesByName("node").Select(p => p.Id);
+            IEnumerable<int> nodeProcessesBeforeHostRestart = GetCurrentWorkerPids();
+
             // Trigger a restart
             await _fixture.Host.RestartAsync(CancellationToken.None);
 
             await SamplesTestHelpers.InvokeAndValidateHttpTrigger(_fixture, "HttpTrigger");
 
             // Wait for all the 3 process to start
-            await Task.Delay(TimeSpan.FromMinutes(1));
+            await WaitForWebHostChannelCountAsync(3);
 
-            IEnumerable<int> nodeProcessesAfter = Process.GetProcessesByName("node").Select(p => p.Id);
+            IEnumerable<int> nodeProcessesAfter = GetCurrentWorkerPids();
 
             // Verify node process is different after host restart
-            var result = nodeProcessesAfter.Where(pId1 => !nodeProcessesBeforeHostRestart.Any(pId2 => pId2 == pId1) && !_fixture.NodeProcessesBeforeTestStarted.Any(pId3 => pId3 == pId1));
+            var result = nodeProcessesAfter.Where(pId1 => !nodeProcessesBeforeHostRestart.Any(pId2 => pId2 == pId1) && !nodeProcessesBeforeHostRestart.Any(pId3 => pId3 == pId1));
             Assert.Equal(3, result.Count());
-        }
-
-        private async Task<IEnumerable<int>> WaitAndGetAllNodeProcesses(int expectedProcessCount)
-        {
-            IEnumerable<int> nodeProcessesBeforeHostRestart = Process.GetProcessesByName("node").Select(p => p.Id);
-            while (nodeProcessesBeforeHostRestart.Count() < expectedProcessCount)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(5));
-                nodeProcessesBeforeHostRestart = Process.GetProcessesByName("node").Select(p => p.Id);
-            }
-
-            return nodeProcessesBeforeHostRestart;
         }
 
         [Fact]
         public async Task NodeProcessCount_RemainsSame_AfterMultipleTimeouts()
         {
+            await _fixture.InitializeAsync();
+
             // Wait for all the 3 process to start
             List<Task<HttpResponseMessage>> timeoutTasks = new List<Task<HttpResponseMessage>>();
-            Task timeoutTask = Task.Delay(TimeSpan.FromMinutes(1));
-            Task<IEnumerable<int>> getNodeTask = WaitAndGetAllNodeProcesses(3);
-            Task result = await Task.WhenAny(getNodeTask, timeoutTask);
-            if (result.Equals(timeoutTask))
-            {
-                throw new Exception("Failed to start all 3 node processes");
-            }
+            await WaitForWebHostChannelCountAsync(3);
+            IEnumerable<int> nodeProcessesBeforeHostRestart = GetCurrentWorkerPids();
+
             var oldHostInstanceId = await _fixture.GetActiveHostInstanceIdAsync();
-            IEnumerable<int> nodeProcessesBeforeHostRestart = await getNodeTask;
+
             string functionKey = await _fixture.Host.GetFunctionSecretAsync("httptrigger-timeout");
             string uri = $"api/httptrigger-timeout?code={functionKey}&name=Yogi";
 
@@ -90,40 +75,70 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
                 request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
                 timeoutTasks.Add(_fixture.Host.HttpClient.SendAsync(request));
             }
+
             var results = await Task.WhenAll(timeoutTasks);
             foreach (var timeoutResult in results)
             {
                 Assert.Equal(HttpStatusCode.InternalServerError, timeoutResult.StatusCode);  // Confirm response code after timeout (10 seconds)
             }
 
-            IEnumerable<int> nodeProcessesAfter = Process.GetProcessesByName("node").Select(p => p.Id);
+            // restarted workers run at JobHost level (?)
+            await WaitForJobHostChannelCountAsync(3);
+            var nodeProcessesAfter = GetCurrentWorkerPids();
 
             // Confirm count remains the same
             Assert.Equal(nodeProcessesBeforeHostRestart.Count(), nodeProcessesAfter.Count());
 
-
-            // Confirm all processes are different
-            Assert.Equal(3, nodeProcessesAfter.Except(nodeProcessesBeforeHostRestart).Count());
-
             // Confirm host instance ids are the same
             Assert.Equal(oldHostInstanceId, await _fixture.GetActiveHostInstanceIdAsync());
+
+            var result = nodeProcessesAfter.Where(pId1 => !nodeProcessesBeforeHostRestart.Any(pId2 => pId2 == pId1) && !nodeProcessesBeforeHostRestart.Any(pId3 => pId3 == pId1));
+            Assert.Equal(3, result.Count());
+        }
+
+        public async Task WaitForWebHostChannelCountAsync(int expectedCount)
+        {
+            await TestHelpers.Await(() =>
+            {
+                var channelManager = _fixture.Host.WebHostServices.GetService<IWebHostRpcWorkerChannelManager>();
+                var channels = channelManager.GetChannels("node");
+                return channels is not null && channels.Count == expectedCount;
+            }, pollingInterval: 1000, userMessageCallback: _fixture.Host.GetLog);
+        }
+
+        public async Task WaitForJobHostChannelCountAsync(int expectedCount)
+        {
+            await TestHelpers.Await(() =>
+            {
+                var channelManager = _fixture.Host.JobHostServices.GetService<IJobHostRpcWorkerChannelManager>();
+                var channels = channelManager.GetChannels("node");
+                return channels is not null && channels.Count() == expectedCount;
+            }, pollingInterval: 1000, userMessageCallback: _fixture.Host.GetLog);
+        }
+
+        public IEnumerable<int> GetCurrentWorkerPids()
+        {
+            var webHostChannelManager = _fixture.Host.WebHostServices.GetService<IWebHostRpcWorkerChannelManager>();
+            var webHostChannels = webHostChannelManager.GetChannels("node");
+            var webHostPids = webHostChannels.Select(c => c.Value.Task.Result.WorkerProcess.Id).ToArray();
+
+            var jobHostChannelManager = _fixture.Host.JobHostServices.GetService<IJobHostRpcWorkerChannelManager>();
+            var jobHostChannels = jobHostChannelManager.GetChannels("node");
+            var jobHostPids = jobHostChannels.Select(c => c.WorkerProcess.Id).ToArray();
+
+            return webHostPids.Concat(jobHostPids);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _fixture.DisposeAsync();
         }
 
         public class MultiplepleProcessesTestFixture : EndToEndTestFixture
         {
-            private IEnumerable<int> _nodeProcessesBeforeTestStarted;
-
-            public IEnumerable<int> NodeProcessesBeforeTestStarted => _nodeProcessesBeforeTestStarted;
-
-            static MultiplepleProcessesTestFixture()
-            {
-            }
-
             public MultiplepleProcessesTestFixture()
                 : base(Path.Combine(Environment.CurrentDirectory, @"..", "..", "..", "..", "sample", "node"), "samples", RpcWorkerConstants.NodeLanguageWorkerName, 3)
             {
-                _nodeProcessesBeforeTestStarted = Process.GetProcessesByName("node").Select(p => p.Id);
-                _nodeProcessesBeforeTestStarted = _nodeProcessesBeforeTestStarted ?? new List<int>();
             }
 
             public override void ConfigureScriptHost(IWebJobsBuilder webJobsBuilder)
@@ -131,12 +146,19 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.EndToEnd
                 base.ConfigureScriptHost(webJobsBuilder);
                 webJobsBuilder.Services.Configure<ScriptJobHostOptions>(o =>
                 {
-                    o.Functions = new[]
-                    {
+                    o.Functions =
+                    [
                         "HttpTrigger",
                         "HttpTrigger-Timeout",
-                    };
+                    ];
                 });
+
+                webJobsBuilder.Services.AddOptions<LanguageWorkerOptions>()
+                    .PostConfigure(o =>
+                    {
+                        var nodeConfig = o.WorkerConfigs.Single(c => c.Description.Language == "node");
+                        nodeConfig.CountOptions.ProcessStartupInterval = TimeSpan.FromSeconds(3);
+                    });
             }
         }
     }
