f7d0c58483c491f9e4fdbfe68ba50102692f7636
Shyju Krishnankutty <connectshyju@gmail.com>
2025-02-26 11:30:40 -0800

    fix entra line break

--- src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
+++ src/WebJobs.Script.WebHost/WebJobsApplicationBuilderExtension.cs
@@ -35,7 +35,6 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             HttpBodyControlOptions httpBodyControlOptions = httpBodyControlOptionsMonitor.CurrentValue;
             httpBodyControlOptionsMonitor.OnChange(newOptions => httpBodyControlOptions = newOptions);
 
-
             // Ensure the ClrOptimizationMiddleware is registered before all middleware
             builder.UseMiddleware<ClrOptimizationMiddleware>();
             builder.UseMiddleware<HttpRequestBodySizeMiddleware>();
