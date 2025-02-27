f0388cb92d9e32af45fefd365b8de0d1acc2ffcc
Ishank Gupta <ishankgupta08@gmail.com>
2025-02-27 15:59:39 +0530

    Sync trigger error when AzureWebjobsStorage is not set in ManagedApp Environment (#10894)
    * Fix sync trigger error when AzureWebjobsStorage is not set in case of ManagedAppEnvironment for Hybrid Logic Apps
    
    Issue : https://github.com/Azure/azure-functions-host/issues/10686
    (cherry picked from commit cd15f8f983afe86de4c47b228a619450f50dd5e7)
    
    * Add test
    
    (cherry picked from commit 9f30d197a06cad9462f980b96d0a1d53445fa17e)
    
    * Resolved comments
    
    (cherry picked from commit 33ef8a18bd5ab15c4418075d366a66863df9b4b2)
    
    * Fixed comment
    
    (cherry picked from commit be030c53db82cb46ddbd60c1f978b01bdf36b391)
    
    * fix comment - skip hash check when client is not set
    
    (cherry picked from commit bb83e10cce6d813b8776b34f75337d31d2bc8da7)
    
    * update test for managedenv
    
    (cherry picked from commit 95d44098150519ab0d43df3adf9bfe7f70b5ac44)
    
    * add test for kubernetes managed env
    
    (cherry picked from commit 88801b0c3afaef7a68e0f1400b31bb0529a39fb9)
    
    * resolve comments - to use cache disabled in test
    
    (cherry picked from commit 4b24959a6dcaf4a420a4a431a0a972821c522933)
    
    ---------
    
    Co-authored-by: Ishank Gupta <ishankgupta@microsoft.com>
    (cherry picked from commit 9b6bd9387613165099670034471ecbe8ba18ba12)

--- test/WebJobs.Script.Tests.Integration/Management/FunctionsSyncManagerTests.cs
+++ test/WebJobs.Script.Tests.Integration/Management/FunctionsSyncManagerTests.cs
@@ -300,6 +300,40 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Managment
         }
 
         [Fact]
+        public async Task TrySyncTriggers_ManagedAppEnv_WithNo_AzureWebJobsStorage_ReturnsTrue()
+        {
+            _mockEnvironment.Setup(p => p.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteArmCacheEnabled)).Returns("0");
+
+            using (var env = new TestScopedEnvironmentVariable(_vars))
+            {
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable(EnvironmentSettingNames.ManagedEnvironment)).Returns("true");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("FUNCTIONS_API_SERVER")).Returns("https://appname.azurewebsites.net");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("CONTAINER_APP_NAME")).Returns("appname");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("CONTAINER_APP_NAMESPACE")).Returns("appns");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("CONTAINER_APP_REVISION")).Returns("appname--r1");
+                var result = await _functionsSyncManager.TrySyncTriggersAsync(isBackgroundSync: true);
+                Assert.True(result.Success);
+                VerifyResultWithCacheOff(durableVersion: "V1");
+            }
+        }
+
+        [Fact]
+        public async Task TrySyncTriggers_KubernetesManagedEnv_WithNo_AzureWebJobsStorage_ReturnsTrue()
+        {
+            _mockEnvironment.Setup(p => p.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteArmCacheEnabled)).Returns("0");
+
+            using (var env = new TestScopedEnvironmentVariable(_vars))
+            {
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("FUNCTIONS_API_SERVER")).Returns("https://appname.azurewebsites.net");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("KUBERNETES_SERVICE_HOST")).Returns("kubhost");
+                _mockEnvironment.Setup(p => p.GetEnvironmentVariable("POD_NAMESPACE")).Returns("podns");
+                var result = await _functionsSyncManager.TrySyncTriggersAsync(isBackgroundSync: true);
+                Assert.True(result.Success);
+                VerifyResultWithCacheOff(durableVersion: "V1");
+            }
+        }
+
+        [Fact]
         public void ArmCacheEnabled_VerifyDefault()
         {
             _mockEnvironment.Setup(p => p.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteArmCacheEnabled)).Returns((string)null);
