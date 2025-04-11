d093e556cdf8f012b4ec4c3c8498d24d09ed5182
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2025-04-11 00:46:31 -0700

    Adding a check for uri.

--- src/WebJobs.Script/Sanitizer.cs
+++ src/WebJobs.Script/Sanitizer.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Azure.WebJobs.Logging
 
         // List of keywords that should not be replaced with [Hidden Credential]
         private static readonly string[] AllowedTokens = new string[] { "PublicKeyToken=" };
-        internal static readonly string[] CredentialTokens = new string[] { "Token=", "DefaultEndpointsProtocol=http", "AccountKey=", "Data Source=", "Server=", "Password=", "pwd=", "&amp;sig=", "&sig=", "?sig=", "SharedAccessKey=", "&amp;code=", "&code=", "?code=", "key=" };
+        internal static readonly string[] CredentialTokens = new string[] { "Token=", "DefaultEndpointsProtocol=http", "AccountKey=", "Data Source=", "Server=", "Password=", "pwd=", "&amp;sig=", "&sig=", "?sig=", "SharedAccessKey=", "&amp;code=", "&code=", "?code=", "/code=", "key=" };
         private static readonly string[] CredentialNameFragments = new[] { "password", "pwd", "key", "secret", "token", "sas" };
 
         // Pattern of format : "<protocol>://<username>:<password>@<address>:<port>"
