e892cddfa1197455a4c0e3dfa16765f29ca9e977
Manikanta Nallagatla <mnallagatla@microsoft.com>
2025-04-20 21:12:25 +0530

    spaces change

--- src/WebJobs.Script.WebHost/Diagnostics/AzureMonitorDiagnosticLogger.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/AzureMonitorDiagnosticLogger.cs
@@ -54,8 +54,9 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
 
         public bool IsEnabled(LogLevel logLevel)
         {
-            if ((_environment.IsFlexConsumptionSku() || _environment.IsLinuxConsumptionOnLegion()) 
-                && !_environment.IsAzureMonitorEnabledOnLegionSkus()) {
+            if ((_environment.IsFlexConsumptionSku() || _environment.IsLinuxConsumptionOnLegion())
+                && !_environment.IsAzureMonitorEnabledOnLegionSkus())
+            {
                 return false;
             }
             // We want to instantiate this Logger in placeholder mode to warm it up, but do not want to log anything.
