72e15203e13ae76fe1557edf8e72c0d8b88e0f9c
Surbhi Gupta <surgupta@microsoft.com>
2025-05-12 22:57:09 -0500

    Fixing tests

--- test/WebJobs.Script.Tests/Description/FunctionAppValidationServiceTests.cs
+++ test/WebJobs.Script.Tests/Description/FunctionAppValidationServiceTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -122,6 +123,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         public async Task StartAsync_MissingAzureFunctionsFolder_LogsWarning()
         {
             _testLoggerProvider.ClearAllLogMessages();
+            Directory.CreateDirectory("test-root-path");
 
             // Arrange
             var functionMetadataList = ImmutableArray.Create(new FunctionMetadata());
