63f8cd196edc0c47814e83a1494465e8840e1953
Surbhi Gupta <surgupta@microsoft.com>
2025-05-12 22:56:08 -0500

    Fixing tests

--- test/WebJobs.Script.Tests/Description/FunctionAppValidationServiceTests.cs
+++ test/WebJobs.Script.Tests/Description/FunctionAppValidationServiceTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         private readonly Mock<IOptions<ScriptJobHostOptions>> _scriptOptionsMock;
         private readonly ScriptJobHostOptions _scriptJobHostOptions;
         private readonly TestLoggerProvider _testLoggerProvider;
-        private readonly TimeSpan _initializationDelay = TimeSpan.FromSeconds(11);
+        private readonly TimeSpan _initializationDelay = TimeSpan.FromSeconds(6);
 
         public FunctionAppValidationServiceTests()
         {
@@ -122,6 +123,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
         public async Task StartAsync_MissingAzureFunctionsFolder_LogsWarning()
         {
             _testLoggerProvider.ClearAllLogMessages();
+            Directory.CreateDirectory("test-root-path");
 
             // Arrange
             var functionMetadataList = ImmutableArray.Create(new FunctionMetadata());
