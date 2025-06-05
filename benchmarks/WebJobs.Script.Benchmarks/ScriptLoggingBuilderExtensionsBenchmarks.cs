9deb94b2e9a7ebf5396c40f49ca4fe20704602a2
Shyju Krishnankutty <connectshyju@gmail.com>
2025-06-05 16:57:29 -0700

    WIP

--- benchmarks/WebJobs.Script.Benchmarks/ScriptLoggingBuilderExtensionsBenchmarks.cs
+++ benchmarks/WebJobs.Script.Benchmarks/ScriptLoggingBuilderExtensionsBenchmarks.cs
@@ -1,17 +1,17 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+﻿// // Copyright (c) .NET Foundation. All rights reserved.
+// // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using BenchmarkDotNet.Attributes;
-using Microsoft.Extensions.Logging;
+// using BenchmarkDotNet.Attributes;
+// using Microsoft.Extensions.Logging;
 
-namespace Microsoft.Azure.WebJobs.Script.Benchmarks
-{
-    public class ScriptLoggingBuilderExtensionsBenchmarks
-    {
-        [Benchmark]
-        public void Filter()
-        {
-            ScriptLoggingBuilderExtensions.Filter("test", LogLevel.Information, LogLevel.Information);
-        }
-    }
-}
+// namespace Microsoft.Azure.WebJobs.Script.Benchmarks
+// {
+//     public class ScriptLoggingBuilderExtensionsBenchmarks
+//     {
+//         [Benchmark]
+//         public void Filter()
+//         {
+//             ScriptLoggingBuilderExtensions.Filter("test", LogLevel.Information, LogLevel.Information);
+//         }
+//     }
+// }
