4ee497b5c1042a19fd24e01ea497a92e2f7692bf
Surbhi Gupta <surgupta@microsoft.com>
2025-06-09 16:57:00 -0500

    Updated release notes

--- src/WebJobs.Script/Workers/Rpc/Configuration/LanguageWorkerOptionsSetup.cs
+++ src/WebJobs.Script/Workers/Rpc/Configuration/LanguageWorkerOptionsSetup.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc
 {
     internal class LanguageWorkerOptionsSetup : IConfigureOptions<LanguageWorkerOptions>
     {
-        private const string _windowsWorkerProbingPath = "c:\\home\\SiteExtensions\\workers"; // Harcoded site extensions path for Windows until sets it as an Environment variable.
+        private const string _windowsWorkerProbingPath = "c:\\home\\SiteExtensions\\workers"; // Harcoded site extensions path for Windows until Antares sets it as an Environment variable.
         private readonly IConfiguration _configuration;
         private readonly ILogger _logger;
         private readonly IEnvironment _environment;
@@ -109,7 +109,7 @@ namespace Microsoft.Azure.WebJobs.Script.Workers.Rpc
         {
             var probingPaths = new List<string>();
 
-            // If Environment variable is set, read probing paths from Env (works for linux)
+            // If Environment variable is set, read probing paths from Environment
             string probingPathsEnvValue = _environment.GetEnvironmentVariableOrDefault(EnvironmentSettingNames.WorkerProbingPaths, null);
 
             if (!string.IsNullOrEmpty(probingPathsEnvValue))
