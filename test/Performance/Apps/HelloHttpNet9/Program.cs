df89d5dc591fb174ee0531eeb5206f7b7d2f9f31
Shyju Krishnankutty <connectshyju@gmail.com>
2025-06-21 10:38:34 -0700

    test from managed code

--- test/Performance/Apps/HelloHttpNet9/Program.cs
+++ test/Performance/Apps/HelloHttpNet9/Program.cs
@@ -1,8 +1,82 @@
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            await WriteResultsToSql(DateTime.UtcNow,
+                @"Server=tcp:azure-functions-benchmarks-dbs1.database.windows.net,1433;Initial Catalog=azure-functions-benchmarks-db;Persist Security Info=False;Authentication=Active Directory Default; User Id=Azure-Functions-Host-Performance-CI-MI; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", 
+                "YourSession", 
+                "YourScenario", 
+                "YourDescription", 
+                "YourDocument");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Environment.Exit(1);
+        }
+    }
 
-builder.ConfigureFunctionsWebApplication();
 
-builder.Build().Run();
+    private static async Task WriteResultsToSql(
+            DateTime utcNow,
+            string connectionString,
+            string session,
+            string scenario,
+            string description,
+            string document
+            )
+    {
+
+        var insertCmd =
+            $$"""
+                INSERT INTO [dbo].HttpBenchmarks
+                           ([DateTimeUtc]
+                           ,[Session]
+                           ,[Scenario]
+                           ,[Description]
+                           ,[Document])
+                     VALUES
+                           (@DateTimeUtc
+                           ,@Session
+                           ,@Scenario
+                           ,@Description
+                           ,@Document)
+                """;
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            await connection.OpenAsync();
+            var transaction = connection.BeginTransaction();
+
+            try
+            {
+                var command = new SqlCommand(insertCmd, connection, transaction);
+                var p = command.Parameters;
+                p.AddWithValue("@DateTimeUtc", utcNow);
+                p.AddWithValue("@Session", session);
+                p.AddWithValue("@Scenario", scenario ?? "");
+                p.AddWithValue("@Description", description ?? "");
+                p.AddWithValue("@Document", document);
+
+                await command.ExecuteNonQueryAsync();
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+    }
+}
