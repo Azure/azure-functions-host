08c2f8deff455c36407c043a857ce26a88a720ab
Shyju Krishnankutty <connectshyju@gmail.com>
2025-07-01 16:37:30 -0700

    minimal

--- test/Performance/Apps/HelloHttpNet9/Program.cs
+++ test/Performance/Apps/HelloHttpNet9/Program.cs
@@ -7,75 +7,9 @@ internal class Program
         var connectionString = args[0];
         Console.WriteLine("Starting the Azure Functions Benchmarks SQL Writer...");
         Console.WriteLine($"Connection String: {connectionString}");
-
-            await WriteResultsToSql(DateTime.UtcNow,
-                connectionString,
-                "YourSessionC#AdoTask",
-                "YourScenario",
-                "YourDescription",
-                "YourDocument");
-
-            await WriteColdStartToSql(DateTime.UtcNow, connectionString, "foo", "bar", "{}");
+        await WriteColdStartToSql(DateTime.UtcNow, connectionString, "foo", "bar", "{}");
        
-    }
-
-
-    private static async Task WriteResultsToSql(
-            DateTime utcNow,
-            string connectionString,
-            string session,
-            string scenario,
-            string description,
-            string document
-            )
-    {
-
-        var insertCmd =
-            $$"""
-                INSERT INTO [dbo].HttpBenchmarks
-                           ([DateTimeUtc]
-                           ,[Session]
-                           ,[Scenario]
-                           ,[Description]
-                           ,[Document])
-                     VALUES
-                           (@DateTimeUtc
-                           ,@Session
-                           ,@Scenario
-                           ,@Description
-                           ,@Document)
-                """;
-
-        using (var connection = new SqlConnection(connectionString))
-        {
-            await connection.OpenAsync();
-            var transaction = connection.BeginTransaction();
-
-            try
-            {
-                var command = new SqlCommand(insertCmd, connection, transaction);
-                var p = command.Parameters;
-                p.AddWithValue("@DateTimeUtc", utcNow);
-                p.AddWithValue("@Session", session);
-                p.AddWithValue("@Scenario", scenario ?? "");
-                p.AddWithValue("@Description", description ?? "");
-                p.AddWithValue("@Document", document);
-
-                await command.ExecuteNonQueryAsync();
-
-                transaction.Commit();
-            }
-            catch
-            {
-                transaction.Rollback();
-                throw;
-            }
-            finally
-            {
-                transaction.Dispose();
-            }
-        }
-    }
+    }    
 
     private static async Task WriteColdStartToSql(
         DateTime utcNow,
@@ -91,31 +25,29 @@ internal class Program
                 (@DateTimeUtc, @OS, @Description, @Document)
             """;
 
-        using (var connection = new SqlConnection(connectionString))
+        using var connection = new SqlConnection(connectionString);
+        await connection.OpenAsync();
+        var transaction = connection.BeginTransaction();
+        try
         {
-            await connection.OpenAsync();
-            var transaction = connection.BeginTransaction();
-            try
-            {
-                var command = new SqlCommand(insertCmd, connection, transaction);
-                var p = command.Parameters;
-                p.AddWithValue("@DateTimeUtc", utcNow);
-                p.AddWithValue("@OS", os);
-                p.AddWithValue("@Description", description);
-                p.AddWithValue("@Document", document);
+            var command = new SqlCommand(insertCmd, connection, transaction);
+            var p = command.Parameters;
+            p.AddWithValue("@DateTimeUtc", utcNow);
+            p.AddWithValue("@OS", os);
+            p.AddWithValue("@Description", description);
+            p.AddWithValue("@Document", document);
 
-                await command.ExecuteNonQueryAsync();
-                transaction.Commit();
-            }
-            catch
-            {
-                transaction.Rollback();
-                throw;
-            }
-            finally
-            {
-                transaction.Dispose();
-            }
+            await command.ExecuteNonQueryAsync();
+            transaction.Commit();
+        }
+        catch
+        {
+            transaction.Rollback();
+            throw;
+        }
+        finally
+        {
+            transaction.Dispose();
         }
     }
 }
