db76a7e32ad9130e3d344bde9062f9edb1de0ee9
Shyju Krishnankutty <connectshyju@gmail.com>
2025-07-01 16:11:26 -0700

    Change table to ColdStart

--- test/Performance/Apps/HelloHttpNet9/Program.cs
+++ test/Performance/Apps/HelloHttpNet9/Program.cs
@@ -14,6 +14,8 @@ internal class Program
                 "YourScenario",
                 "YourDescription",
                 "YourDocument");
+
+            await WriteColdStartToSql(DateTime.UtcNow, connectionString, "foo", "bar", "{}");
        
     }
 
@@ -61,6 +63,48 @@ internal class Program
 
                 await command.ExecuteNonQueryAsync();
 
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                transaction.Dispose();
+            }
+        }
+    }
+
+    private static async Task WriteColdStartToSql(
+        DateTime utcNow,
+        string connectionString,
+        string os,
+        string description,
+        string document)
+    {
+        var insertCmd = $$"""
+            INSERT INTO ColdStart
+                (DateTimeUtc, OS, Description, Document)
+            VALUES
+                (@DateTimeUtc, @OS, @Description, @Document)
+            """;
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            await connection.OpenAsync();
+            var transaction = connection.BeginTransaction();
+            try
+            {
+                var command = new SqlCommand(insertCmd, connection, transaction);
+                var p = command.Parameters;
+                p.AddWithValue("@DateTimeUtc", utcNow);
+                p.AddWithValue("@OS", os);
+                p.AddWithValue("@Description", description);
+                p.AddWithValue("@Document", document);
+
+                await command.ExecuteNonQueryAsync();
                 transaction.Commit();
             }
             catch
