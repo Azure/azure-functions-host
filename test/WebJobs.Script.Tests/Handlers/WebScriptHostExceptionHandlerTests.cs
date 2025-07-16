0144713a1be487372d940053b92263709df5b193
satvu <sarahvu@microsoft.com>
2025-07-15 18:51:03 -0700

    resolve comments

--- test/WebJobs.Script.Tests/Handlers/WebScriptHostExceptionHandlerTests.cs
+++ test/WebJobs.Script.Tests/Handlers/WebScriptHostExceptionHandlerTests.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using System;
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
