f67f5348c4414f807b52586d903d623dc504c1e8
Jacob Viau <javia@microsoft.com>
2025-09-03 14:28:33 -0700

    Refactor ExtensionRequirementOptionsTest

--- src/WebJobs.Script/Config/ExtensionRequirementOptions.cs
+++ src/WebJobs.Script/Config/ExtensionRequirementOptions.cs
@@ -1,15 +1,13 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using Microsoft.Azure.WebJobs.Script.ExtensionRequirements;
-using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
 
 namespace Microsoft.Azure.WebJobs.Script.Config
 {
     /// <summary>
-    /// Represents collection of minumum bundle and extension requirements for a function app.
+    /// Represents collection of minimum bundle and extension requirements for a function app.
     /// </summary>
     public class ExtensionRequirementOptions
     {
