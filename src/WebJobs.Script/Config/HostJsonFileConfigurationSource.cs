a55f4a7541f8311b54af3f4866beff18fffb4d32
Jacob Viau <javia@microsoft.com>
2025-09-16 10:37:11 -0700

    Remove unused usings

--- src/WebJobs.Script/Config/HostJsonFileConfigurationSource.cs
+++ src/WebJobs.Script/Config/HostJsonFileConfigurationSource.cs
@@ -7,7 +7,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.Azure.WebJobs.Script.Diagnostics.Extensions;
