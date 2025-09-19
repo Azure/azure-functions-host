0ce5b3636f666d5012402db00c9380bc416b3613
Jacob Viau <javia@microsoft.com>
2025-09-19 13:47:31 -0700

    Fix merge

--- test/WebJobs.Script.Tests/Configuration/HostConfigurationProfileTests.cs
+++ test/WebJobs.Script.Tests/Configuration/HostConfigurationProfileTests.cs
@@ -3,7 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Azure.WebJobs.Script.Configuration;
 using Xunit;
 
--- test/WebJobs.Script.Tests/Configuration/HostJsonFileConfigurationOptionsTests.cs
+++ test/WebJobs.Script.Tests/Configuration/HostJsonFileConfigurationOptionsTests.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Azure.WebJobs.Script.Configuration;
 using Newtonsoft.Json.Linq;
 using Xunit;
