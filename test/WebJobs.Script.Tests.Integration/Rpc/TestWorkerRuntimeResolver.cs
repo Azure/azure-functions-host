c93e78274baddebc586215cda3fb4da8771c7f6d
Shyju Krishnankutty <connectshyju@gmail.com>
2025-09-23 10:10:38 -0700

    Remove accidental partial

--- test/WebJobs.Script.Tests.Integration/Rpc/TestWorkerRuntimeResolver.cs
+++ test/WebJobs.Script.Tests.Integration/Rpc/TestWorkerRuntimeResolver.cs
@@ -2,16 +2,13 @@ using Microsoft.Azure.WebJobs.Script.Workers;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests
 {
-    public partial class WorkerConcurrencyManagerEndToEndTests
+    internal class TestWorkerRuntimeResolver : IWorkerRuntimeResolver
     {
-        internal class TestWorkerRuntimeResolver : IWorkerRuntimeResolver
+        private readonly string _workerRuntime;
+        internal TestWorkerRuntimeResolver(string workerRuntime)
         {
-            private readonly string _workerRuntime;
-            internal TestWorkerRuntimeResolver(string workerRuntime)
-            {
-                _workerRuntime = workerRuntime;
-            }
-            public string GetWorkerRuntime(string defaultValue = null) => _workerRuntime;
+            _workerRuntime = workerRuntime;
         }
+        public string GetWorkerRuntime(string defaultValue = null) => _workerRuntime;
     }
 }
