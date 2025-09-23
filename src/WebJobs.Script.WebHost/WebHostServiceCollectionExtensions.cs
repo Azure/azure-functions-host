541a72c13e094371eb7c120dac26caba7127550a
Shyju Krishnankutty <connectshyju@gmail.com>
2025-09-22 22:08:24 -0700

    register WorkerRuntimeResolver to container.

--- src/WebJobs.Script.WebHost/WebHostServiceCollectionExtensions.cs
+++ src/WebJobs.Script.WebHost/WebHostServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -29,8 +29,8 @@ using Microsoft.Azure.WebJobs.Script.WebHost.Middleware;
 using Microsoft.Azure.WebJobs.Script.WebHost.Security.Authorization;
 using Microsoft.Azure.WebJobs.Script.WebHost.Security.Authorization.Policies;
 using Microsoft.Azure.WebJobs.Script.WebHost.Standby;
+using Microsoft.Azure.WebJobs.Script.Workers;
 using Microsoft.Azure.WebJobs.Script.Workers.FunctionDataCache;
-using Microsoft.Azure.WebJobs.Script.Workers.Profiles;
 using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
 using Microsoft.Azure.WebJobs.Script.Workers.Rpc.Configuration;
 using Microsoft.Azure.WebJobs.Script.Workers.SharedMemoryDataTransfer;
@@ -97,6 +97,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
             // Standby services
             services.AddStandbyServices();
 
+            services.AddSingleton<IWorkerRuntimeResolver, WorkerRuntimeResolver>();
             services.AddSingleton<IScriptHostManager>(s => s.GetRequiredService<WebJobsScriptHostService>());
             services.AddSingleton<IScriptWebHostEnvironment, ScriptWebHostEnvironment>();
             services.TryAddSingleton<IStandbyManager, StandbyManager>();
