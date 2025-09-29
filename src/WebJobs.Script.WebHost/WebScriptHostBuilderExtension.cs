5dbc1e70c9a308dd63a5865b367e0f26f9773127
Mathew Charles <mathewc@microsoft.com>
2025-09-29 16:48:46 -0700

    Supporting configuration for extension webhook Authorization level

--- src/WebJobs.Script.WebHost/WebScriptHostBuilderExtension.cs
+++ src/WebJobs.Script.WebHost/WebScriptHostBuilderExtension.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -58,6 +58,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost
                     services.ConfigureOptions<AppServiceOptionsSetup>();
                     services.ConfigureOptions<HostEasyAuthOptionsSetup>();
                     services.ConfigureOptions<PrimaryHostCoordinatorOptionsSetup>();
+                    services.ConfigureOptions<ExtensionSystemOptionsSetup>();
                 })
                 .AddScriptHost(webHostOptions, configLoggerFactory, metricsLogger, webJobsBuilder =>
                 {
