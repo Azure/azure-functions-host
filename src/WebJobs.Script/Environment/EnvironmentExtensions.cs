1f25ed4206f35adccbf6e440100de7329713cda4
Aprana Seth <apseth@microsoft.com>
2025-10-22 16:24:44 -0700

    Fixing the multi langauge worker to get the worker indexed for Logic Apps.

--- src/WebJobs.Script/Environment/EnvironmentExtensions.cs
+++ src/WebJobs.Script/Environment/EnvironmentExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
@@ -441,7 +441,7 @@ namespace Microsoft.Azure.WebJobs.Script
         {
             if (!isMultiLanguageEnabled.HasValue)
             {
-                isMultiLanguageEnabled = environment.IsLogicApp() && !environment.IsLogicAppCodefulModeEnabled();
+                isMultiLanguageEnabled = environment.IsLogicApp();
             }
             return isMultiLanguageEnabled.Value;
         }
