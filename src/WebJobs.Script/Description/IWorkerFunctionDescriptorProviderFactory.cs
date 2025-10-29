30bb6e908d7d90ef3bf9516e5cde086bec690901
Brett Samblanet <brettsam@microsoft.com>
2025-10-28 17:31:36 -0700

    testing ci

---
+++ src/WebJobs.Script/Description/IWorkerFunctionDescriptorProviderFactory.cs
@@ -0,0 +1,19 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Script.Extensibility;
+
+namespace Microsoft.Azure.WebJobs.Script.Description;
+
+/// <summary>
+/// Hooks for creating FunctionDescriptorProviders for external workers (Http, gRPC, etc.)
+/// </summary>
+public interface IWorkerFunctionDescriptorProviderFactory
+{
+    FunctionDescriptorProvider CreateHttpDescriptorProvider(ScriptHost host, ICollection<IScriptBindingProvider> bindingProviders);
+
+    FunctionDescriptorProvider CreateMultiWorkerDescriptorProvider(ScriptHost host, ICollection<IScriptBindingProvider> bindingProviders);
+
+    FunctionDescriptorProvider CreateWorkerDescriptorProvider(ScriptHost host, string workerRuntime, ICollection<IScriptBindingProvider> bindingProviders);
+}
--- src/WebJobs.Script/Host/ScriptHost.cs
+++ src/WebJobs.Script/Host/ScriptHost.cs
@@ -69,6 +69,7 @@ namespace Microsoft.Azure.WebJobs.Script
         private readonly IEnvironment _environment;
         private readonly IFunctionDataCache _functionDataCache;
         private readonly IOptions<FunctionsHostingConfigOptions> _hostingConfigOptions;
+        private readonly IWorkerFunctionDescriptorProviderFactory _descriptorProviderFactory;
         private readonly IOptionsMonitor<LanguageWorkerOptions> _languageWorkerOptions;
         private readonly ILogger _logger;
         private readonly IPrimaryHostStateProvider _primaryHostStateProvider;
@@ -110,6 +111,7 @@ namespace Microsoft.Azure.WebJobs.Script
             IFunctionDataCache functionDataCache,
             IOptionsMonitor<LanguageWorkerOptions> languageWorkerOptions,
             IOptions<FunctionsHostingConfigOptions> hostingConfigOptions,
+            IWorkerFunctionDescriptorProviderFactory descriptorProviderFactory,
             ScriptSettingsManager settingsManager = null)
             : base(options, jobHostContextFactory)
         {
@@ -158,6 +160,7 @@ namespace Microsoft.Azure.WebJobs.Script
 
             _functionDataCache = functionDataCache;
             _hostingConfigOptions = hostingConfigOptions;
+            _descriptorProviderFactory = descriptorProviderFactory;
         }
 
         public event EventHandler HostInitializing;
@@ -568,15 +571,15 @@ namespace Microsoft.Azure.WebJobs.Script
             {
                 _logger.AddingDescriptorProviderForLanguage("All (Multi Language)");
 
-                var workerOptions = _languageWorkerOptions.CurrentValue;
-
-                _descriptorProviders.Add(new MultiLanguageFunctionDescriptorProvider(this, workerOptions.WorkerConfigs, ScriptOptions, _bindingProviders,
-                    _functionDispatcher, _loggerFactory, _applicationLifetime, workerOptions.WorkerConfigs.Max(wc => wc.CountOptions.InitializationTimeout)));
+                var descriptorProvider = _descriptorProviderFactory.CreateMultiWorkerDescriptorProvider(this, _bindingProviders);
+                _descriptorProviders.Add(descriptorProvider);
             }
             else if (_isHttpWorker)
             {
                 _logger.AddingDescriptorProviderForHttpWorker();
-                _descriptorProviders.Add(new HttpFunctionDescriptorProvider(this, ScriptOptions, _bindingProviders, _functionDispatcher, _loggerFactory, _applicationLifetime, _httpWorkerOptions.InitializationTimeout));
+
+                var descriptorProvider = _descriptorProviderFactory.CreateHttpDescriptorProvider(this, _bindingProviders);
+                _descriptorProviders.Add(descriptorProvider);
             }
             else if (string.Equals(workerRuntime, RpcWorkerConstants.DotNetLanguageWorkerName, StringComparison.OrdinalIgnoreCase))
             {
@@ -587,13 +590,8 @@ namespace Microsoft.Azure.WebJobs.Script
             {
                 _logger.AddingDescriptorProviderForLanguage(workerRuntime);
 
-                var workerConfig = _languageWorkerOptions.CurrentValue.WorkerConfigs?.FirstOrDefault(c => c.Description.Language.Equals(workerRuntime, StringComparison.OrdinalIgnoreCase));
-
-                // If there's no worker config, use the default (for legacy behavior; mostly for tests).
-                TimeSpan initializationTimeout = workerConfig?.CountOptions?.InitializationTimeout ?? WorkerProcessCountOptions.DefaultInitializationTimeout;
-
-                _descriptorProviders.Add(new RpcFunctionDescriptorProvider(this, workerRuntime, ScriptOptions, _bindingProviders,
-                    _functionDispatcher, _loggerFactory, _applicationLifetime, initializationTimeout));
+                var descriptorProvider = _descriptorProviderFactory.CreateWorkerDescriptorProvider(this, workerRuntime, _bindingProviders);
+                _descriptorProviders.Add(descriptorProvider);
             }
 
             // Codeless functions run side by side with regular functions.
