ef15b0c233fa26e3039e172fff93f12f11609091
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2025-11-05 15:32:48 -0800

    Patch/4.45.200 (#11474)
    * Update version to 4.1045.200

--- test/WebJobs.Script.Tests/Extensions/ExceptionExtensionsTests.cs
+++ test/WebJobs.Script.Tests/Extensions/ExceptionExtensionsTests.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Script.Workers.Rpc;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Script.Tests.Extensions
@@ -31,7 +33,55 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Extensions
             Assert.Contains("System.Exception : some outer exception ---> System.InvalidOperationException : Some inner exception", exceptionDetails);
             Assert.Contains("End of inner exception", exceptionDetails);
             Assert.Contains("at Microsoft.Azure.WebJobs.Script.Tests.Extensions.ExceptionExtensionsTests.GetExceptionDetails_ReturnsExpectedResult()", exceptionDetails);
-            Assert.Contains("ExceptionExtensionsTests.cs : 20", exceptionDetails);
+            Assert.Contains("ExceptionExtensionsTests.cs", exceptionDetails);
+        }
+
+        [Fact]
+        public void GetExceptionDetails_Rpc()
+        {
+            string rpcMessage = "rpcMessage";
+            Exception innerException = new RpcException("result", rpcMessage, "stack");
+            Exception outerException = new FunctionInvocationException("message", innerException);
+            Exception fullException;
+
+            try
+            {
+                throw outerException;
+            }
+            catch (Exception e)
+            {
+                fullException = e;
+            }
+
+            (string exceptionType, string exceptionMessage, string exceptionDetails, string formattedText) = fullException.GetSanitizedExceptionDetails("safe text");
+
+            Assert.Equal("Microsoft.Azure.WebJobs.Script.Workers.Rpc.RpcException", exceptionType);
+            Assert.DoesNotContain(rpcMessage, exceptionMessage);
+            Assert.DoesNotContain(rpcMessage, exceptionDetails);
+            Assert.Contains("safe text", formattedText);
+        }
+
+        [Fact]
+        public void GetExceptionDetails_Rpc_Empty()
+        {
+            Exception innerException = new RpcException(string.Empty, string.Empty, string.Empty);
+            Exception outerException = new FunctionInvocationException(string.Empty, innerException);
+            Exception fullException;
+
+            try
+            {
+                throw outerException;
+            }
+            catch (Exception e)
+            {
+                fullException = e;
+            }
+
+            (string exceptionType, string exceptionMessage, _, string formattedText) = fullException.GetSanitizedExceptionDetails("safe text");
+
+            Assert.Equal("Microsoft.Azure.WebJobs.Script.Workers.Rpc.RpcException", exceptionType);
+            Assert.Equal("Result: \nType: \nException: \nStack: ", exceptionMessage);
+            Assert.Contains("safe text", formattedText);
         }
     }
 }
