a68f7c1bf6f96d26e291a29002798e093d56a3f7
Sarah Vu <sarahvu@microsoft.com>
2026-03-18 14:06:33 -0700

    add abstractions test proj to sln file, add some tests to bindingMetadata

--- test/WebJobs.Script.Abstractions/BindingMetadataTests.cs
+++ test/WebJobs.Script.Abstractions/BindingMetadataTests.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs.Script.Description;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
-namespace Microsoft.Azure.WebJobs.Script.Tests
+namespace Microsoft.Azure.WebJobs.Script.Abstractions.Tests
 {
     public class BindingMetadataTests
     {
@@ -83,6 +83,73 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
             JObject inputBinding = JObject.Parse("{\"name\":\"myBlob\",\"direction\":\"In\",\"type\":\"blob\",\"blobPath\":\"input-container//hello.txt\",\"connection\":\"AzureWebJobsStorage\"}");
             var result = BindingMetadata.Create(inputBinding);
             Assert.Empty(result.Properties);
+        }
+
+        [Fact]
+        public void BindingMetadata_Properties_IsCaseInsensitive()
+        {
+            JObject binding = JObject.Parse("{\"name\":\"test\",\"direction\":\"In\",\"type\":\"blob\",\"properties\":{\"testProperty\":\"value\"}}");
+            var result = BindingMetadata.Create(binding);
+
+            Assert.True(result.Properties.TryGetValue("testProperty", out var value1));
+            Assert.Equal("value", value1);
+
+            Assert.True(result.Properties.TryGetValue("TestProperty", out var value2));
+            Assert.Equal("value", value2);
+
+            Assert.True(result.Properties.TryGetValue("TESTPROPERTY", out var value3));
+            Assert.Equal("value", value3);
+
+            Assert.True(result.Properties.TryGetValue("testproperty", out var value4));
+            Assert.Equal("value", value4);
+        }
+
+        [Fact]
+        public void BindingMetadata_Properties_CaseInsensitive_ContainsKey()
+        {
+            JObject binding = JObject.Parse("{\"name\":\"test\",\"direction\":\"In\",\"type\":\"blob\",\"properties\":{\"myKey\":\"myValue\"}}");
+            var result = BindingMetadata.Create(binding);
+
+            Assert.True(result.Properties.ContainsKey("myKey"));
+            Assert.True(result.Properties.ContainsKey("MyKey"));
+            Assert.True(result.Properties.ContainsKey("MYKEY"));
+            Assert.True(result.Properties.ContainsKey("mykey"));
+        }
+
+        [Fact]
+        public void BindingMetadata_Constructor_PropertiesIsCaseInsensitive()
+        {
+            var metadata = new BindingMetadata
+            {
+                Name = "test",
+                Type = "blob",
+                Direction = BindingDirection.In
+            };
+
+            metadata.Properties["TestKey"] = "value1";
+
+            Assert.True(metadata.Properties.TryGetValue("testkey", out var value));
+            Assert.Equal("value1", value);
+            Assert.True(metadata.Properties.ContainsKey("TESTKEY"));
+            Assert.Single(metadata.Properties);
+        }
+
+        [Fact]
+        public void BindingMetadata_Properties_CaseInsensitive_MultipleProperties()
+        {
+            JObject binding = JObject.Parse("{\"name\":\"test\",\"direction\":\"In\",\"type\":\"blob\",\"properties\":{\"firstProperty\":\"first\",\"secondProperty\":\"second\",\"thirdProperty\":\"third\"}}");
+            var result = BindingMetadata.Create(binding);
+
+            Assert.Equal(3, result.Properties.Count);
+
+            Assert.True(result.Properties.TryGetValue("FirstProperty", out var first));
+            Assert.Equal("first", first);
+
+            Assert.True(result.Properties.TryGetValue("SECONDPROPERTY", out var second));
+            Assert.Equal("second", second);
+
+            Assert.True(result.Properties.TryGetValue("thirdproperty", out var third));
+            Assert.Equal("third", third);
         }
     }
 }
