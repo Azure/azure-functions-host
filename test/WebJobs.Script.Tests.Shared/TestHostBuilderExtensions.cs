fb9146d8e32d33310f7d07ef837e92c9744160e4
Naren Soni <nasoni@microsoft.com>
2026-03-24 04:45:47 -0700

    fixing unit test for RestorePackagesAsync

--- test/WebJobs.Script.Tests.Shared/TestHostBuilderExtensions.cs
+++ test/WebJobs.Script.Tests.Shared/TestHostBuilderExtensions.cs
@@ -72,7 +72,6 @@ namespace Microsoft.WebJobs.Script.Tests
             AddMockedSingleton<IEventGenerator>(services);
             AddMockedSingleton<IFunctionInvocationDispatcherFactory>(services);
             AddMockedSingleton<IHttpWorkerService>(services);
-            AddMockedSingleton<IHostApplicationLifetime>(services);
             AddMockedSingleton<IDependencyValidator>(services);
             AddMockedSingleton<ISharedMemoryManager>(services);
             AddMockedSingleton<IFunctionDataCache>(services);
