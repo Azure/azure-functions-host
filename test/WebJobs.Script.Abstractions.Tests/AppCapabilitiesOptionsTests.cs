7011c10c5f0d65a176bf4a2e44d25d6463f4c735
Sarah Vu <sarahvu@microsoft.com>
2026-04-01 15:24:31 -0700

    update tests

--- test/WebJobs.Script.Abstractions.Tests/AppCapabilitiesOptionsTests.cs
+++ test/WebJobs.Script.Abstractions.Tests/AppCapabilitiesOptionsTests.cs
@@ -13,45 +13,42 @@ namespace Microsoft.Azure.WebJobs.Script.Abstractions.Tests
         public void AppCapabilitiesOptions_Keys_IsCaseInsensitive()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict.Add("MyCapability", "value1");
+            options.Add("MyCapability", "value1");
 
-            Assert.True(dict.ContainsKey("MyCapability"));
-            Assert.True(dict.ContainsKey("mycapability"));
-            Assert.True(dict.ContainsKey("MYCAPABILITY"));
-            Assert.True(dict.ContainsKey("myCapability"));
+            Assert.True(options.ContainsKey("MyCapability"));
+            Assert.True(options.ContainsKey("mycapability"));
+            Assert.True(options.ContainsKey("MYCAPABILITY"));
+            Assert.True(options.ContainsKey("myCapability"));
         }
 
         [Fact]
         public void AppCapabilitiesOptions_Indexer_IsCaseInsensitive()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict["MyCapability"] = "value1";
+            options["MyCapability"] = "value1";
 
-            Assert.Equal("value1", dict["MyCapability"]);
-            Assert.Equal("value1", dict["mycapability"]);
-            Assert.Equal("value1", dict["MYCAPABILITY"]);
-            Assert.Equal("value1", dict["myCapability"]);
+            Assert.Equal("value1", options["MyCapability"]);
+            Assert.Equal("value1", options["mycapability"]);
+            Assert.Equal("value1", options["MYCAPABILITY"]);
+            Assert.Equal("value1", options["myCapability"]);
         }
 
         [Fact]
         public void AppCapabilitiesOptions_TryGetValue_IsCaseInsensitive()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict["TestCapability"] = "testValue";
+            options["TestCapability"] = "testValue";
 
-            Assert.True(dict.TryGetValue("TestCapability", out var value1));
+            Assert.True(options.TryGetValue("TestCapability", out var value1));
             Assert.Equal("testValue", value1);
 
-            Assert.True(dict.TryGetValue("testcapability", out var value2));
+            Assert.True(options.TryGetValue("testcapability", out var value2));
             Assert.Equal("testValue", value2);
 
-            Assert.True(dict.TryGetValue("TESTCAPABILITY", out var value3));
+            Assert.True(options.TryGetValue("TESTCAPABILITY", out var value3));
             Assert.Equal("testValue", value3);
         }
 
@@ -59,51 +56,47 @@ namespace Microsoft.Azure.WebJobs.Script.Abstractions.Tests
         public void AppCapabilitiesOptions_AddDifferentCasing_OverwritesSameKey()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict.Add("MyCapability", "value1");
-            dict["mycapability"] = "value2";
+            options.Add("MyCapability", "value1");
+            options["mycapability"] = "value2";
 
-            Assert.Single(dict);
-            Assert.Equal("value2", dict["MyCapability"]);
+            Assert.Single(options);
+            Assert.Equal("value2", options["MyCapability"]);
         }
 
         [Fact]
         public void AppCapabilitiesOptions_Remove_IsCaseInsensitive()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict.Add("MyCapability", "value1");
-            Assert.True(dict.Remove("mycapability"));
-            Assert.Empty(dict);
+            options.Add("MyCapability", "value1");
+            Assert.True(options.Remove("mycapability"));
+            Assert.Empty(options);
         }
 
         [Fact]
         public void AppCapabilitiesOptions_Contains_IsCaseInsensitive()
         {
             var options = new AppCapabilitiesOptions();
-            ICollection<KeyValuePair<string, string>> collection = options;
 
-            collection.Add(new KeyValuePair<string, string>("MyCapability", "value1"));
+            options.Add(new KeyValuePair<string, string>("MyCapability", "value1"));
 
-            Assert.True(collection.Contains(new KeyValuePair<string, string>("MyCapability", "value1")));
-            Assert.True(collection.Contains(new KeyValuePair<string, string>("mycapability", "value1")));
-            Assert.True(collection.Contains(new KeyValuePair<string, string>("MYCAPABILITY", "value1")));
+            Assert.True(options.Contains(new KeyValuePair<string, string>("MyCapability", "value1")));
+            Assert.True(options.Contains(new KeyValuePair<string, string>("mycapability", "value1")));
+            Assert.True(options.Contains(new KeyValuePair<string, string>("MYCAPABILITY", "value1")));
         }
 
         [Fact]
         public void AppCapabilitiesOptions_Keys_ReturnsAllKeys()
         {
             var options = new AppCapabilitiesOptions();
-            IDictionary<string, string> dict = options;
 
-            dict.Add("capability1", "value1");
-            dict.Add("capability2", "value2");
+            options.Add("capability1", "value1");
+            options.Add("capability2", "value2");
 
-            Assert.Equal(2, dict.Keys.Count);
-            Assert.Contains("capability1", dict.Keys);
-            Assert.Contains("capability2", dict.Keys);
+            Assert.Equal(2, options.Keys.Count);
+            Assert.Contains("capability1", options.Keys);
+            Assert.Contains("capability2", options.Keys);
         }
     }
 }
