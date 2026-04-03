0ef8ff0c0497c61923cb946638ef10288d67529d
Brett Samblanet <brettsam@microsoft.com>
2026-04-03 07:19:31 -0700

    some pr comments

--- src/WebJobs.Script/Workers/WorkerConnectionInfo.cs
+++ src/WebJobs.Script/Workers/WorkerConnectionInfo.cs
@@ -2,41 +2,8 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Microsoft.Azure.WebJobs.Script.Workers;
-
-/// <summary>
-/// Represents the connection lifecycle state of an external worker.
-/// </summary>
-[JsonConverter(typeof(StringEnumConverter))]
-public enum WorkerConnectionState
-{
-    /// <summary>
-    /// The gRPC connection and init handshake are in progress.
-    /// </summary>
-    Connecting,
-
-    /// <summary>
-    /// The worker is fully connected and ready for invocations.
-    /// </summary>
-    Connected,
-
-    /// <summary>
-    /// Deallocation has been requested. In-flight invocations are draining.
-    /// </summary>
-    Draining,
-
-    /// <summary>
-    /// The connection has been closed and the channel removed.
-    /// </summary>
-    Disconnected,
-
-    /// <summary>
-    /// The connection attempt failed.
-    /// </summary>
-    Error
-}
 
 /// <summary>
 /// Tracks the connection state of an external worker.
@@ -45,21 +12,21 @@ public enum WorkerConnectionState
 public class WorkerConnectionInfo
 {
     /// <summary>
-    /// Gets or sets the platform-assigned worker identifier.
+    /// Gets the platform-assigned worker identifier.
     /// </summary>
     [JsonProperty("workerId")]
-    public string WorkerId { get; set; }
+    public string WorkerId { get; init; }
 
     /// <summary>
-    /// Gets or sets the current connection state.
+    /// Gets the current connection state.
     /// </summary>
     [JsonProperty("state")]
-    public WorkerConnectionState State { get; set; }
+    public WorkerConnectionState State { get; init; }
 
     /// <summary>
-    /// Gets or sets the error message when <see cref="State"/> is <see cref="WorkerConnectionState.Error"/>.
+    /// Gets the error message when <see cref="State"/> is <see cref="WorkerConnectionState.Error"/>.
     /// Null for all other states.
     /// </summary>
     [JsonProperty("errorMessage", NullValueHandling = NullValueHandling.Ignore)]
-    public string ErrorMessage { get; set; }
+    public string ErrorMessage { get; init; }
 }
