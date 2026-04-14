01eafa6085221b863db266339565fff1134411d1
Sarah Vu <sarahvu@microsoft.com>
2026-04-14 15:40:41 -0700

    initial test update packages

--- test/WebJobs.Script.Tests/Description/Worker/WorkerFunctionDescriptorProviderTests.cs
+++ test/WebJobs.Script.Tests/Description/Worker/WorkerFunctionDescriptorProviderTests.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Azure.WebJobs.Script.Tests
 
             _host = new HostBuilder().ConfigureDefaultTestWebScriptHost(b =>
             {
-                b.AddAzureStorage();
+                b.AddAzureStorageCoreServices();
             },
             o =>
             {
