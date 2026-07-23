35fa110b64a22adf6643032ecbcbc9b1010c5c6b
Fabio Cavalcante <facaval@microsoft.com>
2026-07-23 16:06:03 -0700

    Moving away from IEnvironment. Improving configuration injection

--- test/WebJobs.Script.Tests/Middleware/ReservedRouteGuardMiddlewareTests.cs
+++ test/WebJobs.Script.Tests/Middleware/ReservedRouteGuardMiddlewareTests.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Script.WebHost;
+using Microsoft.Azure.WebJobs.Script.WebHost.Configuration;
 using Microsoft.Azure.WebJobs.Script.WebHost.Middleware;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -26,9 +27,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [InlineData("/runtime/x/y")]
         public async Task Invoke_ReservedPath_ReturnsNotFound(string path)
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, _ =>
+            var middleware = CreateMiddleware(_ =>
             {
                 nextInvoked = true;
                 return Task.CompletedTask;
@@ -50,9 +50,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [InlineData("/runtimefoo")]
         public async Task Invoke_NonReservedPath_InvokesNext(string path)
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, context =>
+            var middleware = CreateMiddleware(context =>
             {
                 nextInvoked = true;
                 context.Response.StatusCode = StatusCodes.Status202Accepted;
@@ -71,9 +70,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [InlineData("/ADMIN/WARMUP")]
         public async Task Invoke_AdminWarmupOnSupportedSku_InvokesNext(string path)
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, context =>
+            var middleware = CreateMiddleware(context =>
             {
                 nextInvoked = true;
                 context.Response.StatusCode = StatusCodes.Status202Accepted;
@@ -93,9 +91,8 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [InlineData("/admin/warmup/foo")]
         public async Task Invoke_AdminWarmupDescendant_ReturnsNotFound(string path)
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, _ =>
+            var middleware = CreateMiddleware(_ =>
             {
                 nextInvoked = true;
                 return Task.CompletedTask;
@@ -111,15 +108,16 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [Fact]
         public async Task Invoke_AdminWarmupOnConsumptionSku_ReturnsNotFound()
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
-            environment.Platform = OSPlatform.Windows;
-            environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, ScriptConstants.DynamicSku);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, _ =>
+            var reservedRouteOptions = new ReservedRouteOptions
+            {
+                AdminWarmupRouteEnabled = false
+            };
+            var middleware = CreateMiddleware(_ =>
             {
                 nextInvoked = true;
                 return Task.CompletedTask;
-            });
+            }, reservedRouteOptions: reservedRouteOptions);
             var context = CreateContext("/admin/warmup");
 
             await middleware.Invoke(context);
@@ -131,13 +129,16 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [Fact]
         public async Task Invoke_InPlaceholderMode_EnforcesWithoutRewiring()
         {
-            var environment = CreateEnvironment(inPlaceholderMode: true);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, _ =>
+            var standbyOptions = new StandbyOptions
+            {
+                InStandbyMode = true
+            };
+            var middleware = CreateMiddleware(_ =>
             {
                 nextInvoked = true;
                 return Task.CompletedTask;
-            });
+            }, standbyOptions: standbyOptions);
             var context = CreateContext("/admin/foo");
 
             await middleware.Invoke(context);
@@ -150,10 +151,6 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [Fact]
         public async Task Invoke_AfterSpecializationWithOptOut_RewiresToNext()
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
-            environment.SetEnvironmentVariable(
-                EnvironmentSettingNames.AzureWebJobsFeatureFlags,
-                ScriptConstants.FeatureFlagDisableReservedRouteEnforcement);
             int nextInvocations = 0;
             RequestDelegate next = context =>
             {
@@ -161,10 +158,15 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
                 context.Response.StatusCode = StatusCodes.Status202Accepted;
                 return Task.CompletedTask;
             };
-            var middleware = CreateMiddleware(environment, next);
+            var reservedRouteOptions = new ReservedRouteOptions
+            {
+                AdminWarmupRouteEnabled = true,
+                DisableReservedRouteEnforcement = true
+            };
+            var middleware = CreateMiddleware(next, reservedRouteOptions: reservedRouteOptions);
 
             await middleware.Invoke(CreateContext("/admin/foo"));
-            environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsFeatureFlags, null);
+            reservedRouteOptions.DisableReservedRouteEnforcement = false;
             var secondContext = CreateContext("/runtime/foo");
             await middleware.Invoke(secondContext);
 
@@ -176,18 +178,19 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [Fact]
         public async Task Invoke_AfterSpecializationWithoutOptOut_RewiresToEnforcement()
         {
-            var environment = CreateEnvironment(inPlaceholderMode: false);
             bool nextInvoked = false;
-            var middleware = CreateMiddleware(environment, _ =>
+            var reservedRouteOptions = new ReservedRouteOptions
+            {
+                AdminWarmupRouteEnabled = true
+            };
+            var middleware = CreateMiddleware(_ =>
             {
                 nextInvoked = true;
                 return Task.CompletedTask;
-            });
+            }, reservedRouteOptions: reservedRouteOptions);
 
             await middleware.Invoke(CreateContext("/admin/foo"));
-            environment.SetEnvironmentVariable(
-                EnvironmentSettingNames.AzureWebJobsFeatureFlags,
-                ScriptConstants.FeatureFlagDisableReservedRouteEnforcement);
+            reservedRouteOptions.DisableReservedRouteEnforcement = true;
             var secondContext = CreateContext("/runtime/foo");
             await middleware.Invoke(secondContext);
 
@@ -199,32 +202,46 @@ namespace Microsoft.Azure.WebJobs.Script.Tests.Middleware
         [Theory]
         [InlineData(true, null, true)]
         [InlineData(true, ScriptConstants.DynamicSku, false)]
-        public void IsAdminWarmupRouteEnabled_ReturnsExpectedResult(bool isWindows, string sku, bool expected)
+        public void ReservedRouteOptionsSetup_ReturnsExpectedWarmupRouteValue(bool isWindows, string sku, bool expected)
         {
             var environment = new TestEnvironment
             {
                 Platform = isWindows ? OSPlatform.Windows : OSPlatform.Linux
             };
             environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku, sku);
+            var options = new ReservedRouteOptions();
 
-            Assert.Equal(expected, environment.IsAdminWarmupRouteEnabled());
+            new ReservedRouteOptionsSetup(environment).Configure(options);
+
+            Assert.Equal(expected, options.AdminWarmupRouteEnabled);
         }
 
-        private static ReservedRouteGuardMiddleware CreateMiddleware(TestEnvironment environment, RequestDelegate next)
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("OtherFeature", false)]
+        [InlineData(ScriptConstants.FeatureFlagDisableReservedRouteEnforcement, true)]
+        public void ReservedRouteOptionsSetup_ReturnsExpectedFeatureFlagValue(string featureFlags, bool expected)
+        {
+            var environment = new TestEnvironment();
+            environment.SetEnvironmentVariable(EnvironmentSettingNames.AzureWebJobsFeatureFlags, featureFlags);
+            var options = new ReservedRouteOptions();
+
+            new ReservedRouteOptionsSetup(environment).Configure(options);
+
+            Assert.Equal(expected, options.DisableReservedRouteEnforcement);
+        }
+
+        private static ReservedRouteGuardMiddleware CreateMiddleware(
+            RequestDelegate next,
+            StandbyOptions standbyOptions = null,
+            ReservedRouteOptions reservedRouteOptions = null)
         {
             return new ReservedRouteGuardMiddleware(
                 next,
-                environment,
+                new TestOptionsMonitor<StandbyOptions>(standbyOptions),
+                new TestOptionsMonitor<ReservedRouteOptions>(
+                    reservedRouteOptions ?? new ReservedRouteOptions { AdminWarmupRouteEnabled = true }),
                 Mock.Of<ILogger<ReservedRouteGuardMiddleware>>());
-        }
-
-        private static TestEnvironment CreateEnvironment(bool inPlaceholderMode)
-        {
-            var environment = new TestEnvironment();
-            environment.SetEnvironmentVariable(
-                EnvironmentSettingNames.AzureWebsitePlaceholderMode,
-                inPlaceholderMode ? "1" : "0");
-            return environment;
         }
 
         private static DefaultHttpContext CreateContext(string path)
